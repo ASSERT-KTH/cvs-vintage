db00d51609da844f6950bbcff71a3e6c3fe38162
akv <akv>
1999-10-14 23:50:34 +0000

    runtest.bat is "fixed" so that the tomcat tests run. However, the built files still end up in the wrong directory. I haven't verified it works with dist, runtest.sh needs fixing and so on...

--- src/shell/runtest.bat
+++ src/shell/runtest.bat
@@ -1,5 +1,5 @@
 @echo off
-rem $Id: runtest.bat,v 1.1 1999/10/09 00:20:56 duncan Exp $
+rem $Id: runtest.bat,v 1.2 1999/10/14 23:50:34 akv Exp $
 rem Startup batch file for servlet runner.
 
 rem This batch file written and tested under Windows NT
@@ -9,12 +9,14 @@ set host=localhost
 set port=8080
 set test=testlist.txt
 
-set baseDir=..\..\dist\tomcat
+set topDir=..\..\..
+set tomcatBuildDir=%topDir%\build\tomcat
+set toolsDir=%topDir%\jakarta-tools
 
-set jsdkJars=%baseDir%\webserver.jar;%baseDir%\lib\servlet.jar
-set jspJars=%baseDir%\lib\jasper.jar
+set jsdkJars=%tomcatBuildDir%\webserver.jar;%tomcatBuildDir%\lib\servlet.jar
+set jspJars=%tomcatBuildDir%\lib\jasper.jar
 set beanJars=
-set miscJars=%baseDir%\lib\xml.jar;.\lib\moo.jar
+set miscJars=%toolsDir%\projectx-tr2.jar;%toolsDir%\moo.jar
 set appJars=%jsdkJars%;%jspJars%;%miscJars%
 set sysJars=%JAVA_HOME%\lib\tools.jar
 
@@ -34,7 +36,7 @@ echo Using classpath: %CLASSPATH%
 start java org.apache.tomcat.shell.Startup %1 %2 %3 %4 %5 %6 %7 %8 %9
 rem java org.apache.tomcat.shell.Startup %1 %2 %3 %4 %5 %6 %7 %8 %9
 sleep 5
-java -Dtest.hostName=%host% -Dtest.port=%port% com.sun.moo.Main \
+java -Dtest.hostName=%host% -Dtest.port=%port% org.apache.tools.moo.Main \
     -testfile %test%
 java org.apache.tomcat.shell.Shutdown %1 %2 %3 %4 %5 %6 %7 %8 %9
 
