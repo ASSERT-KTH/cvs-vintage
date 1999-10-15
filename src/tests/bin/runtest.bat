b242cf2953e73886bbb9b82070ce784e90df8404
arun <arun>
1999-10-15 23:41:17 +0000

    Fix for growing classpath problem

--- src/tests/bin/runtest.bat
+++ src/tests/bin/runtest.bat
@@ -1,5 +1,5 @@
 @echo off
-rem $Id: runtest.bat,v 1.3 1999/10/15 22:55:48 mode Exp $
+rem $Id: runtest.bat,v 1.4 1999/10/15 23:41:17 arun Exp $
 rem Startup batch file for servlet runner.
 
 rem This batch file written and tested under Windows NT
@@ -49,6 +49,7 @@ java org.apache.tomcat.shell.Shutdown %1 %2 %3 %4 %5 %6 %7 %8 %9
 rem clean up
 
 set CLASSPATH=%cp%
+set TOM_CLASSPATH=
 set port=
 set host=
 set test=
