3c337d163e1ebf4fda2b363733f105d6003b8a6a
harishp <harishp>
1999-12-08 16:36:28 +0000

    Modified runtest script to use tomcat start/stop. PR: Obtained from: Submitted by: Reviewed by:

--- src/tests/bin/runtest.sh
+++ src/tests/bin/runtest.sh
@@ -1,6 +1,6 @@
 #! /bin/ksh
 #
-# $Id: runtest.sh,v 1.4 1999/11/23 23:03:23 costin Exp $
+# $Id: runtest.sh,v 1.5 1999/12/08 16:36:28 harishp Exp $
 
 # Shell script to run test harness
  
@@ -20,20 +20,36 @@ if [ "$1" != "" ]; then
     port=$1
 fi
 
+if [ -z "$JAVA_HOME" ]
+then
+JAVACMD=`which java`
+if [ -z "$JAVACMD" ]
+then
+echo "Cannot find JAVA. Please set your PATH."
+exit 1
+fi
+JAVA_BINDIR=`dirname $JAVACMD`
+JAVA_HOME=$JAVA_BINDIR/..
+fi
+
+JAVACMD=$JAVA_HOME/bin/java
+
 # Add tomcat-related classes
 TOMCAT_HOME=..
 . ${TOMCAT_HOME}/env.tomcat
 
+CLASSPATH=$CLASSPATH:$JAVA_HOME/lib/tools.jar
 export CLASSPATH
 
 echo Using classpath: ${CLASSPATH}
 echo
 
-java org.apache.tomcat.shell.Startup "$@" &
+#$JAVACMD org.apache.tomcat.shell.Startup "$@" &
+$TOMCAT_HOME/tomcat.sh start &
 sleep 15
-java -Dtest.hostname=$host -Dtest.port=$port org.apache.tools.moo.Main \
-    -testfile $test -debug
-java org.apache.tomcat.shell.Shutdown "$@"
+$JAVACMD -Dtest.hostname=$host -Dtest.port=$port org.apache.tools.moo.Main -testfile $test -debug
+$TOMCAT_HOME/tomcat.sh stop
+#$JAVACMD org.apache.tomcat.shell.Shutdown "$@"
 
 if [ "$cp" != "" ]; then
     CLASSPATH=${cp}
