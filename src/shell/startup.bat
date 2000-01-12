e89d11bd3e239f4017245534fc41b78c8593bc28
rubys <rubys>
2000-01-12 13:55:03 +0000

    Add -nospawn option on Win32 platforms Submitted by: Conor MacNeill <conor@m64.com>

--- src/shell/startup.bat
+++ src/shell/startup.bat
@@ -1,8 +1,15 @@
 @echo off
-rem $Id: startup.bat,v 1.2 1999/12/05 17:02:42 harishp Exp $
+rem $Id: startup.bat,v 1.3 2000/01/12 13:55:03 rubys Exp $
 rem Startup batch file for tomcat servner.
 
 rem This batch file written and tested under Windows NT
 rem Improvements to this file are welcome
-call tomcat start  
+
+set arg=start
+if not "%1" == "-nospawn" goto noSpawnControl
+set arg=-nospawn start
+shift
+:noSpawnControl
+
+call tomcat %arg% %1 %2 %3 %4 %5 %6 %7 %8 %9 
 rem pause
--- src/shell/tomcat.bat
+++ src/shell/tomcat.bat
@@ -1,9 +1,11 @@
 @echo off
-rem $Id: tomcat.bat,v 1.5 1999/12/12 14:18:38 rubys Exp $
+rem $Id: tomcat.bat,v 1.6 2000/01/12 13:55:03 rubys Exp $
 rem A batch file to start/stop tomcat server.
 
 rem This batch file written and tested under Windows NT
 rem Improvements to this file are welcome
+
+set spawnCommand=start
 
 set jsdkJars=.\webserver.jar;.\lib\servlet.jar
 set jspJars=.\lib\jasper.jar
@@ -23,27 +25,26 @@ rem else
 set CLASSPATH=%CLASSPATH%;%cp%
 
 :next
+if not "%1" == "-nospawn" goto noSpawnControl
+set spawnCommand=
+shift
+
+:noSpawnControl
 if "%1" == "start" goto startServer
 if "%1" == "stop" goto stopServer
-@echo on
-@echo Usage:
-@echo "tomcat [start|stop]"
-@echo off
+echo Usage:
+echo "tomcat [-nospawn] (start|stop)"
 goto cleanup
 
 :startServer
 rem Start the Tomcat Server
-@echo on
-@echo Using classpath: %CLASSPATH%
-@echo off
-start java org.apache.tomcat.shell.Startup %2 %3 %4 %5 %6 %7 %8 %9
+echo Using classpath: %CLASSPATH%
+%spawnCommand% java org.apache.tomcat.shell.Startup %2 %3 %4 %5 %6 %7 %8 %9
 goto cleanup
 
 :stopServer
 rem Stop the Tomcat Server
-@echo on
-@echo Using classpath: %CLASSPATH%
-@echo off
+echo Using classpath: %CLASSPATH%
 java org.apache.tomcat.shell.Shutdown %2 %3 %4 %5 %6 %7 %8 %9
 goto cleanup
 
