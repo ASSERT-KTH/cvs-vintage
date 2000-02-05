f4a61330d62524b20f9662456a2f3bafe55f5ce1
rubys <rubys>
2000-02-05 18:28:43 +0000

    call tomcat.bat in its new location

--- src/shell/shutdown.bat
+++ src/shell/shutdown.bat
@@ -1,5 +1,12 @@
 @echo off
-rem $Id: shutdown.bat,v 1.2 1999/12/05 17:02:41 harishp Exp $
+rem $Id: shutdown.bat,v 1.3 2000/02/05 18:28:43 rubys Exp $
 rem Startup batch file for tomcat server.
-call tomcat stop
-rem pause
+
+if "%TOMCAT_HOME%" == "" goto bin
+cmd /c "cd %TOMCAT_HOME% & bin\tomcat stop"
+goto :eof
+
+:bin
+call bin\tomcat stop
+
+:eof
--- src/shell/startup.bat
+++ src/shell/startup.bat
@@ -1,9 +1,16 @@
 @echo off
-rem $Id: startup.bat,v 1.4 2000/01/13 17:01:44 rubys Exp $
+rem $Id: startup.bat,v 1.5 2000/02/05 18:28:43 rubys Exp $
 rem Startup batch file for tomcat servner.
 
 rem This batch file written and tested under Windows NT
 rem Improvements to this file are welcome
 
-call tomcat start %1 %2 %3 %4 %5 %6 %7 %8 %9 
-rem pause
+
+if "%TOMCAT_HOME%" == "" goto bin
+cmd /c "cd %TOMCAT_HOME% & bin\tomcat start %1 %2 %3 %4 %5 %6 %7 %8 %9"
+goto :eof
+
+:bin
+call bin\tomcat start %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+:eof
