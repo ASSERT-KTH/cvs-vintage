aef8756d4a0b2fb3b20a80baadacb8320519e6bf
jhunter <jhunter>
2000-03-20 23:47:12 +0000

    Made the build work under IRIX by adding $JAVA_HOME/lib/dev.jar to the "cp" classpath.  That's where IRIX puts the javac classes.  Go figure.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-# $Id: build.sh,v 1.8 2000/02/03 07:13:41 akv Exp $
+# $Id: build.sh,v 1.9 2000/03/20 23:47:12 jhunter Exp $
 
 if [ -z "$JAVA_HOME" ]
 then
@@ -16,6 +16,6 @@ fi
 
 JAVACMD=$JAVA_HOME/bin/java
 
-cp=../jakarta-ant/lib/ant.jar:../jakarta-tools/moo.jar:../jakarta-ant/lib/xml.jar:../build/tomcat/classes:$JAVA_HOME/lib/tools.jar
+cp=../jakarta-ant/lib/ant.jar:../jakarta-tools/moo.jar:../jakarta-ant/lib/xml.jar:../build/tomcat/classes:$JAVA_HOME/lib/tools.jar:$JAVA_HOME/lib/dev.jar
 
 $JAVACMD -classpath $cp:$CLASSPATH org.apache.tools.ant.Main "$@"
