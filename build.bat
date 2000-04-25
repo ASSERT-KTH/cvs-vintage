17979f81413506c95740cad882ce69e3fe26ff41
craigmcc <craigmcc>
2000-04-25 16:56:24 +0000

    Support the ANT_OPTS environment variable (similar to the support found in the "bin/tomcat.bat" and "bin/tomcat.sh" scripts in the binary distribution) to allow JVM options -- such as maximum heap size setting -- to be customized without modifying the basic scripts.
    PR: 202

--- build.bat
+++ build.bat
@@ -18,7 +18,7 @@ goto next
 
 :next
 
-java org.apache.tools.ant.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
+java %ANT_OPTS% org.apache.tools.ant.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 :clean
 
--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-# $Id: build.sh,v 1.9 2000/03/20 23:47:12 jhunter Exp $
+# $Id: build.sh,v 1.10 2000/04/25 16:56:24 craigmcc Exp $
 
 if [ -z "$JAVA_HOME" ]
 then
@@ -14,8 +14,14 @@ JAVA_BINDIR=`dirname $JAVACMD`
 JAVA_HOME=$JAVA_BINDIR/..
 fi
 
-JAVACMD=$JAVA_HOME/bin/java
+if [ "$ANT_OPTS" = "" ] ; then
+  ANT_OPTS=""
+fi
+
+JAVACMD=$JAVA_HOME/bin/java $ANT_OPTS
 
 cp=../jakarta-ant/lib/ant.jar:../jakarta-tools/moo.jar:../jakarta-ant/lib/xml.jar:../build/tomcat/classes:$JAVA_HOME/lib/tools.jar:$JAVA_HOME/lib/dev.jar
 
 $JAVACMD -classpath $cp:$CLASSPATH org.apache.tools.ant.Main "$@"
+
+
