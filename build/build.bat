0eb84a5f4a2705c70a508867669a4c1e774f9250
fleury <fleury>
2000-05-06 22:15:10 +0000

    updating path to javac.jar

--- build/build.bat
+++ build/build.bat
@@ -1,3 +1,9 @@
 @echo off
 REM convenience bat file to build with
-java -classpath "%CLASSPATH%;..\..\lib\ant.jar;..\..\lib\xml.jar;..\..\lib\xmlbeans.jar;..\..\build\classes" org.apache.tools.ant.Main %1 %2 %3 %4 %5
+
+set CLASSPATH=..\lib\ant.jar
+set CLASSPATH=%CLASSPATH%;..\lib\xml.jar
+set CLASSPATH=%CLASSPATH%;..\lib\xmlbeans.jar
+set CLASSPATH=%CLASSPATH%;..\lib\javac.jar
+
+java -classpath "%CLASSPATH%" org.apache.tools.ant.Main %1 %2 %3 %4 %5
