a14828b5492aec2ad5e8e4523ed65f67a17ba29a
fleury <fleury>
2000-05-06 20:53:39 +0000

    We move the build to the build directory, Fixed missing javac.jar

--- src/etc/build.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-REM convenience bat file to build with
-java -classpath "%CLASSPATH%;..\..\lib\ant.jar;..\..\lib\xml.jar;..\..\lib\xmlbeans.jar;..\..\build\classes" org.apache.tools.ant.Main %1 %2 %3 %4 %5
