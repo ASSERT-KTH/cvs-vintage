d115e4c21e9445a6fbea700098e5fb0a3607f3ae
oberg <oberg>
2000-05-15 09:24:38 +0000

    Added scripts (again)

---
+++ src/build/build.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+# $Id: build.sh,v 1.1 2000/05/15 09:24:38 oberg Exp $
+
+TARGET_CLASSPATH=../../lib/ant.jar:\
+../../lib/xml.jar:\
+../../lib/xmlbeans.jar:\
+../../build/classes:\
+../../lib/javac.jar
+
+java -classpath $TARGET_CLASSPATH org.apache.tools.ant.Main $*
