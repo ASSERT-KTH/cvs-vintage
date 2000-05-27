5dc22d8b2184891acac3e3874bcaf2ecd2e002ea
fleury <fleury>
2000-05-27 01:48:15 +0000

    The default configurations

--- src/main/org/jboss/ejb/deployment/ContainerConfigurations.java
+++ src/main/org/jboss/ejb/deployment/ContainerConfigurations.java
@@ -28,11 +28,12 @@ import com.dreambean.ejx.xml.XmlExternalizable;
 import com.dreambean.ejx.Util;
 
 /**
- *   <description> 
+ *   ContainerConfigurations holds the container configurations from a jar
  *      
  *   @see <related>
  *   @author Rickard Öberg (rickard.oberg@telkel.com)
- *   @version $Revision: 1.3 $
+ *   @author <a href="mailto:marc.fleury@telkel.com>Marc Fleury</a>
+ *   @version $Revision: 1.4 $
  */
 public class ContainerConfigurations
    extends BeanContextSupport
@@ -44,6 +45,58 @@ public class ContainerConfigurations
    Component c;
     
    // Static --------------------------------------------------------
+   
+   /*
+   * For default purposes we provide methods to get access to the conf
+   * Note that because the ContainerConfigurations work from the ThreadContext
+   * we provide a call instead of a static method to access these guys.
+   *
+   */
+   public static StatelessSessionContainerConfiguration getDefaultStatelessBeanContainerConfiguration() {
+	   
+	    StatelessSessionContainerConfiguration configuration = new StatelessSessionContainerConfiguration();
+	    configuration.setCallLogging(false);
+		configuration.setContainerInvoker("org.jboss.ejb.plugins.jrmp13.server.JRMPContainerInvoker");
+		configuration.setInstancePool("org.jboss.ejb.plugins.StatelessSessionInstancePool");
+		configuration.setName("Default StatelessBean");
+		configuration.setTransactionManager("org.jboss.tm.TxManager");
+		
+		return configuration;
+   }
+   
+   /*   MF FIXME Not there yet.... soon
+   
+    public static ContainerConfiguration getDefaultStatefulBeanContainerConfiguration() {
+	   
+	    ContainerConfiguration configuration = new StatefulSessionContainerConfiguration();
+	    configuration.setCallLogging(false);
+		configuration.setContainerInvoker("org.jboss.ejb.plugins.jrmp13.server.JRMPContainerInvoker");
+		configuration.setInstancePool("org.jboss.ejb.plugins.StatefulSessionInstancePool");
+		configuration.setInstanceCache("org.jboss.ejb.plugins.NoPassivationStatefulSessionInstanceCache");
+		configuration.setName("Default StatefulBean");
+		configuration.setTransactionManager("org.jboss.tm.TxManager");
+		
+		return configuration;
+   }
+   */
+   
+   public static EntityContainerConfiguration getDefaultEntityBeanContainerConfiguration(String type) {
+	   
+	    EntityContainerConfiguration configuration = new EntityContainerConfiguration();
+	    configuration.setCallLogging(false);
+		configuration.setCommitOption("A");
+		configuration.setContainerInvoker("org.jboss.ejb.plugins.jrmp13.server.JRMPContainerInvoker");
+		configuration.setInstancePool("org.jboss.ejb.plugins.EntityInstancePool");
+		configuration.setInstanceCache("org.jboss.ejb.plugins.NoPassivationEntityInstanceCache");
+		if (type.equalsIgnoreCase("BMP"))
+		    configuration.setPersistenceManager("org.jboss.ejb.plugins.BMPPersistenceManager");
+		else 
+			configuration.setPersistenceManager("org.jboss.ejb.plugins.jaws.JAWSPersistenceManager");
+		configuration.setName("Default EntityBean");
+		configuration.setTransactionManager("org.jboss.tm.TxManager");
+		
+		return configuration;
+   }
 
    // Constructors --------------------------------------------------
     
