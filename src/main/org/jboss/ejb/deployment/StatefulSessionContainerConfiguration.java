88742ed54787436cb874d429ace8a599a302e750
fleury <fleury>
2000-05-31 18:55:59 +0000

    We give the configuration file, nothing for now but soon plugin configuration could happen here

---
+++ src/main/org/jboss/ejb/deployment/StatefulSessionContainerConfiguration.java
@@ -0,0 +1,41 @@
+/*
+ * jBoss, the OpenSource EJB server
+ *
+ * Distributable under GPL license.
+ * See terms of license at gnu.org.
+ */
+package org.jboss.ejb.deployment;
+
+/**
+ *   StatefulSessionContainerConfiguration
+ *
+ *   We hold the stateful specific configuration in this class.
+ *   It is an empty class at this point.
+ *      
+ *   @see ContainerConfiguration
+ *   @author <a href="marc.fleury@telkel.com">Marc Fleury</a>
+ *   @version $Revision: 1.1 $
+ */
+public class StatefulSessionContainerConfiguration
+   extends ContainerConfiguration
+{
+   // Constants -----------------------------------------------------
+    
+   // Attributes ----------------------------------------------------
+
+   // Static --------------------------------------------------------
+
+   // Constructors --------------------------------------------------
+    
+   // Public --------------------------------------------------------
+
+   // XmlExternalizable implementation ------------------------------
+   
+   // Package protected ---------------------------------------------
+    
+   // Protected -----------------------------------------------------
+    
+   // Private -------------------------------------------------------
+
+   // Inner classes -------------------------------------------------
+}
