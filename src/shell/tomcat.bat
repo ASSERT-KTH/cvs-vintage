e5f624a9db339860f1e880527c7c916a8649c166
craigmcc <craigmcc>
2000-06-19 00:35:46 +0000

    Automatically add all JAR files in %TOMCAT_HOME%\lib, and all classes in %TOMCAT_HOME%\classes, to the Tomcat class path before it is started.
    Could someone please make sure this works correctly on Windows NT as well?

--- src/shell/tomcat.bat
+++ src/shell/tomcat.bat
@@ -1,121 +1,122 @@
 @echo off
-rem A batch file to start/stop tomcat server.
+rem -------------------------------------------------------------------------
+rem tomcat.bat - Start/Stop Script for the TOMCAT Server
+rem
+rem Environment Variable Prerequisites:
+rem
+rem   TOMCAT_HOME  (Optional) May point at your Tomcat distribution
+rem                directory.  If not present, the current working
+rem                directory is assumed.
+rem
+rem   TOMCAT_OPTS  (Optional) Java runtime options used when the "start",
+rem                "stop", or "run" command is executed
+rem
+rem   CLASSPATH    Must contain a JAXP-compatible XML parser, such as the
+rem                "jaxp.jar" and "parser.jar" files from the JAXP Reference
+rem                implementation.
+rem
+rem   JAVA_HOME    Must point at your Java Development Kit installation.
+rem
+rem $Id: tomcat.bat,v 1.23 2000/06/19 00:35:46 craigmcc Exp $
+rem -------------------------------------------------------------------------
 
-rem This batch file written and tested under Windows NT
-rem Improvements to this file are welcome
 
-rem Guess TOMCAT_HOME if it is not present
+rem ----- Save Environment Variables That May Change ------------------------
 
-if not "%TOMCAT_HOME%" == "" goto gothome
+set _CP=%CP%
+set _TOMCAT_HOME=%TOMCAT_HOME%
 
-SET TOMCAT_HOME=.
-if exist %TOMCAT_HOME%\bin\tomcat.bat goto gothome
 
-SET TOMCAT_HOME=..
-if exist %TOMCAT_HOME%\bin\tomcat.bat goto gothome
+rem ----- Verify and Set Required Environment Variables ---------------------
 
-SET TOMCAT_HOME=
-echo Unable to determine the value of TOMCAT_HOME.
-goto eof
+if not "%JAVA_HOME%" == "" goto gotJavaHome
+echo You must have set JAVA_HOME to point at your Java Development Kit installation
+goto cleanup
+:gotJavaHome
 
-:gothome
+if not "%TOMCAT_HOME%" == "" goto gotTomcatHome
+set TOMCAT_HOME=.
+:gotTomcatHome
 
-rem Set up the CLASSPATH that we need
 
-set cp=%CLASSPATH%
+rem ----- Set Up The Runtime Classpath --------------------------------------
 
-set CLASSPATH=.
-set CLASSPATH=%TOMCAT_HOME%\classes
-set CLASSPATH=%CLASSPATH%;%TOMCAT_HOME%\lib\webserver.jar
-set CLASSPATH=%CLASSPATH%;%TOMCAT_HOME%\lib\jasper.jar
-set CLASSPATH=%CLASSPATH%;%TOMCAT_HOME%\lib\servlet.jar
-set CLASSPATH=%CLASSPATH%;%JAVA_HOME%\lib\tools.jar
+set CP=%TOMCAT_HOME%\classes
+for %%i in (%TOMCAT_HOME%\lib\*.jar) do call %TOMCAT_HOME%\bin\cpappend.bat %%i
+if "%CLASSPATH%" == "" goto noClasspath
+set CP=%CP%;%CLASSPATH%
+:noClasspath
+set CP=%CP%;%JAVA_HOME%\lib\tools.jar
+echo Using CLASSPATH: %CP%
 
-if "%cp%" == "" goto next
 
-rem else
-set CLASSPATH=%CLASSPATH%;%cp%
+rem ----- Execute The Requested Command -------------------------------------
 
-:next
 if "%1" == "start" goto startServer
 if "%1" == "stop" goto stopServer
 if "%1" == "run" goto runServer
-if "%1" == "env" goto setupEnv
-if "%1" == "ant" goto runAnt
+if "%1" == "ant goto runAnt
+if "%1" == "env" goto doEnv
 if "%1" == "jspc" goto runJspc
 
-echo Usage:
-echo tomcat (start^|run^|env^|stop)
-echo         start - start tomcat in a separate window
-echo         run   - start tomcat in the current window
-echo               -security - use a SecurityManager when starting
-echo         env   - setup the environment for tomcat
-echo         stop  - stop tomcat
-echo         ant   - run ant with tomcat context
-echo         jspc  - run jsp pre compiler
+:doUsage
+echo Usage:  tomcat ( ant | env | jspc | run | start | stop )
+echo Commands:
+echo   ant -   Run Ant in Tomcat's environment
+echo   env -   Set up environment variables that Tomcat would use
+echo   jspc -  Run JSPC in Tomcat's environment
+echo   run -   Start Tomcat in the current window
+echo   start - Start Tomcat in a separate window
+echo   stop -  Stop Tomcat
 goto cleanup
 
+:doEnv
+goto finish
+
 :startServer
-echo Starting tomcat in new window
-echo Using classpath: %CLASSPATH%
+echo Starting Tomcat in new window
 if "%2" == "-security" goto startSecure
-start java %TOMCAT_OPTS% -Dtomcat.home="%TOMCAT_HOME%" org.apache.tomcat.startup.Tomcat %2 %3 %4 %5 %6 %7 %8 %9
+start java %TOMCAT_OPTS% -classpath %CP% -Dtomcat.home="%TOMCAT_HOME%" org.apache.tomcat.startup.Tomcat %2 %3 %4 %5 %6 %7 %8 %9
 goto cleanup
 
 :startSecure
-echo Starting with a SecurityManager
-start java %TOMCAT_OPTS% -Djava.security.manager -Djava.security.policy=="%TOMCAT_HOME%/conf/tomcat.policy" -Dtomcat.home="%TOMCAT_HOME%" org.apache.tomcat.startup.Tomcat %3 %4 %5 %6 %7 %8 %9
-to cleanup
+echo Starting Tomcat with a SecurityManager
+start java %TOMCAT_OPTS% -classpath %CP% -Djava.security.manager -Djava.security.policy=="%TOMCAT_HOME%/conf/tomcat.policy" -Dtomcat.home="%TOMCAT_HOME%" org.apache.tomcat.startup.Tomcat %3 %4 %5 %6 %7 %8 %9
+goto cleanup
 
 :runServer
-rem Start the Tomcat Server
-echo Using classpath: %CLASSPATH%
+rem Running Tomcat in this window
 if "%2" == "-security" goto runSecure
-java %TOMCAT_OPTS% -Dtomcat.home="%TOMCAT_HOME%" org.apache.tomcat.startup.Tomcat %2 %3 %4 %5 %6 %7 %8 %9
+java %TOMCAT_OPTS% -classpath %CP% -Dtomcat.home="%TOMCAT_HOME%" org.apache.tomcat.startup.Tomcat %2 %3 %4 %5 %6 %7 %8 %9
 goto cleanup
 
 :runSecure
-echo Starting with a SecurityManager
-java %TOMCAT_OPTS% -Djava.security.manager -Djava.security.policy=="%TOMCAT_HOME%/conf/tomcat.policy" -Dtomcat.home="%TOMCAT_HOME%" org.apache.tomcat.startup.Tomcat %3 %4 %5 %6 %7 %8 %9
+rem Running Tomcat with a SecurityManager
+java %TOMCAT_OPTS% -classpath %CP% -Djava.security.manager -Djava.security.policy=="%TOMCAT_HOME%/conf/tomcat.policy" -Dtomcat.home="%TOMCAT_HOME%" org.apache.tomcat.startup.Tomcat %3 %4 %5 %6 %7 %8 %9
 goto cleanup
 
 :stopServer
-rem Stop the Tomcat Server
-echo Using classpath: %CLASSPATH%
-java %TOMCAT_OPTS% -Dtomcat.home="%TOMCAT_HOME%" org.apache.tomcat.startup.Tomcat -stop %2 %3 %4 %5 %6 %7 %8 %9
-goto cleanup
+rem Stopping the Tomcat Server
+java %TOMCAT_OPTS% -classpath %CP% -Dtomcat.home="%TOMCAT_HOME%" org.apache.tomcat.startup.Tomcat -stop %2 %3 %4 %5 %6 %7 %8 %9
 goto cleanup
 
 :runAnt
-rem Run ant
-set CLASSPATH=%CLASSPATH%;%TOMCAT_HOME%\lib\ant.jar
-echo Using classpath: %CLASSPATH%
-java %ANT_OPTS% -Dant.home="%TOMCAT_HOME%" -Dtomcat.home="%TOMCAT_HOME%" org.apache.tools.ant.Main %2 %3 %4 %5 %6 %7 %8 %9
+rem Run ANT in Tomcat's Environment
+set CP=%CP%;%TOMCAT_HOME%\lib\ant.jar
+java %ANT_OPTS% -classpath %CP% -Dant.home="%TOMCAT_HOME%" -Dtomcat.home="%TOMCAT_HOME%" org.apache.tools.ant.Main %2 %3 %4 %5 %6 %7 %8 %9
 goto cleanup
 
 :runJspc
-rem Run ant
-echo Using classpath: %CLASSPATH%
-java %JSPC_OPTS% -Dtomcat.home="%TOMCAT_HOME%" org.apache.jasper.JspC %2 %3 %4 %5 %6 %7 %8 %9
+rem Run JSPC in Tomcat's Environment
+java %JSPC_OPTS% -classpath %CP% -Dtomcat.home="%TOMCAT_HOME%" org.apache.jasper.JspC %2 %3 %4 %5 %6 %7 %8 %9
 goto cleanup
 
-:setupEnv
-set cp=%CLASSPATH%
+
+rem ----- Restore Environment Variables ---------------------------------------
 
 :cleanup
-rem clean up
-
-set CLASSPATH=%cp%
-set port=
-set host=
-set test=
-set jsdkJars=
-set jspJars=
-set beanJars=
-set miscJars=
-set appJars=
-set appClassPath=
-set cp=
-
-rem pause
-:eof
+set TOMCAT_HOME=%_TOMCAT_HOME%
+set _TOMCAT_HOME=
+set CP=%_CP%
+set _CP=
+:finish
