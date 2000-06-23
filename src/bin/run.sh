74c3be3f89e29849207b6569b57c358443e12ae8
sylvain <sylvain>
2000-06-23 17:17:55 +0000

    updated run.sh to automatically start Tomcat if available

--- src/bin/run.sh
+++ src/bin/run.sh
@@ -1,2 +1,14 @@
 ps auxww | grep run.jar | grep -v grep | head -1 | awk " { split(\$0,bob,\" \*\"); print \"kill -9 \", bob[2] }" | sh
-java -jar run.jar
+
+if [ ! -d "$TOMCAT_HOME" -o ! -d "$SERVLETAPI_HOME" ] ; then 
+   echo In order to launch Tomcat with jBoss, you must set the 
+   echo environment variables TOMCAT_HOME and SERVLETAPI_HOME
+
+else
+   CLASSPATH=$CLASSPATH:$TOMCAT_HOME/classes
+   CLASSPATH=$CLASSPATH:$SERVLETAPI_HOME/lib/servlet.jar
+fi
+
+CLASSPATH=$CLASSPATH:run.jar
+
+java -classpath $CLASSPATH -Dtomcat.home=$TOMCAT_HOME org.jboss.Main
