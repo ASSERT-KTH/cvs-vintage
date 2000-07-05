61267da17c15f8b0856c4401bae6d0b53508c08d
sylvain <sylvain>
2000-07-05 23:20:42 +0000

    Removed automatic starting of Tomcat. if a user wants to start Tomcat, he will have to add the MLET to jBoss.conf Help will be available on www.jboss.org

--- src/bin/run.bat
+++ src/bin/run.bat
@@ -5,10 +5,6 @@ rem java -Djboss.verifier.isEnabled=true -jar run.jar
 set CLASSPATH=%CLASSPATH%;run.jar
 
 if not "%TOMCAT_HOME%" == "" if not "%SERVLETAPI_HOME%" == "" goto gotTomcatHome
-echo WARNING
-echo If you want to run jBoss with Tomcat integrated, 
-echo set the TOMCAT_HOME environment variable to point to the root dir of Tomcat
-echo Set the env variable SERVLET_API to the directory 'jakarta-servletapi' (CVS checkout of Tomcat's modules)
 goto startJBoss
 
 :gotTomcatHome
--- src/bin/run.sh
+++ src/bin/run.sh
@@ -1,10 +1,6 @@
 ps auxww | grep run.jar | grep -v grep | head -1 | awk " { split(\$0,bob,\" \*\"); print \"kill -9 \", bob[2] }" | sh
 
-if [ ! -d "$TOMCAT_HOME" -o ! -d "$SERVLETAPI_HOME" ] ; then 
-   echo In order to launch Tomcat with jBoss, you must set the 
-   echo environment variables TOMCAT_HOME and SERVLETAPI_HOME
-
-else
+if [ -d "$TOMCAT_HOME" -a -d "$SERVLETAPI_HOME" ] ; then 
    CLASSPATH=$CLASSPATH:$TOMCAT_HOME/classes
    CLASSPATH=$CLASSPATH:$SERVLETAPI_HOME/lib/servlet.jar
 fi
