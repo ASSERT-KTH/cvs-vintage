294e7e821a99e1180e33bef5e252aa72a0408c9e
sylvain <sylvain>
2000-07-08 02:30:19 +0000

    fixed run.sh
    added TestBeanClient.sh

--- src/bin/run.sh
+++ src/bin/run.sh
@@ -1,5 +1,3 @@
-ps auxww | grep run.jar | grep -v grep | head -1 | awk " { split(\$0,bob,\" \*\"); print \"kill -9 \", bob[2] }" | sh
-
 if [ -d "$TOMCAT_HOME" -a -d "$SERVLETAPI_HOME" ] ; then 
    CLASSPATH=$CLASSPATH:$TOMCAT_HOME/classes
    CLASSPATH=$CLASSPATH:$SERVLETAPI_HOME/lib/servlet.jar
