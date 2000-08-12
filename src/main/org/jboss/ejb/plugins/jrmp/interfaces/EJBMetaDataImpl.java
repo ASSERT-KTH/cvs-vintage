567eeb68b802c5b2b1d127d9365d2c461928d381
fleury <fleury>
2000-08-12 00:05:45 +0000

    EJBMetaDataImpl works from the home handle to retrieve the home

--- src/main/org/jboss/ejb/plugins/jrmp/interfaces/EJBMetaDataImpl.java
+++ src/main/org/jboss/ejb/plugins/jrmp/interfaces/EJBMetaDataImpl.java
@@ -1,53 +1,73 @@
 /*
- * jBoss, the OpenSource EJB server
- *
- * Distributable under GPL license.
- * See terms of license at gnu.org.
- */
+* jBoss, the OpenSource EJB server
+*
+* Distributable under GPL license.
+* See terms of license at gnu.org.
+*/
 package org.jboss.ejb.plugins.jrmp.interfaces;
 
-import javax.ejb.EJBHome;
+import javax.ejb.HomeHandle;
 import javax.ejb.EJBMetaData;
+import javax.ejb.EJBHome;
+import java.rmi.RemoteException;
 
 /**
- *	<description> 
- *      
- *	@see <related>
- *	@author Rickard Öberg (rickard.oberg@telkel.com)
- *	@version $Revision: 1.3 $
- */
+*	<description> 
+*      
+*	@see <related>
+*	@author Rickard Öberg (rickard.oberg@telkel.com)
+*	@author <a href="mailto:marc.fleury@telkel.com>Marc Fleury</a>  
+*	@version $Revision: 1.4 $
+*/
 public class EJBMetaDataImpl
-   implements EJBMetaData, java.io.Serializable
+implements EJBMetaData, java.io.Serializable
 {
-   // Constants -----------------------------------------------------
-    
-   // Attributes ----------------------------------------------------
-   Class remote;
-   Class home;
-   Class pkClass;
-   
-   boolean session;
-   boolean statefulSession;
-   
-   EJBHome homeHandle;
-   
-   // Constructors --------------------------------------------------
-   public EJBMetaDataImpl(Class remote, Class home, Class pkClass, boolean session, boolean statefulSession, EJBHome homeHandle)
-   {
-      this.remote = remote;
-      this.home = home;
-      this.pkClass = pkClass;
-      this.session = session;
-      this.statefulSession = statefulSession;
-      this.homeHandle = homeHandle;
-   }
-   
-   // EJBMetaData ---------------------------------------------------
-   public EJBHome getEJBHome() { return homeHandle; }
-   public java.lang.Class getHomeInterfaceClass() { return home; }
-   public java.lang.Class getRemoteInterfaceClass() { return remote; }
-   public java.lang.Class getPrimaryKeyClass() { return pkClass; }
-   public boolean isSession() { return session; }
-   public boolean isStatelessSession() { return statefulSession; }
+	// Constants -----------------------------------------------------
+	
+	// Attributes ----------------------------------------------------
+	Class remote;
+	Class home;
+	Class pkClass;
+	
+	boolean session;
+	boolean statefulSession;
+	
+	HomeHandle homeHandle;
+	
+	// Constructors --------------------------------------------------
+	public EJBMetaDataImpl(Class remote, Class home, Class pkClass, boolean session, boolean statefulSession, HomeHandle homeHandle)
+	{
+		
+		this.remote = remote;
+		this.home = home;
+		this.pkClass = pkClass;
+		this.session = session;
+		this.statefulSession = statefulSession;
+		this.homeHandle = homeHandle;
+	}
+	
+	// EJBMetaData ---------------------------------------------------
+	public EJBHome getEJBHome() 
+	{ 
+		/* 
+		* MF BUG?????? 
+		* The java.ejb.HomeHandle says throws RemoteException but if I let it be propagated it doesn't compile
+		* ???????????
+		*/
+		try {
+			return homeHandle.getEJBHome();
+		}
+		catch (RemoteException re) {
+			re.printStackTrace();
+			return null;
+		}
+	
+	}
+	
+	public java.lang.Class getHomeInterfaceClass() { return home; }
+	public java.lang.Class getRemoteInterfaceClass() { return remote; }
+	public java.lang.Class getPrimaryKeyClass() { return pkClass; }
+	public boolean isSession() { return session; }
+	public boolean isStatelessSession() { return statefulSession; }
 }
 
