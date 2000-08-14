59d8d221f664846f0f255bf3eb9a9cfbdfa1d956
costin <costin>
2000-08-14 18:08:09 +0000

    Fix for JDK1.1 problem from Jim Rudnicki <jdr5list@pacbell.net>
    There is another patch from Jim ( regarding recycle() ), but I don't
    understand where should it be applyed ( it seems to be a real problem, but ContextManager
    changed and the diff doesn't have enough context - line numbers don't match ).
    
    Submitted by: Jim Rudnicki <jdr5list@pacbell.net>

--- src/share/org/apache/tomcat/loader/SimpleFileFilter.java
+++ src/share/org/apache/tomcat/loader/SimpleFileFilter.java
@@ -64,7 +64,7 @@ import java.io.*;
  * interface.
  *
  * @author <a href="mailto:mjenning@islandnet.com">Mike Jennings</a>
- * @version $Revision: 1.4 $
+ * @version $Revision: 1.5 $
  * @deprecated No longer used, AdaptiveClassLoader1 doesn't need it
  */
 public class SimpleFileFilter implements FilenameFilter {
@@ -102,9 +102,19 @@ public class SimpleFileFilter implements FilenameFilter {
       */
       public static String[] fileOrFiles(File f) {
           if (f==null) return null;
+
+	  String[] files;
+
+	  /* directories return null for getName()
+	     Test for these first */
+	  if( f.isDirectory() ) {
+	      files=new String[1];
+	      files[0]=f.getPath();
+	      return files;
+	  }
+	  String fname=f.getName();
           File parent=new File(f.getParent());
-          String fname=f.getName();
-          String[] files;
+
           if (fname.charAt(0)=='*') {
               String filter=fname.substring(1,fname.length());
               files=parent.list(new SimpleFileFilter(filter));
