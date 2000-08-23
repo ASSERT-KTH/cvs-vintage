7a4f78cf7873bcefde9e7f704f5747fd158ba51f
juha <juha>
2000-08-23 18:42:40 +0000

    Shouldn't mess with the global CLASSPATH variable. This helps with at least some of my JMX bad voodoo.

--- src/build/build.bat
+++ src/build/build.bat
@@ -1,10 +1,10 @@
 @echo off
 REM convenience bat file to build with
 
-set CLASSPATH=..\..\lib\ant.jar
-set CLASSPATH=%CLASSPATH%;..\..\lib\xml.jar
-set CLASSPATH=%CLASSPATH%;..\..\lib\xmlbeans.jar
-set CLASSPATH=%CLASSPATH%;..\..\build\classes
-set CLASSPATH=%CLASSPATH%;..\..\lib\javac.jar
+set CP=..\..\lib\ant.jar
+set CP=%CP%;..\..\lib\xml.jar
+set CP=%CP%;..\..\lib\xmlbeans.jar
+set CP=%CP%;..\..\build\classes
+set CP=%CP%;..\..\lib\javac.jar
 
-java -classpath "%CLASSPATH%" org.apache.tools.ant.Main %1 %2 %3 %4 %5
+java -classpath "%CP%" org.apache.tools.ant.Main %1 %2 %3 %4 %5
