a039afd4115717eaf89adbc20dd28642c0c1147d
fleury <fleury>
2000-09-08 22:25:54 +0000

    Removing the MBeanProxy for now, buggy deployment

--- src/main/org/jboss/ejb/ContainerFactory.java
+++ src/main/org/jboss/ejb/ContainerFactory.java
@@ -76,7 +76,7 @@ import org.jboss.logging.Logger;
 *   @author <a href="mailto:jplindfo@helsinki.fi">Juha Lindfors</a>
 *   @author <a href="mailto:sebastien.alborini@m4x.org">Sebastien Alborini</a>
 *
-*   @version $Revision: 1.38 $
+*   @version $Revision: 1.39 $
 */
 public class ContainerFactory
     extends org.jboss.util.ServiceMBeanSupport
@@ -595,8 +595,8 @@ public class ContainerFactory
          app.start();
 
          // Add to webserver so client can access classes through dynamic class downloading
-         WebServiceMBean webServer = (WebServiceMBean)MBeanProxy.create(WebServiceMBean.class, WebServiceMBean.OBJECT_NAME);
-         webServer.addClassLoader(cl);
+         //WebServiceMBean webServer = (WebServiceMBean)MBeanProxy.create(WebServiceMBean.class, WebServiceMBean.OBJECT_NAME);
+         //webServer.addClassLoader(cl);
 
          // Done
          log.log("Deployed application: "+app.getName());
@@ -610,10 +610,11 @@ public class ContainerFactory
                 // Avoids useless 'null' messages on a server trace.
                 // Let's be honest and spam them with a stack trace.
                 // NPE should be considered an internal server error anyways.
-                //Logger.exception(e);
+                Logger.exception(e);
             }
             
-         Logger.debug(e.getMessage());
+		  Logger.exception(e);
+         //Logger.debug(e.getMessage());
 
          app.stop();
          app.destroy();
