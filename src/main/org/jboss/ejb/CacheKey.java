7f3d1fe2c4e70d22df14f98533e07ae7d9a35cd8
fleury <fleury>
2000-09-26 17:38:31 +0000

    The new cache key is a class and does the jonathan weedon based serialization (based on the MarshalledObject)

--- src/main/org/jboss/ejb/CacheKey.java
+++ src/main/org/jboss/ejb/CacheKey.java
@@ -11,41 +11,47 @@ package org.jboss.ejb;
 /**
 *   CacheKey
 * 
-*   CacheKey is an encapsulation of both the PrimaryKey and any cache specific key
+*   CacheKey is an encapsulation of both the PrimaryKey and a cache specific key
 *   
-* 	There are several strategies to implement the cache. 
-* 	One is to use the hash and equals of this class to implement tables
-* 	Another one is to work from the value of the CacheKey
+*   This implementation is a safe implementation in the sense that it doesn't rely 
+*   on the user supplied hashcode and equals.   It is also fast since the hashCode operation 
+*   is pre-calculated.
 *
 *   @see org.jboss.ejb.plugins.NoPassivationInstanceCache.java
 *   @author <a href="marc.fleury@telkel.com">Marc Fleury</a>
-*   @version $Revision: 1.1 $
+*   @version $Revision: 1.2 $
 */
 public class CacheKey
+    implements java.io.Externalizable
 {
     // Constants -----------------------------------------------------
     
     // Attributes ----------------------------------------------------
     
-	// The database primaryKey
-	public Object id;
-	
-	// In case the cache doesn't use advanced cache keys, this key is virtual
-    public boolean isVirtual = false;
-	
+    // The database primaryKey
+    public Object id;
+     
+    private int hashCode;
+    
     // Static --------------------------------------------------------  
     
     // Public --------------------------------------------------------
     
-	public CacheKey() {
-		// For externalization only
+    public CacheKey() {
+       // For externalization only
+    }
+    public CacheKey(Object id) {
+       
+       if (id == null) throw new Error("id may not be null");
+         
+       this.id = id;
+        
+		try {
+        	hashCode = new java.rmi.MarshalledObject(id).hashCode();
+    	}
+		catch (Exception e) {e.printStackTrace();}
 	}
-	public CacheKey(Object id) {
-		
-		if (id == null) throw new Error("id may not be null");
-			
-		this.id = id;
-	}
+    
     // Z implementation ----------------------------------------------
     
     // Package protected ---------------------------------------------
@@ -54,16 +60,30 @@ public class CacheKey
     
     // Private -------------------------------------------------------
     
+   public void writeExternal(java.io.ObjectOutput out)
+      throws java.io.IOException
+   {
+        out.writeObject(id);
+       	out.write(hashCode);
+   }
+   
+   public void readExternal(java.io.ObjectInput in)
+      throws java.io.IOException, ClassNotFoundException
+   {
+        id = in.readObject();
+       hashCode = in.read();
+   }
+
     // HashCode and Equals over write --------------------------------
     
-	/**
-	* these should be overwritten by extending Cache key
-	* since they define what the cache does in the first place
-	*/
+    /**
+    * these should be overwritten by extending Cache key
+    * since they define what the cache does in the first place
+    */
     public int hashCode() {
         
         // we default to the pK id
-        return id.hashCode();
+        return hashCode;
     }
     
     
@@ -71,7 +91,7 @@ public class CacheKey
         
         if (object instanceof CacheKey) {
             
-            return id.equals(((CacheKey) object).id);
+            return (hashCode ==(((CacheKey) object).hashCode));
         }
         return false;
     }
