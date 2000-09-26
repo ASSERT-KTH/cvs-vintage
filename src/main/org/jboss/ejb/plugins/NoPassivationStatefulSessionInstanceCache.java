55b7a4ee6a72c5d395f20a6ea52e72254766314b
fleury <fleury>
2000-09-26 18:55:50 +0000

    The new cache does not worry about locking.  Passivating algo go here.

--- src/main/org/jboss/ejb/plugins/NoPassivationStatefulSessionInstanceCache.java
+++ src/main/org/jboss/ejb/plugins/NoPassivationStatefulSessionInstanceCache.java
@@ -32,7 +32,7 @@ import org.jboss.logging.Logger;
 *	@see <related>
 *	@author Rickard Öberg (rickard.oberg@telkel.com)
 *   @author <a href="marc.fleury@telkel.com">Marc Fleury</a>
-*	@version $Revision: 1.8 $
+*	@version $Revision: 1.9 $
 */
 public class NoPassivationStatefulSessionInstanceCache
 implements InstanceCache
@@ -82,79 +82,48 @@ implements InstanceCache
     public synchronized EnterpriseContext get(Object id)
     throws RemoteException
     {
-       InstanceInfo info = null;
        
-        Logger.log("I AM LOOKING FOR THE ID "+id);
+        //DEBUG Logger.log("Stateful cache looking for ID "+id);
+        Logger.log("Stateful cache looking for ID "+id);
+     
        // Do we have the context in cache?
        StatefulSessionEnterpriseContext ctx = 
          (StatefulSessionEnterpriseContext)active.get(id);
-       
-       // We have it in cache
-       if (ctx != null) {
-         
-         info = (InstanceInfo)ctx.getCacheContext();
-         
-         if (info.isLocked()) {
-         
-          //MF DESIGN: talk about this one... I know it is spec compliant but it sucks
-          throw new RemoteException("Concurrent call to stateful session is not allowed");
-         }
-         
-         else {
-          
-          info.lock();
-         }
-       }
          
         // We don't have it in cache
-       if (ctx == null) {
-         
-         // Get new instance from pool (bogus in our case)
-         ctx = (StatefulSessionEnterpriseContext)((InstancePoolContainer)con).getInstancePool().get();
-         
-         // Activate
-         ctx.setId(id);
-         
+        if (ctx == null) {
+            
+            // Get new instance from pool (bogus in our case)
+            ctx = (StatefulSessionEnterpriseContext)((InstancePoolContainer)con).getInstancePool().get();
+            
+            // Activate
+            ctx.setId(id);
+            
             try {
-         
+                
                 ((StatefulSessionContainer)con).getPersistenceManager().activateSession(ctx);
             }
             catch (Exception e) {
                 
                 throw new RemoteException("Object was not found");
             }
-         insert(ctx);
-       }
+            
+            insert(ctx);
+        }
        // The context has the instance as well and the right id
        return ctx;
     }
     
     public synchronized void insert(EnterpriseContext ctx)
     {
-       InstanceInfo info = createInstanceInfo((StatefulSessionEnterpriseContext)ctx);
-       ((StatefulSessionEnterpriseContext)ctx).setCacheContext(info);
-       info.lock();
        active.put(ctx.getId(), ctx) ;
     }
     
-    public void release(EnterpriseContext ctx)
-    {
-       // This context is now available for other threads
-       ((InstanceInfo)((StatefulSessionEnterpriseContext)ctx).getCacheContext()).unlock();
-       
-       // The following code makes sense if we put threads to sleep (not the case anymore)
-       //if (!((InstanceInfo)((StatefulSessionEnterpriseContext)ctx).getCacheContext()).isLocked())
-       //		ctx.notify();
-       
-    }
+    
     
     public synchronized void remove(Object id)
     {
        Object ctx = active.remove(id);
-       synchronized(ctx)
-       {
-         ctx.notifyAll();
-       }
     }
     
     // Z implementation ----------------------------------------------
@@ -162,43 +131,8 @@ implements InstanceCache
     // Package protected ---------------------------------------------
     
     // Protected -----------------------------------------------------
-    protected InstanceInfo createInstanceInfo(StatefulSessionEnterpriseContext ctx)
-    {
-       return new InstanceInfo(ctx);
-    }
     
     // Private -------------------------------------------------------
     
     // Inner classes -------------------------------------------------
-    class InstanceInfo
-    {
-       int locked = 0; // 0 == unlocked, >0 == locked
-       
-       StatefulSessionEnterpriseContext ctx;
-       
-       InstanceInfo(StatefulSessionEnterpriseContext ctx)
-       {
-         this.ctx = ctx;
-       }
-       
-       public void lock()
-       {
-         locked++;
-       }
-       
-       public void unlock()
-       {
-         locked--;
-       }
-       
-       public boolean isLocked()
-       {
-         return locked > 0;
-       }
-       
-       public StatefulSessionEnterpriseContext getContext()
-       {
-         return ctx;
-       }
-    }
 }
