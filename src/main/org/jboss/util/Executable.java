be1062317a826964bf8707f2be5d69118466910a
simone <simone>
2000-10-16 23:16:03 +0000

    Interface for job execution

---
+++ src/main/org/jboss/util/Executable.java
@@ -0,0 +1,27 @@
+/*
+ * jBoss, the OpenSource EJB server
+ *
+ * Distributable under GPL license.
+ * See terms of license at gnu.org.
+ */
+package org.jboss.util;
+
+/**
+ * Interface for the execution of a task. <p>
+ * 
+ * @see WorkerQueue
+ * @author Simone Bordet (simone.bordet@compaq.com)
+ * @version $Revision: 1.1 $
+ */
+public interface Executable
+{
+	// Constants -----------------------------------------------------
+
+	// Static --------------------------------------------------------
+
+	// Public --------------------------------------------------------
+	/**
+	 * Executes the implemented task.
+	 */
+	public void execute() throws Exception;
+}
