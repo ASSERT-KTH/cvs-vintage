78df1d829be1b86a324281f5b66bd899711289fc
simone <simone>
2000-10-19 21:40:52 +0000

    Editor for cache policies

---
+++ src/main/org/jboss/ejb/deployment/editors/CachePolicyTypeEditor.java
@@ -0,0 +1,25 @@
+/*
+ * jBoss, the OpenSource EJB server
+ *
+ * Distributable under GPL license.
+ * See terms of license at gnu.org.
+ */
+package org.jboss.ejb.deployment.editors;
+
+import com.dreambean.awt.editors.TagsEditor;
+
+/**
+ *  Editor for selecting the cache policy
+ *      
+ *	@author Simone Bordet (simone.bordet@compaq.com)
+ *  @version $Revision: 1.1 $
+ */
+public class CachePolicyTypeEditor
+   extends TagsEditor
+{
+   // Constructors --------------------------------------------------
+   public CachePolicyTypeEditor()
+   {
+      super("org/jboss/ejb/deployment/editors/cachepolicy.properties");
+   }
+}
