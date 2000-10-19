9447a8baacca24039f40fabed96883fbda6cef30
simone <simone>
2000-10-19 21:43:30 +0000

    Bean used by EJX for the configuration of the new passivating caches

---
+++ src/main/org/jboss/ejb/deployment/EntityInstanceCacheConfiguration.java
@@ -0,0 +1,40 @@
+/*
+ * jBoss, the OpenSource EJB server
+ *
+ * Distributable under GPL license.
+ * See terms of license at gnu.org.
+ */
+package org.jboss.ejb.deployment;
+
+/**
+ *
+ *
+ * @see
+ * @author Simone Bordet (simone.bordet@compaq.com)
+ * @version $Revision: 1.1 $
+ */
+public class EntityInstanceCacheConfiguration 
+	extends InstanceCacheSupportConfiguration 
+{
+	// Constants -----------------------------------------------------
+
+	// Attributes ----------------------------------------------------
+
+	// Static --------------------------------------------------------
+
+	// Constructors --------------------------------------------------
+
+	// Public --------------------------------------------------------
+
+	// Z implementation ----------------------------------------------
+
+	// Y overrides ---------------------------------------------------
+
+	// Package protected ---------------------------------------------
+
+	// Protected -----------------------------------------------------
+
+	// Private -------------------------------------------------------
+
+	// Inner classes -------------------------------------------------
+}
