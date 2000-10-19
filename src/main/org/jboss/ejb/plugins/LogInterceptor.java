c7763b4a3528bcc53452d89b9cea212a2b3d4354
oleg <oleg>
2000-10-19 16:05:22 +0000

    Minor exception logging improvements: 1) an exceptions wrapped by both TransactionRolledbackException and EJBException are now unwrapped for logging 2) application exception's stack trace is logged as a Debug info if callLogging is on.

--- src/main/org/jboss/ejb/plugins/LogInterceptor.java
+++ src/main/org/jboss/ejb/plugins/LogInterceptor.java
@@ -36,7 +36,7 @@ import org.jboss.logging.Logger;
  *      
  *   @see <related>
  *   @author Rickard Öberg (rickard.oberg@telkel.com)
- *   @version $Revision: 1.10 $
+ *   @version $Revision: 1.11 $
  */
 public class LogInterceptor
    extends AbstractInterceptor
@@ -46,10 +46,10 @@ public class LogInterceptor
    // Attributes ----------------------------------------------------
    protected Log log;
    
-	protected boolean callLogging;
-	
-	protected Container container;
-	
+    protected boolean callLogging;
+    
+    protected Container container;
+    
    // Static --------------------------------------------------------
 
    // Constructors --------------------------------------------------
@@ -57,12 +57,12 @@ public class LogInterceptor
    // Public --------------------------------------------------------
    public void setContainer(Container container) 
    { 
-   	this.container = container; 
+    this.container = container; 
    }
-	
+    
    public  Container getContainer()
    {
-   	return container;
+    return container;
    }
 
    // Container implementation --------------------------------------
@@ -72,10 +72,10 @@ public class LogInterceptor
       super.start();
       
       String name = getContainer().getBeanMetaData().getEjbName();
-		
-	  // Should we log all calls?
-	  callLogging = getContainer().getBeanMetaData().getContainerConfiguration().getCallLogging();
-		
+        
+      // Should we log all calls?
+      callLogging = getContainer().getBeanMetaData().getContainerConfiguration().getCallLogging();
+        
       log = new Log(name);
    }
    
@@ -84,62 +84,69 @@ public class LogInterceptor
    {
       Log.setLog(log);
       
-		// Log calls?
-		if (callLogging)
-		{
-			StringBuffer str = new StringBuffer();
-			str.append(mi.getMethod().getName());
-			str.append("(");
-			Object[] args = mi.getArguments();
-			if (args != null)
-			   for (int i = 0; i < args.length; i++)
-				{
-			      str.append(i==0?"":",");
-					str.append(args[i]);
-				}
-			str.append(")");
-			log.log(str.toString());
-		}
+        // Log calls?
+        if (callLogging)
+        {
+            StringBuffer str = new StringBuffer();
+            str.append(mi.getMethod().getName());
+            str.append("(");
+            Object[] args = mi.getArguments();
+            if (args != null)
+               for (int i = 0; i < args.length; i++)
+                {
+                  str.append(i==0?"":",");
+                    str.append(args[i]);
+                }
+            str.append(")");
+            log.log(str.toString());
+        }
       
       try
       {
          return getNext().invokeHome(mi);
       } catch (Exception e)
       {
-		// Log system exceptions
-		if (e instanceof EJBException)
-		{
-			Logger.error("BEAN EXCEPTION:"+e.getMessage());
-			if (((EJBException)e).getCausedByException() != null)
-				Logger.exception(((EJBException)e).getCausedByException());
-			
-			// Client sees RemoteException
-			throw new ServerException("Bean exception. Notify the application administrator", e);
-		} else if (e instanceof RuntimeException)
-		{
-			Logger.error("CONTAINER EXCEPTION:"+e.getMessage());
-			Logger.exception(e);
-			
-			// Client sees RemoteException
-			throw new ServerException("Container exception. Notify the container developers :-)", e);
-		} else if (e instanceof TransactionRolledbackException)
-		{
-			Logger.error("TRANSACTION ROLLBACK EXCEPTION:"+e.getMessage());
-			// Log the rollback cause
-			Logger.exception(((RemoteException)e).detail);
-			
-			throw e;
-		} else
-		{
-			// Application exception, or (in case of RemoteException) already handled system exc
-			// Call debugging -> show exceptions
-			if (callLogging)
-			{
-				Logger.warning(e.getMessage());
-			}
-			
-			throw e;
-		}
+        // Log system exceptions
+        if (e instanceof EJBException)
+        {
+            Logger.error("BEAN EXCEPTION:"+e.getMessage());
+            if (((EJBException)e).getCausedByException() != null)
+                Logger.exception(((EJBException)e).getCausedByException());
+            
+            // Client sees RemoteException
+            throw new ServerException("Bean exception. Notify the application administrator", e);
+        } else if (e instanceof RuntimeException)
+        {
+            Logger.error("CONTAINER EXCEPTION:"+e.getMessage());
+            Logger.exception(e);
+            
+            // Client sees RemoteException
+            throw new ServerException("Container exception. Notify the container developers :-)", e);
+        } else if (e instanceof TransactionRolledbackException)
+        {
+            Logger.error("TRANSACTION ROLLBACK EXCEPTION:"+e.getMessage());
+            // Log the rollback cause
+            // Sometimes it wraps EJBException - let's unwrap it
+            Throwable cause = ((RemoteException) e).detail;
+            if (cause != null) {
+                if ((cause instanceof EJBException) &&
+                        (((EJBException) cause).getCausedByException() != null)) {
+                    cause = ((EJBException) cause).getCausedByException();
+                }
+                Logger.exception(cause);
+            }
+            throw e;
+        } else
+        {
+            // Application exception, or (in case of RemoteException) already handled system exc
+            // Call debugging -> show exceptions
+            if (callLogging)
+            {
+                Logger.warning(e.getMessage());
+            }
+            
+            throw e;
+        }
       } finally
       {
          Log.unsetLog();
@@ -165,60 +172,70 @@ public class LogInterceptor
       // Log calls?
       if (callLogging)
       {
-      	StringBuffer str = new StringBuffer();
+        StringBuffer str = new StringBuffer();
          str.append(mi.getId() == null ? "" : "["+mi.getId().toString()+"] ");
-      	str.append(mi.getMethod().getName());
-      	str.append("(");
+        str.append(mi.getMethod().getName());
+        str.append("(");
          Object[] args = mi.getArguments();
          if (args != null)
             for (int i = 0; i < args.length; i++)
-      		{
+            {
                str.append(i==0?"":",");
-      			str.append(args[i]);
-      		}
-      	str.append(")");
+                str.append(args[i]);
+            }
+        str.append(")");
          log.log(str.toString());
       }
-		
+        
       try
       {
          return getNext().invoke(mi);
       } catch (Exception e)
       {
-		// Log system exceptions
-		if (e instanceof EJBException)
-		{
-			Logger.error("BEAN EXCEPTION:"+e.getMessage());
-			if (((EJBException)e).getCausedByException() != null)
-				Logger.exception(((EJBException)e).getCausedByException());
-			
-			// Client sees RemoteException
-			throw new ServerException("Bean exception. Notify the application administrator", e);
-		} else if (e instanceof RuntimeException)
-		{
-			Logger.error("CONTAINER EXCEPTION:"+e.getMessage());
-			Logger.exception(e);
-			
-			// Client sees RemoteException
-			throw new ServerException("Container exception. Notify the container developers :-)", e);
-		} else if (e instanceof TransactionRolledbackException)
-		{
-			Logger.error("TRANSACTION ROLLBACK EXCEPTION:"+e.getMessage());
-			// Log the rollback cause
-			Logger.exception(((RemoteException)e).detail);
-			
-			throw e;
-		} else
-		{
-			// Application exception, or (in case of RemoteException) already handled system exc
-			// Call debugging -> show exceptions
-			if (callLogging)
-			{
-				Logger.warning(e.getMessage());
-			}
-			
-			throw e;
-		}
+        // Log system exceptions
+        if (e instanceof EJBException)
+        {
+            Logger.error("BEAN EXCEPTION:"+e.getMessage());
+            if (((EJBException)e).getCausedByException() != null)
+                Logger.exception(((EJBException)e).getCausedByException());
+            
+            // Client sees RemoteException
+            throw new ServerException("Bean exception. Notify the application administrator", e);
+        } else if (e instanceof RuntimeException)
+        {
+            Logger.error("CONTAINER EXCEPTION:"+e.getMessage());
+            Logger.exception(e);
+            
+            // Client sees RemoteException
+            throw new ServerException("Container exception. Notify the container developers :-)", e);
+        } else if (e instanceof TransactionRolledbackException)
+        {
+            Logger.error("TRANSACTION ROLLBACK EXCEPTION:"+e.getMessage());
+            // Log the rollback cause
+            // Sometimes it wraps EJBException - let's unwrap it
+            Throwable cause = ((RemoteException) e).detail;
+            if (cause != null) {
+                if ((cause instanceof EJBException) && 
+                        (((EJBException) cause).getCausedByException() != null)) {
+                    cause = ((EJBException) cause).getCausedByException();
+                }
+                Logger.exception(cause);
+            }
+            throw e;
+        } else
+        {
+            // Application exception, or (in case of RemoteException) already handled system exc
+            // Call debugging -> show exceptions
+            if (callLogging)
+            {
+                Logger.warning(e.getMessage());
+                // The full stack trace is much more useful for debugging
+                // On the other hand, it may be turned off by the logger filter
+                Logger.debug(e);
+            }
+            
+            throw e;
+        }
       } finally
       {
          Log.unsetLog();
