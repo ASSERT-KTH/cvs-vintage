2a1a0a17e38ccb89fd22e61d1f55c4f4b6b2a768
oberg <oberg>
2000-11-07 12:26:48 +0000

    Added configurable parameter for format

--- src/main/org/jboss/logging/FileLoggingMBean.java
+++ src/main/org/jboss/logging/FileLoggingMBean.java
@@ -10,14 +10,18 @@ package org.jboss.logging;
  *
  *   @see <related>
  *   @author Rickard Öberg (rickard.oberg@telkel.com)
- *   @version $Revision: 1.2 $
+ *   @version $Revision: 1.3 $
  */
 public interface FileLoggingMBean
 {
    // Constants -----------------------------------------------------
+   public static final String OBJECT_NAME = ":service=Logging,type=File";
 
    // Public --------------------------------------------------------
    public void setLogName(String logName) throws java.io.FileNotFoundException;
    public String getLogName();
+   
+   public void setFormat(String format);
+   public String getFormat();
 }
 
