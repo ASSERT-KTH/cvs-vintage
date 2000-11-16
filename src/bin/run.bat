1ce5ecd845cf6e1c58e5b90931071e85c5bf34a9
salborini <salborini>
2000-11-16 01:37:46 +0000

    Cleaner scripts.  Jetty/tomcat home is now guessed from the classpath.

--- src/bin/run.bat
+++ src/bin/run.bat
@@ -8,6 +8,6 @@ REM Add all login modules for JAAS-based security
 REM and all libraries that are used by them here
 set CLASSPATH=%CLASSPATH%;..\lib\jdbc2_0-stdext.jar;..\lib\jboss-jaas.jar
 
-java -classpath "%CLASSPATH%" -Dtomcat.home=%TOMCAT_HOME% org.jboss.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -classpath "%CLASSPATH%" org.jboss.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 pause
--- src/bin/run.sh
+++ src/bin/run.sh
@@ -4,28 +4,9 @@
 
 CLASSPATH=$CLASSPATH:run.jar
 
-JETTY_FLAGS=
-if [ "${JETTY_HOME}X" != "X" ] ; then
-    if [ -x $JETTY_HOME ] ; then
-        echo "Adding jar files in ${JETTY_HOME}/lib to CLASSPATH"
-        CLASSPATH="$CLASSPATH:${JETTY_HOME}/lib"
-        CLASSPATH="$CLASSPATH:${JETTY_HOME}/lib/com.sun.net.ssl.jar"
-        CLASSPATH="$CLASSPATH:${JETTY_HOME}/lib/javax.servlet.jar"
-        CLASSPATH="$CLASSPATH:${JETTY_HOME}/lib/com.mortbay.jetty.jar"
-        CLASSPATH="$CLASSPATH:${JETTY_HOME}/lib/org.apache.jasper.jar"
-
-        # Add the tools.jar file so that Jetty can find the Java compiler.
-        CLASSPATH="$CLASSPATH:$JAVA_HOME/lib/tools.jar"
-    else 
-        echo "JETTY_HOME is set, but is an invalid directory"
-    fi
-    JETTY_FLAGS="$JETTY_FLAGS -Djetty.home=$JETTY_HOME"
-    JETTY_FLAGS="$JETTY_FLAGS -Dorg.xml.sax.parser=com.sun.xml.parser.Parser"
-fi
-
 # Add all login modules for JAAS-based security
 # and all libraries that are used by them here
 CLASSPATH="$CLASSPATH:../lib/jdbc2_0-stdext.jar:../lib/jboss-jaas.jar"
 
 echo $CLASSPATH
-java -server -classpath $CLASSPATH $JETTY_FLAGS -Dtomcat.home=$TOMCAT_HOME org.jboss.Main $@
+java -server -classpath $CLASSPATH org.jboss.Main $@
