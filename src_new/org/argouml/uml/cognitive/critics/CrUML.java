b8cf479be87710889f8a7cf68ae82c98484be331
carnold <carnold>
2000-11-30 20:59:53 +0000

    Reestablished Argo variant of OCLEvaluator
    Added an Apache acknowledgement to the AboutBox when
    Xerces is being used.

--- src_new/org/argouml/uml/cognitive/critics/CrUML.java
+++ src_new/org/argouml/uml/cognitive/critics/CrUML.java
@@ -24,7 +24,7 @@
 // File: CrUML.java
 // Classes: CrUML
 // Original Author: jrobbins@ics.uci.edu
-// $Id: CrUML.java,v 1.2 2000/11/29 07:45:16 carnold Exp $
+// $Id: CrUML.java,v 1.3 2000/11/30 20:59:53 carnold Exp $
 
 package org.argouml.uml.cognitive.critics;
 
@@ -37,7 +37,7 @@ import org.argouml.kernel.*;
 import org.argouml.ui.*;
 import org.argouml.cognitive.*;
 import org.argouml.cognitive.critics.*;
-import org.tigris.gef.ocl.OCLEvaluator;
+import org.argouml.ocl.OCLEvaluator;
 
 /** "Abstract" Critic subclass that captures commonalities among all
  *  critics in the UML domain.  This class also defines and registers
@@ -176,7 +176,7 @@ public class CrUML extends Critic {
     while (matchPos != -1) {
       int endExpr = res.indexOf(OCL_END, matchPos + 1);
       String expr = res.substring(matchPos + OCL_START.length(), endExpr);
-      String evalStr = OCLEvaluator.evalToString(off1, expr);
+      String evalStr = OCLEvaluator.SINGLETON.evalToString(off1, expr);
       //System.out.println("expr='" + expr + "' = '" + evalStr + "'");
       if (expr.endsWith("") && evalStr.equals(""))
 	evalStr = "(anon)";
