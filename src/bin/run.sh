3d6e69e9bf30da92f50267bf875e61c493b056ee
tobias <tobias>
2000-12-07 20:04:38 +0000

    bugfix. deleting lines helps sometimes :-)

--- src/bin/run.sh
+++ src/bin/run.sh
@@ -6,7 +6,6 @@ CLASSPATH=$CLASSPATH:run.jar
 
 # Add all login modules for JAAS-based security
 # and all libraries that are used by them here
-CLASSPATH="$CLASSPATH
 
 echo $CLASSPATH
 java -server -classpath $CLASSPATH org.jboss.Main $@
