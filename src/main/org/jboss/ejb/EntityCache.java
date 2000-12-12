ef6674f73ea4a015282a9af1d8ca8d94496fee05
simone <simone>
2000-12-12 09:48:51 +0000

    New name of EntityInstanceCache, to avoid name clash with another class.

---
+++ src/main/org/jboss/ejb/EntityCache.java
@@ -0,0 +1,35 @@
+package org.jboss.ejb;
+
+/*
+ * JBoss, the OpenSource EJB server
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+
+import java.rmi.RemoteException;
+import javax.ejb.NoSuchEntityException;
+
+/**
+ *	EntityCaches can work from several keys
+ *
+ *  A cache can use the natural primaryKey from the EJBObject, or DB dependent
+ *  keys or a proprietary key
+ *
+ *	@see EntityInstanceCache
+ *	@author <a href="marc.fleury@telkel.com">Marc Fleury</a>
+ *	@version $Revision: 1.1 $
+ */
+public interface EntityCache
+   extends InstanceCache
+{
+   /**
+    * Returns the key used to cache the context
+	*
+	* @param id				Object id / primary key
+	*
+	* @return				Cache key
+	*/
+   public Object createCacheKey( Object id );
+
+}
