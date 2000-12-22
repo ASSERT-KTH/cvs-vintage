9a3ebc0f026f0fff90dbb60aa10feb9cfa4beed4
artem <artem>
2000-12-22 01:34:58 +0000

    Made script more verbose. Made dropping database automatic.

--- src/sql/create-database.bat
+++ src/sql/create-database.bat
@@ -1,26 +1,27 @@
 @echo off
-mysqladmin drop scarab
-echo y
+echo Drop Scarab database...
+echo y |mysqladmin drop scarab
 
+echo Create Scarab database...
 mysqladmin create scarab
 
-echo "Importing MySQL_users_roles_permissions.sql..."
+echo Importing MySQL_users_roles_permissions.sql...
 mysql scarab < MySQL_users_roles_permissions.sql
 
-echo "Importing MySQL_id_table.sql..."
+echo Importing MySQL_id_table.sql...
 mysql scarab < MySQL_id_table.sql
 
-echo "Importing id_broker_init.sql..."
+echo Importing id_broker_init.sql...
 mysql scarab < id_broker_init.sql
 
-echo "Importing scarab-mysql.sql..."
+echo Importing scarab-mysql.sql...
 mysql scarab < scarab-mysql.sql
 
-echo "Importing default_roles_permissions.sql..."
+echo Importing default_roles_permissions.sql...
 mysql scarab < default_roles_permissions.sql
 
-echo "Importing MySQL_default_data.sql..."
+echo Importing MySQL_default_data.sql...
 mysql scarab < MySQL_default_data.sql
 
-echo "Importing MySQL_sample_data.sql..."
+echo Importing MySQL_sample_data.sql...
 mysql scarab < MySQL_sample_data.sql
