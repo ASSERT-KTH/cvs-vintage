5aa70772860ff39d2fe43dbcc92fc6ecd66af5fb
jon <jon>
2001-01-23 01:36:28 +0000

    added id's for scarab

---
+++ src/sql/mysql-scarab-id-table-init.sql
@@ -0,0 +1,14 @@
+insert into ID_TABLE (table_name, next_id, quantity) VALUES ('SCARAB_ATTRIBUTE_CLASS', 100, 10);
+insert into ID_TABLE (table_name, next_id, quantity) VALUES ('SCARAB_ATTRIBUTE_OPTION', 100, 10);
+insert into ID_TABLE (table_name, next_id, quantity) VALUES ('SCARAB_ATTRIBUTE_TYPE', 100, 10);
+insert into ID_TABLE (table_name, next_id, quantity) VALUES ('SCARAB_DEPEND', 100, 10);
+insert into ID_TABLE (table_name, next_id, quantity) VALUES ('SCARAB_DEPEND_TYPE', 100, 10);
+insert into ID_TABLE (table_name, next_id, quantity) VALUES ('SCARAB_ISSUE', 100, 10);
+insert into ID_TABLE (table_name, next_id, quantity) VALUES ('SCARAB_ISSUE_ATTRIBUTE_VALUE', 100, 10);
+insert into ID_TABLE (table_name, next_id, quantity) VALUES ('SCARAB_MODIFICATION', 100, 10);
+insert into ID_TABLE (table_name, next_id, quantity) VALUES ('SCARAB_MODULE', 100, 10);
+insert into ID_TABLE (table_name, next_id, quantity) VALUES ('SCARAB_R_MODULE_ATTRIBUTE', 100, 10);
+insert into ID_TABLE (table_name, next_id, quantity) VALUES ('SCARAB_R_MODULE_USER', 100, 10);
+insert into ID_TABLE (table_name, next_id, quantity) VALUES ('SCARAB_R_MODULE_USER_ROLE', 100, 10);
+insert into ID_TABLE (table_name, next_id, quantity) VALUES ('SCARAB_TRANSACTION', 100, 10);
+insert into ID_TABLE (table_name, next_id, quantity) VALUES ('SCARAB_ISSUE_ATTRIBUTE_VOTE', 100, 10);
