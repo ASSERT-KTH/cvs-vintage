83798b352e29c2beb91688182b6f07ea4bf8dd08
tobias <tobias>
2001-01-29 23:50:40 +0000

    roll back to revision 1.6. 1.7 was reported to be broken. Will clarify this first. Changes may be reapplied later.

--- src/build/build.bat
+++ src/build/build.bat
@@ -1,24 +1,14 @@
 @echo off
+@if not "%ECHO%" == ""  echo %ECHO%
+@if "%OS%" == "Windows_NT"  setlocal
+
 REM convenience bat file to build with
 
-REM get the pwd
-
-@echo off
-dir | find "Directory" > }1{.bat
-set quote="
-echo if %%3!==! set quote=> directory.bat
-echo set PWD=%%quote%%%%2 %%3 %%4 %%5 %%6 %%7 %%8 %%9 %%quote%%>> directory.bat
-call }1{
-for %%a in (}1{ directory) do del %%a.bat
-set quote=
-
-
-set CP=%PWD%\..\..\lib\ant.jar
-set CP=%CP%;%PWD%\..\..\lib\parser.jar
-set CP=%CP%;%PWD%\..\..\lib\jaxp.jar
-set CP=%CP%;%PWD%\..\..\lib\xmlbeans.jar
-set CP=%CP%;%PWD%\..\..\build\classes
-set CP=%CP%;%PWD%\..\..\lib\javac.jar
+set CP=%CP%;..\..\lib\ant.jar
+set CP=%CP%;..\..\lib\jaxp.jar
+set CP=%CP%;..\..\lib\parser.jar
+set CP=%CP%;..\..\lib\xmlbeans.jar
+set CP=%CP%;..\..\build\classes
+set CP=%CP%;..\..\lib\javac.jar
 
 java -classpath "%CP%" org.apache.tools.ant.Main %1 %2 %3 %4 %5
-
