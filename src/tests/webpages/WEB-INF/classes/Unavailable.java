88eaf4cd0bc75326fa1d30a041a2e25054d911aa
larryi <larryi>
2001-02-12 15:01:21 +0000

    Update test script and Unavailable.java so test can be re-run without generating errors.

--- src/tests/webpages/WEB-INF/classes/Unavailable.java
+++ src/tests/webpages/WEB-INF/classes/Unavailable.java
@@ -4,25 +4,32 @@ import java.io.*;
 
 public class Unavailable extends HttpServlet {
     static int attempt=0;
+    static boolean success=false;
     
     public void init(ServletConfig conf)
         throws ServletException
     {
-	System.out.println("Try to init ");
-	attempt++;
-	if( attempt < 2 ) 
-	    throw new UnavailableException("Testing ", 10);
-	System.out.println("Init ok ");
+        attempt++;
+        if( attempt < 2 ) 
+            throw new UnavailableException("Testing ", 0);
     }
 
     public void service(HttpServletRequest req, HttpServletResponse res)
-        throws IOException
+        throws IOException,ServletException
     {
         res.setContentType("text/plain");
-
         ServletOutputStream out = res.getOutputStream();
 
-	out.println("Hello");
+        if (attempt == 1)
+            out.println("Error: UnavailableException not handled correctly");
+        else if ((attempt % 2) == 0) {
+            attempt++;
+            out.println("Hello");
+        } else {
+            attempt++;
+            // in case test is re-run
+            throw new UnavailableException("Testing ", 0);
+        }
 
         out.close();
     }
