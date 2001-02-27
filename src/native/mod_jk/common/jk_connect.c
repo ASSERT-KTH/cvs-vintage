d1f4ecaf6d52d507e697023a6a04d3961819d0c0
danmil <danmil>
2001-02-27 04:20:40 +0000

    Fixed bug which was bombing if a socket descriptor numbered 0 was created.
    Submitted by: Kevin Seguin (seguin@motive.com)

--- src/native/mod_jk/common/jk_connect.c
+++ src/native/mod_jk/common/jk_connect.c
@@ -57,7 +57,7 @@
  * Description: Socket/Naming manipulation functions                       *
  * Author:      Gal Shachor <shachor@il.ibm.com>                           *
  * Based on:    Various Jserv files                                        *
- * Version:     $Revision: 1.2 $                                               *
+ * Version:     $Revision: 1.3 $                                               *
  ***************************************************************************/
 
 
@@ -108,7 +108,7 @@ int jk_open_socket(struct sockaddr_in *addr,
     jk_log(l, JK_LOG_DEBUG, "Into jk_open_socket\n");
 
     sock = socket(AF_INET, SOCK_STREAM, 0);
-    if(sock > 0) {
+    if(sock > -1) {
         int ret;
         /* Tries to connect to JServ (continues trying while error is EINTR) */
         do {
