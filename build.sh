d9f4dd1e4ea761017a0b94c69fb2f83e09fce6fc
larryi <larryi>
2001-03-15 20:41:14 +0000

    Restore text for $ANT_OPTS

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-# $Id: build.sh,v 1.12 2001/03/14 03:38:40 larryi Exp $
+# $Id: build.sh,v 1.13 2001/03/15 20:41:14 larryi Exp $
 
 if [ -z "$JAVA_HOME" ]
 then
@@ -16,6 +16,10 @@ fi
 
 if [ "$ANT_HOME" = "" ] ; then
   ANT_HOME=../jakarta-ant-1.3
+fi
+
+if [ "$ANT_OPTS" = "" ] ; then
+  ANT_OPTS=""
 fi
 
 $ANT_HOME/bin/ant "$@" 
