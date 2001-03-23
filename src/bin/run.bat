f9050f634da2e4995afd965b1361ec6c5ee1ef1a
starksm <starksm>
2001-03-23 09:58:54 +0000

    Update to use the JAXP1.1 xml parser properties for choosing the xml parser implementation. Set the default choice to the crimson parser that ships with the jaxp1.1 ri.

--- src/bin/run.bat
+++ src/bin/run.bat
@@ -8,6 +8,13 @@ REM Add all login modules for JAAS-based security
 REM and all libraries that are used by them here
 set CLASSPATH=%CLASSPATH%
 
-java -classpath "%CLASSPATH%" org.jboss.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
+REM Add the XML parser jars and set the JAXP factory names
+REM Crimson parser JAXP setup(default)
+set CLASSPATH=%CLASSPATH%;../lib/crimson.jar
+set JAXP=-Djavax.xml.parsers.DocumentBuilderFactory=org.apache.crimson.jaxp.DocumentBuilderFactoryImpl
+set JAXP=%JAXP% -Djavax.xml.parsers.SAXParserFactory=org.apache.crimson.jaxp.SAXParserFactoryImpl
+
+echo CLASSPATH=%CLASSPATH%
+java %JAXP% -classpath "%CLASSPATH%" org.jboss.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 pause
--- src/bin/run.sh
+++ src/bin/run.sh
@@ -6,6 +6,14 @@ CLASSPATH=$CLASSPATH:run.jar
 
 # Add all login modules for JAAS-based security
 # and all libraries that are used by them here
+CLASSPATH=$CLASSPATH
 
-echo $CLASSPATH
-java -server -classpath $CLASSPATH org.jboss.Main $@
+# Add the XML parser jars and set the JAXP factory names
+# Crimson parser JAXP setup(default)
+CLASSPATH=$CLASSPATH:../lib/crimson.jar
+JAXP=-Djavax.xml.parsers.DocumentBuilderFactory=org.apache.crimson.jaxp.DocumentBuilderFactoryImpl
+JAXP="$JAXP -Djavax.xml.parsers.SAXParserFactory=org.apache.crimson.jaxp.SAXParserFactoryImpl"
+
+echo CLASSPATH=$CLASSPATH
+java -server $JAXP -classpath $CLASSPATH org.jboss.Main $@
+
