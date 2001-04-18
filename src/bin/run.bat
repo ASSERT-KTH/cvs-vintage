0e9e9b0c4cc4f18a618c0b5a592c423322837cd6
starksm <starksm>
2001-04-18 02:21:10 +0000

    Don't include the system CLASSPATH as this results in too many conflicts in general. Change CLASSPATH to JBOSS_CLASSPATH to make it clear that the CLASSPATH variable is not used.

--- src/bin/run.bat
+++ src/bin/run.bat
@@ -2,19 +2,19 @@
 @if not "%ECHO%" == ""  echo %ECHO%
 @if "%OS%" == "Windows_NT"  setlocal
 
-set CLASSPATH=%CLASSPATH%;run.jar
+set JBOSS_CLASSPATH=%JBOSS_CLASSPATH%;run.jar
 
 REM Add all login modules for JAAS-based security
 REM and all libraries that are used by them here
-set CLASSPATH=%CLASSPATH%
+set JBOSS_CLASSPATH=%JBOSS_CLASSPATH%
 
 REM Add the XML parser jars and set the JAXP factory names
 REM Crimson parser JAXP setup(default)
-set CLASSPATH=%CLASSPATH%;../lib/crimson.jar
+set JBOSS_CLASSPATH=%JBOSS_CLASSPATH%;../lib/crimson.jar
 set JAXP=-Djavax.xml.parsers.DocumentBuilderFactory=org.apache.crimson.jaxp.DocumentBuilderFactoryImpl
 set JAXP=%JAXP% -Djavax.xml.parsers.SAXParserFactory=org.apache.crimson.jaxp.SAXParserFactoryImpl
 
-echo CLASSPATH=%CLASSPATH%
-java %JAXP% -classpath "%CLASSPATH%" org.jboss.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
+echo JBOSS_CLASSPATH=%JBOSS_CLASSPATH%
+java %JAXP% -classpath "%JBOSS_CLASSPATH%" org.jboss.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
 
 pause
--- src/bin/run.sh
+++ src/bin/run.sh
@@ -2,18 +2,18 @@
 
 # Minimal jar file to get JBoss started.
 
-CLASSPATH=$CLASSPATH:run.jar
+JBOSS_CLASSPATH=$JBOSS_CLASSPATH:run.jar
 
 # Add all login modules for JAAS-based security
 # and all libraries that are used by them here
-CLASSPATH=$CLASSPATH
+JBOSS_CLASSPATH=$JBOSS_CLASSPATH
 
 # Add the XML parser jars and set the JAXP factory names
 # Crimson parser JAXP setup(default)
-CLASSPATH=$CLASSPATH:../lib/crimson.jar
+JBOSS_CLASSPATH=$JBOSS_CLASSPATH:../lib/crimson.jar
 JAXP=-Djavax.xml.parsers.DocumentBuilderFactory=org.apache.crimson.jaxp.DocumentBuilderFactoryImpl
 JAXP="$JAXP -Djavax.xml.parsers.SAXParserFactory=org.apache.crimson.jaxp.SAXParserFactoryImpl"
 
-echo CLASSPATH=$CLASSPATH
-java -server $JAXP -classpath $CLASSPATH org.jboss.Main $@
+echo JBOSS_CLASSPATH=$JBOSS_CLASSPATH
+java -server $JAXP -classpath $JBOSS_CLASSPATH org.jboss.Main $@
 
