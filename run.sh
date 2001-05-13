35d27324d8da45349c45bd29416ebe068984114f
fdietz <fdietz>
2001-05-13 11:02:20 +0000

    *** empty log message ***

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-java  -cp .:./ext/jaxp.jar:./ext/parser.jar org/columba/main/Main
+#java  -cp .:./ext/jaxp.jar:./ext/parser.jar org/columba/main/Main
+
+java -cp .:./ext/jaxp.jar:./ext/parser.jar org/columba/main/Main ~/.stable
