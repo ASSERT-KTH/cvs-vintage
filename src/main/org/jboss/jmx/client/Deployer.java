9da1b87b5cf33abc77d61dc84a83ecd9a77694aa
cgjung <cgjung>
2001-05-25 12:07:05 +0000

    factored out some code. added undeploy method. main method can now batch-deploy/undeploy several urls where  the "-undeploy" flag marks the next url spec as to be undeployed.

--- src/main/org/jboss/jmx/client/Deployer.java
+++ src/main/org/jboss/jmx/client/Deployer.java
@@ -20,7 +20,7 @@ import org.jboss.jmx.interfaces.JMXAdaptor;
  *      
  *   @see <related>
  *   @author Rickard Öberg (rickard.oberg@telkel.com)
- *   @version $Revision: 1.5 $
+ *   @version $Revision: 1.6 $
  */
 public class Deployer
 {
@@ -29,13 +29,20 @@ public class Deployer
    // Attributes ----------------------------------------------------
    
    // Static --------------------------------------------------------
-	public static void main(String[] args)
-		throws Exception
-	{
-		System.out.println("Deploying " + args[0]);		
-		new Deployer().deploy(args[0]);
-		System.out.println(args[0] + " has been deployed successfully");		
-	}
+    public static void main(String[] args)
+    throws Exception {
+        for(int count=0;count<args.length;count++) {            
+            if(!args[count].equalsIgnoreCase("-undeploy")) { 
+                System.out.println("Deploying " + args[count]);
+                new Deployer().deploy(args[count]);
+                System.out.println(args[count] + " has been deployed successfully");
+            } else {
+                System.out.println("Undeploying " + args[++count]);
+                new Deployer().undeploy(args[count]);
+                System.out.println(args[count] + " has been successfully undeployed");
+            }
+        }
+    }
 
    // Constructors --------------------------------------------------
    
@@ -43,21 +50,61 @@ public class Deployer
    public void deploy(String url)
 		throws Exception
    {
-	   ObjectName containerFactory = new ObjectName("J2EE:service=J2eeDeployer");
+	   ObjectName containerFactory = createFactoryName();
 
-	   URL deploymentUrl;
-	   if (new File(url).exists())
-	   	deploymentUrl = new File(url).toURL();
-	   else
-	   	deploymentUrl = new URL(url);
-
-	   JMXAdaptor server = (JMXAdaptor)new InitialContext().lookup("jmx");
+	   URL deploymentUrl =createUrl(url);
+          
+	   JMXAdaptor server = lookupAdaptor();
+           
 	   server.invoke(containerFactory, 
-	   					"deploy", 
-	   					new Object[] { deploymentUrl.toString() }, 
-	   					new String[] { "java.lang.String" });
+	   		 "deploy", 
+                         new Object[] { deploymentUrl.toString() }, 
+	   		 new String[] { "java.lang.String" });
    }
 	
+  /** undeploys the app under the given url spec
+   *  @author cgjung 
+   */
+  public void undeploy(String url)
+		throws Exception
+   {
+	   ObjectName containerFactory = createFactoryName();
+           
+	   URL deploymentUrl=createUrl(url);
+          
+	   JMXAdaptor server = lookupAdaptor();
+           server.invoke(containerFactory, 
+                         "undeploy", 
+	   		 new Object[] { deploymentUrl.toString() }, 
+	   		 new String[] { "java.lang.String" });
+   }
+
+   /** creation of objectname for the deployer
+    *  factored out
+    *  @author cgjung
+    */
+   protected ObjectName createFactoryName() throws javax.management.MalformedObjectNameException {
+       return new ObjectName("J2EE:service=J2eeDeployer");
+   }
+   
+   /** creation of url factored out
+    *  @author cgjung
+    */
+   protected URL createUrl(String url) throws java.net.MalformedURLException {
+       if (new File(url).exists())
+           return new File(url).toURL();
+       else
+           return new URL(url);
+   }
+   
+   /** lookup of JMXadaptor factored out
+    *  @author cgjung
+    */
+   protected JMXAdaptor lookupAdaptor() throws javax.naming.NamingException {
+        return (JMXAdaptor)new InitialContext().lookup("jmx");
+   }
+   
+
    // Protected -----------------------------------------------------
 }
 
