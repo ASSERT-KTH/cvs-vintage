3fe1b017c4a33192e8d43cc903a622d45d262adf
fdietz <fdietz>
2001-06-05 18:37:01 +0000

    fixed pop3(apop) authentification

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #java  -cp .:./ext/jaxp.jar:./ext/parser.jar org/columba/main/Main
 
-java -cp .:./ext/jaxp.jar:./ext/parser.jar org/columba/main/Main ~/.stable
+java -cp .:./ext/jaxp.jar:./ext/parser.jar org/columba/main/Main --path ~/.stable
