5132c988cc5863aa19bf758412dd238cf0699ec1
olegnitz <olegnitz>
2001-07-10 23:59:51 +0000

    Added CastorJDO integration module to the main distribution, because castor-0.9.1.jar is already here.

---
+++ src/main/org/jboss/jdo/castor/CastorJDOImplMBean.java
@@ -0,0 +1,45 @@
+/*
+ * JBoss, the OpenSource EJB server
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+
+package org.jboss.jdo.castor;
+    
+/**
+ *   Castor JDO support
+ *      
+ *   @author Oleg Nitz (on@ibis.odessa.ua)
+ *   @version $Revision: 1.1 $
+ */
+public interface CastorJDOImplMBean
+       extends org.jboss.util.ServiceMBean
+{
+    public static final String OBJECT_NAME = ":service=CastorJDO";
+    
+    public void setJndiName(String jndiName);
+
+    public String getJndiName();
+
+    public void setConfiguration(String dbConf);
+
+    public String getConfiguration();
+
+    public void setLockTimeout(int lockTimeout);
+
+    public int getLockTimeout();
+
+    public void setLoggingEnabled(boolean loggingEnabled);
+
+    public boolean getLoggingEnabled();
+
+    public void setCommonClassPath(boolean commonClassPath);
+
+    public boolean getCommonClassPath();
+
+    public void setAutoStore(boolean autoStore);
+
+    public boolean isAutoStore();
+
+}
