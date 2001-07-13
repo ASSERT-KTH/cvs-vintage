c6685c652acd1a8b470e1c38c0a4aaa670ade2a0
user57 <user57>
2001-07-13 00:39:13 +0000

    o re-indent & javadoc'd

--- src/main/org/jboss/ejb/InstancePoolContainer.java
+++ src/main/org/jboss/ejb/InstancePoolContainer.java
@@ -6,26 +6,24 @@
  */
 package org.jboss.ejb;
 
-import java.rmi.RemoteException;
-import java.util.Collection;
-
-import javax.ejb.EJBMetaData;
-import javax.ejb.EJBHome;
-import javax.ejb.EJBObject;
-import javax.naming.Name;
-
 /**
- *	This is an interface for Containers that uses InstancePools.
+ * This is an interface for Containers that uses InstancePools.
  *
- *	Plugins wanting to access pools from containers should use this interface
+ * <p>Plugins wanting to access pools from containers should use this
+ *    interface.
  *
- *	@see InstancePool
- *	@author <a href="mailto:rickard.oberg@telkel.com">Rickard Öberg</a>
- *	@version $Revision: 1.3 $
+ * @see InstancePool
+ * 
+ * @author <a href="mailto:rickard.oberg@telkel.com">Rickard Öberg</a>
+ * @version $Revision: 1.4 $
  */
 public interface InstancePoolContainer
 {
-   // Public --------------------------------------------------------
-	public InstancePool getInstancePool();
+   /**
+    * Get the instance pool for the container.
+    *
+    * @return   The instance pool for the container.
+    */
+   InstancePool getInstancePool();
 }
 
