3f0f8928f5ccd25b754102cfec138e4929fe83b0
user57 <user57>
2001-07-13 03:27:21 +0000

    o re-indent & cleanup javadoc

--- src/main/org/jboss/ejb/EntityPersistenceStore2.java
+++ src/main/org/jboss/ejb/EntityPersistenceStore2.java
@@ -8,19 +8,34 @@ package org.jboss.ejb;
 
 import java.rmi.RemoteException;
 
-public interface EntityPersistenceStore2 extends EntityPersistenceStore {
- 	/**
-	* Returns a new instance of the bean class or a subclass of the bean class.
-	* 
-	* @return the new instance
-	*/
-	public Object createBeanClassInstance() throws Exception;
+/**
+ * ???
+ *
+ * @author Unknown
+ * @version $Revision: 1.2 $
+ */
+public interface EntityPersistenceStore2
+   extends EntityPersistenceStore
+{
+   /**
+    * Returns a new instance of the bean class or a subclass of the bean class.
+    * 
+    * @return   the new instance
+    *
+    * @throws Exception
+    */
+   public Object createBeanClassInstance() throws Exception;
 
- 	/**
-	* Initializes the instance context.
-	* This method is called before createEntity, and should
-	* reset the value of all cmpFields to 0 or null. 
-	*/
+   /**
+    * Initializes the instance context.
+    * 
+    * <p>This method is called before createEntity, and should
+    *    reset the value of all cmpFields to 0 or null.
+    *
+    * @param ctx
+    * 
+    * @throws RemoteException
+    */
    public void initEntity(EntityEnterpriseContext ctx) throws RemoteException;
 }
 
