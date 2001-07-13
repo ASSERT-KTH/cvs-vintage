883f5df51ae3710320cd3adfbc82671bd8edec0b
user57 <user57>
2001-07-13 22:35:30 +0000

    o cleaned up

--- src/main/org/jboss/ejb/plugins/MessageDrivenInstanceInterceptor.java
+++ src/main/org/jboss/ejb/plugins/MessageDrivenInstanceInterceptor.java
@@ -11,46 +11,50 @@ import java.rmi.RemoteException;
 import org.jboss.ejb.Container;
 import org.jboss.ejb.MessageDrivenContainer;
 import org.jboss.ejb.MethodInvocation;
-import org.jboss.logging.Logger;
+
 /**
- *   This container acquires the given instance. This must be used after
- *   the EnvironmentInterceptor, since acquiring instances requires a proper
- *   JNDI environment to be set
- * For MessageDriven Beans, we inherit the StatelessSession for now,
- * since message driven beans is much like them
+ * This container acquires the given instance. This must be used after
+ * the EnvironmentInterceptor, since acquiring instances requires a proper
+ * JNDI environment to be set.
+ * 
+ * <p>For MessageDriven Beans, we inherit the StatelessSession for now,
+ *    since message driven beans is much like them.
  *
- *   @see <related>
- *   @author <a href="mailto:peter.antman@tim.se">Peter Antman</a>.
- *   @author <a href="mailto:rickard.oberg@telkel.com">Rickard Öberg</a>
- *   @version $Revision: 1.4 $
+ * @author <a href="mailto:peter.antman@tim.se">Peter Antman</a>.
+ * @author <a href="mailto:rickard.oberg@telkel.com">Rickard Öberg</a>
+ * @version $Revision: 1.5 $
  */
 public class MessageDrivenInstanceInterceptor
    extends StatelessSessionInstanceInterceptor
 {
-    protected MessageDrivenContainer container;
+   protected MessageDrivenContainer container;
 
-    public void setContainer(Container container) 
+   public void setContainer(final Container container) 
    { 
-   	this.container = (MessageDrivenContainer)container; 
+      this.container = (MessageDrivenContainer)container; 
    }
 
-    // Overriden here, since these bastards don't have homes
-    public Object invokeHome(MethodInvocation mi)
+   /**
+    * Message driven beans do not have homes.
+    *
+    * @throws Error  Not valid for MessageDriven beans
+    */
+   public Object invokeHome(final MethodInvocation mi)
       throws Exception
    {
-       throw new Error("Not valid for MessageDriven beans");
+      throw new Error("Not valid for MessageDriven beans");
    }
-    // Interceptor implementation --------------------------------------
+   
+   // Interceptor implementation --------------------------------------
 
-
-   public Object invoke(MethodInvocation mi)
+   public Object invoke(final MethodInvocation mi)
       throws Exception
    {
       // Get context
       mi.setEnterpriseContext(container.getInstancePool().get());
       
-	  // There is no need for synchronization since the instance is always fresh also there should
-	  // never be a tx associated with the instance.
+      // There is no need for synchronization since the instance is always
+      // fresh also there should never be a tx associated with the instance.
 	 
       try
       {
@@ -58,21 +62,21 @@ public class MessageDrivenInstanceInterceptor
          return getNext().invoke(mi);
       } catch (RuntimeException e) // Instance will be GC'ed at MI return
       {
-	  	mi.setEnterpriseContext(null);
-	  	throw e;
+         mi.setEnterpriseContext(null);
+         throw e;
       } catch (RemoteException e) // Instance will be GC'ed at MI return
       {
-    	mi.setEnterpriseContext(null);
-    	throw e;
+         mi.setEnterpriseContext(null);
+         throw e;
       } catch (Error e) // Instance will be GC'ed at MI return
       {
-	    mi.setEnterpriseContext(null);
-	    throw e;
+         mi.setEnterpriseContext(null);
+         throw e;
       } finally
       {
-		// Return context
-		if (mi.getEnterpriseContext() != null)
-			container.getInstancePool().free(mi.getEnterpriseContext());
+         // Return context
+         if (mi.getEnterpriseContext() != null)
+            container.getInstancePool().free(mi.getEnterpriseContext());
       }
    }
 }
