80d639fe4addac988a06770aacfccbc5d20f9f72
user57 <user57>
2001-07-13 01:33:15 +0000

    o re-indent & javadoc clean up

--- src/main/org/jboss/ejb/EntityCache.java
+++ src/main/org/jboss/ejb/EntityCache.java
@@ -1,35 +1,30 @@
-package org.jboss.ejb;
-
 /*
  * JBoss, the OpenSource EJB server
  *
  * Distributable under LGPL license.
  * See terms of license at gnu.org.
  */
-
-import java.rmi.RemoteException;
-import javax.ejb.NoSuchEntityException;
+package org.jboss.ejb;
 
 /**
- *	EntityCaches can work from several keys
+ * EntityCaches can work from several keys.
  *
- *  A cache can use the natural primaryKey from the EJBObject, or DB dependent
- *  keys or a proprietary key
+ * <p>A cache can use the natural primaryKey from the EJBObject, or DB
+ *    dependent keys or a proprietary key
  *
- *	@see EntityInstanceCache
- *	@author <a href="mailto:marc.fleury@telkel.com">Marc Fleury</a>
- *	@version $Revision: 1.2 $
+ * @see EntityInstanceCache
+ * 
+ * @author <a href="mailto:marc.fleury@telkel.com">Marc Fleury</a>
+ * @version $Revision: 1.3 $
  */
 public interface EntityCache
    extends InstanceCache
 {
    /**
     * Returns the key used to cache the context
-	*
-	* @param id				Object id / primary key
-	*
-	* @return				Cache key
-	*/
-   public Object createCacheKey( Object id );
-
+    *
+    * @param id    Object id / primary key
+    * @return      Cache key
+    */
+   Object createCacheKey(Object id);
 }
