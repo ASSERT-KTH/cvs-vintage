5bf9648f828124cbb575d9d3d958c50cb3b2c3d1
user57 <user57>
2001-07-13 00:42:49 +0000

    o javadoc'd

--- src/main/org/jboss/ejb/AutoDeployerMBean.java
+++ src/main/org/jboss/ejb/AutoDeployerMBean.java
@@ -9,24 +9,47 @@ package org.jboss.ejb;
 import org.jboss.util.ServiceMBean;
 
 /**
- *   This is the interface of the AutoDeployer that is exposed for
- *   administration
+ * This is the interface of the AutoDeployer that is exposed for
+ * administration
  *      
- *   @see AutoDeployer
- *   @author <a href="mailto:rickard.oberg@telkel.com">Rickard Öberg</a>
- *   @author <a href="mailto:toby.allsopp@peace.com">Toby Allsopp</a>
- *   @version $Revision: 1.5 $
+ * @see AutoDeployer
+ * 
+ * @author <a href="mailto:rickard.oberg@telkel.com">Rickard Öberg</a>
+ * @author <a href="mailto:toby.allsopp@peace.com">Toby Allsopp</a>
+ * @version $Revision: 1.6 $
  */
 public interface AutoDeployerMBean
-	extends ServiceMBean
+   extends ServiceMBean
 {
-   // Constants -----------------------------------------------------
-   public static final String OBJECT_NAME = "EJB:service=AutoDeployer";
-    
-   // Public --------------------------------------------------------
-   public void setURLs(String urlList);
-   public String getURLs();
-   public void setDeployers(String deployers);
-   public String getDeployers();
+   /** The default object name. */
+   String OBJECT_NAME = "EJB:service=AutoDeployer";
+
+   /**
+    * Set the list of urls to watch.
+    *
+    * @param urlList    The list of urls to watch.
+    */
+   void setURLs(String urlList);
+
+   /**
+    * Get the list of urls that are currently being watched.
+    *
+    * @return   The list of urls that are currently being watched.
+    */
+   String getURLs();
+
+   /**
+    * Set the list of deployers that will be used.
+    *
+    * @param deployers    The list of deployers that will be used.
+    */
+   void setDeployers(String deployers);
+   
+   /**
+    * Get the list of deployers that is currently being used.
+    *
+    * @return   The list of deployers that is currently being used.
+    */
+   String getDeployers();
 }
 
