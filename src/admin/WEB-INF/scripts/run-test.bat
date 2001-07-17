d45992869b133f8cf43a91e8c9b6302e147655c7
larryi <larryi>
2001-07-17 13:46:00 +0000

    Add a batch file to run the sanity test from a DOS prompt.

---
+++ src/admin/WEB-INF/scripts/run-test.bat
@@ -0,0 +1,31 @@
+rem @echo off
+
+rem Batch file to run the tomcat sanity test suite
+rem Note: You may send the output to a file using "run-tomcat -l file"
+
+if not "%TOMCAT_HOME%" == "" goto gotTomcatHome
+echo You need to set TOMCAT_HOME
+goto exit
+
+:gotTomcatHome
+
+set _OLDCP=%CLASSPATH%
+
+set CLASSPATH=%TOMCAT_HOME%/lib/container/tomcat_util.jar
+set CLASSPATH=%TOMCAT_HOME%/lib/common/core_util.jar;%CLASSPATH%
+set CLASSPATH=%TOMCAT_HOME%/lib/container/parser.jar;%CLASSPATH%
+set CLASSPATH=%TOMCAT_HOME%/lib/container/crimson.jar;%CLASSPATH%
+set CLASSPATH=%TOMCAT_HOME%/lib/container/xerces.jar;%CLASSPATH%
+set CLASSPATH=%TOMCAT_HOME%/lib/container/jaxp.jar;%CLASSPATH%
+set CLASSPATH=%TOMCAT_HOME%/webapps/admin/WEB-INF/lib/ant.jar;%CLASSPATH%
+set CLASSPATH=%TOMCAT_HOME%/webapps/admin/WEB-INF/classes;%CLASSPATH%
+
+if "%_OLDCP%" == "" goto runant
+set CLASSPATH=%CLASSPATH%;%_OLDCP%
+
+:runant
+ant -Dgdir="%TOMCAT_HOME%/webapps/test/Golden" -f "%TOMCAT_HOME%/webapps/test/WEB-INF/test-tomcat.xml" %1 %2 %3 %4 %5 %6 %7 %8 %9 client
+
+set CLASSPATH=%_OLDCP%
+
+:exit
