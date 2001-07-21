81b0415b773d6d96174ce56da61f4f51fd1177b4
user57 <user57>
2001-07-21 02:04:57 +0000

    o reformated and mostly documented.

--- src/main/org/jboss/jms/asf/StdServerSessionPoolFactory.java
+++ src/main/org/jboss/jms/asf/StdServerSessionPoolFactory.java
@@ -17,34 +17,77 @@
  */
 package org.jboss.jms.asf;
 
-import java.util.Hashtable;
+import java.io.Serializable;
 
 import javax.jms.ServerSessionPool;
 import javax.jms.MessageListener;
 import javax.jms.Connection;
 import javax.jms.JMSException;
+
 /**
  * StdServerSessionPoolFactory.java
  *
- *
  * Created: Fri Dec 22 09:47:41 2000
  *
- * @author Peter Antman
- * @version
+ * @author <a href="mailto:peter.antman@tim.se">Peter Antman</a>.
+ * @version $Revision: 1.4 $
  */
+public class StdServerSessionPoolFactory
+   implements ServerSessionPoolFactory, Serializable
+{
+   /** The name of this factory. */
+   private String name;
 
-public class StdServerSessionPoolFactory implements ServerSessionPoolFactory, java.io.Serializable {
-    private Hashtable pools = new Hashtable();
-    
-    private String name;
-    public StdServerSessionPoolFactory() {
-	
-    }
-    public void setName(String name){this.name = name;}
-    public String getName(){return name;}
-    
-    public ServerSessionPool getServerSessionPool(Connection con, int maxSession, boolean isTransacted, int ack, MessageListener listener) throws JMSException {
-	    ServerSessionPool pool =  (ServerSessionPool)new StdServerSessionPool(con,  isTransacted, ack, listener,maxSession);
-	    return pool;
-    }
-} // StdServerSessionPoolFactory
+   /**
+    * Construct a <tt>StdServerSessionPoolFactory</tt>.
+    */
+   public StdServerSessionPoolFactory() {
+      super();
+   }
+
+   /**
+    * Set the name of the factory.
+    *
+    * @param name    The name of the factory.
+    */
+   public void setName(final String name) {
+      this.name = name;
+   }
+   
+   /**
+    * Get the name of the factory.
+    *
+    * @return    The name of the factory.
+    */
+   public String getName() {
+      return name;
+   }
+
+   /**
+    * Create a new <tt>ServerSessionPool</tt>.
+    *
+    * @param con
+    * @param maxSession
+    * @param isTransacted
+    * @param ack
+    * @param listener
+    * @return                A new pool.
+    *
+    * @throws JMSException
+    */
+   public ServerSessionPool getServerSessionPool(Connection con,
+                                                 int maxSession,
+                                                 boolean isTransacted,
+                                                 int ack,
+                                                 MessageListener listener)
+      throws JMSException
+   {
+      ServerSessionPool pool = (ServerSessionPool)
+         new StdServerSessionPool(con,
+                                  isTransacted,
+                                  ack,
+                                  listener,
+                                  maxSession);
+      return pool;
+   }
+}
