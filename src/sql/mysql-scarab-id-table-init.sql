2573c341251d13d85bd76f9ee6ca4aea9a63b9ba
elicia <elicia>
2001-07-26 22:35:29 +0000

    added Query table.

--- src/sql/mysql-scarab-id-table-init.sql
+++ src/sql/mysql-scarab-id-table-init.sql
@@ -14,3 +14,4 @@ insert into ID_TABLE (table_name, next_id, quantity) VALUES ('SCARAB_R_MODULE_US
 insert into ID_TABLE (table_name, next_id, quantity) VALUES ('SCARAB_TRANSACTION', 100, 10);
 insert into ID_TABLE (table_name, next_id, quantity) VALUES ('SCARAB_ISSUE_ATTRIBUTE_VOTE', 100, 10);
 insert into ID_TABLE (table_name, next_id, quantity) VALUES ('SCARAB_WORD', 100, 10);
+insert into ID_TABLE (table_name, next_id, quantity) VALUES ('SCARAB_QUERY', 100, 10);
