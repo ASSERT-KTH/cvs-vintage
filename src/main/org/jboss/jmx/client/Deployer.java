f1f5d59ab9157408725283579d94d7eab9302146
user57 <user57>
2001-07-26 03:50:49 +0000

    o re-indent and documented (mostly) JMXAdapter  o moved server.invoke() in Deployer to Deployer.invoke(), which will    decode JMX exceptions.

--- src/main/org/jboss/jmx/client/Deployer.java
+++ src/main/org/jboss/jmx/client/Deployer.java
@@ -9,102 +9,166 @@ package org.jboss.jmx.client;
 
 import java.io.File;
 import java.net.URL;
-
+import java.net.MalformedURLException;
+   
 import javax.management.ObjectName;
+import javax.management.MalformedObjectNameException;
+import javax.management.MBeanException;
+import javax.management.ReflectionException;
+import javax.management.RuntimeOperationsException;
+import javax.management.RuntimeMBeanException;
+import javax.management.RuntimeErrorException;
+
 import javax.naming.InitialContext;
+import javax.naming.NamingException;
 
 import org.jboss.jmx.interfaces.JMXAdaptor;
 
 /**
- *   <description> 
+ * A JMX client to deploy an application into a running JBoss server.
  *      
- *   @see <related>
- *   @author <a href="mailto:rickard.oberg@telkel.com">Rickard Öberg</a>
- *   @version $Revision: 1.7 $
+ * @author <a href="mailto:rickard.oberg@telkel.com">Rickard Öberg</a>
+ * @author <a href="mailto:jason@planet57.com">Jason Dillon</a>
+ * @version $Revision: 1.8 $
  */
 public class Deployer
 {
-   // Constants -----------------------------------------------------
-    
-   // Attributes ----------------------------------------------------
-   
-   // Static --------------------------------------------------------
-    public static void main(String[] args)
-    throws Exception {
-        for(int count=0;count<args.length;count++) {            
-            if(!args[count].equalsIgnoreCase("-undeploy")) { 
-                System.out.println("Deploying " + args[count]);
-                new Deployer().deploy(args[count]);
-                System.out.println(args[count] + " has been deployed successfully");
-            } else {
-                System.out.println("Undeploying " + args[++count]);
-                new Deployer().undeploy(args[count]);
-                System.out.println(args[count] + " has been successfully undeployed");
-            }
-        }
-    }
+   /**
+    * A command line driven interface to the deployer.
+    */
+   public static void main(final String[] args)
+      throws Exception {
+      for (int count=0;count<args.length;count++) {            
+         if (!args[count].equalsIgnoreCase("-undeploy")) { 
+            System.out.println("Deploying " + args[count]);
+            new Deployer().deploy(args[count]);
+            System.out.println(args[count] +
+                               " has been deployed successfully");
+         } else {
+            System.out.println("Undeploying " + args[++count]);
+            new Deployer().undeploy(args[count]);
+            System.out.println(args[count] +
+                               " has been successfully undeployed");
+         }
+      }
+   }
 
-   // Constructors --------------------------------------------------
-   
-   // Public --------------------------------------------------------
-   public void deploy(String url)
-		throws Exception
+   /**
+    * Deploys the app under the given url spec.
+    *
+    * @param url    The url spec of the application to deploy.
+    *
+    * @throws Exception    Failed to deploy application.
+    */
+   public void deploy(final String url) throws Exception
    {
-	   ObjectName containerFactory = createFactoryName();
-
-	   URL deploymentUrl =createUrl(url);
-          
-	   JMXAdaptor server = lookupAdaptor();
-           
-	   server.invoke(containerFactory, 
-	   		 "deploy", 
-                         new Object[] { deploymentUrl.toString() }, 
-	   		 new String[] { "java.lang.String" });
+      invoke(url, "deploy");
    }
 	
-  /** undeploys the app under the given url spec
-   *  @author cgjung 
-   */
-  public void undeploy(String url)
-		throws Exception
+   /**
+    * Undeploys the app under the given url spec.
+    *
+    * @param url    The url spec of the application to undeploy.
+    *
+    * @throws Exception    Failed to undeploy application.
+    */
+   public void undeploy(final String url) throws Exception
    {
-	   ObjectName containerFactory = createFactoryName();
-           
-	   URL deploymentUrl=createUrl(url);
-          
-	   JMXAdaptor server = lookupAdaptor();
-           server.invoke(containerFactory, 
-                         "undeploy", 
-	   		 new Object[] { deploymentUrl.toString() }, 
-	   		 new String[] { "java.lang.String" });
+      invoke(url, "undeploy");
    }
 
-   /** creation of objectname for the deployer
-    *  factored out
-    *  @author cgjung
+   /**
+    * Get the JMX object name of the factory to use.
     */
-   protected ObjectName createFactoryName() throws javax.management.MalformedObjectNameException {
-       return new ObjectName("J2EE:service=J2eeDeployer");
+   protected ObjectName getFactoryName()
+      throws MalformedObjectNameException
+   {
+      return new ObjectName("J2EE:service=J2eeDeployer");
    }
    
-   /** creation of url factored out
-    *  @author cgjung
+   /**
+    * Create a url from the given application url spec.
+    *
+    * <p>Will attempt to create a file:// url first.
+    * 
+    * @author cgjung
     */
-   protected URL createUrl(String url) throws java.net.MalformedURLException {
-       if (new File(url).exists())
-           return new File(url).toURL();
-       else
-           return new URL(url);
+   protected URL createURL(final String url) throws MalformedURLException {
+      if (new File(url).exists()) {
+         return new File(url).toURL();
+      }
+      else {
+         return new URL(url);
+      }
    }
-   
-   /** lookup of JMXadaptor factored out
-    *  @author cgjung
-    */
-   protected JMXAdaptor lookupAdaptor() throws javax.naming.NamingException {
-        return (JMXAdaptor)new InitialContext().lookup("jmx");
-   }
-   
 
-   // Protected -----------------------------------------------------
+   /**
+    * Lookup of JMXadaptor factored out.
+    *  
+    * @author cgjung
+    */
+   protected JMXAdaptor lookupAdaptor() throws NamingException {
+      JMXAdaptor adapter;
+      InitialContext ctx = new InitialContext();
+      try {
+         adapter = (JMXAdaptor)ctx.lookup("jmx");
+      }
+      finally {
+         ctx.close();
+      }
+      
+      return adapter;
+   }
+   
+   /**
+    * Invoke the target deployer and decode JMX exceptions that
+    * are thrown.
+    *
+    * @param url      The raw url
+    * @param method   Either "deploy" or "undeploy"
+    *
+    * @throws Exception    Server invoke failed.
+    */
+   protected void invoke(final String url, final String method)
+      throws Exception
+   {
+      URL _url = createURL(url);
+      JMXAdaptor server = lookupAdaptor();
+
+      // lookup the adapter to use
+      JMXAdaptor adapter;
+      InitialContext ctx = new InitialContext();
+      try {
+         adapter = (JMXAdaptor)ctx.lookup("jmx");
+      }
+      finally {
+         ctx.close();
+      }
+
+      // get the deployer name to use
+      ObjectName name = getFactoryName();
+
+      // invoke the server and decode jmx exceptions
+      try {
+         server.invoke(name, method,
+                       new Object[] { _url.toString() },
+                       new String[] { "java.lang.String" });
+      }
+      catch (MBeanException e) {
+         throw e.getTargetException();
+      }
+      catch (ReflectionException e) {
+         throw e.getTargetException();
+      }
+      catch (RuntimeOperationsException e) {
+         throw e.getTargetException();
+      }
+      catch (RuntimeMBeanException e) {
+         throw e.getTargetException();
+      }
+      catch (RuntimeErrorException e) {
+         throw e.getTargetError();
+      }
+   }
 }
 
--- src/main/org/jboss/jmx/interfaces/JMXAdaptor.java
+++ src/main/org/jboss/jmx/interfaces/JMXAdaptor.java
@@ -6,6 +6,9 @@
  */
 package org.jboss.jmx.interfaces;
 
+import java.util.Set;
+import java.util.Collection;
+
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
@@ -20,50 +23,102 @@ import javax.management.MBeanException;
 import javax.management.ReflectionException;
 
 /**
- *   <description> 
+ * The remote interface of a JMX adapter.
  *      
- *   @see <related>
- *   @author <firstname> <lastname> (<email>)
- *   @version $Revision: 1.2 $
+ * @author Unknown
+ * @version $Revision: 1.3 $
  */
 public interface JMXAdaptor
    extends Remote
 {
-   // Constants -----------------------------------------------------
-    
-   // Static --------------------------------------------------------
+   /**
+    * Invokes an operation on an MBean.
+    *
+    * @param name          The object name of the MBean on which the method
+    *                      is to be invoked.
+    * @param actionName    The name of the operation to be
+    * @param params        An array containing the parameters to be set when
+    *                      the operation is invoked.
+    * @param signature     An array containing the signature of the operation.
+    * @return              The object returned by the operation, which
+    *                      represents the result ofinvoking the operation on
+    *                      the MBean specified.
+    *
+    * @throws InstanceNotFoundException
+    * @throws MBeanException
+    * @throws ReflectionException
+    * @throws RemoteException
+    */
+   Object invoke(ObjectName name,
+                 String actionName,
+                 Object[] params,
+                 String[] signature)
+      throws InstanceNotFoundException, MBeanException, ReflectionException,
+             RemoteException;
 
-   // Public --------------------------------------------------------
-	public java.lang.Object invoke(ObjectName name,
-   	                            java.lang.String actionName,
-      	                         java.lang.Object[] params,
-         	                      java.lang.String[] signature)
-		throws InstanceNotFoundException,
-             MBeanException,
-             ReflectionException,
-				 RemoteException;
+   /**
+    * Gets MBeans controlled by the MBean server.
+    *
+    * @param name     The object name pattern identifying the MBeans to be
+    *                 retrieved. If null or no domain and key properties are
+    *                 specified, all the MBeans registered will be retrieved.
+    * @param query    The query expression to be applied for selecting MBeans.
+    *                 If null no query expression will be applied for selecting
+    *                 MBeans.
+    * @return         A set containing the ObjectInstance objects for the
+    *                 selected MBeans. If no MBean satisfies the query an
+    *                 empty list is returned.
+    *
+    * @throws RemoteException
+    */
+   Set queryMBeans(ObjectName name, QueryExp query) throws RemoteException;
 
-	public java.util.Set queryMBeans(ObjectName name,
-                                 	QueryExp query)
-		throws RemoteException;
-	
-	public java.util.Collection getMBeanInfos()
-		throws RemoteException;													 
-	
-	public void setAttribute(ObjectName name,
-                         Attribute attribute)
-      throws InstanceNotFoundException,
-             AttributeNotFoundException,
-             InvalidAttributeValueException,
-             MBeanException,
-             ReflectionException,
-				 RemoteException;
-				 
-	public java.lang.Object getAttribute(ObjectName name,
-                                     java.lang.String attribute)
-	   throws MBeanException,
-	          AttributeNotFoundException,
-	          InstanceNotFoundException,
-	          ReflectionException,
-				 RemoteException;
+   /**
+    * ???
+    *
+    * @return    ???
+    *
+    * @throws RemoteException
+    */
+   Collection getMBeanInfos() throws RemoteException;
+
+   /**
+    * Sets the value of a specific attribute of a named MBean.
+    *
+    * @param name         The name of the MBean within which the attribute is
+    *                     to be set.
+    * @param attribute    The identification of the attribute to be set and
+    *                      the value it is to be set to.
+    *
+    * @throws InstanceNotFoundException
+    * @throws AttributeNotFoundException
+    * @throws InvalidAttributeValueException
+    * @throws MBeanException
+    * @throws ReflectionException
+    * @throws RemoteException
+    */
+   void setAttribute(ObjectName name, Attribute attribute)
+      throws InstanceNotFoundException, AttributeNotFoundException,
+             InvalidAttributeValueException, MBeanException,
+             ReflectionException, RemoteException;
+
+   /**
+    * Gets the value of a specific attribute of a named MBean.
+    *
+    * @param name         The object name of the MBean from which the
+    *                     attribute is to be retrieved.
+    * @param attribute    A String specifying the name of the attribute to be
+    *                     retrieved.
+    * 
+    * @throws InstanceNotFoundException
+    * @throws AttributeNotFoundException
+    * @throws InvalidAttributeValueException
+    * @throws MBeanException
+    * @throws ReflectionException
+    * @throws RemoteException
+    */
+   Object getAttribute(ObjectName name, String attribute)
+      throws MBeanException, AttributeNotFoundException,
+             InstanceNotFoundException, ReflectionException,
+             RemoteException;
 }
