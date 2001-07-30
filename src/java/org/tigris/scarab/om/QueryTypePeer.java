758296b9acee4d21ac9521f2a8ad6860cee1a4f7
elicia <elicia>
2001-07-30 21:40:40 +0000

    business objects for Query.

---
+++ src/java/org/tigris/scarab/om/QueryTypePeer.java
@@ -0,0 +1,25 @@
+package org.tigris.scarab.om;
+
+// JDK classes
+import java.util.*;
+
+// Village classes
+import com.workingdogs.village.*;
+
+// Turbine classes
+import org.apache.turbine.services.db.*;
+import org.apache.turbine.services.db.map.*;
+import org.apache.turbine.services.db.pool.DBConnection;
+
+// Local classes
+import org.tigris.scarab.om.map.*;
+
+/** 
+ *  You should add additional methods to this class to meet the
+ *  application requirements.  This class will only be generated as
+ *  long as it does not already exist in the output directory.
+ */
+public class QueryTypePeer 
+    extends org.tigris.scarab.om.BaseQueryTypePeer
+{
+}
