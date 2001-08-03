7b6e03dfaab70cef0ff942f48d6baf69e80246d4
patriot1burke <patriot1burke>
2001-08-03 20:11:25 +0000

    LogCategory and correct indentation.
    Marc!  Please stop using TABS!!!

--- src/main/org/jboss/ejb/plugins/EntityLockInterceptor.java
+++ src/main/org/jboss/ejb/plugins/EntityLockInterceptor.java
@@ -41,8 +41,8 @@ import org.jboss.metadata.EntityMetaData;
 import org.jboss.tm.TxManager;
 
 /**
-* The lock interceptors role is to schedule thread wanting to invoke method on a target bean
-*
+ * The lock interceptors role is to schedule thread wanting to invoke method on a target bean
+ *
 * <p>The policies for implementing scheduling (pessimistic locking etc) is implemented by pluggable
 *    locks
 *
@@ -56,7 +56,7 @@ import org.jboss.tm.TxManager;
 *    
 * @author <a href="mailto:marc.fleury@jboss.org">Marc Fleury</a>
 * @author <a href="mailto:bill@burkecentral.com">Bill Burke</a>
-* @version $Revision: 1.3 $
+* @version $Revision: 1.4 $
 *
 * <p><b>Revisions:</b><br>
 * <p><b>2001/07/30: marcf</b>
@@ -67,105 +67,105 @@ import org.jboss.tm.TxManager;
 * </ol>
 */
 public class EntityLockInterceptor
-extends AbstractInterceptor
+   extends AbstractInterceptor
 {
-	// Constants -----------------------------------------------------
-	
-	// Attributes ----------------------------------------------------
-	
-	protected EntityContainer container;
-	
-	// Static --------------------------------------------------------
-	
-	/** Use a JBoss custom log4j category for trace level logging */
-	static JBossCategory log = (JBossCategory) JBossCategory.getInstance(EntityInstanceInterceptor.class);
-	
-	// Constructors --------------------------------------------------
-	
-	// Public --------------------------------------------------------
-	
-	public void setContainer(Container container)
-	{
-		this.container = (EntityContainer)container;
-	
-	}
-	
-	public Container getContainer()
-	{
-		return container;
-	}
-	
-	// Interceptor implementation --------------------------------------
-	
-	public Object invokeHome(MethodInvocation mi)
-	throws Exception
-	{		
-		// Invoke through interceptors
-		return getNext().invokeHome(mi);
-		
-	}
-	
-	public Object invoke(MethodInvocation mi)
-	throws Exception
-	{
-		
-		// The key.
-		Object key = (CacheKey) mi.getId();
-		
-		// The lock.
-		BeanLock lock ;
-		
-		boolean threadIsScheduled = false;
-		boolean trace = log.isTraceEnabled();
-		
-		if( trace ) log.trace("Begin invoke, key="+key);
-			
-		
-		try 
-		{
-			lock = (BeanLock)container.getLockManager().getLock(key);
-			
-			while (!threadIsScheduled)
-			{	
-				if( trace ) log.trace("new while(threadNotScheduled) for key"+key);
-					
-				
-				/**
-				* schedule will implement the actual lock policy in the lock
-				* we provide a default "pessimistic lock" on the transaction
-				* as well as the default serialization required by the specification
-				* the method releases the sync if the thread isn't scheduled and goes to wait
-				* upon waking up it must go through the schedule again
-				* Also schedule must add a methodLock
-				*/
-				threadIsScheduled = lock.schedule(mi);
-			}
-			
-			try {
-				
-				return getNext().invoke(mi);	
-			}
-			
-			finally 
-			{
-				
-				// we are done with the method, decrease the count, if it reaches 0 it will wake up 
-				// the next thread 
-				lock.sync();
-				lock.releaseMethodLock();
-				lock.releaseSync();	
-			}
-		}
-		finally
-		{
-			
-			// We are done with the lock in general
-			container.getLockManager().removeLockRef(key);
-			
-			if( trace ) log.trace("End invoke, key="+key);
-		}
-	}
-}		
+   // Constants -----------------------------------------------------
+ 
+   // Attributes ----------------------------------------------------
+ 
+   protected EntityContainer container;
+ 
+   // Static --------------------------------------------------------
+ 
+   /** Use a JBoss custom log4j category for trace level logging */
+   static JBossCategory log = (JBossCategory) JBossCategory.getInstance(EntityLockInterceptor.class);
+ 
+   // Constructors --------------------------------------------------
+ 
+   // Public --------------------------------------------------------
+ 
+   public void setContainer(Container container)
+   {
+      this.container = (EntityContainer)container;
+ 
+   }
+ 
+   public Container getContainer()
+   {
+      return container;
+   }
+ 
+   // Interceptor implementation --------------------------------------
+ 
+   public Object invokeHome(MethodInvocation mi)
+      throws Exception
+   {  
+      // Invoke through interceptors
+      return getNext().invokeHome(mi);
+  
+   }
+ 
+   public Object invoke(MethodInvocation mi)
+      throws Exception
+   {
+  
+      // The key.
+      Object key = (CacheKey) mi.getId();
+  
+      // The lock.
+      BeanLock lock ;
+  
+      boolean threadIsScheduled = false;
+      boolean trace = log.isTraceEnabled();
+  
+      if( trace ) log.trace("Begin invoke, key="+key);
+   
+  
+      try 
+      {
+         lock = (BeanLock)container.getLockManager().getLock(key);
+   
+         while (!threadIsScheduled)
+         { 
+            if( trace ) log.trace("new while(threadNotScheduled) for key"+key);
+     
+    
+            /**
+             * schedule will implement the actual lock policy in the lock
+             * we provide a default "pessimistic lock" on the transaction
+             * as well as the default serialization required by the specification
+             * the method releases the sync if the thread isn't scheduled and goes to wait
+             * upon waking up it must go through the schedule again
+             * Also schedule must add a methodLock
+             */
+            threadIsScheduled = lock.schedule(mi);
+         }
+   
+         try {
+    
+            return getNext().invoke(mi); 
+         }
+   
+         finally 
+         {
+    
+            // we are done with the method, decrease the count, if it reaches 0 it will wake up 
+            // the next thread 
+            lock.sync();
+            lock.releaseMethodLock();
+            lock.releaseSync(); 
+         }
+      }
+      finally
+      {
+   
+         // We are done with the lock in general
+         container.getLockManager().removeLockRef(key);
+   
+         if( trace ) log.trace("End invoke, key="+key);
+      }
+   }
+}
 
 
 
