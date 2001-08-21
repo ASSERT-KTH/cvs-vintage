7319b39dd0e6bbddfbd2b729966883ea524207d1
jon <jon>
2001-08-21 03:51:59 +0000

    the start of a framework for a testing suite for Scarab.
    woo hoo.
    
    -jon

---
+++ src/test/org/tigris/scarab/test/BaseTestCase.java
@@ -0,0 +1,109 @@
+package org.tigris.scarab.test;
+
+/* ================================================================
+ * Copyright (c) 2000-2001 CollabNet.  All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 
+ * 3. The end-user documentation included with the redistribution, if
+ * any, must include the following acknowlegement: "This product includes
+ * software developed by Collab.Net <http://www.Collab.Net/>."
+ * Alternately, this acknowlegement may appear in the software itself, if
+ * and wherever such third-party acknowlegements normally appear.
+ * 
+ * 4. The hosted project names must not be used to endorse or promote
+ * products derived from this software without prior written
+ * permission. For written permission, please contact info@collab.net.
+ * 
+ * 5. Products derived from this software may not use the "Tigris" or 
+ * "Scarab" names nor may "Tigris" or "Scarab" appear in their names without 
+ * prior written permission of Collab.Net.
+ * 
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL COLLAB.NET OR ITS CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ====================================================================
+ * 
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of Collab.Net.
+ */ 
+
+import java.io.File;
+
+import junit.framework.TestCase;
+
+/**
+ * Base test case that provides a few utility methods for
+ * the rest of the tests.
+ *
+ * @author <a href="mailto:jon@latchkey.com">Jon S. Stevens</a>
+ * @author <a href="mailto:dlr@finemaltcoding.com">Daniel Rall</a>
+ * @version $Id: BaseTestCase.java,v 1.1 2001/08/21 03:51:59 jon Exp $
+ */
+public class BaseTestCase extends TestCase
+{
+    /**
+     * Default constructor.
+     */
+    public BaseTestCase(String name)
+    {
+        super(name);
+    }
+
+    /**
+     * Concatenates the file name parts together appropriately.
+     *
+     * @return The full path to the file.
+     */
+    protected static String getFileName (String dir, String base, String ext)
+    {
+        StringBuffer buf = new StringBuffer();
+        if (dir != null)
+        {
+            buf.append(dir).append('/');
+        }
+        buf.append(base).append('.').append(ext);
+        return buf.toString();
+    }
+
+    /**
+     * Assures that the results directory exists.  If the results directory
+     * cannot be created, fails the test.
+     */
+    protected static void assureResultsDirectoryExists (String resultsDirectory)
+    {
+        File dir = new File(resultsDirectory);
+        if (!dir.exists())
+        {
+            System.out.println("Template results directory does not exist");
+            if (dir.mkdirs())
+            {
+                System.out.println("Created template results directory");
+            }
+            else
+            {
+                String errMsg = "Unable to create template results directory";
+                System.out.println(errMsg);
+                fail(errMsg);
+            }
+        }
+    }    
+}
