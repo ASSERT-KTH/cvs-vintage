771c4abd491955c172f4e82fce2f25a0cb7e2b89
chirino <chirino>
2001-08-26 03:20:39 +0000

    Adding a MBean that will patch sun's file URL implementation so that JBoss does not get an error when it is  run in a directory with a space in it.

---
+++ src/main/org/jboss/util/FileURLPatchMBean.java
@@ -0,0 +1,24 @@
+/*
+ * JBoss, the OpenSource EJB server
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package org.jboss.util;
+
+import java.lang.Object;
+
+/**
+ *   <description> 
+ *      
+ *   @see <related>
+ *   @author <a href="mailto:rickard.oberg@telkel.com">Rickard Öberg</a>.
+ *   @author <a href="mailto:hiram.chirino@jboss.org">Hiram Chirino</a>.
+ *   @version $Revision: 1.1 $
+ */
+public interface FileURLPatchMBean {
+
+
+
+	public void setEnabled(boolean enable);
+}
