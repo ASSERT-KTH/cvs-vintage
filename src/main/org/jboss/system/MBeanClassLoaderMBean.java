8fcf02cc10e496edae8aaf6faa70525d946b9ede
mnf999 <mnf999>
2001-08-29 23:13:46 +0000

    We need to trick CVS into thinking CVS: ---------------------------------------------------------------------- CVS: Enter Log.  Lines beginning with `CVS:' are removed automatically

---
+++ src/main/org/jboss/system/MBeanClassLoaderMBean.java
@@ -0,0 +1,28 @@
+/*
+* JBoss, the OpenSource EJB server
+*
+* Distributable under LGPL license.
+* See terms of license at gnu.org.
+*/
+
+package org.jboss.system;
+
+import java.net.URL;
+import java.io.InputStream;
+
+/**
+*
+* @author <a href="marc.fleury@jboss.org"> Marc Fleury</a>
+* @version
+*
+*   <p><b>20010830 marc fleury:</b>
+*   <ul>
+*      initial import
+*   <li> 
+*   </ul>
+*/
+
+public interface MBeanClassLoaderMBean 
+{
+	// Empty, just a trick for the MBean base man jmx is dumb sometimes
+}
