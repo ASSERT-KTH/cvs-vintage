ae8d59290f9a151ac84ef5d945cab0905db7b0e6
mnf999 <mnf999>
2001-08-29 23:15:38 +0000

    Just moving the Shutdown stuff to system

---
+++ src/main/org/jboss/system/ShutdownMBean.java
@@ -0,0 +1,32 @@
+/*
+ * JBoss, the OpenSource J2EE webOS
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package org.jboss.system;
+
+/**
+ * The management interface for the Shutdown bean.
+ * 
+ * @author <a href="mailto:rickard.oberg@telkel.com">Rickard Öberg</a>
+ * @author <a href="mailto:jason@planet57.com">Jason Dillon</a>
+ * @version $Revision: 1.1 $
+ */
+public interface ShutdownMBean
+{
+   /**
+    * Shutdown the virtual machine and run shutdown hooks.
+    */
+   void shutdown();
+   
+   /**
+    * Forcibly terminates the currently running Java virtual machine.
+    */
+   void halt();
+}
+
+
+
+
+
