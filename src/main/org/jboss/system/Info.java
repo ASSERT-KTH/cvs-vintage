fa407d6358d8b1bd2078a197dfa1d1adbb839a43
mnf999 <mnf999>
2001-08-29 22:57:44 +0000

    The little brother of the old Info stuff with everything exposed and System stuff most notably we add the file system anchor

---
+++ src/main/org/jboss/system/Info.java
@@ -0,0 +1,248 @@
+/*
+* JBoss, the OpenSource J2EE webOS
+*
+* Distributable under LGPL license.
+* See terms of license at gnu.org.
+*/
+package org.jboss.system;
+
+import java.util.Enumeration;
+
+import javax.management.ObjectName;
+import javax.management.MBeanServer;
+import javax.management.MBeanRegistration;
+
+// Part of the boot, don't use org4j
+import org.jboss.logging.Log;
+
+/**
+* A simple mbean that dumps out info like the system properties, etc.
+*      
+* @author <a href="mailto:rickard.oberg@telkel.com">Rickard Öberg</a>.
+* @author <a href="mailto:Scott.Stark@jboss.org">Scott Stark</a>.
+* @author <a href="mailto:hiram.chirino@jboss.org">Hiram Chirino</a>.
+* @author <a href="mailto:jason@planet57.com">Jason Dillon</a>
+* @author <a href="mailto:marc.fleury@jboss.org">Marc Fleury</a>
+* @version $Revision: 1.1 $
+* 
+*/
+public class Info
+implements InfoMBean, MBeanRegistration
+{
+	// Constants -----------------------------------------------------
+	
+	// Attributes ----------------------------------------------------
+	
+	/*System information */
+	protected String javaVersion, javaVendor , javaVMName , javaVMVersion, libraryDirectory,
+	javaVMVendor, osName, osVersion, osArch, jbossLocalHomeDirectory,
+	installationURL, configurationDirectory, patchDirectory, jbossVersion;
+	
+	
+	/** Instance logger. */
+	private Log log = Log.createLog("GPA");
+	
+	// Static --------------------------------------------------------
+	
+	// Constructors --------------------------------------------------
+	
+	// Public --------------------------------------------------------
+	
+	public ObjectName preRegister(MBeanServer server, ObjectName name)
+	throws Exception
+	{
+		
+		//VM stuff
+		javaVersion = System.getProperty("java.version");
+		javaVendor = System.getProperty("java.vendor");
+		javaVMName = System.getProperty("java.vm.name");
+		javaVMVersion = System.getProperty("java.vm.version");
+		javaVMVendor =  System.getProperty("java.vm.vendor");
+		
+		// OS stuff
+		osName = System.getProperty("os.name");
+		osVersion = System.getProperty("os.version");
+		osArch = System.getProperty("os.arch");
+		
+		// JBoss stuff
+		jbossVersion = System.getProperty("jboss.system.version");
+		jbossLocalHomeDirectory = System.getProperty("jboss.system.home");
+		installationURL = System.getProperty("jboss.system.installationURL");
+		configurationDirectory = System.getProperty("jboss.system.configurationDirectory");
+		libraryDirectory = System.getProperty("jboss.system.libraryDirectory");
+		patchDirectory = System.getProperty("jboss.system.patchDirectory");
+		
+		log.log("General Purpose Architecture [GPA]");
+		
+		// Dump out basic info as INFO priority msgs
+		log.log("Java version: " +
+			javaVersion + "," +
+			javaVendor);
+		log.log("Java VM: " +
+			javaVMName + " " +
+			javaVMVersion + "," +
+			javaVMVendor);
+		log.log("OS-System: " +
+			osName + " " +
+			osVersion + "," +
+			osArch);
+		log.log("JBoss Version : " + jbossVersion);
+		log.log("localDir : " + jbossLocalHomeDirectory);
+		log.log("installationURL : "+ installationURL);
+		log.log("configuration : " + configurationDirectory);
+		log.log("libraries : "+libraryDirectory);
+		//	   log.info("local patch directory " + patchDirectory);
+		log.log("Oh, and remember we love you");
+		
+		// dump out the entire system properties if debug is enabled
+		log.debug("+++ Full System Properties Dump");
+		Enumeration names= System.getProperties().propertyNames();
+		while (names.hasMoreElements()) {
+			String pname= (String) names.nextElement();
+			log.debug(pname + ": " + System.getProperty(pname));
+		}
+		
+		
+		return new ObjectName(OBJECT_NAME);
+	}
+	
+	public void postRegister(Boolean registrationDone) {
+		// empty
+	}
+	
+	public void preDeregister() throws Exception {
+		// empty
+	}
+	
+	public void postDeregister() {
+		// empty
+	}
+	
+	public String getJavaVersion() {return javaVersion;} 
+	public String getJavaVendor() {return javaVendor;}
+	public String getJavaVMName() {return javaVMName;}
+	public String getJavaVMVersion() {return javaVMVersion;}
+	public String getJavaVMVendor() { return javaVMVendor;}
+	public String getOSName() {return osName;}
+	public String getOSVersion() { return osVersion;}
+	public String getOSArch() { return osArch;}
+	
+	public String getLocalJBossSystemHomeDirectory() {return jbossLocalHomeDirectory;}
+	public String getLocalInstallationURL(){return installationURL;}
+	public String getConfigurationDirectoryURL(){return configurationDirectory;}
+	public String getLocalPatchDirectory(){ return patchDirectory;}
+	public String getJBossVersion() { return jbossVersion;}
+	public String getLibraryDirectoryURL() { return libraryDirectory;}
+	public String getInstallationURL() { return installationURL;}
+	
+	public String getThreadGroupInfo(ThreadGroup group) {
+		StringBuffer rc= new StringBuffer();
+		
+		rc.append("<BR><B>");
+		rc.append("Thread Group: " + group.getName());
+		rc.append("</B> : ");
+		rc.append("max priority:" + group.getMaxPriority() +
+			", demon:" + group.isDaemon());
+		
+		rc.append("<blockquote>");
+		Thread threads[]= new Thread[group.activeCount()];
+		group.enumerate(threads, false);
+		for (int i= 0; i < threads.length && threads[i] != null; i++) {
+			rc.append("<B>");
+			rc.append("Thread: " + threads[i].getName());
+			rc.append("</B> : ");
+			rc.append("priority:" + threads[i].getPriority() +
+				", demon:" + threads[i].isDaemon());
+			rc.append("<BR>");
+		}
+		
+		ThreadGroup groups[]= new ThreadGroup[group.activeGroupCount()];
+		group.enumerate(groups, false);
+		for (int i= 0; i < groups.length && groups[i] != null; i++) {
+			rc.append(getThreadGroupInfo(groups[i]));
+		}
+		rc.append("</blockquote>");
+		return rc.toString();
+	}
+	
+	public String runGarbageCollector() {
+		StringBuffer buff = new StringBuffer();
+		buff.append("<h3>Before</h3>");
+		buff.append(listMemoryUsage());
+		buff.append("<h3>After</h3>");
+		
+		log.debug("hinting the VM to run the garbage collector");
+		System.gc();
+		
+		buff.append(listMemoryUsage());
+		return buff.toString();
+	}
+	
+	public String listMemoryUsage() {
+		String rc= "<P><B>Total Memory: </B>" +
+		(Runtime.getRuntime().totalMemory()) +
+		" </P>" + "<P><B>Free Memory: </B>" +
+		(Runtime.getRuntime().freeMemory()) + " </P>";
+		return rc;
+	}
+	
+	public String listSystemInfo() {
+		// Dump out basic info as INFO priority msgs
+		StringBuffer rc= new StringBuffer();
+		rc.append("<pre>");
+		rc.append("Java version: " +
+			System.getProperty("java.version") + "," +
+			System.getProperty("java.vendor"));
+		rc.append("\n");
+		rc.append("Java VM: " +
+			System.getProperty("java.vm.name") + " " +
+			System.getProperty("java.vm.version") + "," +
+			System.getProperty("java.vm.vendor"));
+		rc.append("\n");
+		rc.append("System: " +
+			System.getProperty("os.name") + " " +
+			System.getProperty("os.version") + "," +
+			System.getProperty("os.arch"));
+		rc.append("\n");
+		rc.append("</pre>");
+		
+		// HRC: Should we also do a full system properties dump??
+		
+		return rc.toString();
+	}
+	
+	public String listThreadDump() {
+		// Get the root thread group
+		ThreadGroup root= Thread.currentThread().getThreadGroup();
+		while (root.getParent() != null) {
+			root = root.getParent();
+		}
+		
+		// I'm not sure why what gets reported is off by +1, 
+		// but I'm adjusting so that it is consistent with the display
+		int activeThreads = root.activeCount()-1;
+		// I'm not sure why what gets reported is off by -1
+		// but I'm adjusting so that it is consistent with the display
+		int activeGroups = root.activeGroupCount()+1;
+		
+		String rc=
+		"<b>Total Threads:</b> "+activeThreads+"<br>"+
+		"<b>Total Thread Groups:</b> "+activeGroups+"<br>"+
+		getThreadGroupInfo(root) ;
+		return rc;
+	}
+	
+	/**
+	* Enable or disable tracing method calls at the Runtime level.
+	*/
+	public void traceMethodCalls(final boolean flag) {
+		Runtime.getRuntime().traceMethodCalls(flag);
+	}
+	
+	/**
+	* Enable or disable tracing instructions the Runtime level.
+	*/
+	public void traceInstructions(final boolean flag) {
+		Runtime.getRuntime().traceInstructions(flag);
+	}
+}
