4c0b8eef68c02e27415de36abcef05b2612c6456
mnf999 <mnf999>
2001-08-29 23:19:40 +0000

    sometimes JMX is just dumb

---
+++ src/main/org/jboss/system/URLClassLoaderMBean.java
@@ -0,0 +1,22 @@
+/*
+* JBoss, the OpenSource EJB server
+*
+* Distributable under LGPL license.
+* See terms of license at gnu.org.
+*/
+
+package org.jboss.system;
+
+import java.net.URL;
+import java.io.InputStream;
+
+/**
+*
+* @author <a href="marc.fleury@jboss.org"> Marc Fleury</a>
+* @version
+*/
+
+public interface URLClassLoaderMBean 
+{
+	// Empty, just a trick for the MBean base man jmx is dumb sometimes
+}
