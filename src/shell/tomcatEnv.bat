a2d985632341d4701a25979752ceb02d32413663
larryi <larryi>
2001-08-30 11:45:53 +0000

    Modify to not set TOMCAT_HOME so tomcat.bat can always guess if TOMCAT_HOME hasn't been set by the user. Fix passing of arguments for Win9x.

--- src/shell/tomcatEnv.bat
+++ src/shell/tomcatEnv.bat
@@ -4,27 +4,34 @@ if "%1" == "restore" goto restore
 
 set TOM_PREV_CLASSPATH=%CLASSPATH%
 set TOM_PREV_HOME=%TOMCAT_HOME%
+set TOM_PREV_INSTALL=%TOMCAT_INSTALL%
 
-if not "%TOMCAT_HOME%" == "" goto start
+set _TC_BIN_DIR=%TOMCAT_HOME%\bin
+if not "%_TC_BIN_DIR%" == "\bin" goto start
 
-SET TOMCAT_HOME=.
-if exist "%TOMCAT_HOME%\bin\tomcat.bat" goto start
+set _TC_BIN_DIR=.\bin
+if exist "%_TC_BIN_DIR%\tomcat.bat" goto start
 
-SET TOMCAT_HOME=..
-if exist "%TOMCAT_HOME%\bin\tomcat.bat" goto start
+set _TC_BIN_DIR=.
+if exist "%_TC_BIN_DIR%\tomcat.bat" goto start
 
-SET TOMCAT_HOME=
-echo Unable to determine the value of TOMCAT_HOME.
+set _TC_BIN_DIR=..\bin
+if exist "%_TC_BIN_DIR%\tomcat.bat" goto start
+
+echo Unable to determine the location of Tomcat.
 goto eof
 
 :start
-call "%TOMCAT_HOME%\bin\tomcat" env %*
+call "%_TC_BIN_DIR%\tomcat" env %1 %2 %3 %4 %5 %6 %7 %8 %9
 goto eof
 
 :restore
 set CLASSPATH=%TOM_PREV_CLASSPATH%
 set TOMCAT_HOME=%TOM_PREV_HOME%
+set TOMCAT_INSTALL=%TOM_PREV_INSTALL%
 set TOM_PREV_CLASSPATH=
 set TOM_PREV_HOME=
+set TOM_PREV_INSTALL=
 
 :eof
+set _TC_BIN_DIR=
