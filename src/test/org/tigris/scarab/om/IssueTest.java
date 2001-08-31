dba2b1d011877f1529ff7bf9ed08ccef9b6ea176
nreizer <nreizer>
2001-08-31 17:36:38 +0000

    Added tests for the getUniqueId() and getEligibleAssignees() methods.
    There's a known NPE when executing the getEligibleAssignees() method call.
    -Neal

--- src/test/org/tigris/scarab/om/IssueTest.java
+++ src/test/org/tigris/scarab/om/IssueTest.java
@@ -55,7 +55,7 @@ import org.tigris.scarab.om.Issue;
  * A Testing Suite for the om.Issue class.
  *
  * @author <a href="mailto:jon@latchkey.com">Jon S. Stevens</a>
- * @version $Id: IssueTest.java,v 1.2 2001/08/21 05:09:01 jon Exp $
+ * @version $Id: IssueTest.java,v 1.3 2001/08/31 17:36:38 nreizer Exp $
  */
 public class IssueTest extends BaseTestCase
 {
@@ -76,15 +76,40 @@ public class IssueTest extends BaseTestCase
     protected void runTest()
         throws Throwable
     {
-        testGetAllAttributeValuesMap();
-    }
-    
-    private void testGetAllAttributeValuesMap() throws Exception
-    {
         Issue issue = new Issue();
         issue.setModuleCast(getModule());
+
+        testGetAllAttributeValuesMap(issue);
+	testGetUniqueId(issue);
+	testGetEligibleAssignees(issue); 
+    }
+    
+    private void testGetAllAttributeValuesMap(Issue issue) throws Exception
+    {
+        System.out.println ("testGetAllAttributeValuesMap()");
         HashMap map = issue.getAllAttributeValuesMap();
         System.out.println ("getAllAttributeValuesMap().size(): " + map.size());
-        assertEquals (map.size(), 10);
+        assertEquals (map.size(), 10);	
     }
+
+    private void testGetUniqueId(Issue issue) throws Exception
+    {
+        System.out.println ("testGetUniqueId()");
+
+	String strUniqueID = null;
+	strUniqueID = issue.getUniqueId();
+	System.out.println ("Unique id: " + strUniqueID);	
+    }
+
+    private void testGetEligibleAssignees(Issue issue) throws Exception
+    {
+        System.out.println ("testGetEligibleAssignees()");
+
+	java.util.List eligibleAssignees = null;
+	eligibleAssignees = issue.getEligibleAssignees();
+	System.out.println ("Number of eligible assignees: "
+		+ eligibleAssignees.size());
+	
+    }
+
 }
