790b0ae93a7acdda66f2201b949e2dc0fede9bb9
elicia <elicia>
2001-09-08 01:45:04 +0000

    classes for enter issue templates

---
+++ src/java/org/tigris/scarab/om/IssueTemplate.java
@@ -0,0 +1,83 @@
+package org.tigris.scarab.om;
+
+import java.util.List;
+import org.apache.turbine.Turbine;
+import org.apache.turbine.modules.ContextAdapter; 
+import org.apache.turbine.TemplateContext;
+import org.apache.torque.om.NumberKey;
+import org.apache.torque.om.Persistent;
+import org.apache.torque.util.Criteria;
+import org.tigris.scarab.security.ScarabSecurity;
+import org.tigris.scarab.security.SecurityFactory;
+import org.tigris.scarab.services.module.ModuleEntity;
+import org.tigris.scarab.util.ScarabConstants;
+import org.tigris.scarab.tools.Email;
+
+/** 
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ */
+public  class IssueTemplate 
+    extends org.tigris.scarab.om.BaseIssueTemplate
+    implements Persistent
+{
+    public static final NumberKey USER__PK = new NumberKey("1");
+    public static final NumberKey GLOBAL__PK = new NumberKey("2");
+
+    /**
+     * A new IssueTemplate object
+     */
+    public static IssueTemplate getInstance() 
+    {
+        return new IssueTemplate();
+    }
+
+
+    public void save( ScarabUser user, ModuleEntity module, 
+                      TemplateContext context )
+        throws Exception
+    {
+        ScarabSecurity security = SecurityFactory.getInstance();
+        // If it's a global template, user must have Item | Approve 
+        // permission,  Or its Approved field gets set to false
+        if (getTypeId().equals(USER__PK))
+        {
+            setApproved(true);
+        }
+        else
+        {
+            setApproved(security.hasPermission(ScarabSecurity.ITEM__APPROVE, 
+                                               user, module));
+        } 
+
+        if (!security.hasPermission(ScarabSecurity.ITEM__APPROVE, 
+                                    user, module))
+        {
+            // Send Email
+            // add data to context for email template
+            context.put("user", user);
+            context.put("module", module);
+
+            String subject = "New template requires approval";
+            String template = Turbine.getConfiguration().
+                getString("scarab.email.requireapproval.template",
+                          "email/RequireApproval.vm");
+            ScarabUser toUser = (ScarabUser) ScarabUserImplPeer
+                              .retrieveByPK((NumberKey)module.getOwnerId());
+            Email.sendEmail(new ContextAdapter(context), null, toUser,
+                            subject, template);
+        }
+        super.save();
+    }
+
+    /**
+     * Returns list of all issue template types.
+     */
+    public List getAllTemplateTypes() throws Exception
+    {
+        return IssueTemplateTypePeer.doSelect(new Criteria());
+    }
+
+
+}
---
+++ src/java/org/tigris/scarab/om/IssueTemplateType.java
@@ -0,0 +1,15 @@
+package org.tigris.scarab.om;
+
+
+import org.apache.torque.om.Persistent;
+
+/** 
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ */
+public  class IssueTemplateType 
+    extends org.tigris.scarab.om.BaseIssueTemplateType
+    implements Persistent
+{
+}
