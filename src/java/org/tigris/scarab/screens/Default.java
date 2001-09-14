409fb6e0f11980ee8b30f3c0819e5d3f321f760a
jon <jon>
2001-09-14 01:51:34 +0000

    cleanup.
    removed the passing of parameters for now...will be adding a better
    solution in at some point...

--- src/java/org/tigris/scarab/screens/Default.java
+++ src/java/org/tigris/scarab/screens/Default.java
@@ -63,17 +63,17 @@ import org.tigris.scarab.util.ScarabConstants;
     for the Default Screen.
 
     @author <a href="mailto:jon@collab.net">Jon S. Stevens</a>
-    @version $Id: Default.java,v 1.15 2001/09/07 22:10:11 elicia Exp $
+    @version $Id: Default.java,v 1.16 2001/09/14 01:51:34 jon Exp $
 */
 public class Default extends TemplateSecureScreen
 {
     /**
         builds up the context for display of variables on the page.
     */
-    public void doBuildTemplate( RunData data, TemplateContext context ) throws Exception 
+    public void doBuildTemplate( RunData data, TemplateContext context )
+        throws Exception 
     {
     }
-
 
     /**
      * sets the template to Login.vm if the user hasn't logged in yet
@@ -81,10 +81,10 @@ public class Default extends TemplateSecureScreen
      */
     protected boolean isAuthorized( RunData data ) throws Exception
     {
-	String template = ScarabPage.getScreenTemplate(data);
-	if (template != null)
+        String template = ScarabPage.getScreenTemplate(data);
+        if (template != null)
         {
-	    template = template.replace('/','.');
+            template = template.replace('/','.');
 
             String perm = Turbine.getConfiguration()
                 .getString("scarab.security." + template);
@@ -100,15 +100,15 @@ public class Default extends TemplateSecureScreen
                 if ( !(data.getUser().hasLoggedIn()
                    && security.hasPermission(perm, 
                                              scarabR.getCurrentModule())))
-                    {
-                        data.setMessage("Please log in with an account " +
-                                        " that has permissions to " +
-                                        "access this page.");
-                        setTargetLogin(data);
-                        return false;
-                    }
+                {
+                    data.setMessage("Please log in with an account " +
+                                    " that has permissions to " +
+                                    "access this page.");
+                    setTargetLogin(data);
+                    return false;
+                }
             }
-	}
+        }
         return true;
     }
 
