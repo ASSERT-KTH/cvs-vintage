696ea67a2ad0b763804dd057ea803127e4c5c431
tstich <tstich>
2001-09-17 19:29:40 +0000

    -Fixed Bug in pop3 AUTO login\n

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 PATH=/usr/bin
 
-cd /home/freddy/src/columba
-/usr/java/jdk1.3.1/bin/java  -cp .:./ext/jaxp.jar:./ext/parser.jar org/columba/main/Main --path ~/.stable
+cd /home/timo/src/columba
+/usr/java/jdk1.3.1_01/bin/java  -cp .:./ext/jaxp.jar:./ext/parser.jar org/columba/main/Main
