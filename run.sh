5aedcf5edcc3d4dccf37047807720e8da609bf1b
fdietz <fdietz>
2001-09-24 09:54:57 +0000

    *** empty log message ***

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-PATH=/usr/bin
 
-cd /home/timo/src/columba
-/usr/java/jdk1.3.1_01/bin/java  -cp .:./ext/jaxp.jar:./ext/parser.jar org/columba/main/Main
+cd ~/src/columba
+java  -cp .:./ext/jaxp.jar:./ext/parser.jar org/columba/main/Main --path ~/.stable
