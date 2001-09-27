11c939f0ea715a93614526e3a6893be818afc4ff
hgomez <hgomez>
2001-09-27 12:40:35 +0000

    Fix stupid copy/paste in jspc.sh PR: Obtained from: Submitted by: Reviewed by:

--- src/shell/jspc.sh
+++ src/shell/jspc.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #
-# $Id: jspc.sh,v 1.4 2001/08/21 05:43:23 costin Exp $
+# $Id: jspc.sh,v 1.5 2001/09/27 12:40:35 hgomez Exp $
 
 # Shell script to runt JspC
 
 
 BASEDIR=`dirname $0`
 
-$BASEDIR/tomcat.sh start "$@"
+$BASEDIR/tomcat.sh jspc "$@"
