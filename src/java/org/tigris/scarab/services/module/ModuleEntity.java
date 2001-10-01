3f17b3d608af887a85145e3a2d70c34ced75389b
jmcnally <jmcnally>
2001-10-01 03:42:22 +0000

    added method to get RModuleAttribute related to a RModuleOption and added a method already implemented in ScarabModule to ModuleEntity
    Issue number:
    Obtained from:
    Submitted by:
    Reviewed by:
    Issue number:
    Obtained from:
    Submitted by:
    Reviewed by:

--- src/java/org/tigris/scarab/services/module/ModuleEntity.java
+++ src/java/org/tigris/scarab/services/module/ModuleEntity.java
@@ -50,6 +50,7 @@ import java.util.List;
 import java.util.Vector;
 
 import org.tigris.scarab.om.Attribute;
+import org.tigris.scarab.om.RModuleAttribute;
 import org.tigris.scarab.om.Issue;
 import org.tigris.scarab.om.ScarabUser;
 import org.tigris.scarab.om.RModuleAttribute;
@@ -62,7 +63,7 @@ import org.apache.torque.util.Criteria;
  * This class describes a Module within the Scarab system
  *
  * @author <a href="mailto:jon@collab.net">Jon S. Stevens</a>
- * @version $Id: ModuleEntity.java,v 1.16 2001/09/30 18:31:39 jon Exp $
+ * @version $Id: ModuleEntity.java,v 1.17 2001/10/01 03:42:23 jmcnally Exp $
  */
 public interface ModuleEntity
 {
@@ -130,6 +131,9 @@ public interface ModuleEntity
     public Vector getRModuleAttributes(Criteria criteria)
         throws Exception;
 
+    public RModuleAttribute getRModuleAttribute(Attribute attribute)
+        throws Exception;
+
     public String getQueryKey();
 
     public boolean getDeleted();
@@ -158,6 +162,7 @@ public interface ModuleEntity
 
     public List getLeafRModuleOptions(Attribute attribute, boolean activeOnly)
         throws Exception;
+
 
     public RModuleAttribute getRModuleAttribute(Attribute attribute)
         throws Exception;
