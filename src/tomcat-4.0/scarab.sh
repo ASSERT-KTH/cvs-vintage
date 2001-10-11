b5a633c0df8b46c282a40149acbf3966304c1c73
jon <jon>
2001-10-11 20:42:50 +0000

    added some options

--- src/tomcat-4.0/scarab.sh
+++ src/tomcat-4.0/scarab.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
-CATALINA_OPTS="-Xms128M -Xmx256M"
-export CATALINA_OPTS
+if [ "${1}" = "-h" ] ; then
+    echo "Usage: scarab.sh [-l] [-h]"
+    echo "-l    Default JVM memory allocation"
+    echo "-h    This usage information"
+    exit
+fi
+
+if [ "${1}" != "-l" ] ; then
+    CATALINA_OPTS="-Xms96M -Xmx256M"
+    export CATALINA_OPTS
+fi
 
 ./bin/catalina.sh run
