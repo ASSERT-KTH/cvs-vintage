97f6ae6d5cf2542eec62b154380d4a267478ef97
elicia <elicia>
2001-10-26 00:45:06 +0000

    added issue type to query retrieval methods.

--- src/java/org/tigris/scarab/services/module/ModuleEntity.java
+++ src/java/org/tigris/scarab/services/module/ModuleEntity.java
@@ -65,7 +65,7 @@ import org.apache.torque.util.Criteria;
  * This class describes a Module within the Scarab system
  *
  * @author <a href="mailto:jon@collab.net">Jon S. Stevens</a>
- * @version $Id: ModuleEntity.java,v 1.33 2001/10/24 23:41:57 jon Exp $
+ * @version $Id: ModuleEntity.java,v 1.34 2001/10/26 00:45:06 elicia Exp $
  */
 public interface ModuleEntity
 {
@@ -199,16 +199,22 @@ public interface ModuleEntity
         throws Exception;
 
     /**
-     * List of private queries associated with this module and
-     * created by the given user.
+     * List of private queries associated with this module and issue type
+     * And created by the given user.
      */
-    public List getPrivateQueries(ScarabUser user)
+    public List getPrivateQueries(ScarabUser user, IssueType issueType)
         throws Exception;
 
     /**
-     * List of global Query objects associated with this module.
+     * List of global Query objects associated with this module and issuetype.
      */
-    public List getGlobalQueries()
+    public List getGlobalQueries(IssueType issueType)
+        throws Exception;
+
+    /**
+     * List of all Query objects associated with this module and user.
+     */
+    public List getAllUserQueries(ScarabUser user, IssueType issueType)
         throws Exception;
 
     /**
