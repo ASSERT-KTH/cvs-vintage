ab8f00f021e0d60b82db1ec08a17fa1ca7098834
slaboure <slaboure>
2001-11-09 23:05:52 +0000

    Added session-state-manager-jndi-name for clustered SFSB

--- src/main/org/jboss/metadata/ClusterConfigMetaData.java
+++ src/main/org/jboss/metadata/ClusterConfigMetaData.java
@@ -16,14 +16,19 @@ import org.jboss.ejb.DeploymentException;
  * expanded to include other cluster configuration parameters later on.
 
  * @author <a href="mailto:bill@burkecentral.com">Bill Burke</a>.
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 public class ClusterConfigMetaData extends MetaData
 {
    public final static String DEFAULT_PARTITION = "DefaultPartition";
+   public final static String JNDI_PREFIX_FOR_SESSION_STATE = "/HASessionState/";
+   public final static String DEFAULT_SESSION_STATE_NAME = JNDI_PREFIX_FOR_SESSION_STATE + "Default";
+   
    private String partitionName = DEFAULT_PARTITION;
    private String homeLoadBalancePolicy = null;
    private String beanLoadBalancePolicy = null;
+   
+   private String haSessionStateName = DEFAULT_SESSION_STATE_NAME;
 
    public String getPartitionName()
    {
@@ -39,11 +44,22 @@ public class ClusterConfigMetaData extends MetaData
    {
       return homeLoadBalancePolicy;
    }
-
+   
+   // SFSB only
+   //
+   public String getHaSessionStateName ()
+   {
+      return this.haSessionStateName;
+   }
+   
    public void importJbossXml(Element element) throws DeploymentException 
    {
       partitionName = getElementContent(getOptionalChild(element, "partition-name"), DEFAULT_PARTITION);
       homeLoadBalancePolicy = getElementContent(getOptionalChild(element, "home-load-balance-policy"), null);
       beanLoadBalancePolicy = getElementContent(getOptionalChild(element, "bean-load-balance-policy"), null);
+
+      // SFSB settings only
+      //
+      haSessionStateName = getElementContent(getOptionalChild(element, "session-state-manager-jndi-name"), DEFAULT_SESSION_STATE_NAME);
    }
 }
