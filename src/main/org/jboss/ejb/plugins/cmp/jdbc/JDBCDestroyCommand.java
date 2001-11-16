f213b91c291ea57fb253ca9a4b029f3a210070f9
dsundstrom <dsundstrom>
2001-11-16 00:47:50 +0000

    Move all code from destroy to stop.

--- src/main/org/jboss/ejb/plugins/cmp/jdbc/JDBCDestroyCommand.java
+++ src/main/org/jboss/ejb/plugins/cmp/jdbc/JDBCDestroyCommand.java
@@ -7,91 +7,22 @@
 
 package org.jboss.ejb.plugins.cmp.jdbc;
 
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import org.jboss.ejb.plugins.cmp.DestroyCommand;
-import org.jboss.ejb.plugins.cmp.jdbc.bridge.JDBCCMRFieldBridge;
 
 /**
- * JDBCDestroyCommand drops the table for this entity if specified in the xml.
+ * JDBCDestroyCommand does nothing.
  *
  * @author <a href="mailto:dain@daingroup.com">Dain Sundstrom</a>
  * @author <a href="mailto:rickard.oberg@telkel.com">Rickard Öberg</a>
  * @author <a href="mailto:marc.fleury@telkel.com">Marc Fleury</a>
  * @author <a href="mailto:shevlandj@kpi.com.au">Joe Shevland</a>
  * @author <a href="mailto:justin@j-m-f.demon.co.uk">Justin Forder</a>
- * @version $Revision: 1.6 $
+ * @version $Revision: 1.7 $
  */
-public class JDBCDestroyCommand extends JDBCUpdateCommand implements DestroyCommand {
-   // Constructors --------------------------------------------------
-   
+public class JDBCDestroyCommand implements DestroyCommand {
    public JDBCDestroyCommand(JDBCStoreManager manager) {
-      super(manager, "Destroy");
    }
-   
-   // DestroyCommand implementation ------------------------------
    
    public void execute() {
-      if(entityMetaData.getRemoveTable()) {
-         log.debug("Droping tables for entity " + entity.getEntityName());
-         dropTable(entityMetaData.getTableName());
-
-         // drop relation tables
-         JDBCCMRFieldBridge[] cmrFields = entity.getJDBCCMRFields();
-         for(int i=0; i<cmrFields.length; i++) {
-            if(!cmrFields[i].hasForeignKey() && !cmrFields[i].getRelatedCMRField().hasForeignKey()) {
-               if(cmrFields[i].getRelationTableName() == null)  log.debug("Table name null for cmr field " + cmrFields[i].getFieldName());
-               dropTable(cmrFields[i].getRelationTableName());
-            }
-         }
-      }
-   }
-   
-   public void dropTable(String tableName) {
-         Connection con = null;
-      ResultSet rs = null;
-      try {
-         con = manager.getConnection();
-         DatabaseMetaData dmd = con.getMetaData();
-         rs = dmd.getTables(con.getCatalog(), null, tableName, null);
-         if(!rs.next()) {
-            // table already deleted
-            return;
-         }
-      } catch(SQLException e) {
-         // ignore - bad driver
-         return;
-      } finally {
-         JDBCUtil.safeClose(rs);
-         JDBCUtil.safeClose(con);
-      }
-
-      try {
-         // since we use the pools, we have to do this within a transaction
-         manager.getContainer().getTransactionManager().begin ();
-         jdbcExecute("DROP TABLE " + tableName);
-         manager.getContainer().getTransactionManager().commit ();
-         log.info("Dropped table '" + tableName + "' successfully.");
-      } catch (Exception e) {
-         log.debug("Could not drop table " + tableName + ": " + e.getMessage());
-         try {
-            manager.getContainer().getTransactionManager().rollback ();
-         } catch (Exception _e) {
-            log.error("Could not roll back transaction: "+ _e.getMessage());
-         }
-      }
-   }
-   
-   // JDBCUpdateCommand overrides -----------------------------------
-   protected String getSQL(Object sql) throws Exception {
-      return (String) sql;
-   }
-      
-   protected Object handleResult(int rowsAffected, Object argOrArgs) 
-      throws Exception
-   {     
-      return null;
    }
 }
--- src/main/org/jboss/ejb/plugins/cmp/jdbc/JDBCStopCommand.java
+++ src/main/org/jboss/ejb/plugins/cmp/jdbc/JDBCStopCommand.java
@@ -7,26 +7,87 @@
  
 package org.jboss.ejb.plugins.cmp.jdbc;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import org.jboss.ejb.plugins.cmp.StopCommand;
+import org.jboss.ejb.plugins.cmp.jdbc.bridge.JDBCCMRFieldBridge;
 
 /**
- * JDBCStopCommand does nothing.
+ * JDBCStopCommand drops the table for this entity if specified in the xml.
  *    
+ * @author <a href="mailto:dain@daingroup.com">Dain Sundstrom</a>
  * @author <a href="mailto:rickard.oberg@telkel.com">Rickard Öberg</a>
  * @author <a href="mailto:justin@j-m-f.demon.co.uk">Justin Forder</a>
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
-public class JDBCStopCommand implements StopCommand
-{
-   // Constructors --------------------------------------------------
-   
-   public JDBCStopCommand(JDBCStoreManager manager)
-   {
+public class JDBCStopCommand extends JDBCUpdateCommand implements StopCommand {
+
+   public JDBCStopCommand(JDBCStoreManager manager) {
+      super(manager, "Stop");
    }
    
-   // StopCommand implementation ---------------------------------
+   public void execute() {
+      if(entityMetaData.getRemoveTable()) {
+         log.debug("Droping tables for entity " + entity.getEntityName());
+         dropTable(entityMetaData.getTableName());
+
+         // drop relation tables
+         JDBCCMRFieldBridge[] cmrFields = entity.getJDBCCMRFields();
+         for(int i=0; i<cmrFields.length; i++) {
+            // if it uses a relation-table drop it
+            if(!cmrFields[i].hasForeignKey() && 
+                  !cmrFields[i].getRelatedCMRField().hasForeignKey()) {
+
+               dropTable(cmrFields[i].getRelationTableName());
+            }
+         }
+      }
+   }
    
-   public void execute()
-   {
+   public void dropTable(String tableName) {
+      Connection con = null;
+      ResultSet rs = null;
+      try {
+         con = manager.getConnection();
+         DatabaseMetaData dmd = con.getMetaData();
+         rs = dmd.getTables(con.getCatalog(), null, tableName, null);
+         if(!rs.next()) {
+            // table already deleted
+            return;
+         }
+      } catch(SQLException e) {
+         // ignore - bad driver
+         return;
+      } finally {
+         JDBCUtil.safeClose(rs);
+         JDBCUtil.safeClose(con);
+      }
+
+      try {
+         // since we use the pools, we have to do this within a transaction
+         manager.getContainer().getTransactionManager().begin();
+         jdbcExecute("DROP TABLE " + tableName);
+         manager.getContainer().getTransactionManager().commit();
+         log.info("Dropped table '" + tableName + "' successfully.");
+      } catch (Exception e) {
+         log.debug("Could not drop table " + tableName + ": " + e.getMessage());
+         try {
+            manager.getContainer().getTransactionManager().rollback ();
+         } catch (Exception _e) {
+            log.error("Could not roll back transaction: "+ _e.getMessage());
+         }
+      }
+   }
+   
+   protected String getSQL(Object sql) throws Exception {
+      return (String) sql;
+   }
+      
+   protected Object handleResult(int rowsAffected, Object argOrArgs) 
+      throws Exception
+   {     
+      return null;
    }
 }
