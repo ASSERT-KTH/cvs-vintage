2b79822819ac56c7b1dc2db9be4a29082bbd5b9f
elicia <elicia>
2001-11-19 22:33:02 +0000

    starting id's at 200 for new attribute groups

--- src/sql/mysql-scarab-id-table-init.sql
+++ src/sql/mysql-scarab-id-table-init.sql
@@ -22,5 +22,5 @@ insert into ID_TABLE (table_name, next_id, quantity) VALUES ('SCARAB_QUERY', 100
 insert into ID_TABLE (table_name, next_id, quantity) VALUES ('SCARAB_QUERY_TYPE', 100, 10);
 insert into ID_TABLE (table_name, next_id, quantity) VALUES ('SCARAB_FREQUENCY', 100, 10);
 insert into ID_TABLE (table_name, next_id, quantity) VALUES ('SCARAB_ISSUE_TYPE', 100, 10);
-insert into ID_TABLE (table_name, next_id, quantity) VALUES ('SCARAB_ATTRIBUTE_GROUP', 100, 10);
+insert into ID_TABLE (table_name, next_id, quantity) VALUES ('SCARAB_ATTRIBUTE_GROUP', 200, 10);
 insert into ID_TABLE (table_name, next_id, quantity) VALUES ('SCARAB_REPORT', 100, 10);
