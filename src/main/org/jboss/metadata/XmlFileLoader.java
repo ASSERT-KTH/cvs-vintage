3e756ea79d03ced8002398555eb6bdec421749c6
dsundstrom <dsundstrom>
2001-11-25 04:07:37 +0000

    Added jbosscmp-jdbc dtd to known dtds.

--- src/main/org/jboss/metadata/XmlFileLoader.java
+++ src/main/org/jboss/metadata/XmlFileLoader.java
@@ -37,7 +37,7 @@ import org.jboss.logging.Logger;
  *   @author <a href="mailto:WolfgangWerner@gmx.net">Wolfgang Werner</a>
  *   @author <a href="mailto:Darius.D@jbees.com">Darius Davidavicius</a>
  *   @author <a href="mailto:scott.stark@jboss.org">Scott Stark</a>
- *   @version $Revision: 1.21 $
+ *   @version $Revision: 1.22 $
  *
  *   Revisions:
  *   20010620 Bill Burke: Print an error message when failing to load standardjboss.xml
@@ -289,6 +289,9 @@ public class XmlFileLoader
             registerDTD("-//JBoss//DTD JAWS 3.0//EN", "jaws_3_0.dtd");
             registerDTD("-//JBoss//DTD JBOSS//EN","jboss.dtd");
             registerDTD("-//JBoss//DTD JBOSS 2.4//EN","jboss_2_4.dtd");
+
+            registerDTD("-//JBoss//DTD JBOSSCMP-JDBC 3.0//EN",
+                  "jbosscmp-jdbc_3_0.dtd");
         }
 
         /**
