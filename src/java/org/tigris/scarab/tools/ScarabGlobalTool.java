101b1ba52299a299afd145d0e2e8e28b1ec7d9f9
dr <dr>
2001-11-28 18:46:50 +0000

    added a method to retrieve an array of Permission; also added the method getDateFormat() which Jon added to the scope but didn't add to the tool. Jon, please ensure this is what you wanted.

--- src/java/org/tigris/scarab/tools/ScarabGlobalTool.java
+++ src/java/org/tigris/scarab/tools/ScarabGlobalTool.java
@@ -51,6 +51,7 @@ import java.util.ArrayList;
 
 import org.apache.fulcrum.security.TurbineSecurity;
 import org.apache.fulcrum.security.entity.Group;
+import org.apache.fulcrum.security.entity.Permission;
 import org.apache.fulcrum.security.entity.Role;
 import org.apache.fulcrum.security.entity.User;
 import org.apache.fulcrum.security.util.AccessControlList;
@@ -79,7 +80,7 @@ import org.apache.torque.util.Criteria;
  * methodology</a> to be implemented.
  *
  * @author <a href="mailto:jon@collab.net">Jon S. Stevens</a>
- * @version $Id: ScarabGlobalTool.java,v 1.11 2001/11/27 23:30:22 jon Exp $
+ * @version $Id: ScarabGlobalTool.java,v 1.12 2001/11/28 18:46:50 dr Exp $
  */
 public class ScarabGlobalTool implements ScarabGlobalScope
 {
@@ -120,6 +121,18 @@ public class ScarabGlobalTool implements ScarabGlobalScope
     {
         return constant;
     }
+    
+    /**
+     * This is used to get the format for a date
+     * right now, it returns "M/d/yy". In the future, we 
+     * can write code to return the correct date based on
+     * Localization needs.
+     */
+    public String getDateFormat()
+    {
+        return ("M/d/yy");
+    }
+
     
     /**
      * holds the Scarab permission constants.  It will be available to 
@@ -283,7 +296,7 @@ public class ScarabGlobalTool implements ScarabGlobalScope
     }
     
     /** 
-     * Gets a list of all Group's
+     * Gets a list of all Groups
      */
     public Group[] getGroups() throws Exception
     {
@@ -291,7 +304,15 @@ public class ScarabGlobalTool implements ScarabGlobalScope
     }
     
     /** 
-     * Gets a list of all Roles's.
+     * Gets a list of all Permissions
+     */
+    public Permission[] getPermissions() throws Exception
+    {
+        return (TurbineSecurity.getAllPermissions().getPermissionsArray());
+    }
+
+    /** 
+     * Gets a list of all Roles.
      */
     public Role[] getRoles() throws Exception
     {
@@ -306,3 +327,4 @@ public class ScarabGlobalTool implements ScarabGlobalScope
         return TurbineSecurity.getACL(user);
     }    
 }
+
