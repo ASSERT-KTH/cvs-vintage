607a8cfe17f2dbc252b27ad599e63a233f7cb90a
jon <jon>
2001-11-30 20:47:21 +0000

    the start of the processing filter...checking it in so that it doesn't get lost. more testing and work needs to be done on it (as well as turbine)...

--- src/java/org/tigris/scarab/util/ReferenceInsertionFilter.java
+++ src/java/org/tigris/scarab/util/ReferenceInsertionFilter.java
@@ -1,16 +1,115 @@
 package org.tigris.scarab.util;
 
+/* ================================================================
+ * Copyright (c) 2000-2001 CollabNet.  All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 
+ * 3. The end-user documentation included with the redistribution, if
+ * any, must include the following acknowlegement: "This product includes
+ * software developed by Collab.Net <http://www.Collab.Net/>."
+ * Alternately, this acknowlegement may appear in the software itself, if
+ * and wherever such third-party acknowlegements normally appear.
+ * 
+ * 4. The hosted project names must not be used to endorse or promote
+ * products derived from this software without prior written
+ * permission. For written permission, please contact info@collab.net.
+ * 
+ * 5. Products derived from this software may not use the "Tigris" or 
+ * "Scarab" names nor may "Tigris" or "Scarab" appear in their names without 
+ * prior written permission of Collab.Net.
+ * 
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL COLLAB.NET OR ITS CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ====================================================================
+ * 
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of Collab.Net.
+ */ 
+
 import org.apache.velocity.app.event.ReferenceInsertionEventHandler;
 
+/**
+ * This is a Velocity EventCartridge Filter which is responsible
+ * for processing $ variables when they are rendered in a template.
+ * The current purpose of this filter is to process out CSS 
+ * (cross site scripting) vulnerabilities.
+ *
+ * More work needs to be done to make it more efficient as well as to
+ * only filter references that need to be filtered. But it is a start.
+ *
+ * @author <a href="mailto:jon@collab.net">Jon S. Stevens</a>
+ * @version $Id: ReferenceInsertionFilter.java,v 1.2 2001/11/30 20:47:21 jon Exp $
+ */
 public class ReferenceInsertionFilter
     implements ReferenceInsertionEventHandler
 {
     public Object referenceInsert(String reference, Object value)
     {
+//    System.out.println ("reference: " + reference);
         if (value instanceof String)
         {
-            System.out.println (value);
+            if (!reference.startsWith("$renderer") && 
+                !reference.startsWith("$intake"))
+            {
+                return filter((String)value);
+            }
         }
         return value;
+    }
+
+    /**
+     * This method is borrowed from Struts. It converts
+     * &lt; &gt; &amp; &quot; into the appropriate entities.
+     */
+    private static String filter(String value)
+    {
+        if (value == null)
+        {
+            return (null);
+        }
+        char content[] = new char[value.length()];
+        value.getChars(0, value.length(), content, 0);
+        StringBuffer result = new StringBuffer(content.length + 50);
+        for (int i = 0; i < content.length; i++)
+        {
+            switch (content[i])
+            {
+                case '<':
+                    result.append("&lt;");
+                    break;
+                case '>':
+                    result.append("&gt;");
+                    break;
+                case '&':
+                    result.append("&amp;");
+                    break;
+                case '"':
+                    result.append("&quot;");
+                    break;
+                default:
+                    result.append(content[i]);
+            }
+        }
+        return (result.toString());
     }
 }
