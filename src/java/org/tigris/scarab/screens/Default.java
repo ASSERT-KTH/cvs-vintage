6e5f1a4fe6c476a6d48f52de041d8b652f9a07dd
jon <jon>
2001-12-07 20:18:45 +0000

    only get the issueid if we really need to

--- src/java/org/tigris/scarab/screens/Default.java
+++ src/java/org/tigris/scarab/screens/Default.java
@@ -70,7 +70,7 @@ import org.tigris.scarab.om.IssueType;
  * duplication of code.
  *
  * @author <a href="mailto:jon@collab.net">Jon S. Stevens</a>
- * @version $Id: Default.java,v 1.34 2001/12/05 23:52:33 elicia Exp $
+ * @version $Id: Default.java,v 1.35 2001/12/07 20:18:45 jon Exp $
  */
 public class Default extends TemplateSecureScreen
 {
@@ -111,7 +111,6 @@ public class Default extends TemplateSecureScreen
 
             ModuleEntity currentModule = scarabR.getCurrentModule();
             IssueType currentIssueType = scarabR.getCurrentIssueType();
-            String issueId = data.getParameters().getString("id");
             ScarabUser user = (ScarabUser)data.getUser();
             if (perm != null)
             {
@@ -132,8 +131,9 @@ public class Default extends TemplateSecureScreen
                     setTargetSelectModule(data);
                     return false;
                 }
-                else if (currentIssueType == null && issueId == null
-                          && template.indexOf("admin") == -1)
+                else if (currentIssueType == null 
+                         && data.getParameters().getString("id") == null
+                         && template.indexOf("admin") == -1)
                 {
                     data.setMessage("Please select the Artifact Type " +
                                     "that you would like to work " +
