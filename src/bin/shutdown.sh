406f00b5b315c16654798ed05a8a1e1c8dd5eca0
user57 <user57>
2001-12-09 05:21:56 +0000

    o helper scripts to call org.jboss.Shutdown

---
+++ src/bin/shutdown.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+### ====================================================================== ###
+##                                                                          ##
+##  JBoss Shutdown Script                                                   ##
+##                                                                          ##
+### ====================================================================== ###
+
+### $Id: shutdown.sh,v 1.1 2001/12/09 05:21:56 user57 Exp $ ###
+
+DIRNAME=`dirname $0`
+PROGNAME=`basename $0`
+GREP="grep"
+
+#
+# Helper to complain.
+#
+die() {
+    echo "${PROGNAME}: $*"
+    exit 1
+}
+
+# Setup JBOSS_HOME
+if [ "x$JBOSS_HOME" = "x" ]; then
+    JBOSS_HOME=`cd $DIRNAME/..; pwd`
+fi
+export JBOSS_HOME
+
+# Setup the JVM
+if [ "x$JAVA_HOME" != "x" ]; then
+    JAVA=$JAVA_HOME/bin/java
+else
+    JAVA="java"
+fi
+
+# Setup the classpath
+JBOSS_BOOT_CLASSPATH="$JBOSS_HOME/bin/shutdown.jar"
+if [ "x$JBOSS_CLASSPATH" = "x" ]; then
+    JBOSS_CLASSPATH="$JBOSS_BOOT_CLASSPATH"
+else
+    JBOSS_CLASSPATH="$JBOSS_CLASSPATH:$JBOSS_BOOT_CLASSPATH"
+fi
+
+# Execute the JVM
+exec $JAVA \
+    $JAVA_OPTS \
+    -classpath $JBOSS_CLASSPATH \
+    org.jboss.Shutdown "$@"
