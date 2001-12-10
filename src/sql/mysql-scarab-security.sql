88cab7c2f9968bf2fce8b72862a1b0cdd769f973
dr <dr>
2001-12-10 18:22:13 +0000

    added "Domain | Admin" to the "root"r rol

--- src/sql/mysql-scarab-security.sql
+++ src/sql/mysql-scarab-security.sql
@@ -200,7 +200,9 @@ insert into TURBINE_ROLE_PERMISSION (ROLE_ID, PERMISSION_ID)
        select  TURBINE_ROLE.ROLE_ID, TURBINE_PERMISSION.PERMISSION_ID
          from  TURBINE_ROLE, TURBINE_PERMISSION
          where TURBINE_ROLE.ROLE_NAME = "Root"
-           and TURBINE_PERMISSION.PERMISSION_NAME = "Domain | Edit"
+           and TURBINE_PERMISSION.PERMISSION_NAME in (
+                "Domain | Admin",
+                "Domain | Edit")
 ;
 
 
