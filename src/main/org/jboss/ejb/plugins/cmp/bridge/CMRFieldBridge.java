eb16112b413c69b40dfd97b6ad28c1dd952531a0
dsundstrom <dsundstrom>
2001-12-12 19:41:34 +0000

    Remove the commented out getFieldType method. Added isReadOnly method. Fixed some comments.

--- src/main/org/jboss/ejb/plugins/cmp/bridge/CMRFieldBridge.java
+++ src/main/org/jboss/ejb/plugins/cmp/bridge/CMRFieldBridge.java
@@ -20,12 +20,32 @@ import org.jboss.ejb.EntityEnterpriseContext;
  * one per relationship (shared between two beans).      
  *
  * @author <a href="mailto:dain@daingroup.com">Dain Sundstrom</a>
- * @version $Revision: 1.4 $
+ * @version $Revision: 1.5 $
  */                            
 public interface CMRFieldBridge {
+   /**
+    * Gets the name of this cmr.
+    * @return the name of this field
+    */
    public String getFieldName();
-//   public Class getFieldType();
       
+   /**
+    * Is this field read only.
+    * @return true if this field is read only
+    */ 
+   public boolean isReadOnly();
+ 
+   /**
+    * Gets the value of this field for the specified context.
+    * @param ctx the context for which this field's value should be fetched
+    * @return the value of this field
+    */
    public Object getValue(EntityEnterpriseContext ctx);
+      
+   /**
+    * Sets the value of this field for the specified context.
+    * @param ctx the context for which this field's value should be set
+    * @param value the new value of this field
+    */
    public void setValue(EntityEnterpriseContext ctx, Object value);
 }
