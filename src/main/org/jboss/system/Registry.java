23e171c987dfa53a2f3c3a8b4b5501464b3cb9ac
mnf999 <mnf999>
2001-12-19 07:00:50 +0000

    A simple registry to bypass the lack of registry publicly browsable in JMX

---
+++ src/main/org/jboss/system/Registry.java
@@ -0,0 +1,34 @@
+/*
+* JBoss, the OpenSource J2EE webOS
+*
+* Distributable under LGPL license.
+* See terms of license at gnu.org.
+*/
+package org.jboss.system;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Collections;
+
+
+/**
+*  A registry, really, a registry.
+*
+*  All methods static to lookup pointers from anyplace in the VM.  We use it for hooking up JMX
+*  managed objects.  Use the JMX MBeanName to put objects here.
+*  
+*  @author <a href="mailto:marc.fleury@jboss.org>Marc Fleury</a>
+*
+*  @version $Revision: 1.1 $
+*/
+public class Registry
+{
+
+   public static Map entries = Collections.synchronizedMap(new HashMap());
+   
+   public static void bind(Object key, Object value) { entries.put(key,value);}
+   
+   public static Object unbind(Object key) { return entries.remove(key); }
+   
+   public static Object lookup(Object key) { return entries.get(key); }
+}
