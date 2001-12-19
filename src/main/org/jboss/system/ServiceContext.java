ddaa2b4609c7874e9c02511cb83a59c613502536
mnf999 <mnf999>
2001-12-19 07:03:58 +0000

    the context information for a service (who depends onme etc etc)

---
+++ src/main/org/jboss/system/ServiceContext.java
@@ -0,0 +1,58 @@
+/*
+ * JBoss, the OpenSource J2EE webOS
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package org.jboss.system;
+
+import javax.management.ObjectName;
+import java.util.List;
+import java.util.LinkedList;
+
+/**
+ * ServiceContext holds information for the Service
+ *
+ * @see Service
+ * @see ServiceMBeanSupport
+ * 
+ * @author <a href="mailto:andreas@jboss.org">Andreas Schaefer</a>
+ * @version $Revision: 1.1 $
+ *   
+ * <p><b>Revisions:</b>
+ *
+ * <p><b>20011219 marc fleury:</b>
+ * <ul>
+ * <li> initial check in.
+ * </ul>
+ */
+
+public class ServiceContext
+{
+   
+   
+   public static int INSTALLED = 0;
+   public static int CONFIGURED = 1;
+   public static int CREATED = 2;
+   public static int RUNNING = 3;
+   public static int FAILED = 4;
+   public static int STOPPED = 5;
+   public static int DESTROYED = 6;
+   public static int NOTYETINSTALLED = 7;
+   
+   /** The name of the service **/
+   public ObjectName objectName;
+   
+   /** State of the service **/
+   public int state = NOTYETINSTALLED;
+   
+   /** dependent beans **/
+   public List iDependOn = new LinkedList();
+   
+   /** beans that depend on me **/
+   public List dependsOnMe = new LinkedList();
+   
+   /** the fancy proxy to my service calls **/
+   public Service proxy;
+   
+}
