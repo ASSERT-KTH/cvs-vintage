fa353f8eccc4b8c1f032b9db18d32d44cd785ef8
elicia <elicia>
2002-01-04 23:16:17 +0000

    commented out code that prevents attribute options with descendants from showing up in list.

--- src/java/org/tigris/scarab/services/module/AbstractScarabModule.java
+++ src/java/org/tigris/scarab/services/module/AbstractScarabModule.java
@@ -113,7 +113,7 @@ import org.apache.turbine.Log;
  *
  * @author <a href="mailto:jon@collab.net">Jon S. Stevens</a>
  * @author <a href="mailto:jmcnally@collab.net">John McNally</a>
- * @version $Id: AbstractScarabModule.java,v 1.6 2001/12/20 01:53:58 elicia Exp $
+ * @version $Id: AbstractScarabModule.java,v 1.7 2002/01/04 23:16:17 elicia Exp $
  */
 public abstract class AbstractScarabModule
     extends BaseObject
@@ -929,6 +929,7 @@ public abstract class AbstractScarabModule
             optionsMap.put(rmo.getOptionId(), null);
         }
 
+/*
         // remove options with descendants in the list
         for ( int i=rModOpts.size()-1; i>=0; i-- )
         {
@@ -949,7 +950,7 @@ public abstract class AbstractScarabModule
                 }
             }
         }
-
+*/
         return rModOpts;
     }
 
