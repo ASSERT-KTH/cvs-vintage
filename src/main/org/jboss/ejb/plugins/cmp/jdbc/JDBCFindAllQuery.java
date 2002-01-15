8e2d70048fb75bc0094331493ef008a71ec4fd4b
dsundstrom <dsundstrom>
2002-01-15 21:45:17 +0000

    Added support for read ahead on find.

--- src/main/org/jboss/ejb/plugins/cmp/jdbc/JDBCFindAllQuery.java
+++ src/main/org/jboss/ejb/plugins/cmp/jdbc/JDBCFindAllQuery.java
@@ -7,7 +7,12 @@
 
 package org.jboss.ejb.plugins.cmp.jdbc;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.jboss.ejb.plugins.cmp.jdbc.bridge.JDBCEntityBridge;
 import org.jboss.ejb.plugins.cmp.jdbc.metadata.JDBCQueryMetaData;
+import org.jboss.ejb.plugins.cmp.jdbc.metadata.JDBCReadAheadMetaData;
+
 /**
  * JDBCFindAllQuery automatic finder used in CMP 1.x.
  *
@@ -16,17 +21,31 @@ import org.jboss.ejb.plugins.cmp.jdbc.metadata.JDBCQueryMetaData;
  * @author <a href="mailto:marc.fleury@telkel.com">Marc Fleury</a>
  * @author <a href="mailto:shevlandj@kpi.com.au">Joe Shevland</a>
  * @author <a href="mailto:justin@j-m-f.demon.co.uk">Justin Forder</a>
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class JDBCFindAllQuery extends JDBCAbstractQueryCommand {
    
    public JDBCFindAllQuery(JDBCStoreManager manager, JDBCQueryMetaData q) {
       super(manager, q);
+
+      JDBCEntityBridge entity = manager.getEntityBridge();
+
+      // set the preload fields
+      JDBCReadAheadMetaData readAhead = q.getReadAhead();
+      if(!readAhead.isOnFind()) {
+         String eagerLoadGroupName = readAhead.getEagerLoadGroup();
+         setPreloadFields(entity.getLoadGroup(eagerLoadGroupName));
+      }
+
+      // get a list of all fields to be loaded
+      List loadFields = new ArrayList();
+      loadFields.addAll(entity.getPrimaryKeyFields());
+      loadFields.addAll(getPreloadFields());
       
+      // generate the sql
       StringBuffer sql = new StringBuffer();
-      sql.append("SELECT ").append(SQLUtil.getColumnNamesClause(
-               manager.getEntityBridge().getJDBCPrimaryKeyFields()));
-      sql.append(" FROM ").append(manager.getEntityBridge().getTableName());
+      sql.append("SELECT ").append(SQLUtil.getColumnNamesClause(loadFields));
+      sql.append(" FROM ").append(entity.getTableName());
       
       setSQL(sql.toString());
    }
