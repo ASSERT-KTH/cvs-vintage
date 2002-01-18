a33eee1e408df9c7878de2f82c008d262301fb81
jmcnally <jmcnally>
2002-01-18 17:30:27 +0000

    include group info with the rest of report data.
    Issue number:
    Obtained from:
    Submitted by:
    Reviewed by:

--- src/java/org/tigris/scarab/tools/ScarabRequestTool.java
+++ src/java/org/tigris/scarab/tools/ScarabRequestTool.java
@@ -1292,7 +1292,8 @@ try{
         for (int i =0; i<keys.length; i++)
         {
             String key = keys[i].toString();
-            if (key.startsWith("rep") || key.startsWith("intake"))
+            if ( key.startsWith("rep") || key.startsWith("intake")
+                 || key.startsWith("ofg") )
             {
                 String[] values = params.getStrings(key);
                 for (int j=0; j<values.length; j++)
