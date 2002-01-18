ba51febb166e9e4a79cba01a9ce700c3a0993b15
jon <jon>
2002-01-18 17:54:19 +0000

    removed fixme from build.properties (it is ok to have encrypted pass)
    cleaned up nightly.sh indentation
    
    build.xml changes for the SQL changes
    Issue number:
    Obtained from:
    Submitted by:
    Reviewed by:

--- build/nightly.sh
+++ build/nightly.sh
@@ -10,9 +10,9 @@ StopTomcat()
 echo "---------------------------------"
 cd ${DIR}
 if [ -d ${SCARAB}/target/bin ] ; then
-    echo "Stopping Tomcat..."
-    cd ${SCARAB}/target
-    ./bin/catalina.sh stop
+  echo "Stopping Tomcat..."
+  cd ${SCARAB}/target
+  ./bin/catalina.sh stop
 fi
 sleep 5
 echo "---------------------------------"
@@ -23,9 +23,9 @@ StartTomcat()
 echo "---------------------------------"
 cd ${DIR}
 if [ -d ${SCARAB}/target/bin ] ; then
-    echo "Starting Tomcat..."
-    cd ${SCARAB}/target
-    ./bin/catalina.sh start
+  echo "Starting Tomcat..."
+  cd ${SCARAB}/target
+  ./bin/catalina.sh start
 fi
 sleep 5
 echo "---------------------------------"
@@ -37,16 +37,17 @@ date
 TURBINE="../../jakarta-turbine-3"
 TORQUE="../../jakarta-turbine-torque"
 SCARAB=".."
+
 CVSUPDATE=1
 DEPBUILD=0
 
 case "$OSTYPE" in
-    darwin*)
-        DARWIN=1
-    ;;
-    *)
-        DARWIN=0
-    ;;
+  darwin*)
+    DARWIN=1
+  ;;
+  *)
+    DARWIN=0
+  ;;
 esac
 
 DIR=`pwd`
@@ -55,18 +56,18 @@ WGET=/usr/bin/wget
 
 ## Environment variables
 if [ -z ${JAVA_HOME} ] ; then
-    if [ ${DARWIN} -gt 0 ] ; then
-        JAVA_HOME="/System/Library/Frameworks/JavaVM.framework/Home"
-    elif [ ${OSTYPE} = "linux" ] ; then
-        JAVA_HOME="/usr/local/java"
-    fi
-    export JAVA_HOME
+  if [ ${DARWIN} -gt 0 ] ; then
+    JAVA_HOME="/System/Library/Frameworks/JavaVM.framework/Home"
+  elif [ ${OSTYPE} = "linux" ] ; then
+    JAVA_HOME="/usr/local/java"
+  fi
+  export JAVA_HOME
 fi
 echo "JAVA_HOME: ${JAVA_HOME}"
 
 if [ -z ${ANT_HOME} ] ; then
-    ANT_HOME="/usr/local/ant"
-    export ANT_HOME
+  ANT_HOME="/usr/local/ant"
+  export ANT_HOME
 fi
 PATH=${PATH}:/usr/local/bin:${ANT_HOME}/bin:${MYSQL}
 export PATH
@@ -75,55 +76,55 @@ echo "Path: $PATH"
 ## Kill Catalina
 cd ${DIR}
 if [ -x ${SCARAB}/target/bin/catalina.sh ] ; then
-    echo "Killing Catalina..."
-    StopTomcat
+  echo "Killing Catalina..."
+  StopTomcat
 else
-    echo "Target directory doesn't exist. No need to kill Catalina..."
+  echo "Target directory doesn't exist. No need to kill Catalina..."
 fi
 
 ## Clean up old builds
 cd ${DIR}
 if [ -d ${SCARAB}/target ] ; then
-    echo "Removing Scarab ${SCARAB}/target directory..."
-    cd ${DIR}; cd ${SCARAB}
-    rm -r target
+  echo "Removing Scarab ${SCARAB}/target directory..."
+  cd ${DIR}; cd ${SCARAB}
+  rm -r target
 else
-    echo "Scarab target directory does not exist..."    
+  echo "Scarab target directory does not exist..."    
 fi
 
 # remove so we don't have conflicts
 if [ ${CVSUPDATE} -gt 0 ] ; then
-cd ${DIR}
-echo "Removing Torque and Turbine from ${SCARAB}/lib..."
-rm -rf ${SCARAB}/lib/turbine*.jar
-rm -rf ${SCARAB}/lib/torque*.zip
+  cd ${DIR}
+  echo "Removing Torque and Turbine from ${SCARAB}/lib..."
+  rm -rf ${SCARAB}/lib/turbine*.jar
+  rm -rf ${SCARAB}/lib/torque*.zip
 fi
 
 if [ ${DEPBUILD} -gt 0 ] ; then
-cd ${DIR}
-if [ -d ${TURBINE}/bin ] ; then
+  cd ${DIR}
+  if [ -d ${TURBINE}/bin ] ; then
     echo "Removing Turbine ${TURBINE}/bin directory..."
     cd ${DIR}; cd ${TURBINE}
     rm -r bin
-else
+  else
     echo "Turbine bin directory does not exist..."    
-fi
+  fi
 fi
 
 if [ ${DEPBUILD} -gt 0 ] ; then
-cd ${DIR}
-if [ -d ${TORQUE}/bin ] ; then
+  cd ${DIR}
+  if [ -d ${TORQUE}/bin ] ; then
     echo "Removing Torque ${TORQUE}/bin directory..."
     cd ${DIR}; cd ${TORQUE}
     rm -r bin
-else
+  else
     echo "Torque bin directory does not exist..."    
-fi
+  fi
 fi
 
 ## Update things from CVS
 if [ ${CVSUPDATE} -gt 0 ] ; then
-if [ ${DEPBUILD} -gt 0 ] ; then
+  if [ ${DEPBUILD} -gt 0 ] ; then
     echo "Updating Torque From CVS Start..."
     date
     cd ${DIR}; cd ${TORQUE}
@@ -137,44 +138,44 @@ if [ ${DEPBUILD} -gt 0 ] ; then
     cvs update
     echo "Updating Turbine From CVS Finish..."
     date
-fi
+  fi
     echo "Updating Scarab From CVS Start..."
     date
     cd ${DIR}; cd ${SCARAB}
     cvs update
     echo "Updating Scarab From CVS Finish..."
     date
-else
+  else
     echo "Skipping CVS update..."
 fi
 
 if [ ${CVSUPDATE} -gt 0 ] ; then
-if [ ${DEPBUILD} -gt 0 ] ; then
-# remove after the cvs update
-cd ${DIR}
-echo "Removing Torque and Turbine from ${SCARAB}/lib again..."
-rm -rf ${SCARAB}/lib/turbine*.jar
-rm -rf ${SCARAB}/lib/torque*.zip
-fi
+  if [ ${DEPBUILD} -gt 0 ] ; then
+    # remove after the cvs update
+    cd ${DIR}
+    echo "Removing Torque and Turbine from ${SCARAB}/lib again..."
+    rm -rf ${SCARAB}/lib/turbine*.jar
+    rm -rf ${SCARAB}/lib/torque*.zip
+  fi
 fi
 
 ## Build things now
 if [ ${DEPBUILD} -gt 0 ] ; then
-echo "Building Torque Start..."
-cd ${DIR}; cd ${TORQUE}
-ant dist
-echo "Building Torque Finish..."
+  echo "Building Torque Start..."
+  cd ${DIR}; cd ${TORQUE}
+  ant dist
+  echo "Building Torque Finish..."
 
-echo "Building Turbine Start..."
-cd ${DIR}; cd ${TURBINE}/build
-ant jar
-echo "Building Turbine Finish..."
+  echo "Building Turbine Start..."
+  cd ${DIR}; cd ${TURBINE}/build
+  ant jar
+  echo "Building Turbine Finish..."
 fi
 
 echo "Building Scarab Start..."
 cd ${DIR}; cd ${SCARAB}/build
 if [ ${DEPBUILD} -gt 0 ] ; then
-ant upgrade-torque-turbine
+  ant upgrade-torque-turbine
 fi
 ant
 echo "Building Scarab Finish..."
