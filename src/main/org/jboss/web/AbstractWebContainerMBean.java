a7cc3f0b710cc92bf8511462e81e8ee6c72b40fc
mnf999 <mnf999>
2002-01-20 15:23:04 +0000

    moving to the unified deployer

--- src/main/org/jboss/web/AbstractWebContainerMBean.java
+++ src/main/org/jboss/web/AbstractWebContainerMBean.java
@@ -4,18 +4,17 @@ import java.util.Iterator;
 
 import org.w3c.dom.Element;
 
+import org.jboss.deployment.DeployerMBean;
 import org.jboss.deployment.DeploymentException;
 import org.jboss.system.ServiceMBean;
 
 /** A template pattern for web container integration into JBoss.
 
 @author  Scott.Stark@jboss.org
-@version $Revision: 1.6 $
+@version $Revision: 1.7 $
 */
-public interface AbstractWebContainerMBean extends ServiceMBean
+public interface AbstractWebContainerMBean extends DeployerMBean
 {
-    public void deploy(String ctxPath, String warUrl) throws DeploymentException;
-    public void undeploy(String warUrl) throws DeploymentException;
     public boolean isDeployed(String warUrl);
    /** Returns the applications deployed by the container factory
     @return An Iterator of WebApplication objects for the deployed wars.
