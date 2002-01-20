73497ab431d6ba7c1ccdc88edf82c2291170a5a4
mnf999 <mnf999>
2002-01-20 03:37:44 +0000

    Unification of classloaders and deployers the Main deployer is the one with the thread and it calls all the others.  The sub-deployers are responsible for registering with this main deployer

---
+++ src/main/org/jboss/deployment/MainDeployerMBean.java
@@ -0,0 +1,66 @@
+/*
+* JBoss, the OpenSource J2EE webOS
+*
+* Distributable under LGPL license.
+* See terms of license at gnu.org.
+*/
+package org.jboss.deployment;
+
+import java.net.URL;
+import javax.management.ObjectName;
+import org.jboss.system.ServiceMBean;
+
+/**
+* This is the interface of the AutoDeployer that is exposed for
+* administration
+*      
+* @see AutoDeployer
+* 
+* @author <a href="mailto:rickard.oberg@telkel.com">Rickard Öberg</a>
+* @author <a href="mailto:toby.allsopp@peace.com">Toby Allsopp</a>
+* @author <a href="mailto:marc.fleury@jboss.org">Marc Fleury</a>
+* @version $Revision: 1.1 $
+*      <p><b>20011223 marc fleury:</b>
+*      <ul> 
+*      <li>add/RemoveURL, added the capacity to dynamically add a URL to watch
+*      <li>add/Removedeployer, dynamically add a new deployer
+*      <li>Changed ObjectName to JBOSs-SYSTEM realm (from EJB realm)
+*      <li>Rewrite
+*      </ul>
+*/
+public interface MainDeployerMBean
+extends ServiceMBean
+{
+   /** The default object name. */
+   String OBJECT_NAME = "JBOSS-SYSTEM:service=MainDeployer";
+   
+   
+   /** individual URLs for dynamically deploying **/
+   void deploy(String URL);
+   void undeploy(String URL);
+   
+   void deploy(DeploymentInfo sdi)
+   throws DeploymentException;
+   
+   public void undeploy(DeploymentInfo sdi)
+   throws DeploymentException;
+   
+   
+   /** Dynamically add directories to scan **/
+   void addDirectory(String url);
+   void removeDirectory(String url);
+   
+   /** Add the capacity to dynamically add a deployer to the list **/
+   void addDeployer(DeployerMBean deployer);
+   void removeDeployer(DeployerMBean deployer);
+   
+   /** get all the watched URLs in this deployment **/
+   String[] getDeployed();
+   
+   /** Get set scan-period**/
+   void setScan( boolean scan);
+   boolean getScan();
+   void setPeriod(int period);
+   int getPeriod();   
+}
+
