f686ffdf50e6d45427626d296f5ef7f56f7ab41c
jmcnally <jmcnally>
2002-01-24 22:24:02 +0000

    moved the check for whether the comment is required to after the attribute values have been set.
    Issue number:
    Obtained from:
    Submitted by:
    Reviewed by:
    Issue number:
    Obtained from:
    Submitted by:
    Reviewed by:

--- src/java/org/tigris/scarab/actions/ReportIssue.java
+++ src/java/org/tigris/scarab/actions/ReportIssue.java
@@ -98,7 +98,7 @@ import org.tigris.scarab.tools.ScarabRequestTool;
  * This class is responsible for report issue forms.
  *
  * @author <a href="mailto:jmcnally@collab.net">John D. McNally</a>
- * @version $Id: ReportIssue.java,v 1.100 2002/01/24 04:18:46 richard Exp $
+ * @version $Id: ReportIssue.java,v 1.101 2002/01/24 22:24:02 jmcnally Exp $
  */
 public class ReportIssue extends RequireLoginFirstAction
 {
@@ -323,15 +323,8 @@ public class ReportIssue extends RequireLoginFirstAction
         IssueType issueType = issue.getIssueType();
         ScarabUser user = (ScarabUser)data.getUser();
         
-        // set any required flags
+        // set any required flags on attribute values
         setRequiredFlags(issue, intake);
-        String summary = issue.getDefaultText();
-        if ( summary.length() == 0 ) 
-        {
-            Group commentGroup = intake.get("Attachment", "_1", false);
-            Field commentField = commentGroup.get("DataAsString");
-            commentField.setRequired(true);
-        }
 
         if (intake.isAllValid())
         {
@@ -362,6 +355,21 @@ public class ReportIssue extends RequireLoginFirstAction
                         return;
                     }
                 }
+        
+                // we need to see that the default text was filled out if necessary.  We can
+                // only do this after setting the attributes above.
+                boolean saveIssue = true;
+                String summary = issue.getDefaultText();
+                if ( summary.length() == 0 ) 
+                {
+                    Group commentGroup = intake.get("Attachment", "_1", false);
+                    Field commentField = commentGroup.get("DataAsString");
+                    commentField.setRequired(true);
+                    saveIssue = false;
+                }
+
+                if (saveIssue) 
+                {                
                 issue.save();
                 
                 List files = issue.getAttachments();
@@ -455,6 +463,7 @@ public class ReportIssue extends RequireLoginFirstAction
                                 " added to module " +
                                 getScarabRequestTool(context)
                                 .getCurrentModule().getName());
+                }
             }
             else 
             {
