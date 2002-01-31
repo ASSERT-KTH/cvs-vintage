94360e0bcdb12bb42dbaf15c320464a1cff4edf2
dlr <dlr>
2002-01-31 01:33:49 +0000

    o Made case consistant.
    o Added the word "column" to drop of DEDUPE column.

--- src/sql/upgrade/mysql-upgrade-1.0b1-1.0b2-1.sql
+++ src/sql/upgrade/mysql-upgrade-1.0b1-1.0b2-1.sql
@@ -3,11 +3,11 @@
  * the SCARAB_MODULE table to the SCARAB_R_MODULE_ISSUE_TYPE table.
  *
  * Created By: Elicia David
- * $Id: mysql-upgrade-1.0b1-1.0b2-1.sql,v 1.2 2002/01/31 01:08:24 jon Exp $
+ * $Id: mysql-upgrade-1.0b1-1.0b2-1.sql,v 1.3 2002/01/31 01:33:49 dlr Exp $
  */
 
-ALTER TABLE SCARAB_R_MODULE_ISSUE_TYPE add column DEDUPE int(1) not null default 1;
+alter table SCARAB_R_MODULE_ISSUE_TYPE add column DEDUPE int(1) not null default 1;
 
-INSERT INTO SCARAB_R_MODULE_ISSUE_TYPE (dedupe) SELECT DEDUPE from SCARAB_MODULE;
+insert into SCARAB_R_MODULE_ISSUE_TYPE (dedupe) select DEDUPE from SCARAB_MODULE;
 
-ALTER TABLE SCARAB_MODULE drop DEDUPE;
+alter table SCARAB_MODULE drop column DEDUPE;
