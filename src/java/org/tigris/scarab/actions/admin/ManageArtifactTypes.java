c3006d036def7d0559222b85a3432d223a4d65f2
elicia <elicia>
2002-02-01 01:24:45 +0000

    made error message in case of adding too many issue types, more specific

--- src/java/org/tigris/scarab/actions/admin/ManageArtifactTypes.java
+++ src/java/org/tigris/scarab/actions/admin/ManageArtifactTypes.java
@@ -72,7 +72,7 @@ import org.tigris.scarab.tools.ScarabRequestTool;
 
 /**
  * @author <a href="mailto:elicia@collab.net">Elicia David</a>
- * @version $Id: ManageArtifactTypes.java,v 1.4 2002/02/01 00:33:00 elicia Exp $
+ * @version $Id: ManageArtifactTypes.java,v 1.5 2002/02/01 01:24:45 elicia Exp $
  */
 public class ManageArtifactTypes extends RequireLoginFirstAction
 {
@@ -105,8 +105,8 @@ public class ManageArtifactTypes extends RequireLoginFirstAction
                 }
                 if (navCount > 5)
                 {
-                   data.setMessage("You cannot select more than 5 to appear "
-                                   +"in the left hand navigation.");
+                   data.setMessage("You cannot select more than 5 issue types "
+                                   + "to appear in the left hand navigation.");
                    return;
                 }
             }
