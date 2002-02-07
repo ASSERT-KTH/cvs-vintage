050a84879c484eff6e74e0c3330946a8fdbcb938
user57 <user57>
2002-02-07 00:46:07 +0000

    o better error reporting for run.bat.  o no change in Server.java, why is it on this list!

--- src/bin/run.bat
+++ src/bin/run.bat
@@ -1,19 +1,47 @@
 @echo off
+rem -------------------------------------------------------------------------
+rem JBoss Bootstrap Script for Win32
+rem -------------------------------------------------------------------------
+
+rem $Id: run.bat,v 1.30 2002/02/07 00:46:07 user57 Exp $
+
+if exist "run.jar" goto FOUND_RUN_JAR
+echo Could not locate run.jar. Please check that you are in the
+echo bin directory when running this script.
+goto END
+
+:FOUND_RUN_JAR
+
+if not "%JAVA_HOME%" == "" goto ADD_TOOLS
+echo JAVA_HOME is not set.  Unexpected results may occur.
+echo Set JAVA_HOME to the directory of your local JDK to avoid this message.
+goto SKIP_TOOLS
+
+:ADD_TOOLS
+
+if exist "%JAVA_HOME%\lib\tools.jar" goto javaHomeOk
+echo Could not locate %JAVA_HOME%\lib\tools.jar. Unexpected results may occur.
+echo Make sure that JAVA_HOME points to a JDK and not a JRE.
+
+:SKIP_TOOLS
+
 @if not "%ECHO%" == ""  echo %ECHO%
 @if "%OS%" == "Windows_NT"  setlocal
 
-REM Include the JDK javac compiler for JSP pages. The default is for a Sun JDK
-REM compatible distribution to which JAVA_HOME points
+rem Include the JDK javac compiler for JSP pages. The default is for a Sun JDK
+rem compatible distribution to which JAVA_HOME points
+
 set JAVAC_JAR=%JAVA_HOME%\lib\tools.jar
 set JBOSS_CLASSPATH=%JBOSS_CLASSPATH%;%JAVAC_JAR%;run.jar
 
-REM Setup JBoss sepecific properties
+rem Setup JBoss sepecific properties
 set JAVA_OPTS=%JAVA_OPTS% -Djboss.boot.loader.name=run.bat
 
-REM JPDA options. Uncomment and modify as appropriate to enable remote debugging.
-REM set JAVA_OPTS=%JAVA_OPTS% -classic -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=y
+rem JPDA options. Uncomment and modify as appropriate to enable remote debugging.
+rem set JAVA_OPTS=%JAVA_OPTS% -classic -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=y
 
 echo JBOSS_CLASSPATH=%JBOSS_CLASSPATH%
 java %JAVA_OPTS% %JAXP% -classpath "%JBOSS_CLASSPATH%" org.jboss.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
 
+:END
 pause
