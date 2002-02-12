8a297bc014afefd9a908f3791bb2d7b62e80ef82
jmcnally <jmcnally>
2002-02-12 06:33:14 +0000

    Allow get Modules methods to throw an exception
    also modified a javadoc and removed some commented out code
    
    Issue number:  8048
    Obtained from:
    Submitted by:
    Reviewed by:

--- src/java/org/tigris/scarab/om/ScarabUser.java
+++ src/java/org/tigris/scarab/om/ScarabUser.java
@@ -61,7 +61,7 @@ import org.tigris.scarab.util.ScarabException;
  * This is an interface which describes what a ScarabUser is...
  *
  * @author <a href="mailto:jon@collab.net">Jon S. Stevens</a>
- * @version $Id: ScarabUser.java,v 1.51 2002/01/18 22:26:07 jon Exp $
+ * @version $Id: ScarabUser.java,v 1.52 2002/02/12 06:33:14 jmcnally Exp $
  */
 public interface ScarabUser extends User
 {
@@ -214,7 +214,8 @@ public interface ScarabUser extends User
      * @param permissions a <code>String</code> value
      * @return a <code>ModuleEntity[]</code> value
      */
-    public ModuleEntity[] getModules(String permission);
+    public ModuleEntity[] getModules(String permission) 
+        throws Exception;
 
     /**
      * Get a list of <code>ModuleEntity</code>'s that where a user has
@@ -224,33 +225,17 @@ public interface ScarabUser extends User
      * @param permissions a <code>String[]</code> value
      * @return a <code>ModuleEntity[]</code> value
      */
-    public ModuleEntity[] getModules(String[] permissions);
-
+    public ModuleEntity[] getModules(String[] permissions) 
+        throws Exception;
 
     /**
-     * Determine whether the user is associated with the given module
-     * through one or more roles.
+     * Determine whether the user is associated with the given module.
+     * This translates to a check whether the user has any permissions within
+     * the module.
      *
      * @param module a <code>ModuleEntity</code> value
      * @return a <code>boolean</code> value
      */
     public boolean hasAnyRoleIn(ModuleEntity module)
         throws Exception;
-
-
-    /* * not used
-     * !FIXME! need to define a Role interface (maybe the one in fulcrum is 
-     * sufficient?) before making a method like this public.   
-     * Right now it is only used in one place to determine
-     * if the user has any roles available, so we will use a more specific
-     * public method for that.
-     *
-     * @param module a <code>ModuleEntity</code> value
-     * @return a <code>List</code> value
-     * @exception Exception if an error occurs
-     * /
-    public List getRoles(ModuleEntity module)
-        throws Exception;
-    */
-
 }
