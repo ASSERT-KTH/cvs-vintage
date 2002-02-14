759cca5fb97c11b0298ca770e7f50f6f0959f704
jmcnally <jmcnally>
2002-02-14 00:36:02 +0000

    Removed a couple more tables that implemented an index system.  This code was not maintained since lucene was adopted and it is unlikely it still worked.
    Issue number:
    Obtained from:
    Submitted by:
    Reviewed by:

--- src/sql/upgrade/mysql-upgrade-1.0b3-1.0b4-1.sql
+++ src/sql/upgrade/mysql-upgrade-1.0b3-1.0b4-1.sql
@@ -1,11 +1,11 @@
 /*
- * This upgrade script removes the SCARAB_ISSUE_ATTRIBUTE_VOTE table.
- * The table was not used by the application, so there should be no
- * data related changes.
+ * This upgrade script removes a few tables that were not used by the 
+ * application, so there should be no data related changes.
  *
  * Created By: John McNally
- * $Id: mysql-upgrade-1.0b3-1.0b4-1.sql,v 1.1 2002/02/13 23:27:01 jmcnally Exp $
+ * $Id: mysql-upgrade-1.0b3-1.0b4-1.sql,v 1.2 2002/02/14 00:36:03 jmcnally Exp $
  */
 
 drop table if exists SCARAB_ISSUE_ATTRIBUTE_VOTE;
-
+drop table if exists SCARAB_R_ATTRIBUTE_VALUE_WORD;
+drop table if exists SCARAB_WORD;
