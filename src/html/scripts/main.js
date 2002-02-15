acc9a382fb03b742bdf63f625cf5c3770c072ea6
tfahrner <tfahrner>
2002-02-15 02:06:15 +0000

    allow for independent targeting of multiple popup types; ported from sc

--- src/html/scripts/main.js
+++ src/html/scripts/main.js
@@ -13,16 +13,18 @@ atts -- optional -- if the window you wish to create is unique and you do
 	directly to the function with the atts parameter
 */
 
+var w = 0;
 function launch(theURL, type, atts){
-	var theName = 'SourceCast';
+	w+=1;
+	var theName = 'Scarab'+type;
+	if (atts) theName += w
 	var theAttributes;
 	if (type==1) {
 		theAttributes = 'resizable=yes,left=10,top=10,screenX=12,screenY=12,height=485,width=724,status=yes,scrollbars=yes,toolbar=yes,menubar=yes,location=yes'
 	} else if (type==2) {
 		theAttributes = 'resizable=yes,left=10,top=10,screenX=12,screenY=12,height=400,width=400';
-        } else if (type==3) { 
-                theAttributes = 'resizable=yes,left=10,top=10,screenX=12,screenY=12,height=400,width=600,scrollbars=yes'; 
-               
+        } else if (type==3) {
+   theAttributes = 'resizable=yes,left=10,top=10,screenX=12,screenY=12,height=400,width=600,scrollbars=yes'; 
 	} else if (type==4) {
 		theAttributes = 'whatever you want';
 		}
