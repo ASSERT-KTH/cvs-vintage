35e12e4e8d1b2958b84a46960de61e1d5c4036c6
user57 <user57>
2002-02-16 07:16:16 +0000

    o re-indent + drop redunant public modifiers

--- src/main/org/jboss/ejb/plugins/EnterpriseContextCachePolicy.java
+++ src/main/org/jboss/ejb/plugins/EnterpriseContextCachePolicy.java
@@ -4,6 +4,7 @@
  * Distributable under LGPL license.
  * See terms of license at gnu.org.
  */
+
 package org.jboss.ejb.plugins;
 
 import org.jboss.logging.Logger;
@@ -17,53 +18,51 @@ import org.jboss.util.CachePolicy;
  * a MRU one, or any other suitable policy.
  * 
  * @see AbstractInstanceCache
+ * 
  * @author <a href="mailto:simone.bordet@compaq.com">Simone Bordet</a>
- * @version $Revision: 1.9 $
+ * @version $Revision: 1.10 $
  */
-public interface EnterpriseContextCachePolicy extends CachePolicy
+public interface EnterpriseContextCachePolicy
+   extends CachePolicy
 {
-	// Constants -----------------------------------------------------
-
-	// Static --------------------------------------------------------
-	public static Logger log = Logger.getLogger(EnterpriseContextCachePolicy.class);
-	public static Scheduler scheduler = new Scheduler();
+   Logger log = Logger.getLogger(EnterpriseContextCachePolicy.class);
+   Scheduler scheduler = new Scheduler();
 	
-	// Public --------------------------------------------------------
-	/**
-	 * Final class that hold a single instance of a {@link TimerQueue} object
-	 * for all the CachePolicy instances, allowing the cache policies to 
-	 * register {@link TimerTask}s that will be executed in a single background
-	 * thread. <br>
-	 * Usage:
-	 * <pre>
-	 * TimerTask task = new TimerTask() 
-	 * {
-	 * 		public void execute() throws Exception 
-	 *		{
-	 *			doSomething();
-	 *		}
-	 * };
-	 * scheduler.schedule(task);
-	 * </pre>
-	 * 
-	 * @see TimerQueue
-	 */
-	public static final class Scheduler
-	{
-		private static final TimerQueue m_scheduler = new TimerQueue();
-		static
-		{
-			m_scheduler.start();
-			log.info("Cache policy scheduler started");
-		}
-		public final void schedule(TimerTask t) 
-		{
-			schedule(t, 0);
-		}
-		public final void schedule(TimerTask t, long delay) 
-		{
-			m_scheduler.schedule(t, delay);
-		}
-	}
+   /**
+    * Final class that hold a single instance of a {@link TimerQueue} object
+    * for all the CachePolicy instances, allowing the cache policies to 
+    * register {@link TimerTask}s that will be executed in a single background
+    * thread. <br>
+    * Usage:
+    * <pre>
+    * TimerTask task = new TimerTask() 
+    * {
+    * 		public void execute() throws Exception 
+    *		{
+    *			doSomething();
+    *		}
+    * };
+    * scheduler.schedule(task);
+    * </pre>
+    * 
+    * @see TimerQueue
+    */
+   final class Scheduler
+   {
+      private static final TimerQueue m_scheduler = new TimerQueue();
+      static
+      {
+         m_scheduler.start();
+         log.info("Cache policy scheduler started");
+      }
+      public final void schedule(TimerTask t) 
+      {
+         schedule(t, 0);
+      }
+      public final void schedule(TimerTask t, long delay) 
+      {
+         m_scheduler.schedule(t, delay);
+      }
+   }
 }
 
