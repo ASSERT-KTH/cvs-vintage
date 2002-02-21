e62bb0ffc051d071138a07237003fb050cd5f473
chirino <chirino>
2002-02-21 23:33:19 +0000

    Finished integrating the new Boot class into the build system.

---
+++ src/bin/boot.bat
@@ -0,0 +1,47 @@
+@echo off
+rem -------------------------------------------------------------------------
+rem JBoss Bootstrap Script for Win32
+rem -------------------------------------------------------------------------
+
+rem $Id: boot.bat,v 1.1 2002/02/21 23:33:19 chirino Exp $
+
+if exist "run.jar" goto FOUND_RUN_JAR
+echo Could not locate run.jar. Please check that you are in the
+echo bin directory when running this script.
+goto END
+
+:FOUND_RUN_JAR
+
+if not "%JAVA_HOME%" == "" goto ADD_TOOLS
+echo JAVA_HOME is not set.  Unexpected results may occur.
+echo Set JAVA_HOME to the directory of your local JDK to avoid this message.
+goto SKIP_TOOLS
+
+:ADD_TOOLS
+
+if exist "%JAVA_HOME%\lib\tools.jar" goto SKIP_TOOLS
+echo Could not locate %JAVA_HOME%\lib\tools.jar. Unexpected results may occur.
+echo Make sure that JAVA_HOME points to a JDK and not a JRE.
+
+:SKIP_TOOLS
+
+@if not "%ECHO%" == ""  echo %ECHO%
+@if "%OS%" == "Windows_NT"  setlocal
+
+rem Include the JDK javac compiler for JSP pages. The default is for a Sun JDK
+rem compatible distribution to which JAVA_HOME points
+
+set JAVAC_JAR=file:%JAVA_HOME%/lib/tools.jar
+set JBOSS_CLASSPATH=%JBOSS_CLASSPATH%,%JAVAC_JAR%,file:run.jar
+
+rem Setup JBoss sepecific properties
+set JAVA_OPTS=%JAVA_OPTS% -Djboss.boot.loader.name=run.bat
+
+rem JPDA options. Uncomment and modify as appropriate to enable remote debugging.
+rem set JAVA_OPTS=-classic -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=y %JAVA_OPTS%
+
+echo JBOSS_CLASSPATH=%JBOSS_CLASSPATH%
+java %JAVA_OPTS% %JAXP% -classpath boot.jar org.jboss.Boot -cp "%JBOSS_CLASSPATH%" org.jboss.Main %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+:END
+pause
