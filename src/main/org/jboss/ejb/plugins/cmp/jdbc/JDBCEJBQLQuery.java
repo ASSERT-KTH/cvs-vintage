e2de5373edc5acf0140f07356d07f04313cdfd56
dsundstrom <dsundstrom>
2002-02-26 23:47:10 +0000

    Changed to use new JDBCEJBQLCompiler based on JavaCC.

--- src/main/org/jboss/ejb/plugins/cmp/jdbc/JDBCEJBQLQuery.java
+++ src/main/org/jboss/ejb/plugins/cmp/jdbc/JDBCEJBQLQuery.java
@@ -8,12 +8,7 @@
 package org.jboss.ejb.plugins.cmp.jdbc;
 
 import org.jboss.deployment.DeploymentException;
-import org.jboss.ejb.plugins.cmp.ejbql.Assembly;
-import org.jboss.ejb.plugins.cmp.ejbql.Parser;
-import org.jboss.ejb.plugins.cmp.jdbc.bridge.JDBCCMPFieldBridge;
-import org.jboss.ejb.plugins.cmp.jdbc.bridge.JDBCEntityBridge;
-import org.jboss.ejb.plugins.cmp.jdbc.ejbql.EJBQLParser;
-import org.jboss.ejb.plugins.cmp.jdbc.ejbql.SQLTarget;
+import org.jboss.ejb.plugins.cmp.ejbql.Catalog;
 import org.jboss.ejb.plugins.cmp.jdbc.metadata.JDBCQlQueryMetaData;
 import org.jboss.ejb.plugins.cmp.jdbc.metadata.JDBCQueryMetaData;
 
@@ -21,7 +16,7 @@ import org.jboss.ejb.plugins.cmp.jdbc.metadata.JDBCQueryMetaData;
  * This class generates a query from EJB-QL.
  *
  * @author <a href="mailto:dain@daingroup.com">Dain Sundstrom</a>
- * @version $Revision: 1.5 $
+ * @version $Revision: 1.6 $
  */
 public class JDBCEJBQLQuery extends JDBCAbstractQueryCommand {
 
@@ -32,44 +27,37 @@ public class JDBCEJBQLQuery extends JDBCAbstractQueryCommand {
       super(manager, q);
 
       JDBCQlQueryMetaData metadata = (JDBCQlQueryMetaData)q;
-      getLog().debug("EQL-QL: "+metadata.getEjbQl());
-      
-      // get a parser
-      Parser ejbql = new EJBQLParser().ejbqlQuery();
-      
-      // initialize the assembly
-      Assembly a = new Assembly(metadata.getEjbQl());
-      a.setTarget(new SQLTarget(
-            q.getMethod(),
-            manager.getJDBCTypeFactory(),
-            manager.getContainer().getEjbModule(),
-            q.getReadAhead()));
-      
-      // match the query
-      a = ejbql.soleMatch(a);
-      if(a == null) {
-         throw new DeploymentException("Unable to parse EJB-QL: " +
-               metadata.getEjbQl());
+      if(getLog().isDebugEnabled()) {
+         getLog().debug("EJB-QL: " + metadata.getEjbQl());
       }
       
-      // get the final target
-      SQLTarget target = (SQLTarget)a.getTarget();
+      JDBCEJBQLCompiler compiler = new JDBCEJBQLCompiler(
+            (Catalog)manager.getApplicationData("CATALOG"));
+
+      try {
+         compiler.compileEJBQL(
+               metadata.getEjbQl(),
+               metadata.getMethod().getReturnType(),
+               metadata.getMethod().getParameterTypes(),
+               metadata.getReadAhead());
+      } catch(Throwable t) {
+         throw new DeploymentException("Error compiling ejbql", t);
+      }
       
       // set the sql
-      setSQL(target.toSQL());
-      
-      // select bridge object
-      Object selectBridgeObject = target.getSelectObject();
-      if(selectBridgeObject instanceof JDBCEntityBridge) {
-         setSelectEntity((JDBCEntityBridge)selectBridgeObject);
-      } else if(selectBridgeObject instanceof JDBCCMPFieldBridge) {
-         setSelectField((JDBCCMPFieldBridge)selectBridgeObject);
-      } else {
-         throw new IllegalStateException("Select bridge object is instance " +
-               "of unknown type: selectBridgeObject=" + selectBridgeObject);
+      if(getLog().isDebugEnabled()) {
+         getLog().debug("SQL:\r\n" + compiler.getSQL());
       }
+      setSQL(compiler.getSQL());
       
+      // set select object
+      if(compiler.isSelectEntity()) {
+         setSelectEntity(compiler.getSelectEntity());
+      } else {
+         setSelectField(compiler.getSelectField());
+      }
+
       // get the parameter order
-      setParameterList(target.getInputParameters());
+      setParameterList(compiler.getInputParameters());
    }
 }
