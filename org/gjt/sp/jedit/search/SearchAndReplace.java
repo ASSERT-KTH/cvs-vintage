0f75047d19ebe3bba492b2b4be8760f0461f90b7
spestov <spestov>
2002-03-09 08:33:37 +0000

    fixed notorious replace all bug

--- org/gjt/sp/jedit/search/SearchAndReplace.java
+++ org/gjt/sp/jedit/search/SearchAndReplace.java
@@ -38,7 +38,7 @@ import org.gjt.sp.util.Log;
  * Class that implements regular expression and literal search within
  * jEdit buffers.
  * @author Slava Pestov
- * @version $Id: SearchAndReplace.java,v 1.23 2002/03/07 10:03:25 spestov Exp $
+ * @version $Id: SearchAndReplace.java,v 1.24 2002/03/09 08:33:37 spestov Exp $
  */
 public class SearchAndReplace
 {
@@ -947,15 +947,21 @@ loop:			while(path != null)
 		boolean smartCaseReplace = (TextUtilities.getStringCase(replace)
 			== TextUtilities.LOWER_CASE);
 
+		boolean endOfLine = (buffer.getLineEndOffset(
+			buffer.getLineOfOffset(end)) - 1 == end);
+
 		Segment text = new Segment();
 		int offset = start;
 loop:		for(int counter = 0; ; counter++)
 		{
 			buffer.getText(offset,end - offset,text);
+
+			boolean startOfLine = (buffer.getLineStartOffset(
+				buffer.getLineOfOffset(offset)) == offset);
+
 			int[] occur = matcher.nextMatch(
 				new CharIndexedSegment(text,false),
-				start == 0,end == buffer.getLength(),
-				counter == 0);
+				startOfLine,endOfLine,counter == 0);
 			if(occur == null)
 				break loop;
 			int _start = occur[0];
