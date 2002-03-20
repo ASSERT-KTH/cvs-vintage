89e6f36b7643ed446e3f4fed7479ce8d37d83531
jon <jon>
2002-03-20 00:28:10 +0000

    http://scarab.tigris.org/issues/show_bug.cgi?id=92
    -jon

--- src/java/org/tigris/scarab/actions/Register.java
+++ src/java/org/tigris/scarab/actions/Register.java
@@ -46,32 +46,51 @@ package org.tigris.scarab.actions;
  * individuals on behalf of Collab.Net.
  */ 
 
+import java.util.List;
+import java.util.Iterator;
+
 // Turbine Stuff 
-import org.apache.turbine.TemplateContext;
-import org.apache.turbine.RunData;
 import org.apache.turbine.Log;
+import org.apache.turbine.RunData;
+import org.apache.turbine.TemplateContext;
+import org.apache.turbine.Turbine;
+import org.apache.turbine.modules.ContextAdapter;
+import org.apache.turbine.services.pull.ApplicationTool;
 import org.apache.turbine.tool.IntakeTool;
+
+import org.apache.fulcrum.intake.model.Field;
 import org.apache.fulcrum.intake.model.Group;
 import org.apache.fulcrum.security.TurbineSecurity;
+import org.apache.fulcrum.security.entity.Role;
+import org.apache.fulcrum.security.entity.User;
+import org.apache.fulcrum.security.util.TurbineSecurityException;
+import org.apache.fulcrum.template.TemplateEmail;
 
 // Scarab Stuff
 import org.tigris.scarab.om.ScarabUser;
-import org.tigris.scarab.om.ScarabUserImplPeer;
+import org.tigris.scarab.tools.ScarabRequestTool;
 import org.tigris.scarab.util.ScarabConstants;
 import org.tigris.scarab.actions.base.ScarabTemplateAction;
+
+// FIXME: remove the methods that reference this
+import org.tigris.scarab.om.ScarabUserImpl;
+import org.tigris.scarab.om.ScarabUserImplPeer;
+import org.tigris.scarab.om.ScarabModulePeer;
+import org.tigris.scarab.om.Module;
 
 /**
  * This class is responsible for dealing with the Register
  * Action.
  *   
  * @author <a href="mailto:jon@collab.net">Jon S. Stevens</a>
- * @version $Id: Register.java,v 1.17 2002/01/18 22:26:03 jon Exp $
+ * @version $Id: Register.java,v 1.18 2002/03/20 00:28:10 jon Exp $
  */
 public class Register extends ScarabTemplateAction
 {
     /**
-     * This manages clicking the Register button which will end up sending
-     * the user to the RegisterConfirm screen.
+     * This manages clicking the "Register" button in the Register.vm
+     * template. As a result, the user will go to the 
+     * RegisterConfirm.vm screen.
      */
     public void doRegister( RunData data, TemplateContext context ) 
         throws Exception
@@ -155,4 +174,323 @@ public class Register extends ScarabTemplateAction
         }
     }
 
+    public void doConfirmregistration(RunData data, TemplateContext context)
+        throws Exception
+    {
+        String template = getCurrentTemplate(data);
+        String nextTemplate = getNextTemplate(data);
+
+        try
+        {
+            // pull the user object from the session
+            ScarabUser su = (ScarabUser) data.getUser()
+                .getTemp(ScarabConstants.SESSION_REGISTER);
+            if (su == null)
+            {
+                // assign the template to the cancel template, not the 
+                // current template
+                template = getCancelTemplate(data, "Register.vm");
+                throw new Exception(
+                    "Unable to retrive user object from session.");
+            }
+
+            try
+            {
+                // attempt to create a new user!
+                su.createNewUser();
+            }
+            catch (org.apache.fulcrum.security.util.EntityExistsException e)
+            {
+                data.setMessage(e.getMessage());
+                setTarget(data, "Confirm.vm");
+                return;
+            }
+
+            // grab the ScarabRequestTool object so that we can populate the  
+            // User object for redisplay of the form data on the screen
+            ScarabRequestTool scarabR = getScarabRequestTool(context);
+            if (scarabR != null)
+            {
+                scarabR.setUser(su);
+            }
+            
+            // send an email that is for confirming the registration
+            TemplateEmail te = new TemplateEmail();
+            te.setContext(new ContextAdapter(context));
+            te.setTo(su.getFirstName() + " " + su.getLastName(), su.getEmail());
+            te.setFrom(
+                Turbine.getConfiguration()
+                    .getString("scarab.email.register.fromName",
+                                "Scarab System"), 
+                Turbine.getConfiguration()
+                    .getString("scarab.email.register.fromAddress",
+                                "register@scarab.tigris.org"));
+            te.setSubject(
+                Turbine.getConfiguration()
+                    .getString("scarab.email.register.subject",
+                               "Account Confirmation"));
+            te.setTemplate(
+                Turbine.getConfiguration()
+                    .getString("scarab.email.register.template",
+                               "email/Confirmation.vm"));
+            te.send();
+
+            // set the next template on success
+            setTarget(data, nextTemplate);
+        }
+        catch (Exception e)
+        {
+            setTarget(data, template);
+            data.setMessage (e.getMessage());
+            Log.error(e);
+            return;
+        }
+    }
+
+    /**
+     * returns you to Register.vm
+     */
+    public void doBack(RunData data, TemplateContext context) 
+        throws Exception
+    {
+        // set the template to the template that we should be going back to
+        setTarget(data, data.getParameters().getString(
+                ScarabConstants.CANCEL_TEMPLATE, "Register.vm"));
+    }
+
+    /**
+     * calls doRegisterConfirm()
+     */
+    public void doPerform(RunData data, TemplateContext context) 
+        throws Exception
+    {
+        doConfirmregistration(data, context);
+    }
+
+    /**
+     * This manages clicking the Confirm button in the Confirm.vm
+     * template. As a result, this will end up sending
+     * the user to the Confirm screen.
+     */
+    public void doConfirm( RunData data, TemplateContext context ) 
+        throws Exception
+    {
+        String template = getCurrentTemplate(data, null);
+        String nextTemplate = getNextTemplate(data, template);
+
+        IntakeTool intake = getIntakeTool(context);
+        if (intake.isAllValid())
+        {
+            Object user = data
+                            .getUser()
+                            .getTemp(ScarabConstants.SESSION_REGISTER);
+            Group register = null;
+            if (user != null && user instanceof ScarabUser)
+            {
+                register = intake.get("Register", 
+                    ((ScarabUser)user).getQueryKey(), false);
+            }
+            else
+            {
+                register = intake.get("Register",
+                    IntakeTool.DEFAULT_KEY, false);
+            }
+
+            if (register == null)
+            {
+                data.setMessage(
+                    "Register group is null, please report this error.");
+                return;
+            }
+            String username = null;
+            String confirm = null;
+            Field usernameField = register.get("Email");
+            Field confirmField = register.get("Confirm");
+            if (usernameField == null)
+            {
+                data.setMessage(
+                    "Username field is null, please report this error.");
+                return;
+            }
+            else if (confirmField == null)
+            {
+                data.setMessage(
+                    "Confirm field is null, please report this error.");
+                return;
+            }
+            username = usernameField.toString();
+            confirm = confirmField.toString();
+
+            // This reference to ScarabUserImpl is ok because this action
+            // is specific to use with that implementation.
+            if (ScarabUserImpl.checkConfirmationCode(username, confirm))
+            {
+                // update the database to confirm the user
+                if(ScarabUserImpl.confirmUser(username))
+                {
+                    // NO PROBLEMS! :-)
+                    ScarabUser confirmedUser = (ScarabUser)
+                                    TurbineSecurity.getUser(username);
+                    // we set this to false and make people login again
+                    // because of this issue:
+                    // http://scarab.tigris.org/issues/show_bug.cgi?id=115
+                    // there may be a better way, but given that on the confirm
+                    // screen, we aren't asking for a password and checkConfirmationCode
+                    // will return true if someone is already confirmed, 
+                    // we need to do this for security purposes.
+                    confirmedUser.setHasLoggedIn(Boolean.FALSE);
+                    data.setUser(confirmedUser);
+                    data.save();
+    
+                    data.setMessage(
+                        "Your account has been confirmed. Welcome to Scarab! " + 
+                        "Please login now.");
+                    setTarget(data, nextTemplate);
+
+                    // FIXME: Hack to give every new account a Developer Role
+                    // within every Group (ie: Module). This is a major major
+                    // major major major hole. The point however is to allow 
+                    // people using the runbox or downloading scarab a chance 
+                    // to be able to enter an issue without having to muck 
+                    // with Flux to get the right roles. Hopefully someone 
+                    // from the community will contribute code to clean this 
+                    // up. For more information, please read this thread:
+                    // http://scarab.tigris.org/servlets/ReadMsg?msgId=38339&listName=dev
+                    List allModules = ScarabModulePeer.getAllModules();
+                    Iterator itr = allModules.iterator();
+                    Role role = TurbineSecurity.getRole("Developer");
+                    while (itr.hasNext())
+                    {
+                        // have to use the full Group because of conflicts with
+                        // Intake Group
+                        org.apache.fulcrum.security.entity.Group group = 
+                            (org.apache.fulcrum.security.entity.Group) itr.next();
+                        // only give access to the non-global modules
+                        if (!group.getName()
+                            .startsWith(ScarabConstants.GLOBAL_MODULE_NAME + 
+                                Module.NAME_DELIMINATOR))
+                        {
+                            group.grant((User)confirmedUser, role);
+                            ((Module)group).save();
+                        }
+                    }
+                }
+                else
+                {
+                    data.setMessage("Your account has not been confirmed. " + 
+                                    "There has been an error.");
+                    setTarget(data, template);
+                }
+            }
+            else // we don't have confirmation! :-(
+            {
+                data.setMessage("Sorry, that email address and/or confirmation"
+                                + "code is invalid.");
+                setTarget(data, template);
+            }
+        }
+    }
+
+    /**
+     * This manages clicking the "Resend code" button 
+     * in the Confirm.vm template.
+     */
+    public void doResendconfirmationcode(RunData data, TemplateContext context)
+        throws Exception
+    {
+        String template = getCurrentTemplate(data, null);
+        String nextTemplate = getNextTemplate(data, template);
+
+        try
+        {
+            Object user = data
+                            .getUser()
+                            .getTemp(ScarabConstants.SESSION_REGISTER);
+ 
+            IntakeTool intake = getIntakeTool(context);
+            Group register = null;
+            if (user != null && user instanceof ScarabUser)
+            {
+                register = intake.get("Register", 
+                    ((ScarabUser)user).getQueryKey(), false);
+            }
+            else
+            {
+                register = intake.get("Register",
+                    IntakeTool.DEFAULT_KEY, false);
+            }
+        
+            if (register == null)
+            {
+                data.setMessage(
+                    "Register group is null, please report this error.");
+                return;
+            }
+            String username = register.get("Email").toString();
+            try
+            {
+                // Authenticate the user and get the object.
+                user = TurbineSecurity.getUser(username);
+
+                // grab the ScarabRequestTool object so that we can 
+                // populate the User object for redisplay of the form 
+                // data on the screen
+                ScarabRequestTool scarabR = getScarabRequestTool(context);
+                if (scarabR != null)
+                {
+                    scarabR.setUser((ScarabUser) user);
+                }
+            }
+            catch (TurbineSecurityException e)
+            {
+                data.setMessage("Invalid username.");
+                Log.error ("RegisterConfirm: ", e);
+                return;
+            }
+        
+            // send an email that is for confirming the registration
+            sendConfirmationEmail((ScarabUser) user, context);
+            data.setMessage("Confirmation Code sent!");
+
+            // set the next template on success
+            data.getUser().setTemp(ScarabConstants.SESSION_REGISTER, user);
+            intake.remove(register);
+
+            setTarget(data, "Confirm.vm");
+        }
+        catch (Exception e)
+        {
+            setTarget(data, template);
+            data.setMessage (e.getMessage());
+            Log.error(e);
+            return;
+        }
+    }
+
+    /**
+     * Send the confirmation code to the given user.
+     */
+    private void sendConfirmationEmail(ScarabUser su, TemplateContext context)
+        throws Exception
+    {
+        TemplateEmail te = new TemplateEmail();
+        te.setContext(new ContextAdapter(context));
+        te.setTo(su.getFirstName() + " " + su.getLastName(), su.getEmail());
+        te.setFrom(
+            Turbine.getConfiguration()
+                .getString("scarab.email.register.fromName",
+                           "Scarab System"), 
+            Turbine.getConfiguration()
+                .getString("scarab.email.register.fromAddress",
+                           "register@scarab.tigris.org"));
+        te.setSubject(
+            Turbine.getConfiguration()
+                .getString("scarab.email.register.subject",
+                           "Account Confirmation"));
+        te.setTemplate(
+            Turbine.getConfiguration()
+                .getString("scarab.email.register.template",
+                           "email/Confirmation.vm"));
+        te.send();        
+    }
 }
