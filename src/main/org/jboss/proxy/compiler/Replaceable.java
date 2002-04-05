1ba3f013ec8bae44913eec4da305167bc8039897
user57 <user57>
2002-04-05 01:08:36 +0000

    o Added javadoc

--- src/main/org/jboss/proxy/compiler/Replaceable.java
+++ src/main/org/jboss/proxy/compiler/Replaceable.java
@@ -1,16 +1,30 @@
+/*
+ * JBoss, the OpenSource J2EE webOS
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+
 package org.jboss.proxy.compiler;
 
+import java.io.Serializable;
 
-
-import java.io.*;
-
-
-
-public interface Replaceable extends Serializable
-
+/**
+ * ???
+ *      
+ * @author Unknown
+ * @version $Revision: 1.2 $
+ */
+public interface Replaceable 
+   extends Serializable
 {
-
-  public Object writeReplace() throws ObjectStreamException;
-
+   /**
+    * ???
+    *
+    * @return ???
+    *
+    * @throws ObjectStreamException
+    */
+   Object writeReplace() throws ObjectStreamException;
 }
 
