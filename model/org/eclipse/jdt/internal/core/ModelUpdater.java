f95d0c7c05f0c59e729b29b0dbd7c1eb7bca62b1
pmulet <pmulet>
2002-04-22 23:18:39 +0000

    *** empty log message ***

--- model/org/eclipse/jdt/internal/core/ModelUpdater.java
+++ model/org/eclipse/jdt/internal/core/ModelUpdater.java
@@ -1,10 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2001, 2002 International Business Machines Corp. and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v0.5 
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v05.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ ******************************************************************************/
+
 package org.eclipse.jdt.internal.core;
 
-/*
- * (c) Copyright IBM Corp. 2000, 2001.
- * All Rights Reserved.
- */
- 
 import java.util.HashSet;
 import java.util.Iterator;
 
@@ -225,7 +231,9 @@ public class ModelUpdater {
 				elementRemoved(element);
 				break;
 			case IJavaElementDelta.CHANGED :
-				elementChanged(element);
+				if ((delta.getFlags() & IJavaElementDelta.F_CONTENT) != 0){
+					elementChanged(element);
+				}
 				break;
 		}
 		if (processChildren) {
