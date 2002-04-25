b0eb43fa1ac471be2ac95f2d46fc905e07a70621
elicia <elicia>
2002-04-25 22:50:07 +0000

    remove issue type's template type as well when removing issue type from a module.

--- src/java/org/tigris/scarab/actions/admin/ManageArtifactTypes.java
+++ src/java/org/tigris/scarab/actions/admin/ManageArtifactTypes.java
@@ -73,7 +73,7 @@ import org.tigris.scarab.services.cache.ScarabCache;
 
 /**
  * @author <a href="mailto:elicia@collab.net">Elicia David</a>
- * @version $Id: ManageArtifactTypes.java,v 1.11 2002/04/13 02:39:33 jmcnally Exp $
+ * @version $Id: ManageArtifactTypes.java,v 1.12 2002/04/25 22:50:07 elicia Exp $
  */
 public class ManageArtifactTypes extends RequireLoginFirstAction
 {
@@ -204,6 +204,14 @@ public class ManageArtifactTypes extends RequireLoginFirstAction
                             RModuleIssueType rmit = module
                                .getRModuleIssueType(issueType);
                             rmit.delete(user);
+                            // delete module-issue type mappings
+                            // for template type
+                            IssueType template = scarabR
+                                .getIssueType(issueType
+                                .getTemplateId().toString());
+                            RModuleIssueType rmit2 = module
+                               .getRModuleIssueType(template);
+                            rmit2.delete(user);
 
                             scarabR.setConfirmMessage(
                                 "The selected issue Types have"
