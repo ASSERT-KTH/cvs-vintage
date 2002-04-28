4ed56c5cbb595558bc6671d1566d93820a9fe8dd
jmcnally <jmcnally>
2002-04-28 17:06:08 +0000

    add correct class to the list.  fixes broken tests.

--- src/java/org/tigris/scarab/om/AbstractScarabModule.java
+++ src/java/org/tigris/scarab/om/AbstractScarabModule.java
@@ -128,7 +128,7 @@ import org.tigris.scarab.services.cache.ScarabCache;
  *
  * @author <a href="mailto:jon@collab.net">Jon S. Stevens</a>
  * @author <a href="mailto:jmcnally@collab.net">John McNally</a>
- * @version $Id: AbstractScarabModule.java,v 1.22 2002/04/26 21:12:31 jon Exp $
+ * @version $Id: AbstractScarabModule.java,v 1.23 2002/04/28 17:06:08 jmcnally Exp $
  */
 public abstract class AbstractScarabModule
     extends BaseObject
@@ -1043,7 +1043,7 @@ public abstract class AbstractScarabModule
         rma.setAttributeId(attribute.getAttributeId());
         rma.setOrder(getLastAttribute(issueType, attributeType) + 1);
         rma.save();
-        getRModuleAttributes(issueType, false, attributeType).add(attribute);
+        getRModuleAttributes(issueType, false, attributeType).add(rma);
 
         // Add to template type
         IssueType templateType = IssueTypeManager
@@ -1253,11 +1253,20 @@ public abstract class AbstractScarabModule
             rmas = RModuleAttributePeer.doSelect(crit);
             getMethodResult().put(rmas, this, GET_R_MODULE_ATTRIBUTES, 
                 issueType, activeBool, attributeType);
+            System.out.println("Getting new list");
         }
         else 
         {
+            System.out.println("Getting cached list");
             rmas = (List)obj;
         }
+
+        Iterator iter = rmas.iterator();
+        while (iter.hasNext()) 
+        {
+            System.out.println(iter.next().getClass().getName());
+        }
+        
         return rmas;
     }
 
