459248d38594f05bb59e12b42281b8b0a0c6c47e
user57 <user57>
2002-05-09 03:26:49 +0000

    o cleaned, using org.jboss.util.id.GUID to determine if the instance is    local to the invoker

--- src/main/org/jboss/invocation/InvokerInterceptor.java
+++ src/main/org/jboss/invocation/InvokerInterceptor.java
@@ -1,11 +1,11 @@
 /*
-* JBoss, the OpenSource J2EE webOS
-*
-* Distributable under LGPL license.
-* See terms of license at gnu.org.
-*/
-package org.jboss.invocation;
+ * JBoss, the OpenSource J2EE webOS
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
 
+package org.jboss.invocation;
 
 import java.io.Externalizable;
 import java.io.IOException;
@@ -14,127 +14,109 @@ import java.io.ObjectOutput;
 
 import org.jboss.invocation.Invocation;
 import org.jboss.invocation.Invoker;
-/*
-
-import java.rmi.ServerException;
-import java.rmi.NoSuchObjectException;
-import java.rmi.MarshalledObject;
-
-import javax.transaction.SystemException;
-
-import org.jboss.invocation.MarshalledInvocation;
-import org.jboss.invocation.local.LocalInvoker;
-
-import org.jboss.security.SecurityAssociation;
-
-import org.jboss.tm.TransactionPropagationContextFactory;
-*/
 
 import org.jboss.proxy.Interceptor;
 
+import org.jboss.util.id.GUID;
+
 /**
-* InvokerInterceptor
-*
-* a very simple implementation of it that branches to the local stuff
-* 
-* @author <a href="mailto:marc.fleury@jboss.org">Marc Fleury</a>
-* @version $Revision: 1.1 $
-*
-* <p><b>2001/02/28: marcf</b>
-* <ol>
-*   <li>Initial checkin
-* </ol>
-*/
+ * A very simple implementation of it that branches to the local stuff.
+ * 
+ * @author <a href="mailto:marc.fleury@jboss.org">Marc Fleury</a>
+ * @version $Revision: 1.2 $
+ *
+ * <p><b>2001/02/28: marcf</b>
+ * <ol>
+ *   <li>Initial checkin
+ * </ol>
+ */
 public class InvokerInterceptor
-extends Interceptor
-implements Externalizable
+   extends Interceptor
+   implements Externalizable
 {
-   // Constants -----------------------------------------------------
-   
    /** Serial Version Identifier. */
-   //   private static final long serialVersionUID = 1870461898442160570L;
+   // private static final long serialVersionUID = 1870461898442160570L;
    
-   // Attributes ----------------------------------------------------
-   
-   // We use this startup time to find if we are in the original VM
-   private long containerStartup = Invoker.STARTUP;
-   
-   // Invoker to the remote JMX node
+   /** The value of our local Invoker.ID to detect when we are local. */
+   private GUID invokerID = Invoker.ID;
+      
+   /** Invoker to the remote JMX node. */
    protected Invoker remoteInvoker;
    
-   // Static references to local invokers
+   /** Static references to local invokers. */
    protected static Invoker localInvoker; 
-   
-   
-   // Get and set methods
-   
-   //Local invoker reference, useful for optimization
-   public static Invoker getLocal() { return localInvoker;}
-   public static void setLocal(Invoker invoker) { localInvoker = invoker ;}
-   
-   // Constructors --------------------------------------------------
-   
+
+   /**
+    * Get the local invoker reference, useful for optimization.
+    */
+   public static Invoker getLocal() {
+      return localInvoker;
+   }
+
+   /**
+    * Set the local invoker reference, useful for optimization.
+    */
+   public static void setLocal(Invoker invoker) {
+      localInvoker = invoker;
+   }
+
+   /**
+    * Exposed for externalization.
+    */
    public InvokerInterceptor() {
-      // For externalization to work
+      super();
    }
    
-   
-   // Public --------------------------------------------------------
-   
+   /**
+    * Returns wether we are local to the originating container or not. 
+    */
+   public boolean isLocal() {
+      return invokerID.equals(Invoker.ID);
+      // return containerStartup == Invoker.STARTUP;
+   }
    
    /**
-   * Returns wether we are local to the originating container or not. 
-   */
-   public boolean isLocal() { return containerStartup == Invoker.STARTUP; }
-   
-   /**
-   * The invocation on the delegate, calls the right invoker.  Remote if we are remote, local if we
-   * are local. 
-   */
+    * The invocation on the delegate, calls the right invoker.  
+    * Remote if we are remote, local if we are local. 
+    */
    public Object invoke(Invocation invocation)
-   throws Exception
+      throws Exception
    {
-      
       // optimize if calling another bean in same EJB-application
       if (isLocal()) {
-         
          // The payload as is is good
          return localInvoker.invoke(invocation);
-         
       }
       else {
-         
          // this payload will go through marshalling
          return invocation.getInvocationContext().getInvoker().invoke(invocation);
-         
       }   
    }
    
    /**
-   *  Externalize this instance.
-   *
-   *  If this instance lives in a different VM than its container
-   *  invoker, the remote interface of the container invoker is
-   *  not externalized.
-   */
+    * Externalize this instance.
+    *
+    * <p>
+    * If this instance lives in a different VM than its container
+    * invoker, the remote interface of the container invoker is
+    * not externalized.
+    */
    public void writeExternal(final ObjectOutput out)
-   throws IOException
-   { 
-      out.writeLong(containerStartup);
+      throws IOException
+   {
+      out.writeObject(invokerID);
    }
    
    /**
-   *  Un-externalize this instance.
-   *
-   *  We check timestamps of the interfaces to see if the instance is in the original VM of creation
-   */
+    * Un-externalize this instance.
+    *
+    * <p>
+    * We check timestamps of the interfaces to see if the instance is in the original
+    * VM of creation
+    */
    public void readExternal(final ObjectInput in)
-   throws IOException, ClassNotFoundException
+      throws IOException, ClassNotFoundException
    {
-      containerStartup = in.readLong();
+      invokerID = (GUID)in.readObject();
    }
-   
-   // Private -------------------------------------------------------
-   
-   // Inner classes -------------------------------------------------
 }
