1fd79a981201142695ea78cbcafdda318635b6ef
user57 <user57>
2002-05-10 01:35:54 +0000

    o Using xdoclet to create MBean intf

--- src/main/org/jboss/naming/NamingAlias.java
+++ src/main/org/jboss/naming/NamingAlias.java
@@ -4,6 +4,7 @@
  * Distributable under LGPL license.
  * See terms of license at gnu.org.
  */
+
 package org.jboss.naming;
 
 import javax.naming.Context;
@@ -16,106 +17,141 @@ import javax.naming.NameNotFoundException;
 import org.jboss.system.ServiceMBean;
 import org.jboss.system.ServiceMBeanSupport;
 
-/** A simple utility mbean that allows one to create an alias in
-the form of a LinkRef from one JNDI name to another.
-
-@author <a href="mailto:Scott_Stark@displayscape.com">Scott Stark</a>.
-@version $Revision: 1.4 $
-*/
-public class NamingAlias extends ServiceMBeanSupport implements NamingAliasMBean
+/**
+ * A simple utility mbean that allows one to create an alias in
+ * the form of a LinkRef from one JNDI name to another.
+ *
+ * @jmx:mbean extends="org.jboss.system.ServiceMBean"
+ *
+ * @author <a href="mailto:Scott_Stark@displayscape.com">Scott Stark</a>.
+ * @version $Revision: 1.5 $
+ */
+public class NamingAlias
+   extends ServiceMBeanSupport
+   implements NamingAliasMBean
 {
-    private String fromName;
-    private String toName;
+   private String fromName;
+   private String toName;
 
-    /** Creates new NamingAlias */
-    public NamingAlias()
-    {
-    }
-    public NamingAlias(String fromName, String toName)
-    {
-    }
-
-// --- Begin NamingAliasMBean interface methods
-    public String getFromName()
-    {
-        return fromName;
-    }
-    public void setFromName(String name) throws NamingException
-    {
-        removeLinkRef(fromName);
-        this.fromName = name;
-        createLinkRef();        
-    }
-    public String getToName()
-    {
-        return toName;
-    }
-    public void setToName(String name) throws NamingException
-    {
-        this.toName = name;
-        createLinkRef();
-    }
-// --- End NamingAliasMBean interface methods
-
-// --- Begin ServiceMBeanSupport methods
-    public String getName()
-    {
-        return "NamingAlias("+fromName+" -> "+toName+")";
-    }
-    public void startService() throws Exception
-    {
-        if( fromName == null )
-            throw new IllegalStateException("fromName is null");
-        if( toName == null )
-            throw new IllegalStateException("toName is null");
-        createLinkRef();
-    }
-    public void stopService()
-    {
-        try
-        {
-            removeLinkRef(fromName);
-        }
-        catch(Exception e)
-        {
-        }
-    }
-// --- Begin ServiceMBeanSupport methods
-
-    /**
+   public NamingAlias()
+   {
+      this(null, null);
+   }
+   
+   public NamingAlias(final String fromName, final String toName)
+   {
+      this.fromName = fromName;
+      this.toName = toName;
+   }
+   
+   /**
+    * Get the from name of the alias. This is the location where the
+    * LinkRef is bound under JNDI.
+    *
+    * @jmx:managed-attribute
+    * 
+    * @return the location of the LinkRef
     */
-    private void createLinkRef() throws NamingException
-    {
-        if( super.getState() == ServiceMBean.STARTING || super.getState() == ServiceMBean.STARTED )
-        {
-            InitialContext ctx = new InitialContext();
-            LinkRef link = new LinkRef(toName);
-            Context fromCtx = ctx;
-            Name name = ctx.getNameParser("").parse(fromName);
-            String atom = name.get(name.size()-1);
-            for(int n = 0; n < name.size()-1; n ++)
+   public String getFromName()
+   {
+      return fromName;
+   }
+
+   /**
+    * Set the from name of the alias. This is the location where the
+    * LinkRef is bound under JNDI.
+    *
+    * @jmx:managed-attribute
+    * 
+    * @param name, the location where the LinkRef will be bound
+    */
+   public void setFromName(String name) throws NamingException
+   {
+      removeLinkRef(fromName);
+      this.fromName = name;
+      createLinkRef();        
+   }
+
+   /**
+    * Get the to name of the alias. This is the target name to
+    * which the LinkRef refers. The name is a URL, or a name to be resolved
+    * relative to the initial context, or if the first character of the name
+    * is ".", the name is relative to the context in which the link is bound.
+    *
+    * @jmx:managed-attribute
+    * 
+    * @return the target JNDI name of the alias.
+    */
+   public String getToName()
+   {
+      return toName;
+   }
+
+   /**
+    * Set the to name of the alias. This is the target name to
+    * which the LinkRef refers. The name is a URL, or a name to be resolved
+    * relative to the initial context, or if the first character of the name
+    * is ".", the name is relative to the context in which the link is bound.
+    *
+    * @jmx:managed-attribute
+    * 
+    * @param name, the target JNDI name of the alias.
+    */
+   public void setToName(String name) throws NamingException
+   {
+      this.toName = name;
+      
+      createLinkRef();
+   }
+   
+   protected void startService() throws Exception
+   {
+      if( fromName == null )
+         throw new IllegalStateException("fromName is null");
+      if( toName == null )
+         throw new IllegalStateException("toName is null");
+      createLinkRef();
+   }
+   
+   protected void stopService() throws Exception
+   {
+      removeLinkRef(fromName);
+   }
+   
+   private void createLinkRef() throws NamingException
+   {
+      if( super.getState() == ServiceMBean.STARTING || super.getState() == ServiceMBean.STARTED )
+      {
+         InitialContext ctx = new InitialContext();
+         LinkRef link = new LinkRef(toName);
+         Context fromCtx = ctx;
+         Name name = ctx.getNameParser("").parse(fromName);
+         String atom = name.get(name.size()-1);
+         for(int n = 0; n < name.size()-1; n ++)
+         {
+            String comp = name.get(n);
+            try
             {
-                String comp = name.get(n);
-                try
-                {
-                    fromCtx = (Context) fromCtx.lookup(comp);
-                }
-                catch(NameNotFoundException e)
-                {
-                    fromCtx = fromCtx.createSubcontext(comp);
-                }
+               fromCtx = (Context) fromCtx.lookup(comp);
             }
-            fromCtx.rebind(atom, link);
-        }
-    }
-    /** Unbind the name value if we are in the STARTED state.
+            catch(NameNotFoundException e)
+            {
+               fromCtx = fromCtx.createSubcontext(comp);
+            }
+         }
+         fromCtx.rebind(atom, link);
+      }
+   }
+   
+   /**
+    * Unbind the name value if we are in the STARTED state.
     */
-    private void removeLinkRef(String name) throws NamingException
-    {
-        if( super.getState() == ServiceMBean.STARTED )
-        {
-            InitialContext ctx = new InitialContext();
-            ctx.unbind(name);
-        }
-    }
+   private void removeLinkRef(String name) throws NamingException
+   {
+      if( super.getState() == ServiceMBean.STARTED )
+      {
+         InitialContext ctx = new InitialContext();
+         ctx.unbind(name);
+      }
+   }
 }
