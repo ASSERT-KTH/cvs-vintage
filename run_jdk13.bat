33d924e35413f8ecc4f7336feb495c18862b2fac
fdietz <fdietz>
2002-05-13 13:42:58 +0000

    added run-scripts

---
+++ run_jdk13.bat
@@ -0,0 +1,1 @@
+java -cp ./bin/columba.jar;./bin/xalan.jar;./bin/jaxp.jar;./bin/parser.jar org.columba.main.Main
---
+++ run_jdk13.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -cp ./bin/columba.jar:./bin/xalan.jar:./bin/jaxp.jar:./bin/parser.jar org.columba.main.Main
