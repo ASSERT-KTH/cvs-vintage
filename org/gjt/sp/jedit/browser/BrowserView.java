4ebc9331eb8800a8709a7d730a2b2427a82fffdd
spestov <spestov>
2002-05-19 04:32:33 +0000

    starting display rewrite

--- org/gjt/sp/jedit/browser/BrowserView.java
+++ org/gjt/sp/jedit/browser/BrowserView.java
@@ -12,7 +12,7 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
@@ -40,7 +40,7 @@ import org.gjt.sp.jedit.*;
 /**
  * VFS browser tree view.
  * @author Slava Pestov
- * @version $Id: BrowserView.java,v 1.35 2002/05/16 08:01:24 spestov Exp $
+ * @version $Id: BrowserView.java,v 1.36 2002/05/19 04:32:33 spestov Exp $
  */
 public class BrowserView extends JPanel
 {
