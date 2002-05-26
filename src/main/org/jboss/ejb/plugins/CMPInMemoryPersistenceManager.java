aad1ab7bf670c885fa7f0b3f0d9b7905ae0fb789
user57 <user57>
2002-05-26 23:44:53 +0000

    o extend ServiceMBeanSupport  o use imports to make code more readable

--- src/main/org/jboss/ejb/plugins/CMPInMemoryPersistenceManager.java
+++ src/main/org/jboss/ejb/plugins/CMPInMemoryPersistenceManager.java
@@ -10,14 +10,25 @@ package org.jboss.ejb.plugins;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+
 import java.io.IOException;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Collection;
 import java.util.Collections;
 
 import javax.ejb.EJBException;
 
+import org.jboss.ejb.Container;
 import org.jboss.ejb.EntityEnterpriseContext;
+import org.jboss.ejb.EntityPersistenceStore;
+import org.jboss.ejb.EntityContainer;
+
+import org.jboss.metadata.EntityMetaData;
+
+import org.jboss.system.ServiceMBeanSupport;
 
 /**
  * EntityPersistenceStore implementation storing values in-memory
@@ -26,8 +37,9 @@ import org.jboss.ejb.EntityEnterpriseContext;
  * @see org.jboss.ejb.EntityPersistenceStore
  * @see org.jboss.ejb.plugins.CMPFilePersistenceManager
  *
- * @author  <a href="mailto:sacha.labourey@cogito-info.ch">Sacha Labourey</a>.
- * @version $Revision: 1.3 $
+ * @version <tt>$Revision: 1.4 $</tt>
+ * @author <a href="mailto:sacha.labourey@cogito-info.ch">Sacha Labourey</a>.
+ * @author <a href="mailto:jason@planet57.com">Jason Dillon</a>
  *
  * <p><b>Revisions:</b>
  *
@@ -40,24 +52,20 @@ import org.jboss.ejb.EntityEnterpriseContext;
  * <li> Replaced FinderResults with Collection</li>
  * </ul>
  */
-
-public class CMPInMemoryPersistenceManager implements org.jboss.ejb.EntityPersistenceStore
+public class CMPInMemoryPersistenceManager
+   extends ServiceMBeanSupport
+   implements EntityPersistenceStore
 {
-   
-   // Constants -----------------------------------------------------
-   
    // Attributes ----------------------------------------------------
    
-   protected org.jboss.ejb.EntityContainer con = null;   
-   protected java.util.HashMap beans = null;   
-   protected Field idField = null;
+   protected EntityContainer con;
+   protected HashMap beans;
+   protected Field idField;
    
    /**
-    *  Optional isModified method used by storeEntity
+    * Optional isModified method used by storeEntity.
     */
-   protected Method isModified = null;
-   
-   // Static --------------------------------------------------------
+   protected Method isModified;
    
    // Constructors --------------------------------------------------
    
@@ -66,60 +74,50 @@ public class CMPInMemoryPersistenceManager implements org.jboss.ejb.EntityPersis
    }
    
    /**
+    * This callback is set by the container so that the plugin may access it
+    *
+    * @param con    The container using this plugin.
+    *
+    * @throws ClassCastException  Container is not a EntityContainer.
+    */
+   public void setContainer (final Container con)
+   {
+      this.con = (EntityContainer)con;
+   }
+   
+   /**
     * create the service, do expensive operations etc
     */
-   public void create () throws Exception
+   protected void createService() throws Exception
    {
-      this.beans = new java.util.HashMap (1000);
+      boolean debug = log.isDebugEnabled();
+      
+      this.beans = new HashMap(1000);
       
       String ejbName = con.getBeanMetaData ().getEjbName ();
       
       idField = con.getBeanClass ().getField ("id");
-      
+      if (debug) log.debug("Using id field: " + idField);
+
+      // Lookup the isModified method if it exists
       try
       {
-         isModified = con.getBeanClass ().getMethod ("isModified", new Class[0]);
-         if (!isModified.getReturnType ().equals (Boolean.TYPE))
+         isModified = con.getBeanClass().getMethod("isModified", new Class[0]);
+         if (!isModified.getReturnType().equals(Boolean.TYPE)) {
             isModified = null; // Has to have "boolean" as return type!
+            log.warn("Found isModified method, but return type is not boolean; ignoring");
+         }
+         else {
+            if (debug) log.debug("Using isModified method: " + isModified);
+         }
       }
-      catch (NoSuchMethodException ignored)
-      {}
+      catch (NoSuchMethodException ignored) {}
    }
-   
-   /**
-    * start the service, create is already called
-    */
-   public void start () throws Exception
+
+   protected void stopService() throws Exception
    {
+      this.beans.clear();
    }
-   
-   /**
-    * This callback is set by the container so that the plugin may access it
-    *
-    * @param con    The container using this plugin.
-    */
-   public void setContainer (org.jboss.ejb.Container con)
-   {
-      this.con = (org.jboss.ejb.EntityContainer)con;
-   }
-   
-   /**
-    * stop the service
-    */
-   public void stop ()
-   {
-      this.beans.clear ();
-   }
-   
-   
-   /**
-    * destroy the service, tear down
-    */
-   public void destroy ()
-   {
-   }
-   
-   // Public --------------------------------------------------------
    
    // EntityPersistenceStore implementation ----------------------------------------------
    
@@ -153,62 +151,60 @@ public class CMPInMemoryPersistenceManager implements org.jboss.ejb.EntityPersis
       Field cmpField;
       Class cmpFieldType;
       
-      org.jboss.metadata.EntityMetaData metaData = (org.jboss.metadata.EntityMetaData)con.getBeanMetaData ();
-      java.util.Iterator i= metaData.getCMPFields ();
+      EntityMetaData metaData = (EntityMetaData)con.getBeanMetaData ();
+      Iterator i= metaData.getCMPFields ();
       
       while(i.hasNext ())
       {
+         // get the field declaration
          try
          {
-            // get the field declaration
-            try
+            cmpField = ejbClass.getField ((String)i.next ());
+            cmpFieldType = cmpField.getType ();
+            
+            // find the type of the field and reset it
+            // to the default value
+            if (cmpFieldType.equals (boolean.class))
             {
-               cmpField = ejbClass.getField ((String)i.next ());
-               cmpFieldType = cmpField.getType ();
-               // find the type of the field and reset it
-               // to the default value
-               if (cmpFieldType.equals (boolean.class))
-               {
-                  cmpField.setBoolean (instance,false);
-               }
-               else if (cmpFieldType.equals (byte.class))
-               {
-                  cmpField.setByte (instance,(byte)0);
-               }
-               else if (cmpFieldType.equals (int.class))
-               {
-                  cmpField.setInt (instance,0);
-               }
-               else if (cmpFieldType.equals (long.class))
-               {
-                  cmpField.setLong (instance,0L);
-               }
-               else if (cmpFieldType.equals (short.class))
-               {
-                  cmpField.setShort (instance,(short)0);
-               }
-               else if (cmpFieldType.equals (char.class))
-               {
-                  cmpField.setChar (instance,'\u0000');
-               }
-               else if (cmpFieldType.equals (double.class))
-               {
-                  cmpField.setDouble (instance,0d);
-               }
-               else if (cmpFieldType.equals (float.class))
-               {
-                  cmpField.setFloat (instance,0f);
-               }
-               else
-               {
-                  cmpField.set (instance,null);
-               }
+               cmpField.setBoolean (instance,false);
             }
-            catch (NoSuchFieldException e)
+            else if (cmpFieldType.equals (byte.class))
             {
-               // will be here with dependant value object's private attributes
-               // should not be a problem
+               cmpField.setByte (instance,(byte)0);
             }
+            else if (cmpFieldType.equals (int.class))
+            {
+               cmpField.setInt (instance,0);
+            }
+            else if (cmpFieldType.equals (long.class))
+            {
+               cmpField.setLong (instance,0L);
+            }
+            else if (cmpFieldType.equals (short.class))
+            {
+               cmpField.setShort (instance,(short)0);
+            }
+            else if (cmpFieldType.equals (char.class))
+            {
+               cmpField.setChar (instance,'\u0000');
+            }
+            else if (cmpFieldType.equals (double.class))
+            {
+               cmpField.setDouble (instance,0d);
+            }
+            else if (cmpFieldType.equals (float.class))
+            {
+               cmpField.setFloat (instance,0f);
+            }
+            else
+            {
+               cmpField.set (instance,null);
+            }
+         }
+         catch (NoSuchFieldException e)
+         {
+            // will be here with dependant value object's private attributes
+            // should not be a problem
          }
          catch (Exception e)
          {
@@ -234,12 +230,11 @@ public class CMPInMemoryPersistenceManager implements org.jboss.ejb.EntityPersis
    {
       try
       {
-         
          Object id = idField.get (ctx.getInstance ());
          
          // Check exist
          if (this.beans.containsKey (id))
-            throw new javax.ejb.DuplicateKeyException ("Already exists:"+id);
+            throw new javax.ejb.DuplicateKeyException ("Already exists: "+id);
          
          // Store to file
          storeEntity (id, ctx.getInstance ());
@@ -248,7 +243,7 @@ public class CMPInMemoryPersistenceManager implements org.jboss.ejb.EntityPersis
       }
       catch (IllegalAccessException e)
       {
-         throw new javax.ejb.CreateException ("Could not create entity:"+e);
+         throw new javax.ejb.CreateException ("Could not create entity: "+e);
       }
    }
    
@@ -267,7 +262,8 @@ public class CMPInMemoryPersistenceManager implements org.jboss.ejb.EntityPersis
     * @throws RemoteException    thrown if some system exception occurs
     * @throws FinderException    thrown if some heuristic problem occurs
     */
-   public Object findEntity (Method finderMethod, Object[] args, EntityEnterpriseContext instance) throws Exception
+   public Object findEntity (Method finderMethod, Object[] args, EntityEnterpriseContext instance)
+      throws Exception
    {
       if (finderMethod.getName ().equals ("findByPrimaryKey"))
       {
@@ -276,8 +272,8 @@ public class CMPInMemoryPersistenceManager implements org.jboss.ejb.EntityPersis
          
          return args[0];
       }
-      else
-         return null;
+
+      return null;
    }
    
    /**
@@ -296,12 +292,16 @@ public class CMPInMemoryPersistenceManager implements org.jboss.ejb.EntityPersis
     * @throws RemoteException    thrown if some system exception occurs
     * @throws FinderException    thrown if some heuristic problem occurs
     */
-   public Collection findEntities (Method finderMethod, Object[] args, EntityEnterpriseContext instance) throws Exception
+   public Collection findEntities(final Method finderMethod,
+                                  final Object[] args,
+                                  final EntityEnterpriseContext instance)
+      throws Exception
    {
       if (finderMethod.getName ().equals ("findAll"))
       {
          return new ArrayList(this.beans.keySet());
-      } else
+      }
+      else
       {
          // we only support find all
          return Collections.EMPTY_LIST;
@@ -309,21 +309,11 @@ public class CMPInMemoryPersistenceManager implements org.jboss.ejb.EntityPersis
    }
    
    /**
-    * This method is called when an entity shall be activated.
-    *
-    * <p>With the PersistenceManager factorization most EJB calls should not
-    *   exists However this calls permits us to introduce optimizations in
-    *   the persistence store.  Particularly the context has a
-    *   "PersistenceContext" that a PersistenceStore can use (JAWS does for
-    *   smart updates) and this is as good a callback as any other to set it
-    *   up.
-    *
-    * @param instance    the instance to use for the activation
-    *
-    * @throws RemoteException    thrown if some system exception occurs
+    * Non-operation.
     */
    public void activateEntity (EntityEnterpriseContext instance)
    {
+      // nothing to do
    }
    
    /**
@@ -341,8 +331,8 @@ public class CMPInMemoryPersistenceManager implements org.jboss.ejb.EntityPersis
       {
          // Read fields
          
-         java.io.ObjectInputStream in = new org.jboss.ejb.plugins.CMPInMemoryPersistenceManager.CMPObjectInputStream (
-         new java.io.ByteArrayInputStream ((byte[])this.beans.get (ctx.getId ())));
+         java.io.ObjectInputStream in = new CMPObjectInputStream
+            (new java.io.ByteArrayInputStream ((byte[])this.beans.get (ctx.getId ())));
          
          Object obj = ctx.getInstance ();
          
@@ -353,8 +343,8 @@ public class CMPInMemoryPersistenceManager implements org.jboss.ejb.EntityPersis
          }
          
          in.close ();
-         
-      } catch (Exception e)
+      }
+      catch (Exception e)
       {
          throw new EJBException ("Load failed", e);
       }
@@ -374,9 +364,7 @@ public class CMPInMemoryPersistenceManager implements org.jboss.ejb.EntityPersis
          return true;
       }
       
-      Object[] args =
-      {};
-      Boolean modified = (Boolean) isModified.invoke (ctx.getInstance (), args);
+      Boolean modified = (Boolean) isModified.invoke (ctx.getInstance (), new Object[0]);
       return modified.booleanValue ();
    }
    
@@ -384,7 +372,7 @@ public class CMPInMemoryPersistenceManager implements org.jboss.ejb.EntityPersis
     * This method is called whenever an entity shall be stored to the
     * underlying storage. The persistence manager must call ejbStore on the
     * supplied instance and then store the state to the underlying storage.
-    *B
+    *
     * @param instance    the instance to synchronize
     *
     * @throws RemoteException    thrown if some system exception occurs
@@ -395,15 +383,7 @@ public class CMPInMemoryPersistenceManager implements org.jboss.ejb.EntityPersis
    }
    
    /**
-    * This method is called when an entity shall be passivate. The persistence
-    * manager must call the ejbPassivate method on the instance.
-    *
-    * <p>See the activate discussion for the reason for exposing EJB callback
-    *   calls to the store.
-    *
-    * @param instance    the instance to passivate
-    *
-    * @throws RemoteException    thrown if some system exception occurs
+    * Non-operation.
     */
    public void passivateEntity (EntityEnterpriseContext instance)
    {
@@ -423,13 +403,8 @@ public class CMPInMemoryPersistenceManager implements org.jboss.ejb.EntityPersis
    public void removeEntity (EntityEnterpriseContext ctx) throws javax.ejb.RemoveException
    {
       if (this.beans.remove (ctx.getId ()) == null)
-         throw new javax.ejb.RemoveException ("Could not remove bean:" +
-         ctx.getId ());
+         throw new javax.ejb.RemoveException ("Could not remove bean:" + ctx.getId ());
    }
-   
-   // Y overrides ---------------------------------------------------
-   
-   // Package protected ---------------------------------------------
    
    // Protected -----------------------------------------------------
    
@@ -439,25 +414,26 @@ public class CMPInMemoryPersistenceManager implements org.jboss.ejb.EntityPersis
       {
          // Store fields
          java.io.ByteArrayOutputStream baos = new java.io.ByteArrayOutputStream ();
-         java.io.ObjectOutputStream out = new org.jboss.ejb.plugins.CMPInMemoryPersistenceManager.CMPObjectOutputStream (baos);
-         
-         Field[] f = obj.getClass ().getFields ();
-         for (int i = 0; i < f.length; i++)
-         {
-            out.writeObject (f[i].get (obj));
+         java.io.ObjectOutputStream out = new CMPObjectOutputStream (baos);
+
+         try {
+            Field[] f = obj.getClass ().getFields ();
+            for (int i = 0; i < f.length; i++)
+            {
+               out.writeObject (f[i].get (obj));
+            }
+         }
+         finally {
+            out.close();
          }
          
-         out.close ();
-         
          this.beans.put (id, baos.toByteArray ());
-         
-      } catch (Exception e)
+      }
+      catch (Exception e)
       {
          throw new EJBException ("Store failed", e);
       }
    }
-   
-   // Private -------------------------------------------------------
    
    // Inner classes -------------------------------------------------
    
@@ -470,7 +446,7 @@ public class CMPInMemoryPersistenceManager implements org.jboss.ejb.EntityPersis
       }
       
       protected Object replaceObject (Object obj)
-      throws IOException
+         throws IOException
       {
          if (obj instanceof javax.ejb.EJBObject)
             return ((javax.ejb.EJBObject)obj).getHandle ();
@@ -488,7 +464,7 @@ public class CMPInMemoryPersistenceManager implements org.jboss.ejb.EntityPersis
       }
       
       protected Object resolveObject (Object obj)
-      throws IOException
+         throws IOException
       {
          if (obj instanceof javax.ejb.Handle)
             return ((javax.ejb.Handle)obj).getEJBObject ();
