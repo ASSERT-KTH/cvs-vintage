e5cd749774c3cf3a57ee80f9e5458fcf45896a77
spestov <spestov>
2002-06-01 02:46:39 +0000

    new help viewer and quicknotepad icons from krisk

--- jars/QuickNotepad/QuickNotepadToolPanel.java
+++ jars/QuickNotepad/QuickNotepadToolPanel.java
@@ -18,7 +18,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
- * $Id: QuickNotepadToolPanel.java,v 1.2 2001/11/09 20:26:17 jgellene Exp $
+ * $Id: QuickNotepadToolPanel.java,v 1.3 2002/06/01 02:46:39 spestov Exp $
  */
 
 import java.awt.*;
@@ -38,25 +38,27 @@ public class QuickNotepadToolPanel extends JPanel
 	private QuickNotepad pad;
 	private JLabel label;
 
-    public QuickNotepadToolPanel(QuickNotepad qnpad)
+	public QuickNotepadToolPanel(QuickNotepad qnpad)
 	{
 		pad = qnpad;
-        JToolBar toolBar = new JToolBar();
-        toolBar.setFloatable(false);
+		JToolBar toolBar = new JToolBar();
+		toolBar.setFloatable(false);
 
-        toolBar.add(makeCustomButton("quicknotepad.choose-file",
+		toolBar.putClientProperty("JToolBar.isRollover",Boolean.TRUE);
+
+		toolBar.add(makeCustomButton("quicknotepad.choose-file",
 			new ActionListener() {
 				public void actionPerformed(ActionEvent evt) {
 					QuickNotepadToolPanel.this.pad.chooseFile();
 				}
 			}));
-        toolBar.add(makeCustomButton("quicknotepad.save-file",
+		toolBar.add(makeCustomButton("quicknotepad.save-file",
 			new ActionListener() {
 				public void actionPerformed(ActionEvent evt) {
 					QuickNotepadToolPanel.this.pad.saveFile();
 				}
 			}));
-        toolBar.add(makeCustomButton("quicknotepad.copy-to-buffer",
+		toolBar.add(makeCustomButton("quicknotepad.copy-to-buffer",
 			new ActionListener() {
 				public void actionPerformed(ActionEvent evt) {
 					QuickNotepadToolPanel.this.pad.copyToBuffer();
@@ -66,11 +68,11 @@ public class QuickNotepadToolPanel extends JPanel
 		label.setForeground(Color.black);
 		label.setVisible(jEdit.getProperty(
 			QuickNotepadPlugin.OPTION_PREFIX + "show-filepath").equals("true"));
-        this.setLayout(new BorderLayout(10, 0));
-        this.add(BorderLayout.WEST, toolBar);
+		this.setLayout(new BorderLayout(10, 0));
+		this.add(BorderLayout.WEST, toolBar);
 		this.add(BorderLayout.CENTER, label);
-        this.setBorder(BorderFactory.createEmptyBorder(0, 0, 3, 10));
-    }
+		this.setBorder(BorderFactory.createEmptyBorder(0, 0, 3, 10));
+	}
 
 
 	void propertiesChanged()
@@ -80,12 +82,11 @@ public class QuickNotepadToolPanel extends JPanel
 			QuickNotepadPlugin.OPTION_PREFIX + "show-filepath").equals("true"));
 	}
 
-    private AbstractButton makeCustomButton(String name, ActionListener listener)
+	private AbstractButton makeCustomButton(String name, ActionListener listener)
 	{
-        String icon = jEdit.getProperty(name + ".icon");
-        java.net.URL u = getClass().getResource(icon);
-        String toolTip = jEdit.getProperty(name.concat(".label"));
-        AbstractButton b = new JButton(new ImageIcon(u));
+		String toolTip = jEdit.getProperty(name.concat(".label"));
+		AbstractButton b = new JButton(GUIUtilities.loadIcon(
+			jEdit.getProperty(name + ".icon")));
 		if(listener != null)
 		{
 			b.addActionListener(listener);
@@ -95,12 +96,12 @@ public class QuickNotepadToolPanel extends JPanel
 		{
 			b.setEnabled(false);
 		}
-        b.setToolTipText(toolTip);
-        b.setMargin(new Insets(0,0,0,0));
+		b.setToolTipText(toolTip);
+		b.setMargin(new Insets(0,0,0,0));
 		b.setAlignmentY(0.0f);
-        b.setRequestFocusEnabled(false);
-        return b;
-    }
+		b.setRequestFocusEnabled(false);
+		return b;
+	}
 
 }
 
