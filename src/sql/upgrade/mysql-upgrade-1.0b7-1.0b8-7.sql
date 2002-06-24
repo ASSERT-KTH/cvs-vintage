fc007ed7133260719813578414b4dd73d00c5fd7
jon <jon>
2002-06-24 15:38:20 +0000

    fixed upgrade script
    -jon

--- src/sql/upgrade/mysql-upgrade-1.0b7-1.0b8-7.sql
+++ src/sql/upgrade/mysql-upgrade-1.0b7-1.0b8-7.sql
@@ -2,7 +2,9 @@
  * This upgrade script adds an ATTACHMENT_ID column to the SCARAB_ACTIVITY table.
  *
  * Created By: Jon Scott Stevens
- * $Id: mysql-upgrade-1.0b7-1.0b8-7.sql,v 1.3 2002/06/13 22:08:43 jon Exp $
+ * $Id: mysql-upgrade-1.0b7-1.0b8-7.sql,v 1.4 2002/06/24 15:38:20 jon Exp $
  */
 
-ALTER TABLE SCARAB_ACTIVITY ADD column ATTACHMENT_ID integer null default 0;
+ALTER TABLE SCARAB_ACTIVITY ADD column ATTACHMENT_ID integer null;
+
+update SCARAB_ACTIVITY set ATTACHMENT_ID=null where ATTACHMENT_ID=0;
