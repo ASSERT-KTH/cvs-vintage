920398296b4ae4c79083c1d0d4ab6bb1901e0c66
dsundstrom <dsundstrom>
2002-07-01 03:36:05 +0000

    Fixed exception handling.  Should now be EJB 2.0 spec complient.
    Fixed source formatting.

--- src/main/org/jboss/ejb/plugins/LogInterceptor.java
+++ src/main/org/jboss/ejb/plugins/LogInterceptor.java
@@ -9,29 +9,24 @@ package org.jboss.ejb.plugins;
 import java.lang.reflect.Method;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.rmi.NoSuchObjectException;
 import java.rmi.RemoteException;
 import java.rmi.ServerError;
 import java.rmi.ServerException;
 import java.util.Map;
-import java.util.HashMap;
-import java.util.Enumeration;
 
-import javax.ejb.Handle;
-import javax.ejb.HomeHandle;
 import javax.ejb.EJBObject;
-import javax.ejb.EJBMetaData;
 import javax.ejb.EJBException;
-import javax.ejb.CreateException;
-import javax.ejb.FinderException;
-import javax.ejb.RemoveException;
+import javax.ejb.NoSuchEntityException;
+import javax.ejb.NoSuchObjectLocalException;
+import javax.ejb.TransactionRolledbackLocalException;
 import javax.transaction.TransactionRolledbackException;
 
 import org.apache.log4j.NDC;
 
 import org.jboss.ejb.Container;
-import org.jboss.ejb.EnterpriseContext;
-import org.jboss.ejb.Interceptor;
 import org.jboss.invocation.Invocation;
+import org.jboss.invocation.InvocationType;
 import org.jboss.metadata.BeanMetaData;
 
 /** 
@@ -40,10 +35,10 @@ import org.jboss.metadata.BeanMetaData;
  *
  * @author <a href="mailto:rickard.oberg@telkel.com">Rickard Öberg</a>
  * @author <a href="mailto:Scott.Stark@jboss.org">Scott Stark</a>
- * @version $Revision: 1.22 $
+ * @author <a href="mailto:dain@daingroup.com">Dain Sundstrom</a>
+ * @version $Revision: 1.23 $
  */
-public class LogInterceptor
-   extends AbstractInterceptor
+public class LogInterceptor extends AbstractInterceptor
 {
    // Static --------------------------------------------------------
    
@@ -73,38 +68,56 @@ public class LogInterceptor
       
       BeanMetaData md = getContainer().getBeanMetaData();
       ejbName = md.getEjbName();
+
       // Should we log call details
       callLogging = md.getContainerConfiguration().getCallLogging();
    }
    
-   public Object invokeHome(Invocation mi)
+   /**
+    * This method logs the method, calls the next invoker, and handles 
+    * any exception.
+    *
+    * @param invocation contain all infomation necessary to carry out the 
+    * invocation
+    * @return the return value of the invocation
+    * @exception Exception if an exception during the invocation
+    */
+   public Object invokeHome(Invocation invocation)
       throws Exception
    {
       NDC.push(ejbName);
 
       String methodName;
-      if(mi.getMethod() != null) {
-         methodName = mi.getMethod().getName();
-      } else
+      if (invocation.getMethod() != null) 
       {
-         methodName ="<no method>";
+         methodName = invocation.getMethod().getName();
+      }
+      else
+      {
+         methodName = "<no method>";
       }
       
       boolean trace = log.isTraceEnabled();
-      if( trace )
-         log.trace("Start method="+methodName);
+      if (trace)
+      {
+         log.trace("Start method=" + methodName);
+      }
+
       // Log call details
-      if(callLogging)
+      if (callLogging)
       {
          StringBuffer str = new StringBuffer("InvokeHome: ");
          str.append(methodName);
          str.append("(");
-         Object[] args = mi.getArguments();
+         Object[] args = invocation.getArguments();
          if (args != null)
          {
             for (int i = 0; i < args.length; i++)
             {
-               str.append(i==0?"":",");
+               if (i > 0) 
+               {
+                  str.append(",");
+               }
                str.append(args[i]);
             }
          }
@@ -114,62 +127,71 @@ public class LogInterceptor
 
       try
       {
-         Interceptor next = getNext();
-         Object value = next.invokeHome(mi);
-         return value;
+         return getNext().invokeHome(invocation);
       }
       catch(Throwable e)
       {
-         throw handleException(e, trace);
+         throw handleException(e, invocation);
       }
       finally
       {
-         if( trace )
-            log.trace("End method="+methodName);
+         if (trace)
+         {
+            log.trace("End method=" + methodName);
+         }
          NDC.pop();
       }
    }
 
    /**
-    *   This method does invocation interpositioning of tx and security,
-    *   retrieves the instance from an object table, and invokes the method
-    *   on the particular instance
+    * This method logs the method, calls the next invoker, and handles 
+    * any exception.
     *
-    * @param   id
-    * @param   m
-    * @param   args
-    * @return
-    * @exception   Exception
+    * @param invocation contain all infomation necessary to carry out the 
+    * invocation
+    * @return the return value of the invocation
+    * @exception Exception if an exception during the invocation
     */
-   public Object invoke(Invocation mi)
+   public Object invoke(Invocation invocation)
       throws Exception
    {
       NDC.push(ejbName);
 
       String methodName;
-      if(mi.getMethod() != null) {
-         methodName = mi.getMethod().getName();
-      } else 
+      if (invocation.getMethod() != null) 
       {
-         methodName ="<no method>";
+         methodName = invocation.getMethod().getName();
+      }
+      else 
+      {
+         methodName = "<no method>";
       }
 
       boolean trace = log.isTraceEnabled();
-      if( trace )
-         log.trace("Start method="+methodName);
+      if (trace)
+      {
+         log.trace("Start method=" + methodName);
+      }
+
       // Log call details
-      if(callLogging)
+      if (callLogging)
       {
          StringBuffer str = new StringBuffer("Invoke: ");
-         str.append(mi.getId() == null ? "" : "["+mi.getId().toString()+"] ");
+         if (invocation.getId() != null)
+         {
+            str.append("[" + invocation.getId().toString() + "] ");
+         }
          str.append(methodName);
          str.append("(");
-         Object[] args = mi.getArguments();
+         Object[] args = invocation.getArguments();
          if (args != null)
          {
             for (int i = 0; i < args.length; i++)
             {
-               str.append(i==0?"":",");
+               if (i > 0)
+               {
+                  str.append(",");
+               }
                str.append(args[i]);
             }
          }
@@ -179,115 +201,232 @@ public class LogInterceptor
 
       try
       {
-         Interceptor next = getNext();
-         Object value = next.invoke(mi);
-         return value;
+         return getNext().invoke(invocation);
       }
       catch(Throwable e)
       {
-         throw handleException(e, trace);
+         throw handleException(e, invocation);
       }
       finally
       {
-         if( trace )
-            log.trace("End method="+methodName);
+         if (trace)
+         {
+            log.trace("End method=" + methodName);
+         }
          NDC.pop();
       }
    }
 
    // Private -------------------------------------------------------
-   private Exception handleException(Throwable e, boolean trace)
+   private Exception handleException(Throwable e, Invocation invocation)
    {
-      Exception toThrow = null;
-      // Log system exceptions
-      if (e instanceof EJBException)
+      
+      InvocationType type = invocation.getType();
+      boolean isLocal = 
+            type == InvocationType.LOCAL ||
+            type == InvocationType.LOCALHOME;
+      
+      if (e instanceof TransactionRolledbackLocalException ||
+            e instanceof TransactionRolledbackException)
       {
-         EJBException ex = (EJBException) e;
-         if( ex.getCausedByException() != null )
-            log.error("EJBException, causedBy:", ex.getCausedByException());
-         else
-            log.error("EJBException:", ex);
+         // If we got a remote TransactionRolledbackException for a local
+         // invocation convert it into a TransactionRolledbackLocalException
+         if (isLocal && e instanceof TransactionRolledbackException) 
+         {
+            TransactionRolledbackException remoteTxRollback = 
+                  (TransactionRolledbackException)e;
 
-         // Client sees RemoteException
-         toThrow = new ServerException("Bean exception. Notify the application administrator", ex);
-      }
-      else if (e instanceof RuntimeException)
-      {
-         RuntimeException ex = (RuntimeException) e;
-         log.error("CONTAINER EXCEPTION:", ex);
-         
-         // Client sees RemoteException
-         toThrow = new ServerException("Container exception. Notify the container developers :-)", ex);
-      }
-      else if (e instanceof TransactionRolledbackException)
-      {
-         TransactionRolledbackException ex = (TransactionRolledbackException) e;
-         // Log the rollback cause
-         // Sometimes it wraps EJBException - let's unwrap it
-         Throwable cause = ex.detail;
+            Exception cause;
+            if (remoteTxRollback.detail instanceof Exception) 
+            {
+               cause = (Exception)remoteTxRollback.detail;
+            }
+            else if (remoteTxRollback.detail instanceof Error) 
+            {
+               String msg = formatException(
+                     "Unexpected Error A", 
+                     remoteTxRollback.detail);
+               cause = new EJBException(msg);
+            }
+            else 
+            {
+               String msg = formatException(
+                     "Unexpected Throwable B", 
+                     remoteTxRollback.detail);
+               cause = new EJBException(msg);
+            }
+
+            e = new TransactionRolledbackLocalException(
+                  remoteTxRollback.getMessage(),
+                  cause);
+         }
+
+         // If we got a local TransactionRolledbackLocalException for a remote
+         // invocation convert it into a TransactionRolledbackException
+         if (!isLocal && e instanceof TransactionRolledbackLocalException) 
+         {
+            TransactionRolledbackLocalException localTxRollback = 
+                  (TransactionRolledbackLocalException)e;
+            e = new TransactionRolledbackException(
+                  localTxRollback.getMessage());
+            ((TransactionRolledbackException)e).detail = 
+                  localTxRollback.getCausedByException();
+         }
+
+         // get the data we need for logging
+         Throwable cause = null;
+         String exceptionType = null;
+         if (e instanceof TransactionRolledbackException)
+         {
+            cause = ((TransactionRolledbackException)e).detail;
+            exceptionType = "TransactionRolledbackException";
+         } 
+         else 
+         {
+            cause = 
+               ((TransactionRolledbackLocalException)e).getCausedByException();
+            exceptionType = "TransactionRolledbackLocalException";
+         }
+
+         // log the exception
          if (cause != null)
          {
+            // if the cause is an EJBException unwrap it for logging
             if ((cause instanceof EJBException) &&
-            (((EJBException) cause).getCausedByException() != null))
+                  (((EJBException) cause).getCausedByException() != null))
             {
                cause = ((EJBException) cause).getCausedByException();
             }
-            log.error("TransactionRolledbackException, causedBy:", cause);
+            log.error(exceptionType + ", causedBy:", cause);
          }
          else
          {
-            log.error("TransactionRolledbackException:", ex);
+            log.error(exceptionType + ":", e);
          }
-         toThrow = ex;
+         return (Exception)e;
+      }
+      else if (e instanceof NoSuchEntityException)
+      {
+         NoSuchEntityException noSuchEntityException = 
+               (NoSuchEntityException) e;
+         if (noSuchEntityException.getCausedByException() != null)
+         {
+            log.error("NoSuchEntityException, causedBy:", 
+                  noSuchEntityException.getCausedByException());
+         }
+         else
+         {
+            log.error("NoSuchEntityException:", noSuchEntityException);
+         }
+
+         if (isLocal) 
+         {
+            return new NoSuchObjectLocalException(
+                  noSuchEntityException.getMessage(),
+                  noSuchEntityException.getCausedByException());
+         }
+         else 
+         {
+            NoSuchObjectException noSuchObjectException = 
+                  new NoSuchObjectException(noSuchEntityException.getMessage());
+            noSuchObjectException.detail = noSuchEntityException;
+            return noSuchObjectException;
+         }
+      }
+      else if (e instanceof EJBException)
+      {
+         EJBException ejbException = (EJBException) e;
+         if (ejbException.getCausedByException() != null)
+         {
+            log.error("EJBException, causedBy:", 
+                  ejbException.getCausedByException());
+         }
+         else
+         {
+            log.error("EJBException:", ejbException);
+         }
+
+         if (isLocal) 
+         {
+            return ejbException;
+         }
+         else 
+         {
+            // Remote invocation need a remote exception
+            return new ServerException("EJBException:", ejbException);
+         }
+      }
+      else if (e instanceof RuntimeException)
+      {
+         RuntimeException runtimeException = (RuntimeException)e;
+         log.error("RuntimeException:", runtimeException);
+
+         if (isLocal) 
+         {
+            return new EJBException("RuntimeException", runtimeException);
+         } 
+         else 
+         {
+            return new ServerException("RuntimeException", runtimeException);
+         }
+      }
+      else if (e instanceof Error)
+      {
+         log.error("Unexpected Error:", e);
+         if (isLocal) 
+         {
+            String msg = formatException("Unexpected Error C", e);
+            return new EJBException(msg);
+         }
+         else 
+         {
+            return new ServerError("Unexpected Error", (Error)e);
+         }
+      }
+      else if (e instanceof Exception)
+      {
+         if (callLogging)
+         {
+            log.info("Application Exception", e);
+         }
+         return (Exception)e;
       }
       else
       {
-         Exception ex = null;
-         if( e instanceof Exception )
-            ex = (Exception) e;
-         else if( e instanceof Error )
+         // The should not happen
+         String msg = formatException("Unexpected Throwable D", e);
+         log.warn("Unexpected Throwable", e);
+         if (isLocal)
          {
-            // The app should not be throwing this show issue a warning
-            Error err = (Error) e;
-            log.warn("Unexpected Error", err);
-            ex = new ServerError("Unexpected Error", err);
+            return new EJBException(msg);
          }
          else
          {
-            // The app should not be throwing this show issue a warning
-            String msg = formatException("Unexpected Throwable", e);
-            log.warn("Unexpected Throwable", e);
-            ex = new ServerException(msg);
+            return new ServerException(msg);
          }
-         // Application exception, or (in case of RemoteException) already handled system exc
-         // Call debugging -> show exceptions
-         if(callLogging)
-            log.info("AppException", ex);
-         toThrow = ex;
       }
-      return toThrow;
    }
 
    private String formatException(String msg, Throwable t)
    {
       StringWriter sw = new StringWriter();
       PrintWriter pw = new PrintWriter(sw);
-      if( msg != null )
+      if (msg != null)
          pw.println(msg);
       t.printStackTrace(pw);
       return sw.toString();
    }
    
-  // Monitorable implementation ------------------------------------
-  public void sample(Object s)
-  {
-    // Just here to because Monitorable request it but will be removed soon
-  }
-  public Map retrieveStatistic()
-  {
-    return null;
-  }
-  public void resetStatistic()
-  {
-  }
+   // Monitorable implementation ------------------------------------
+   public void sample(Object s)
+   {
+      // Just here to because Monitorable request it but will be removed soon
+   }
+   public Map retrieveStatistic()
+   {
+      return null;
+   }
+   public void resetStatistic()
+   {
+   }
 }
