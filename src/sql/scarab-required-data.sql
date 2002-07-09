f3b4d73585783a3c63d923da0a98fb80d32c5b7a
jmcnally <jmcnally>
2002-07-09 14:43:56 +0000

    use single quotes to be more oracle compatible.

--- src/sql/scarab-required-data.sql
+++ src/sql/scarab-required-data.sql
@@ -127,19 +127,19 @@ insert into ID_TABLE (id_table_id, table_name, next_id, quantity) VALUES (0, 'GL
  * standard x-module search lists
  */
 insert into SCARAB_MIT_LIST (LIST_ID, NAME, ACTIVE, MODIFIABLE)
-    values (1, "All modules and issue types", 1, 0);
+    values (1, 'All modules and issue types', 1, 0);
 insert into SCARAB_MIT_LISTITEM (ITEM_ID, LIST_ID, MODULE_ID, ISSUE_TYPE_ID)
     values (1, 1, 0, 0);
 insert into SCARAB_MIT_LIST (LIST_ID, NAME, ACTIVE, MODIFIABLE)
-    values (2, "All issue types in the current module", 1, 0);
+    values (2, 'All issue types in the current module', 1, 0);
 insert into SCARAB_MIT_LISTITEM (ITEM_ID, LIST_ID, MODULE_ID, ISSUE_TYPE_ID)
     values (2, 2, NULL, 0);
 insert into SCARAB_MIT_LIST (LIST_ID, NAME, ACTIVE, MODIFIABLE)
-    values (3, "Current issue type in all modules", 1, 0);
+    values (3, 'Current issue type in all modules', 1, 0);
 insert into SCARAB_MIT_LISTITEM (ITEM_ID, LIST_ID, MODULE_ID, ISSUE_TYPE_ID)
     values (3, 3, 0, NULL);
 insert into SCARAB_MIT_LIST (LIST_ID, NAME, ACTIVE, MODIFIABLE)
-    values (4, "Current issue type in the current module", 0, 0);
+    values (4, 'Current issue type in the current module', 0, 0);
 insert into SCARAB_MIT_LISTITEM (ITEM_ID, LIST_ID, MODULE_ID, ISSUE_TYPE_ID)
     values (4, 4, NULL, NULL);
 
