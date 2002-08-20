6fa37b29ece559b9ad0f5f64a41221d5c9c41126
kataka <kataka>
2002-08-20 07:10:55 +0000

    Patch for issue 1033

--- src_new/org/argouml/uml/diagram/state/ui/FigTransition.java
+++ src_new/org/argouml/uml/diagram/state/ui/FigTransition.java
@@ -24,7 +24,7 @@
 // File: FigTransition.java
 // Classes: FigTransition
 // Original Author: your email address here
-// $Id: FigTransition.java,v 1.5 2002/08/19 09:24:46 kataka Exp $
+// $Id: FigTransition.java,v 1.6 2002/08/20 07:10:55 kataka Exp $
 
 package org.argouml.uml.diagram.state.ui;
 
@@ -163,13 +163,22 @@ public class FigTransition extends FigEdgeModelElement {
         }
         for (int i = 0; i < transitions.size(); i++) {
             FigEdge fig = ((FigEdge)getLayer().presentationFor(transitions.get(i)));
-            if ((xs.equals(fig.getXs()) && ys.equals(fig.getYs())) || (xs.equals(flip(fig.getXs())) && ys.equals(flip(fig.getYs())))) {
-                Point startPoint = getFirstPoint();
-                Point endPoint = getLastPoint();
+            // next lines patch for loading old 0.10.1 projects in 0.11.1 and further
+            if (fig == null) {
+            	Iterator it = ProjectBrowser.TheInstance.getProject().findFigsForMember(transitions.get(i)).iterator();
+            	if (it.hasNext()) {
+            		fig = (FigEdge)it.next();
+            	}
+            }
+            if (fig != null) {
+            	if ((xs.equals(fig.getXs()) && ys.equals(fig.getYs())) || (xs.equals(flip(fig.getXs())) && ys.equals(flip(fig.getYs())))) {
+                	Point startPoint = getFirstPoint();
+                	Point endPoint = getLastPoint();
                  
-                int x = (int)Math.round(Math.abs((startPoint.getX()-endPoint.getX())/2) + Math.min(startPoint.getX(), endPoint.getX()));
-                int y = (int)Math.round(Math.abs((startPoint.getY()-endPoint.getY())/2) + Math.min(startPoint.getY(), endPoint.getY()));
-                insertPoint(1, x, y);
+	                int x = (int)Math.round(Math.abs((startPoint.getX()-endPoint.getX())/2) + Math.min(startPoint.getX(), endPoint.getX()));
+	                int y = (int)Math.round(Math.abs((startPoint.getY()-endPoint.getY())/2) + Math.min(startPoint.getY(), endPoint.getY()));
+	                insertPoint(1, x, y);
+	            }
             }
         }
     }
