05ceb3ccef4f5cd7f1a174268e23e49a7a589130
jon <jon>
2002-08-28 20:02:24 +0000

    added some debugging worked out some bugs.
    -jon

--- build/release.sh
+++ build/release.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# $Id: release.sh,v 1.1 2002/08/27 21:10:13 jon Exp $
+# $Id: release.sh,v 1.2 2002/08/28 20:02:24 jon Exp $
 #
 # Procedure for release:
 #
@@ -21,9 +21,10 @@
 #
 
 TAG=
+#TAG=SCARAB_1_0_B10
 #TAG=SCARAB_1_0_B9
-VERSION=b9
-VERSIONNEXT=b10
+VERSION=b10
+VERSIONNEXT=b11
 
 if [ -z ${TAG} ] ; then
     echo "This script is not for the faint of heart.";
@@ -34,22 +35,43 @@ fi
 CVSROOT=:pserver:${USER}@scarab.tigris.org:/cvs
 export CVSROOT
 
+echo "removing scarab from /tmp/scarab"
+rm -r /tmp/scarab
+
 cd /tmp
+START_DIR=`pwd`
+
+echo "startdir: ${START_DIR}" 
+echo "current dir1: "; echo `pwd`
 
 cvs co scarab
 
-cd scarab/build
+cd ${START_DIR}/scarab/build
+
+echo "current dir2: "; echo `pwd`
+
+echo "Doing new version replacement 1"
 perl -pi -e "s/${VERSION}-dev/${VERSION}/" default.properties
 cvs ci default.properties
 
-cd ../../
+cd ${START_DIR}
+echo "current dir3: "; echo `pwd`
 
+echo "tagging cvs"
 cvs tag ${TAG} scarab
 
+echo "updating cvs to the tag"
 cvs up -r ${TAG} scarab
 
-cd scarab/build
+cd ${START_DIR}/scarab/build
+echo "current dir4: "; echo `pwd`
+echo "running ant package"
 ant package
 
+echo "Doing new version replacement 2"
 perl -pi -e "s/${VERSION}/${VERSIONNEXT}-dev/" default.properties
+# remove the tag
+cvs up -A default.properties
 cvs ci default.properties
+
+echo "All done!"
