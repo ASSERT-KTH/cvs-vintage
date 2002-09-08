a8f07f3afce3f668ecd88895c25354ddcbc1a35d
kataka <kataka>
2002-09-08 19:24:12 +0000

    Tried to centralize building of generalizations Patched several internationalization bugs in proppanels Added several wellformednessrules for generalizations.

--- src_new/org/argouml/uml/diagram/static_structure/ClassDiagramGraphModel.java
+++ src_new/org/argouml/uml/diagram/static_structure/ClassDiagramGraphModel.java
@@ -24,7 +24,7 @@
 // File: ClassDiagramGraphModel.java
 // Classes: ClassDiagramGraphModel
 // Original Author: jrobbins@ics.uci.edu
-// $Id: ClassDiagramGraphModel.java,v 1.9 2002/08/19 20:43:50 kataka Exp $
+// $Id: ClassDiagramGraphModel.java,v 1.10 2002/09/08 19:24:12 kataka Exp $
 
 package org.argouml.uml.diagram.static_structure;
 
@@ -40,6 +40,7 @@ import ru.novosoft.uml.foundation.extension_mechanisms.MStereotype;
 import org.tigris.gef.graph.*;
 
 import org.argouml.model.uml.UmlFactory;
+import org.argouml.model.uml.foundation.core.CoreFactory;
 import org.argouml.uml.MMUtil;
 
 /** This class defines a bridge between the UML meta-model
@@ -435,9 +436,7 @@ implements MutableGraphModel, VetoableChangeListener, MElementListener {
           MInterface toIntf = (MInterface) toPort;
 
           if (edgeClass == MGeneralization.class) {
-              MGeneralization gen = UmlFactory.getFactory().getCore().createGeneralization();
-              gen.setChild(fromIntf);
-              gen.setParent(toIntf);
+              MGeneralization gen = CoreFactory.getFactory().buildGeneralization(fromIntf, toIntf);
               addEdge(gen);
               return gen;
           }
