a87b7f50bbb13c4446b0fdf0e91615afc0fe41f9
patriot1burke <patriot1burke>
2002-09-11 23:24:10 +0000

    do not insert transaction or security stuff into Invocation unless they are non-null this will avoid the serialization of a key and also avoid puts into the hashmaps.

--- src/main/org/jboss/proxy/SecurityInterceptor.java
+++ src/main/org/jboss/proxy/SecurityInterceptor.java
@@ -1,49 +1,81 @@
 /*
- * JBoss, the OpenSource J2EE webOS
- *
- * Distributable under LGPL license.
- * See terms of license at gnu.org.
- */
-
+* JBoss, the OpenSource J2EE webOS
+*
+* Distributable under LGPL license.
+* See terms of license at gnu.org.
+*/
 package org.jboss.proxy;
 
+import java.io.IOException;
+import java.io.ObjectOutput;
+import java.io.ObjectInput;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.lang.reflect.Method;
+
+import javax.ejb.EJBHome;
+import javax.ejb.EJBMetaData;
+import javax.ejb.RemoveException;
+import javax.ejb.Handle;
+import javax.ejb.EJBHome;
+import javax.ejb.EJBObject;
+import javax.ejb.HomeHandle;
+
+import org.jboss.invocation.Invoker;
+import org.jboss.proxy.ejb.handle.HomeHandleImpl;
 import org.jboss.invocation.Invocation;
 
 import org.jboss.security.SecurityAssociation;
 import org.jboss.security.SimplePrincipal;
 
+import java.security.Principal;
+/*
+import javax.naming.Name;
+import org.jboss.ejb.plugins.jrmp.server.JRMPContainerInvoker;
+*/
 /**
- * A interceptor which sets security related information on the
- * invocation.
- *      
- * @author <a href="mailto:marc.fleury@jboss.org">Marc Fleury</a>
- * @version $Revision: 1.4 $
- *
- * <p><b>2001/11/21: marcf</b>
- * <ol>
- *   <li>Initial checkin
- * </ol>
- */
+* The client-side proxy for an EJB Home object.
+*      
+* @author <a href="mailto:marc.fleury@jboss.org">Marc Fleury</a>
+* @version $Revision: 1.5 $
+*
+* <p><b>2001/11/21: marcf</b>
+* <ol>
+*   <li>Initial checkin
+* </ol>
+*/
 public class SecurityInterceptor
    extends Interceptor
 {
+   // Constants -----------------------------------------------------
+
    /** Serial Version Identifier. */
-   // private static final long serialVersionUID = 432426690456622923L;
+//   private static final long serialVersionUID = 432426690456622923L;
+   
+   // Static --------------------------------------------------------
+   
+   // Attributes ----------------------------------------------------
+   
+   // Constructors --------------------------------------------------
    
    /**
-    * No-argument constructor for externalization.
-    */
-   public SecurityInterceptor()
-   {
-      super();
-   }
+   * No-argument constructor for externalization.
+   */
+   public SecurityInterceptor() {}
+   
+ 
+   // Public --------------------------------------------------------
    
    public Object invoke(Invocation invocation)
-      throws Throwable
+   throws Throwable
    {
       // Get Principal and credentials 
-      invocation.setPrincipal(SecurityAssociation.getPrincipal()); 
-      invocation.setCredential(SecurityAssociation.getCredential());
+      Principal principal = SecurityAssociation.getPrincipal();
+      if (principal != null) invocation.setPrincipal(principal); 
+
+      Object credential = SecurityAssociation.getCredential();
+      if (credential != null) invocation.setCredential(credential);
       
       return getNext().invoke(invocation);
    }
--- src/main/org/jboss/proxy/TransactionInterceptor.java
+++ src/main/org/jboss/proxy/TransactionInterceptor.java
@@ -1,60 +1,92 @@
 /*
- * JBoss, the OpenSource J2EE webOS
- *
- * Distributable under LGPL license.
- * See terms of license at gnu.org.
- */
-
+* JBoss, the OpenSource J2EE webOS
+*
+* Distributable under LGPL license.
+* See terms of license at gnu.org.
+*/
 package org.jboss.proxy;
 
+import java.io.IOException;
+import java.io.ObjectOutput;
+import java.io.ObjectInput;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.lang.reflect.Method;
+
+import javax.ejb.EJBHome;
+import javax.ejb.EJBMetaData;
+import javax.ejb.RemoveException;
+import javax.ejb.Handle;
+import javax.ejb.EJBHome;
+import javax.ejb.EJBObject;
+import javax.ejb.HomeHandle;
 import javax.transaction.TransactionManager;
+import javax.transaction.Transaction;
 
 import org.jboss.proxy.Interceptor;
-
+import org.jboss.invocation.Invoker;
+import org.jboss.proxy.ejb.handle.HomeHandleImpl;
 import org.jboss.invocation.Invocation;
 
+/*
+import javax.naming.Name;
+import org.jboss.ejb.plugins.jrmp.server.JRMPContainerInvoker;
+*/
 /**
- * An interceptor whichs sets the transaction on the invocation
- * if the transaction manager has been previously given to us.
- *      
- * @author <a href="mailto:marc.fleury@jboss.org">Marc Fleury</a>
- * @version $Revision: 1.4 $
- *
- * <p><b>2001/11/21: marcf</b>
- * <ul>
- *   <li>Initial checkin
- * </ul>
- */
+* The client-side proxy for an EJB Home object.
+*      
+* @author <a href="mailto:marc.fleury@jboss.org">Marc Fleury</a>
+* @version $Revision: 1.5 $
+*
+* <p><b>2001/11/21: marcf</b>
+* <ol>
+*   <li>Initial checkin
+* </ol>
+*/
 public class TransactionInterceptor
-   extends Interceptor
+extends Interceptor
 {
+   // Constants -----------------------------------------------------
+   
    /** Serial Version Identifier. */
-   // private static final long serialVersionUID = 432426690456622923L;
+   //   private static final long serialVersionUID = 432426690456622923L;
+   
       
-   private static TransactionManager tm;
+   public static TransactionManager tm;
+
+   
+   // Constructors --------------------------------------------------
    
    /**
-    * No-argument constructor for externalization.
-    */
-   public TransactionInterceptor()
-   {
-      super();
-   }
-    
+   * No-argument constructor for externalization.
+   */
+   public TransactionInterceptor() {}
+   
+   
+   // Public --------------------------------------------------------
+   
    public Object invoke(Invocation invocation) 
-      throws Throwable
+   throws Throwable
    {
-      if (tm != null) {
-         invocation.setTransaction(tm.getTransaction());
+      if (tm != null)
+      {
+         Transaction tx = tm.getTransaction();
+         if (tx != null) invocation.setTransaction(tx);
       }
-      
       return getNext().invoke(invocation);
    }
    
-   /**
-    * Set the transaction manaer.
-    */
-   public static void setTransactionManager(final TransactionManager tm) {
-      TransactionInterceptor.tm = tm;
-   }
+   
+      /** Transaction manager. */
+   public static void setTransactionManager(TransactionManager tmx) { tm = tmx;}
+   
+   
+   // Package protected ---------------------------------------------
+   
+   // Protected -----------------------------------------------------
+   
+   // Private -------------------------------------------------------
+   
+   // Inner classes -------------------------------------------------
 }
