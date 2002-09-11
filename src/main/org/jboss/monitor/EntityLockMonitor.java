d081fb8caa692460d83bcb5e6f1bc30567484a27
patriot1burke <patriot1burke>
2002-09-11 23:16:44 +0000

    Monitors entity bean lock contention and time waiting for locks as well as timeouts waiting for locks.

---
+++ src/main/org/jboss/monitor/EntityLockMonitor.java
@@ -0,0 +1,185 @@
+/*
+ * JBoss, the OpenSource J2EE webOS
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+package org.jboss.monitor;
+
+
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+import javax.management.JMException;
+import javax.management.MBeanRegistration;
+import javax.management.MBeanServer;
+import javax.management.ObjectInstance;
+import javax.management.ObjectName;
+import org.jboss.ejb.Container;
+import org.jboss.ejb.EJBDeployer;
+import org.jboss.ejb.EJBDeployerMBean;
+import org.jboss.ejb.EjbModule;
+import org.jboss.ejb.EntityContainer;
+import org.jboss.ejb.InstanceCache;
+import org.jboss.ejb.StatefulSessionContainer;
+import org.jboss.logging.Logger;
+import org.jboss.system.ServiceMBeanSupport;
+import org.jboss.naming.NonSerializableFactory;
+import javax.naming.NamingException;
+import javax.naming.InitialContext;
+import javax.naming.Context;
+import javax.naming.Name;
+import javax.naming.NameNotFoundException;
+import javax.naming.StringRefAddr;
+import javax.naming.Reference;
+
+/**
+ *
+ * @see Monitorable
+ * @author <a href="mailto:bill@jboss.org">Bill Burke</a>
+ * @version $Revision: 1.2 $
+ */
+public class EntityLockMonitor
+   extends ServiceMBeanSupport
+   implements EntityLockMonitorMBean, MBeanRegistration
+{
+   // Constants ----------------------------------------------------
+   public static final String JNDI_NAME = "EntityLockMonitor";
+   // Attributes ---------------------------------------------------
+   static Logger log = Logger.getLogger(EntityLockMonitor.class);
+   MBeanServer m_mbeanServer;
+   // Static -------------------------------------------------------
+   
+   // Constructors -------------------------------------------------
+   public EntityLockMonitor()
+   {}
+   
+   // Public -------------------------------------------------------
+   
+   // MBeanRegistration implementation -----------------------------------
+   public ObjectName preRegister(MBeanServer server, ObjectName name)
+   throws Exception
+   {
+      m_mbeanServer = server;
+      return name;
+   }
+   
+   public void postRegister(Boolean registrationDone)
+   {}
+   public void preDeregister() throws Exception
+   {}
+   public void postDeregister()
+   {}
+
+   protected HashMap monitor = new HashMap();
+   public LockMonitor getEntityLockMonitor(String ejbName)
+   {
+      LockMonitor lm = null;
+      synchronized(monitor)
+      {
+         lm = (LockMonitor)monitor.get(ejbName);
+         if (lm == null)
+         {
+            lm = new LockMonitor();
+            monitor.put(ejbName, lm);
+         }
+      }
+      return lm;
+   }
+
+   public String printLockMonitor()
+   {
+      StringBuffer rtn = new StringBuffer();
+      rtn.append("<table width=\"1\" border=\"1\">");
+      rtn.append("<tr><td><b>EJB NAME</b></td><td><b>Total Lock Time</b></td><td><b>Num Contentions</b></td><td><b>Time Outs</b></td></tr>");
+      synchronized(monitor)
+      {
+         Iterator it = monitor.keySet().iterator();
+         while (it.hasNext())
+         {
+            rtn.append("<tr>");
+            String ejbName = (String)it.next();
+            rtn.append("<td>");
+            rtn.append(ejbName);
+            rtn.append("</td>");
+            LockMonitor lm = (LockMonitor)monitor.get(ejbName);
+            rtn.append("<td>");
+            rtn.append(("" + lm.total_time));
+            rtn.append("</td>");
+            rtn.append("<td>");
+            rtn.append(("" + lm.num_contentions));
+            rtn.append("</td>");
+            rtn.append("<td>");
+            rtn.append(("" + lm.timeouts));
+            rtn.append("</td>");
+            rtn.append("</tr>");
+         }
+      }
+      rtn.append("</table>");
+      return rtn.toString();
+   }
+   
+   public void clearMonitor()
+   {
+      synchronized(monitor)
+      {
+         Iterator it = monitor.keySet().iterator();
+         while (it.hasNext())
+         {
+            String ejbName = (String)it.next();
+            LockMonitor lm = (LockMonitor)monitor.get(ejbName);
+            synchronized (lm)
+            {
+               lm.timeouts = 0;
+               lm.total_time = 0;
+               lm.num_contentions = 0;
+            }
+         }
+      }
+   }
+   
+
+   protected void startService()
+      throws Exception
+   {
+      System.out.println("******started EntityLockMonitor********");
+      bind();
+   }
+
+   protected void stopService() {
+      try
+      {
+         unbind();
+      }
+      catch (Exception ignored) {}
+   }
+
+   private void bind() throws NamingException
+   {
+      Context ctx = new InitialContext();
+
+      // Ah ! We aren't serializable, so we use a helper class
+      NonSerializableFactory.bind(JNDI_NAME, this);
+      
+      // The helper class NonSerializableFactory uses address type nns, we go on to
+      // use the helper class to bind ourselves in JNDI
+      StringRefAddr addr = new StringRefAddr("nns", JNDI_NAME);
+      Reference ref = new Reference(EntityLockMonitor.class.getName(), addr, NonSerializableFactory.class.getName(), null);
+      ctx.bind(JNDI_NAME, ref);
+      System.out.println("^^^^^^^^ bounded EntityLockMonitor");
+   }
+   
+   private void unbind() throws NamingException
+   {
+      new InitialContext().unbind(JNDI_NAME);
+      NonSerializableFactory.unbind(JNDI_NAME);
+   }
+
+   // Inner classes -------------------------------------------------
+}
+
---
+++ src/main/org/jboss/monitor/LockMonitor.java
@@ -0,0 +1,9 @@
+package org.jboss.monitor;
+
+public class LockMonitor
+{
+   public long total_time = 0;
+   public long num_contentions = 0;
+   public long timeouts = 0;
+}
+
