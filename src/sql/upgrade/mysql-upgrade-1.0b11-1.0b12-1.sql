d5dc0d29c7141fc54ae57a01ddf6b95306255810
elicia <elicia>
2002-09-12 00:27:18 +0000

    upgrade script for new global issue type tables

---
+++ src/sql/upgrade/mysql-upgrade-1.0b11-1.0b12-1.sql
@@ -0,0 +1,52 @@
+/*
+ * new tables and changes to associate global issue types with attribute groups
+ *
+ * Created By: Elicia David
+ */
+
+# -----------------------------------------------------------------------
+# SCARAB_R_ISSUETYPE_ATTRIBUTE
+# -----------------------------------------------------------------------
+
+CREATE TABLE SCARAB_R_ISSUETYPE_ATTRIBUTE
+(
+    ATTRIBUTE_ID INTEGER NOT NULL,
+    ISSUE_TYPE_ID INTEGER NOT NULL,
+    PREFERRED_ORDER INTEGER NOT NULL,
+    ACTIVE INTEGER (1) NOT NULL default 1,
+    REQUIRED INTEGER (1) NOT NULL default 0,
+    QUICK_SEARCH INTEGER (1) NOT NULL default 0,
+    DEFAULT_TEXT_FLAG INTEGER (1) NOT NULL default 0,
+    LOCKED INTEGER (1) default 0,
+    PRIMARY KEY(ATTRIBUTE_ID, ISSUE_TYPE_ID),
+    FOREIGN KEY (ATTRIBUTE_ID) REFERENCES SCARAB_ATTRIBUTE (ATTRIBUTE_ID),
+    FOREIGN KEY (ISSUE_TYPE_ID) REFERENCES SCARAB_ISSUE_TYPE (ISSUE_TYPE_ID)
+);
+
+# -----------------------------------------------------------------------
+# SCARAB_R_ISSUETYPE_OPTION
+# -----------------------------------------------------------------------
+
+CREATE TABLE SCARAB_R_ISSUETYPE_OPTION
+(
+    OPTION_ID INTEGER NOT NULL,
+    ISSUE_TYPE_ID INTEGER NOT NULL,
+    PREFERRED_ORDER INTEGER,
+    WEIGHT INTEGER,
+    ACTIVE INTEGER (1) NOT NULL default 1,
+    LOCKED INTEGER (1) default 0,
+    PRIMARY KEY(OPTION_ID, ISSUE_TYPE_ID),
+    FOREIGN KEY (OPTION_ID) REFERENCES SCARAB_ATTRIBUTE_OPTION (OPTION_ID),
+    FOREIGN KEY (ISSUE_TYPE_ID) REFERENCES SCARAB_ISSUE_TYPE (ISSUE_TYPE_ID)
+);
+
+
+# Add columns to ISSUE_TYPE
+ALTER TABLE SCARAB_ISSUE_TYPE ADD LOCKED INTEGER null;
+ALTER TABLE SCARAB_ISSUE_TYPE ADD DEDUPE INTEGER null;
+
+# Make the module_id column not required for the ATTRIBUTE_GROUP column
+# If Module_id is null, it is a group for a global issue type
+ALTER TABLE SCARAB_ATTRIBUTE_GROUP DROP MODULE_ID;
+ALTER TABLE SCARAB_ATTRIBUTE_GROUP ADD MODULE_ID INTEGER null;
+
