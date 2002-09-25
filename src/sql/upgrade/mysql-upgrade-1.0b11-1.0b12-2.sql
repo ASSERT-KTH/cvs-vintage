5ed451123bd1f4147c5f8a8a194e4e222d10ca16
jon <jon>
2002-09-25 23:57:17 +0000

    added docs

--- src/sql/upgrade/mysql-upgrade-1.0b11-1.0b12-2.sql
+++ src/sql/upgrade/mysql-upgrade-1.0b11-1.0b12-2.sql
@@ -1,2 +1,9 @@
-# Add columns to ISSUE_TYPE
+/*
+ * Add columns to ISSUE_TYPE
+ *
+ * $Id: mysql-upgrade-1.0b11-1.0b12-2.sql,v 1.2 2002/09/25 23:57:17 jon Exp $
+ *
+ * Created By: Elicia David
+ */
+
 ALTER TABLE SCARAB_ISSUE_TYPE ADD ISDEFAULT INTEGER (1) default 0;
