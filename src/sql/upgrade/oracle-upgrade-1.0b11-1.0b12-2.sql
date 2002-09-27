505158f87727b3ca68a9dee0e2d5dae95ad2cb0f
thierrylach <thierrylach>
2002-09-27 15:16:18 +0000

    Keep synchronized with original mysql.

--- src/sql/upgrade/oracle-upgrade-1.0b11-1.0b12-2.sql
+++ src/sql/upgrade/oracle-upgrade-1.0b11-1.0b12-2.sql
@@ -1,2 +1,10 @@
-/* Add columns to ISSUE_TYPE */
+/*
+ * Add columns to ISSUE_TYPE
+ *
+ * $Id: oracle-upgrade-1.0b11-1.0b12-2.sql,v 1.2 2002/09/27 15:16:18 thierrylach Exp $
+ *
+ * Created By: Elicia David
+ * Modified for Oracle By: Thierry Lach
+ */
+
 ALTER TABLE SCARAB_ISSUE_TYPE ADD ISDEFAULT NUMBER (1) default 0;
