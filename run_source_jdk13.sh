3051ca42642b3fb29c7acfbe927d83e8a0e9a64a
javaprog <javaprog>
2002-09-28 15:53:33 +0000

    [comment] added GPL

--- run_source_jdk13.sh
+++ run_source_jdk13.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 COLUMBA_HOME=`dirname $0`
-java -cp "$COLUMBA_HOME":"$COLUMBA_HOME"/ext/xalan.jar:"$COLUMBA_HOME"/ext/jaxp.jar:"$COLUMBA_HOME"/ext/parser.jar org.columba.main.Main
+java -cp "$COLUMBA_HOME":"$COLUMBA_HOME"/ext/xalan.jar:"$COLUMBA_HOME"/ext/jaxp.jar:"$COLUMBA_HOME"/ext/parser.jar:"$COLUMBA_HOME"/ext/jython.jar:"$COLUMBA_HOME"/ext/log4j.jar org.columba.main.Main
