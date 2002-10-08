76b5dbf657a35ce04d5969927c1dfa75523f856c
jmcnally <jmcnally>
2002-10-08 00:56:51 +0000

    use filesystem independent check for absolute path. fixes SCB206

--- src/java/org/tigris/scarab/util/word/LuceneAdapter.java
+++ src/java/org/tigris/scarab/util/word/LuceneAdapter.java
@@ -83,7 +83,7 @@ import org.apache.lucene.search.Hits;
  * Support for searching/indexing text
  *
  * @author <a href="mailto:jmcnally@collab.net">John McNally</a>
- * @version $Id: LuceneAdapter.java,v 1.15 2002/10/01 00:02:54 jmcnally Exp $
+ * @version $Id: LuceneAdapter.java,v 1.16 2002/10/08 00:56:51 jmcnally Exp $
  */
 public class LuceneAdapter 
     implements SearchIndex
@@ -114,12 +114,13 @@ public class LuceneAdapter
         throws IOException
     {
         path = Turbine.getConfiguration().getString(INDEX_PATH);
-        if ( path.charAt(0) != '/' ) 
+        File indexDir = new File(path);
+        if ( !indexDir.isAbsolute() ) 
         {
             path = Turbine.getRealPath(path);
+            indexDir = new File(path);
         }
         
-        File indexDir = new File(path);
         boolean createIndex = false;
         if ( indexDir.exists() ) 
         {
