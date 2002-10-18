a9d839e9bfb2f04132c9042921f4a926c5265bb0
thierrylach <thierrylach>
2002-10-18 15:52:47 +0000

    Submitted by:  Thierry Lach
    SCARAB_GLOBAL_PARAMETER will need an idbroker.  Added to ID table as separate upgrade rather than including it in 1.0b12-1.0b13-3 since someone may already have run it.

---
+++ src/sql/upgrade/mysql-upgrade-1.0b12-1.0b13-4.sql
@@ -0,0 +1,8 @@
+/*
+ * Global Parameter file needs an id broker.
+ *
+ * Created By: Thierry Lach
+ */
+
+insert into ID_TABLE (id_table_id, table_name, next_id, quantity) VALUES (47, 'SCARAB_GLOBAL_PARAMETER', 100, 10);
+
