27500f01f0912209481ef17a7d93346e8022b3c9
elicia <elicia>
2002-10-22 21:16:26 +0000

    use new Issue | Move permission Issue number:  12339

--- src/java/org/tigris/scarab/actions/MoveIssue.java
+++ src/java/org/tigris/scarab/actions/MoveIssue.java
@@ -100,7 +100,7 @@ import org.tigris.scarab.services.security.ScarabSecurity;
  *
  * @author <a href="mailto:elicia@collab.net">Elicia David</a>
  * @author <a href="mailto:jon@collab.net">Jon S. Stevens</a>
- * @version $Id: MoveIssue.java,v 1.34 2002/10/10 01:05:44 jmcnally Exp $
+ * @version $Id: MoveIssue.java,v 1.35 2002/10/22 21:16:26 elicia Exp $
  */
 public class MoveIssue extends RequireLoginFirstAction
 {
@@ -131,13 +131,8 @@ public class MoveIssue extends RequireLoginFirstAction
         ScarabUser user = (ScarabUser)data.getUser();
 
         // Check permissions
-        String permission = ScarabSecurity.ISSUE__ENTER;
-        if (selectAction.equals("move"))
-        {
-            permission = ScarabSecurity.MODULE__EDIT;
-        }
-        if (!user.hasPermission(ScarabSecurity.ISSUE__ENTER, oldModule)
-            || !user.hasPermission(permission, newModule))
+        if (!user.hasPermission(ScarabSecurity.ISSUE__MOVE, oldModule)
+            || !user.hasPermission(ScarabSecurity.ISSUE__MOVE, newModule))
         {
             data.setMessage(l10n.get(NO_PERMISSION_MESSAGE));
             return;
