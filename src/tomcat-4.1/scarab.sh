0790078fe39f1533ebef403156191924bfc45e65
jon <jon>
2002-10-26 00:52:00 +0000

    tomcat 4.1.12

---
+++ src/tomcat-4.1/scarab.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ "${1}" = "-h" ] ; then
+    echo "Usage: scarab.sh [-l] [-h]"
+    echo "-l    Default JVM memory allocation"
+    echo "-h    This usage information"
+    exit
+fi
+
+if [ "${CLASSPATH}" != "" ] ; then
+    echo "Classpath: ${CLASSPATH}"
+fi
+
+if [ "${1}" != "-l" ] ; then
+    CATALINA_OPTS="-Xms96M -Xmx256M"
+    export CATALINA_OPTS
+fi
+
+./bin/catalina.sh run
