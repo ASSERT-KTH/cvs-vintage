a5f3f9e28a07e5eca671f239b416dd7414c576a6
elicia <elicia>
2002-11-05 22:36:00 +0000

    rolling back, changes are breaking AssignIssue

--- src/java/org/tigris/scarab/tools/ScarabRequestTool.java
+++ src/java/org/tigris/scarab/tools/ScarabRequestTool.java
@@ -1275,26 +1275,26 @@ try{
         }
         else
         {
-            try
+	    try
             {
-                issue = Issue.getIssueById(id);
-                if (issue == null)
-                {
-                   String code = getCurrentModule().getCode();
-                       id = code + id;
-                   issue = Issue.getIssueById(id);
-                }
-                if (issue.getDeleted())
-                {
-                    setAlertMessage(l10n.get("InvalidId"));
-                    issue = null;
-                }
-            }
-            catch (Exception e)
+	        issue = Issue.getIssueById(id);
+	        if (issue == null)
+	        {
+	           String code = getCurrentModule().getCode();
+                   id = code + id;
+	           issue = Issue.getIssueById(id);
+	        }
+            if (issue.getDeleted())
             {
                 setAlertMessage(l10n.get("InvalidId"));
-            }
+                issue = null;
+             }
+	    }        
+	    catch (Exception e)
+	    {
+	        setAlertMessage(l10n.get("InvalidId"));
 	    }
+        }
         return issue;
     }
 
@@ -1352,8 +1352,8 @@ try{
                 Iterator i = issueIds.iterator();
                 while (i.hasNext()) 
                 {
-                    Issue issue = getIssue((String)i.next());
-                }
+                    issues.add(getIssue((String)i.next()));
+                }            
             }
             else if (issueIds.get(0) instanceof NumberKey)
             {
@@ -1361,11 +1361,7 @@ try{
                 Iterator i = issueIds.iterator();
                 while (i.hasNext()) 
                 {
-                    Issue issue = IssueManager.getInstance((NumberKey)i.next());
-                    if (issue != null)
-                    {
-                        issues.add(issue);
-                    }
+                    issues.add(IssueManager.getInstance((NumberKey)i.next()));
                 }
             }
             else 
