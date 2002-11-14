d8043e14206f85dee74721cf15f016f7d6564712
jmcnally <jmcnally>
2002-11-14 18:07:28 +0000

    add the other attribute types to the indices
    Issue number:
    Obtained from:
    Submitted by:
    Reviewed by:

--- src/sql/upgrade/mysql-upgrade-1.0b13-1.0b14-6.sql
+++ src/sql/upgrade/mysql-upgrade-1.0b13-1.0b14-6.sql
@@ -2,6 +2,7 @@
  * Adds ISSUE_ID to the index which greatly improves searches for issues
  * based on option_ids (AdvancedQuery.vm).  I think that is what the old index
  * was meant to do as well, so it is replaced.
+ * Also adds similar indices for the other attribute types
  *
  * Created By: John McNally
  */
@@ -11,3 +12,10 @@ ALTER TABLE SCARAB_ISSUE_ATTRIBUTE_VALUE
 
 CREATE INDEX IX_AV_OPTION_ISSUE_DEL ON 
     SCARAB_ISSUE_ATTRIBUTE_VALUE (OPTION_ID, ISSUE_ID, DELETED);
+
+CREATE INDEX IX_AV_USER_ISSUE_DEL ON 
+    SCARAB_ISSUE_ATTRIBUTE_VALUE (USER_ID, ISSUE_ID, DELETED);
+CREATE INDEX IX_AV_INT_ISSUE_DEL ON 
+    SCARAB_ISSUE_ATTRIBUTE_VALUE (NUMERIC_VALUE, ISSUE_ID, DELETED);
+CREATE INDEX IX_AV_VALUE_ISSUE_DEL ON 
+    SCARAB_ISSUE_ATTRIBUTE_VALUE (VALUE, ISSUE_ID, DELETED);
