cb1c2cc1441c2bcb63a2544da3444788c79b99b6
jmcnally <jmcnally>
2002-11-18 04:38:27 +0000

    Added the ability to set a message which shows up on most screens unless a more specific message is applicable.  The message does not persist through a server restart.  The reason for the added functionality is to notify users of an impending action such as a server shutdown that might impact them, so persistence was not a priority, though it could be added.

---
+++ src/java/org/tigris/scarab/pipeline/SetInfoMessageValve.java
@@ -0,0 +1,95 @@
+package org.tigris.scarab.pipeline;
+
+/* ================================================================
+ * Copyright (c) 2001 Collab.Net.  All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 
+ * 3. The end-user documentation included with the redistribution, if
+ * any, must include the following acknowlegement: "This product includes
+ * software developed by Collab.Net <http://www.Collab.Net/>."
+ * Alternately, this acknowlegement may appear in the software itself, if
+ * and wherever such third-party acknowlegements normally appear.
+ * 
+ * 4. The hosted project names must not be used to endorse or promote
+ * products derived from this software without prior written
+ * permission. For written permission, please contact info@collab.net.
+ * 
+ * 5. Products derived from this software may not use the "Tigris" or 
+ * "Scarab" names nor may "Tigris" or "Scarab" appear in their names without 
+ * prior written permission of Collab.Net.
+ * 
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL COLLAB.NET OR ITS CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ====================================================================
+ * 
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of Collab.Net.
+ */ 
+
+import java.io.IOException;
+import org.apache.turbine.Turbine;
+import org.apache.turbine.RunData;
+import org.apache.turbine.ParameterParser;
+import org.apache.turbine.TurbineException;
+import org.apache.turbine.Valve;
+import org.apache.turbine.pipeline.AbstractValve;
+import org.apache.turbine.ValveContext;
+import org.apache.turbine.modules.Module;
+
+import org.tigris.scarab.tools.ScarabRequestTool;
+import org.tigris.scarab.util.ScarabConstants;
+
+/**
+ * This valve adds an informational message to the response. This valve exists
+ * early in the pipeline, so it is possible this message may get overridden
+ * by some more relevant message in a particular case.  But most actions and
+ * those requests that don't involve an action will not set the info message; 
+ * in these cases this message will be shown.  The message is set through an
+ * administrator action and only persists until the server is restarted.
+ *
+ * @author <a href="mailto:jmcnally@collab.net">John McNally</a>
+ * @version $Id: SetInfoMessageValve.java,v 1.1 2002/11/18 04:38:27 jmcnally Exp $
+ */
+public class SetInfoMessageValve 
+    extends AbstractValve
+{
+    private static String message = null;
+
+    /**
+     * @see org.apache.turbine.Valve#invoke(RunData, ValveContext)
+     */
+    public void invoke( RunData data, ValveContext context )
+        throws IOException, TurbineException
+    {
+        ((ScarabRequestTool)Module.getTemplateContext(data)
+            .get(ScarabConstants.SCARAB_REQUEST_TOOL)).setInfoMessage(message);
+
+        // Pass control to the next Valve in the Pipeline
+        context.invokeNext( data );
+    }
+
+    public static void setMessage(String msg)
+    {
+        message = msg;
+    }
+}
