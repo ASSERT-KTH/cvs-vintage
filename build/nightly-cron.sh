9d6f4037db4e95fb42de06cb7e92cd8aedad64b5
jon <jon>
2002-11-18 18:22:37 +0000

    fixed the regex to catch more stuff
    -jon

--- build/nightly-cron.sh
+++ build/nightly-cron.sh
@@ -4,7 +4,7 @@
 # that checks the output for status and mails it to
 # a specific email address.
 #
-# $Id: nightly-cron.sh,v 1.2 2002/10/29 21:58:32 jon Exp $
+# $Id: nightly-cron.sh,v 1.3 2002/11/18 18:22:37 jon Exp $
 
 LOGFILE=/tmp/nightly.log
 LIST='dev@scarab.tigris.org'
@@ -15,7 +15,7 @@ rm -rf $LOGFILE
 ./nightly.sh > $LOGFILE 2>&1
 
 # find results from the tests
-STAT=$(sed -n '/^.*Tests run:[ 0-9,]*/s///p' $LOGFILE)
+STAT=$(sed -n '/^.*Tests run:[ 0-9,]*/s///p;/BUILD FAILED/s//&/p;tX;d;:X;q' $LOGFILE)
 
 case $STAT in
     *0*0|"") STAT=OK ;;
