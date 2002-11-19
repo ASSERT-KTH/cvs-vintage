abfe7b8355963fad437d9a816f40265f3e9aef21
jon <jon>
2002-11-19 22:28:20 +0000

    was getting errors...so i removed the stuff at the end...
    sed: 1: "/^.*Tests run:[ 0-9,]*/ ...": undefined label 'X;d;:X;q'

--- build/nightly-cron.sh
+++ build/nightly-cron.sh
@@ -4,7 +4,7 @@
 # that checks the output for status and mails it to
 # a specific email address.
 #
-# $Id: nightly-cron.sh,v 1.3 2002/11/18 18:22:37 jon Exp $
+# $Id: nightly-cron.sh,v 1.4 2002/11/19 22:28:20 jon Exp $
 
 LOGFILE=/tmp/nightly.log
 LIST='dev@scarab.tigris.org'
@@ -15,7 +15,7 @@ rm -rf $LOGFILE
 ./nightly.sh > $LOGFILE 2>&1
 
 # find results from the tests
-STAT=$(sed -n '/^.*Tests run:[ 0-9,]*/s///p;/BUILD FAILED/s//&/p;tX;d;:X;q' $LOGFILE)
+STAT=$(sed -n '/^.*Tests run:[ 0-9,]*/s///p;/BUILD FAILED/s//&/p;' $LOGFILE)
 
 case $STAT in
     *0*0|"") STAT=OK ;;
