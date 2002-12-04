0ad6736475a2170565d9613491024d855cfd99c5
javaprog <javaprog>
2002-12-04 13:45:15 +0000

    [bug] provide compatibility with Java 1.3, added GPL header

--- src/core/core/org/columba/core/command/CommandCancelledException.java
+++ src/core/core/org/columba/core/command/CommandCancelledException.java
@@ -1,4 +1,21 @@
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Library General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
 package org.columba.core.command;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 /**
  * @author Timo Stich (tstich@users.sourceforge.net)
@@ -27,7 +44,8 @@ public class CommandCancelledException extends Exception {
 	 * @param cause
 	 */
 	public CommandCancelledException(String message, Throwable cause) {
-		super(message, cause);
+		this(message);
+		compatibleInitCause(cause);
 	}
 
 	/**
@@ -35,7 +53,16 @@ public class CommandCancelledException extends Exception {
 	 * @param cause
 	 */
 	public CommandCancelledException(Throwable cause) {
-		super(cause);
+		this();
+		compatibleInitCause(cause);
 	}
-
+	
+	private void compatibleInitCause(Throwable cause) {
+		try{
+			Method initCause = getClass().getMethod("initCause", new Class[]{ Throwable.class });
+			initCause.invoke(this, new Object[]{ cause });
+		}catch(NoSuchMethodException nsme){
+		}catch(IllegalAccessException iae){
+		}catch(InvocationTargetException ite){}
+	}
 }
