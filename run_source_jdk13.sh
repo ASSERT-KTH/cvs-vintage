68d1646f62021e65870ca06c7e00ef5cbafb466b
javaprog <javaprog>
2002-12-04 15:12:58 +0000

    [bug] updated classpath according to merge changes

--- run_source_jdk13.sh
+++ run_source_jdk13.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 COLUMBA_HOME=`dirname $0`
-java -cp "$COLUMBA_HOME":"$COLUMBA_HOME"/ext/xalan.jar:"$COLUMBA_HOME"/ext/jaxp.jar:"$COLUMBA_HOME"/ext/parser.jar:"$COLUMBA_HOME"/ext/jython.jar:"$COLUMBA_HOME"/ext/log4j.jar org.columba.main.Main
+java -cp "$COLUMBA_HOME":"$COLUMBA_HOME"/lib/xalan.jar:"$COLUMBA_HOME"/lib/jaxp.jar:"$COLUMBA_HOME"/lib/parser.jar:"$COLUMBA_HOME"/lib/jython.jar:"$COLUMBA_HOME"/lib/log4j.jar org.columba.main.Main
