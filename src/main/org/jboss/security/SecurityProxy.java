484210610ee3e684e7697a86f6f08acd07566198
starksm <starksm>
2002-12-15 22:45:15 +0000

    Add an init method that takes the local home and local interface classes     to support proxying local method invocations

--- src/main/org/jboss/security/SecurityProxy.java
+++ src/main/org/jboss/security/SecurityProxy.java
@@ -16,23 +16,42 @@ Custom security checks are those that cannot be described using the
 standard EJB deployment time declarative role based security.
 
 @author Scott.Stark@jboss.org
-@version $Revision: 1.4 $
- * @stereotype plug-in point
+@version $Revision: 1.5 $
 */
 public interface SecurityProxy
 {
-    public void init(Class beanHome, Class beanRemote, Object securityMgr) throws InstantiationException;
+   /** Inform a proxy of the context in which it is operating.
+    * @param beanHome The EJB remote home interface class
+    * @param beanRemote The EJB remote interface class
+    * @param securityMgr The security manager from the security domain
+    * @throws InstantiationException
+    */
+   public void init(Class beanHome, Class beanRemote, Object securityMgr)
+      throws InstantiationException;
+   /** Inform a proxy of the context in which it is operating.
+    * @param beanHome The EJB remote home interface class
+    * @param beanRemote The EJB remote interface class
+    * @param beanLocalHome The EJB local home interface class, may be null
+    * @param beanLocal The EJB local interface class, may be null
+    * @param securityMgr The security manager from the security domain
+    * @throws InstantiationException
+    */
+   public void init(Class beanHome, Class beanRemote,
+      Class beanLocalHome, Class beanLocal, Object securityMgr)
+      throws InstantiationException;
     /** Called prior to any method invocation to set the current EJB context.
     */
     public void setEJBContext(EJBContext ctx);
     /** Called to allow the security proxy to perform any custom security
-        checks required for the EJB home interface method.
-    @param m, the EJB home interface method? Or is this the EJB bean impl method?
+        checks required for the EJB remote or local home interface method.
+    @param m , the EJB home or local home interface method
+    @param args , the invocation args
     */
     public void invokeHome(Method m, Object[] args) throws SecurityException;
     /** Called to allow the security proxy to perform any custom security
-        checks required for the EJB remote interface method.
-    @param m, the EJB remote interface method? Or is this the EJB bean impl method?
+        checks required for the EJB remote or local interface method.
+    @param m , the EJB remote or local interface method
+    @param args , the invocation args
     @param bean, the EJB implementation class instance
     */
     public void invoke(Method m, Object[] args, Object bean) throws SecurityException;
