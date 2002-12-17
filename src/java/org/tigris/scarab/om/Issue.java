61a55770cab33f52b67c67f508632150bd8ce584
elicia <elicia>
2002-12-17 21:37:02 +0000

    when switching a user to another user attribute, create two activity records: one for a delete, one for an add (to the new attribute)

--- src/java/org/tigris/scarab/om/Issue.java
+++ src/java/org/tigris/scarab/om/Issue.java
@@ -93,7 +93,7 @@ import org.apache.commons.lang.StringUtils;
  * @author <a href="mailto:jmcnally@collab.net">John McNally</a>
  * @author <a href="mailto:jon@collab.net">Jon S. Stevens</a>
  * @author <a href="mailto:elicia@collab.net">Elicia David</a>
- * @version $Id: Issue.java,v 1.233 2002/12/17 16:19:30 jon Exp $
+ * @version $Id: Issue.java,v 1.234 2002/12/17 21:37:02 elicia Exp $
  */
 public class Issue 
     extends BaseIssue
@@ -499,7 +499,7 @@ public class Issue
      * Adds a comment to an issue.
      */
     public ActivitySet addComment(ActivitySet activitySet, 
-                           Attachment attachment, ScarabUser user)
+                                  Attachment attachment, ScarabUser user)
         throws Exception
     {
         String desc = Localization.getString(
@@ -2728,9 +2728,19 @@ public class Issue
             oldAttVal.startActivitySet(activitySet);
         }
 
-        // Save activity record
-        String actionString = getUserAttributeChangeString(assignee, assigner,
-                                                           oldAttr, newAttr);
+        // Save activity record for deletion of old assignment
+        String actionString = getUserDeleteString(assigner, assignee, 
+                                                  oldAttVal.getAttribute());
+        ActivityManager
+            .createUserActivity(this, oldAttVal.getAttribute(), 
+                                activitySet,
+                                actionString, new Attachment(),
+                                assignee.getUserId(), null);
+
+
+        // Save activity record for new assignment
+        actionString = getAssignUserChangeString(assigner, assignee, 
+                                                 newAttr);
         ActivityManager
             .createUserActivity(this, newAttr, activitySet,
                                 actionString, new Attachment(),
@@ -2792,7 +2802,7 @@ public class Issue
         // Save activity record
         String actionString = getUserDeleteString(assigner, assignee, attr);
         ActivityManager
-            .createUserActivity(attVal.getIssue(), attVal.getAttribute(), 
+            .createUserActivity(this, attVal.getAttribute(), 
                                 activitySet,
                                 actionString, new Attachment(),
                                 assignee.getUserId(), null);
