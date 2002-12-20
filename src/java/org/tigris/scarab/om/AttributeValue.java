b04609c6d59e825957f03c12ad246a108e2e9366
jon <jon>
2002-12-20 18:41:51 +0000

    increase the length of the description

--- src/java/org/tigris/scarab/om/AttributeValue.java
+++ src/java/org/tigris/scarab/om/AttributeValue.java
@@ -76,7 +76,7 @@ import org.tigris.scarab.om.Module;
  * @author <a href="mailto:jmcnally@collab.net">John McNally</a>
  * @author <a href="mailto:jon@collab.net">Jon S. Stevens</a>
  * @author <a href="mailto:elicia@collab.net">Elicia David</a>
- * @version $Id: AttributeValue.java,v 1.81 2002/12/20 03:27:08 jon Exp $
+ * @version $Id: AttributeValue.java,v 1.82 2002/12/20 18:41:51 jon Exp $
  */
 public abstract class AttributeValue 
     extends BaseAttributeValue
@@ -856,9 +856,9 @@ Leaving here so that John can remove or fix.
             else
             {
                 sb.append(" changed from '");
-                if (oldValue.length() > 25) 
+                if (oldValue.length() > 30) 
                 {
-                    sb.append(oldValue.substring(0,25)).append("...");
+                    sb.append(oldValue.substring(0,30)).append("...");
                 }
                 else
                 {
@@ -867,9 +867,9 @@ Leaving here so that John can remove or fix.
                 sb.append('\'');
             }
             sb.append(" to '");
-            if (newValue.length() > 25) 
+            if (newValue.length() > 30) 
             {
-                sb.append(newValue.substring(0,25)).append("...");
+                sb.append(newValue.substring(0,30)).append("...");
             }
             else
             {
@@ -879,7 +879,6 @@ Leaving here so that John can remove or fix.
         }
         return sb.toString();
     }
-
 
     /**
      * Sets the properties of one attribute value based on another 
