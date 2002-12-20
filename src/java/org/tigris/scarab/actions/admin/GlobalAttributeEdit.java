70a7e6cb39242ec81f2fcb107c5989a68e3a3fc9
elicia <elicia>
2002-12-20 22:04:18 +0000

    if adding global option fails, do not try adding to module or display success message

--- src/java/org/tigris/scarab/actions/admin/GlobalAttributeEdit.java
+++ src/java/org/tigris/scarab/actions/admin/GlobalAttributeEdit.java
@@ -75,7 +75,7 @@ import org.tigris.scarab.services.cache.ScarabCache;
  * This class deals with modifying Global Attributes.
  *
  * @author <a href="mailto:jon@collab.net">Jon S. Stevens</a>
- * @version $Id: GlobalAttributeEdit.java,v 1.31 2002/12/06 20:50:55 elicia Exp $
+ * @version $Id: GlobalAttributeEdit.java,v 1.32 2002/12/20 22:04:18 elicia Exp $
  */
 public class GlobalAttributeEdit extends RequireLoginFirstAction
 {
@@ -138,6 +138,7 @@ public class GlobalAttributeEdit extends RequireLoginFirstAction
         ScarabRequestTool scarabR = (ScarabRequestTool)context
            .get(ScarabConstants.SCARAB_REQUEST_TOOL);
         ScarabLocalizationTool l10n = getLocalizationTool(context);
+        boolean newAdded = false;
 
         if ( intake.isAllValid() ) 
         {
@@ -244,6 +245,7 @@ public class GlobalAttributeEdit extends RequireLoginFirstAction
                         {
                             newPCAO.save();
                             pcaoList.add(newPCAO);
+                            newAdded = true;
                         }
                         catch (Exception e)
                         {
@@ -254,7 +256,7 @@ public class GlobalAttributeEdit extends RequireLoginFirstAction
                         // If user came from editing a module,
                         // Add new option to module.
                         String lastTemplate = getCancelTemplate(data);
-                        if (lastTemplate != null && 
+                        if (newAdded && lastTemplate != null && 
                             lastTemplate.indexOf("Module") > -1)
                         {
                             IssueType issueType = scarabR.getIssueType();
@@ -281,9 +283,12 @@ public class GlobalAttributeEdit extends RequireLoginFirstAction
 
                         }
                     }
-                    scarabR.setConfirmMessage(
-                        l10n.get("AttributeOptionAdded") + 
-                        l10n.get(DEFAULT_MSG));
+                    if (newAdded)
+                    {
+                        scarabR.setConfirmMessage(
+                            l10n.get("AttributeOptionAdded") + 
+                            l10n.get(DEFAULT_MSG));
+                    }
 
                     // now remove the group to set the page stuff to null
                     intake.remove(newPCAOGroup);
