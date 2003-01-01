19ad0a96a66dbb6aa6ebe1d265d80c56ac78a228
kataka <kataka>
2003-01-01 22:22:16 +0000

    Changed Proppanelnamespace, package and model to new layout and cleaned them up

--- src_new/org/argouml/uml/ui/foundation/core/PropPanelNamespace.java
+++ src_new/org/argouml/uml/ui/foundation/core/PropPanelNamespace.java
@@ -1,4 +1,4 @@
-// $Id: PropPanelNamespace.java,v 1.7 2002/12/27 10:08:02 linus Exp $
+// $Id: PropPanelNamespace.java,v 1.8 2003/01/01 22:22:16 kataka Exp $
 // Copyright (c) 1996-2002 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
@@ -24,31 +24,33 @@
 
 package org.argouml.uml.ui.foundation.core;
 
+import javax.swing.ImageIcon;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+
 import org.argouml.model.uml.foundation.core.CoreFactory;
 import org.argouml.model.uml.modelmanagement.ModelManagementFactory;
 import org.argouml.swingext.Orientation;
-import org.argouml.ui.ProjectBrowser;
-import org.argouml.uml.ui.*;
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+import org.argouml.uml.ui.UMLLinkedList;
 
-import ru.novosoft.uml.model_management.*;
-import ru.novosoft.uml.foundation.core.*;
-import ru.novosoft.uml.*;
+import ru.novosoft.uml.foundation.core.MModelElement;
+import ru.novosoft.uml.foundation.core.MNamespace;
 
 
 public abstract class PropPanelNamespace extends PropPanelModelElement {
 
+	protected JScrollPane ownedElementsScroll;
 
   ////////////////////////////////////////////////////////////////
   // contructors
     public PropPanelNamespace(String panelName, ImageIcon icon, int columns) {
         super(panelName,icon,columns);
+        initialize();
     }
     
     public PropPanelNamespace(String title, ImageIcon icon, Orientation orientation) {
     	super(title, icon, orientation);
+    	initialize();
     }
 
     public PropPanelNamespace(String panelName,int columns) {
@@ -83,6 +85,11 @@ public abstract class PropPanelNamespace extends PropPanelModelElement {
             ns.addOwnedElement(ownedElem);
             navigateTo(ownedElem);
         }
+    }
+    
+    private void initialize() {
+        JList ownedElementsList  = new UMLLinkedList(new UMLNamespaceOwnedElementListModel());
+        ownedElementsScroll = new JScrollPane(ownedElementsList);
     }
 
 } /* end class PropPanelClass */
