d7f1d25909edad146ba9759bf9afe7ed84374a6b
javaprog <javaprog>
2003-01-12 13:52:55 +0000

    [intern] adapted classpath

--- run_source_jdk13.sh
+++ run_source_jdk13.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 COLUMBA_HOME=`dirname $0`
-java -cp "$COLUMBA_HOME":"$COLUMBA_HOME"/lib/xalan.jar:"$COLUMBA_HOME"/lib/jaxp.jar:"$COLUMBA_HOME"/lib/parser.jar:"$COLUMBA_HOME"/lib/jython.jar:"$COLUMBA_HOME"/lib/log4j.jar org.columba.main.Main
+java -cp "$COLUMBA_HOME":"$COLUMBA_HOME"/lib/xalan.jar:"$COLUMBA_HOME"/lib/jaxp.jar:"$COLUMBA_HOME"/lib/jython.jar:"$COLUMBA_HOME"/lib/log4j.jar org.columba.main.Main
