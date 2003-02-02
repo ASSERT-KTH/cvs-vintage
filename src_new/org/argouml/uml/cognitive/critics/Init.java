ef1fa9ad7564b45050e446b5d0e51ec79f94659d
kataka <kataka>
2003-02-02 18:17:17 +0000

    Fix for issue 1570

--- src_new/org/argouml/uml/cognitive/critics/Init.java
+++ src_new/org/argouml/uml/cognitive/critics/Init.java
@@ -24,7 +24,7 @@
 // File: Init.java
 // Classes: Init
 // Original Author: jrobbins@ics.uci.edu
-// $Id: Init.java,v 1.19 2003/02/02 17:21:25 kataka Exp $
+// $Id: Init.java,v 1.20 2003/02/02 18:17:17 kataka Exp $
 
 // 8 Mar 2002: Jeremy Bennett (mail@jeremybennett.com). CrCrossNamespaceAssoc
 // registration corrected to Association, rather than AssociationClass
@@ -377,6 +377,7 @@ public class Init {
       Agency.register(crOppEndConflict, assocEndCls);
       Agency.register(new CrMultiComposite(), assocEndCls);
       Agency.register(new CrNameConflict(), namespaceCls);
+      Agency.register(crAlreadyRealizes, classCls);
 
       // under testing
       // Agency.register(crConsiderFacade, packageCls);
