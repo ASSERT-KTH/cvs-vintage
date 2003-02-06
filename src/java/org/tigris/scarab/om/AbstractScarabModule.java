2071ce155539b00884a561c81e36e8c2e530a3f6
elicia <elicia>
2003-02-06 23:33:38 +0000

    * ScarabBundle_en_US.properties: fix error message involving artifact types in nav * AbstractScarabModule.java: add artifact types with "appear in nav" property off by default Issue number:  14688

--- src/java/org/tigris/scarab/om/AbstractScarabModule.java
+++ src/java/org/tigris/scarab/om/AbstractScarabModule.java
@@ -126,7 +126,7 @@ import org.tigris.scarab.reports.ReportBridge;
  *
  * @author <a href="mailto:jon@collab.net">Jon S. Stevens</a>
  * @author <a href="mailto:jmcnally@collab.net">John McNally</a>
- * @version $Id: AbstractScarabModule.java,v 1.78 2003/02/06 03:27:23 jmcnally Exp $
+ * @version $Id: AbstractScarabModule.java,v 1.79 2003/02/06 23:33:38 elicia Exp $
  */
 public abstract class AbstractScarabModule
     extends BaseObject
@@ -1857,7 +1857,7 @@ public abstract class AbstractScarabModule
         rmit.setModuleId(getModuleId());
         rmit.setIssueTypeId(issueType.getIssueTypeId());
         rmit.setActive(true);
-        rmit.setDisplay(true);
+        rmit.setDisplay(false);
         rmit.setOrder(getRModuleIssueTypes().size() + 1);
         rmit.save();
 
