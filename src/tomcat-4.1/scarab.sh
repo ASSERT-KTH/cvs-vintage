c489602c54d690ba4be8db5b49df73ffafa4135d
dlr <dlr>
2003-02-14 19:19:24 +0000

    * src/tomcat-4.1/scarab.sh    Added commands recognized as "help me," printing usage.  Switch from    shell "if" to "case" for readability.
       Allow invocation of this script from any location, rather than
       assuming that it's in your current working directory and looking for
       catalina.sh accordingly.

--- src/tomcat-4.1/scarab.sh
+++ src/tomcat-4.1/scarab.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-
-if [ "${1}" = "-h" ] ; then
+case "${1}" in
+    -h|--help|help)
     echo "Usage: scarab.sh [-l] [-h]"
     echo "-l    Default JVM memory allocation"
     echo "-h    This usage information"
     exit
-fi
+    ;;
+esac
 
 if [ "${CLASSPATH}" != "" ] ; then
     echo "Classpath: ${CLASSPATH}"
@@ -16,4 +17,9 @@ if [ "${1}" != "-l" ] ; then
     export CATALINA_OPTS
 fi
 
-./bin/catalina.sh run
+WEBAPP_DIR="`dirname ${0} 2> /dev/null`"
+if [ -z "${WEBAPP_DIR}" -o ! -d "${WEBAPP_DIR}" ]; then
+    WEBAPP_DIR='.'
+fi
+
+"${WEBAPP_DIR}/bin/catalina.sh" run
