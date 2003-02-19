3937afc6530056cb4001eadd0bee40ae9275dfd5
jboynes <jboynes>
2003-02-19 03:47:15 +0000

    Moved into ejb package

---
+++ src/main/org/jboss/cmp/ejb/ejbql/ASTEJBQL.java
@@ -0,0 +1,23 @@
+/***************************************
+ *                                     *
+ *  JBoss: The OpenSource J2EE WebOS   *
+ *                                     *
+ *  Distributable under LGPL license.  *
+ *  See terms of license at gnu.org.   *
+ *                                     *
+ ***************************************/
+
+package org.jboss.cmp.ejb.ejbql;
+
+public class ASTEJBQL extends SimpleNode
+{
+   public ASTEJBQL(int i)
+   {
+      super(i);
+   }
+
+   public Object jjtAccept(ParserVisitor visitor, Object data) throws CompileException
+   {
+      return visitor.visit(this, data);
+   }
+}
