faed25dae16a51dcb485eb4c6cc08274685201e6
cooperfbi <cooperfbi>
2003-03-02 10:39:41 +0000

    added informix and sybase JDBC create command implementations

---
+++ src/main/org/jboss/ejb/plugins/cmp/jdbc/informix/JDBCInformixCreateCommand.java
@@ -0,0 +1,47 @@
+/*
+ * JBoss, the OpenSource J2EE webOS
+ *
+ * Distributable under LGPL license.
+ * See terms of license at gnu.org.
+ */
+
+package org.jboss.ejb.plugins.cmp.jdbc.informix;
+
+import java.sql.PreparedStatement;
+
+import com.informix.jdbc.IfxStatement;
+
+import org.jboss.ejb.plugins.cmp.jdbc.JDBCAbstractVendorCreateCommand;
+
+/**
+ * JDBCInformixCreateCommand executes an INSERT INTO query.
+ * The command uses getSerial method from Informix native
+ * Statement interface implementation to fetch the generated key.
+ * It works under JDK versions 1.3 and 1.4.
+ *
+ * @author <a href="mailto:julien_viet@yahoo.fr">Julien Viet</a>
+ * @author <a href="mailto:loubyansky@hotmail.com">Alex Loubyansky</a>
+ *
+ * @version $Revision: 1.1 $
+ */
+public class JDBCInformixCreateCommand
+   extends JDBCAbstractVendorCreateCommand
+{
+
+   protected Object fetchPK(PreparedStatement statement) throws Exception
+   {
+      // cast to the wrapped statement
+      org.jboss.ejb.plugins.cmp.jdbc.WrappedStatement ws =
+         (org.jboss.ejb.plugins.cmp.jdbc.WrappedStatement) statement;
+      // fetch underlying statement and cast it to Informix native
+      IfxStatement ifxStmt = (IfxStatement) ws.getUnderlyingStatement();
+
+      int serialValue = ifxStmt.getSerial();
+      Object pk = null;
+      if (serialValue != 0) {
+          pk = new Integer(serialValue);
+      }
+      return pk;
+   }
+
+}
--- src/main/org/jboss/ejb/plugins/cmp/jdbc/sybase/JDBCSybaseCreateCommand.java
+++ src/main/org/jboss/ejb/plugins/cmp/jdbc/sybase/JDBCSybaseCreateCommand.java
@@ -5,7 +5,7 @@
  * See terms of license at gnu.org.
  */
 
-package org.jboss.ejb.plugins.cmp.jdbc.hsqldb;
+package org.jboss.ejb.plugins.cmp.jdbc.sybase;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -14,42 +14,44 @@ import org.jboss.ejb.plugins.cmp.jdbc.JDBCAbstractVendorCreateCommand;
 import org.jboss.ejb.plugins.cmp.jdbc.JDBCUtil;
 
 /**
- * JDBCHsqldbCreateCommand executes an INSERT INTO query.
- * The command uses a CALL IDENTITY() statement from hsqldb native
+ * JDBCCSybaseCreateCommand executes an INSERT INTO query.
+ * The command uses a SELECT @@IDENTITY statement from Sybase native
  * to fetch the generated PK.
  * It works under JDK versions 1.3 and 1.4.
  *
+ * @author <a href="mailto:s.m.f@softhome.net">Simone Milani-Foglia</a>
  * @author <a href="mailto:julien_viet@yahoo.fr">Julien Viet</a>
  * @author <a href="mailto:loubyansky@hotmail.com">Alex Loubyansky</a>
  *
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.1 $
  */
-public class JDBCHsqldbCreateCommand
+public class JDBCSybaseCreateCommand
    extends JDBCAbstractVendorCreateCommand
 {
 
-   /** Hsqldb statement that will fetch the last inserted key */
-   private static final String IDENTITY_SQL = "CALL IDENTITY()";
+   /** Informix statement that will fetch the last inserted key */
+   private static final String IDENTITY_SQL = "SELECT @@IDENTITY";
 
    protected Object fetchPK(PreparedStatement statement) throws Exception
    {
-      PreparedStatement ps = null;
+      PreparedStatement mySybaseStmt = null;
       try
       {
          Object pk = null;
          //Retrieve the last identity created for this Connection
-         ps = statement.getConnection().prepareStatement("IDENTITY_SQL");
-         ResultSet rs = ps.executeQuery();
+         mySybaseStmt = statement.getConnection().prepareStatement(IDENTITY_SQL);
+         ResultSet rs = mySybaseStmt.executeQuery();
 
-         if (rs.next())
+         if(rs.next())
          {
             pk = rs.getObject(1);
+            log.info("Generated PK:" + pk);
          }
          return pk;
       }
       finally
       {
-         JDBCUtil.safeClose(ps);
+         JDBCUtil.safeClose(mySybaseStmt);
       }
    }
 
