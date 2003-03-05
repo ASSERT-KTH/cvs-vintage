9486a435284f483cf8e7395bb99f86f8aef05706
loubyansky <loubyansky>
2003-03-05 16:45:52 +0000

    Refactcored and made UUIDKeyGeneratorFactory an MBean

--- src/main/org/jboss/ejb/plugins/keygenerator/uuid/UUIDKeyGenerator.java
+++ src/main/org/jboss/ejb/plugins/keygenerator/uuid/UUIDKeyGenerator.java
@@ -9,7 +9,6 @@ package org.jboss.ejb.plugins.keygenerator.uuid;
 import org.jboss.ejb.plugins.keygenerator.KeyGenerator;
 
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.security.SecureRandom;
 
 
@@ -17,14 +16,13 @@ import java.security.SecureRandom;
  * The implementation of UUID key generator
  * based on the algorithm from Floyd Marinescu's EJB Design Patterns.
  *
- * @author <a href="mailto:loubyansky@ukr.net">Alex Loubyansky</a>
+ * @author <a href="mailto:alex@jboss.org">Alex Loubyansky</a>
  *
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class UUIDKeyGenerator
    implements KeyGenerator
 {
-
    // Attributes ---------------------------------------------------
 
    /** secure random to provide nonrepeating seed */
@@ -34,65 +32,61 @@ public class UUIDKeyGenerator
    private String midValue;
 
    // Constructor --------------------------------------------------
-
    public UUIDKeyGenerator()
       throws Exception
    {
       // cache the middle part for UUID
-
-      StringBuffer buffer = new StringBuffer( 16 );
+      StringBuffer buffer = new StringBuffer(16);
 
       // construct host part of the uuid (8 hex digits)
       byte[] addr = InetAddress.getLocalHost().getAddress();
-      buffer.append( toHex( toInt(addr), 8 ) );
+      buffer.append(toHex(toInt(addr), 8));
 
       // append the hash code for this object (8 hex digits)
-      buffer.append( toHex( System.identityHashCode(this), 8 ) );
+      buffer.append(toHex(System.identityHashCode(this), 8));
 
       // set up midValue
       midValue = buffer.toString();
 
       // load up the randomizer
       seeder = new SecureRandom();
-      int node = seeder.nextInt();
+      seeder.nextInt();
    }
 
    // KeyGenerator implementation ----------------------------------
-
    public Object generateKey()
    {
-      StringBuffer buffer = new StringBuffer( 32 );
+      StringBuffer buffer = new StringBuffer(32);
 
       // append current time as unsigned int value
-      buffer.append(toHex((int)(System.currentTimeMillis() & 0xFFFFFFFF), 8));
+      buffer.append(toHex((int) (System.currentTimeMillis() & 0xFFFFFFFF), 8));
 
       // append cached midValue
-      buffer.append( midValue );
+      buffer.append(midValue);
 
       // append the next random int
-      buffer.append( toHex( seeder.nextInt(), 8 ) );
+      buffer.append(toHex(seeder.nextInt(), 8));
 
       // return the result
       return buffer.toString();
    }
 
    // Private ------------------------------------------------------
-
    /**
     * Converts int value to string hex representation
     */
-   private String toHex( int value, int length )
+   private String toHex(int value, int length)
    {
       // hex digits
       char[] hexDigits =
-         { '0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F' };
+         {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
-      StringBuffer buffer = new StringBuffer( length );
+      StringBuffer buffer = new StringBuffer(length);
       int shift = (length - 1) << 2;
       int i = -1;
-      while( ++i < length )
+      while(++i < length)
       {
-         buffer.append( hexDigits[(value >> shift) & 0x0000000F] );
+         buffer.append(hexDigits[(value >> shift) & 0x0000000F]);
          value <<= 4;
       }
       return buffer.toString();
@@ -101,14 +95,14 @@ public class UUIDKeyGenerator
    /**
     * Constructs int value from byte array
     */
-   private static int toInt( byte[] bytes )
+   private static int toInt(byte[] bytes)
    {
       int value = 0;
       int i = -1;
-      while( ++i < bytes.length )
+      while(++i < bytes.length)
       {
          value <<= 8;
-         value |= bytes[ i ];
+         value |= bytes[i];
       }
       return value;
    }
--- src/main/org/jboss/ejb/plugins/keygenerator/uuid/UUIDKeyGeneratorFactory.java
+++ src/main/org/jboss/ejb/plugins/keygenerator/uuid/UUIDKeyGeneratorFactory.java
@@ -8,31 +8,42 @@ package org.jboss.ejb.plugins.keygenerator.uuid;
 
 import org.jboss.ejb.plugins.keygenerator.KeyGeneratorFactory;
 import org.jboss.ejb.plugins.keygenerator.KeyGenerator;
+import org.jboss.system.ServiceMBeanSupport;
+import org.jboss.util.naming.Util;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import java.io.Serializable;
 
 /**
  * This is the factory for UUID key generator
  *
  * @author <a href="mailto:loubyansky@ukr.net">Alex Loubyansky</a>
  *
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public class UUIDKeyGeneratorFactory
-   implements KeyGeneratorFactory, java.io.Serializable
+   extends ServiceMBeanSupport
+   implements KeyGeneratorFactory, UUIDKeyGeneratorFactoryMBean, Serializable
 {
-   // Constants ----------------------------------------------------
+   // Attributes ---------------------------------------------------
+   private String factoryName;
 
-   public static final String JNDI_NAME = "UUIDKeyGeneratorFactory";
-
-   // KeyGeneratorFactory implementation -----------------------
+   // UUIDKeyGeneratorFactoryMBean implementation ------------------
+   public void setFactoryName(String factoryName)
+   {
+      this.factoryName = factoryName;
+   }
 
    /**
     * Returns the factory name
     */
    public String getFactoryName()
    {
-      return JNDI_NAME;
+      return factoryName;
    }
 
+   // KeyGeneratorFactory implementation ---------------------------
    /**
     * Returns a newly constructed key generator
     */
@@ -40,5 +51,34 @@ public class UUIDKeyGeneratorFactory
       throws Exception
    {
       return new UUIDKeyGenerator();
+   }
+
+   // ServiceMBeanSupport overridding ------------------------------
+   public void startService()
+   {
+      // bind the factory
+      try
+      {
+         Context ctx = new InitialContext();
+         Util.rebind(ctx, factoryName, this);
+      }
+      catch(Exception e)
+      {
+         log.error("Caught exception during startService()", e);
+      }
+   }
+
+   public void stopService()
+   {
+      // unbind the factory
+      try
+      {
+         Context ctx = new InitialContext();
+         Util.unbind(ctx, factoryName);
+      }
+      catch(Exception e)
+      {
+         log.error("Caught exception during stopService()", e);
+      }
    }
 }
