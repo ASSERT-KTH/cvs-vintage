35abbdfd98032e2d38f0979744aed7bf890451ff
jmcnally <jmcnally>
2003-03-17 16:40:14 +0000

    The removed code was added as mysql-upgrade-1.0b13-1.0b14-11.sql.

--- src/sql/upgrade/mysql-upgrade-1.0b13-1.0b14-10.sql
+++ src/sql/upgrade/mysql-upgrade-1.0b13-1.0b14-10.sql
@@ -5,4 +5,3 @@ insert into SCARAB_GLOBAL_PARAMETER values (2, 'email-enabled', 'T', NULL);
 insert into SCARAB_GLOBAL_PARAMETER values (3, 'email-include-issue-details', 'T', NULL);
 insert into SCARAB_GLOBAL_PARAMETER values (4, 'email-allow-module-overrides', 'T', NULL);
 
-alter table drop index NAME;
