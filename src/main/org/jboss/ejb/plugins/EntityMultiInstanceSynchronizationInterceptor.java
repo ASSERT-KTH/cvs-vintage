c220d6fb68e2bc868d292132b448abfa0bb5b9cd
dsundstrom <dsundstrom>
2003-03-23 04:59:13 +0000

    Updated for new EntityInvocationRegistry.

--- src/main/org/jboss/ejb/plugins/EntityMultiInstanceSynchronizationInterceptor.java
+++ src/main/org/jboss/ejb/plugins/EntityMultiInstanceSynchronizationInterceptor.java
@@ -16,23 +16,16 @@ import org.jboss.ejb.Container;
 import org.jboss.ejb.EntityContainer;
 import org.jboss.ejb.EntityEnterpriseContext;
 import org.jboss.ejb.InstancePool;
+import org.jboss.ejb.entity.EntitySynchronizationInterceptor;
 import org.jboss.metadata.ConfigurationMetaData;
 
 /**
- * The role of this interceptor is to synchronize the state of the cache with
- * the underlying storage.  It does this with the ejbLoad and ejbStore
- * semantics of the EJB specification.  In the presence of a transaction this
- * is triggered by transaction demarcation. It registers a callback with the
- * underlying transaction monitor through the JTA interfaces.  If there is no
- * transaction the policy is to store state upon returning from invocation.
- * The synchronization polices A,B,C of the specification are taken care of
- * here.
- *
- * <p><b>WARNING: critical code</b>, get approval from senior developers
- * before changing.
+ * @todo remove when multi instance is mergged as simple flas on 
+ * the enditiy container
  *
  * @author <a href="mailto:bill@burkecentral.com">Bill Burke</a>
- * @version $Revision: 1.8 $
+ * @author <a href="mailto:dain@daingroup.com">Dain Sundstrom</a>
+ * @version $Revision: 1.9 $
  */
 public class EntityMultiInstanceSynchronizationInterceptor
    extends EntitySynchronizationInterceptor
@@ -40,122 +33,16 @@ public class EntityMultiInstanceSynchronizationInterceptor
    public void create() throws Exception
    {
       super.create();
+
+      // DAIN: if not an EntityInstancePool should we throw an exception?
       InstancePool pool = getContainer().getInstancePool();
       if(pool instanceof EntityInstancePool)
       {
          ((EntityInstancePool)pool).setReclaim(true);
       }
-      // DAIN: if not an EntityInstancePool should we throw an exception?
-   }
 
-   protected Synchronization createSynchronization(
-         Transaction tx, 
-         EntityEnterpriseContext ctx)
-   {
-      return new MultiInstanceSynchronization(tx, ctx);
-   } 
- 
-   private class MultiInstanceSynchronization implements Synchronization
-   {
-      /**
-       *  The transaction we follow.
-       */
-      private Transaction tx;
-
-      /**
-       *  The context we manage.
-       */
-      private EntityEnterpriseContext ctx;
-
-      /**
-       * The context lock
-       */
-      private BeanLock lock;
- 
-      /**
-       *  Create a new instance synchronization instance.
-       */
-      MultiInstanceSynchronization(Transaction tx, EntityEnterpriseContext ctx)
-      {
-         this.tx = tx;
-         this.ctx = ctx;
-         this.lock = getContainer().getLockManager().getLock(ctx.getCacheKey());
-      }
-  
-      public void beforeCompletion()
-      {
-         //synchronization is handled by GlobalTxEntityMap.
-      }
-
-      public void afterCompletion(int status)
-      {
-         EntityContainer container = (EntityContainer)getContainer();
-   
-         // This is an independent point of entry. We need to make sure the
-         // thread is associated with the right context class loader
-         ClassLoader oldCl = Thread.currentThread().getContextClassLoader();
-         Thread.currentThread().setContextClassLoader(
-               container.getClassLoader());
-   
-         try
-         {
-            lock.sync();
-            try
-            {
-               try
-               {
-                  // If rolled back -> invalidate instance
-                  if (status != Status.STATUS_ROLLEDBACK)
-                  {
-                     switch (getCommitOption())
-                     {
-                        // Keep instance cached after tx commit
-                     case ConfigurationMetaData.A_COMMIT_OPTION:
-                        throw new IllegalStateException("Commit option A not allowed with this Interceptor");
-                        // Keep instance active, but invalidate state
-                     case ConfigurationMetaData.B_COMMIT_OPTION:
-                        break;
-                        // Invalidate everything AND Passivate instance
-                     case ConfigurationMetaData.C_COMMIT_OPTION:
-                        break;
-                     case ConfigurationMetaData.D_COMMIT_OPTION:
-                        throw new IllegalStateException("Commit option A not allowed with this Interceptor");
-                     }
-                  }
-                  try
-                  {
-                     container.passivateEntity(ctx);
-                  }
-                  catch (Exception ignored) {}
-                  
-                  container.getInstancePool().free(ctx);
-               }
-               finally
-               {
-                  boolean trace = log.isTraceEnabled();
-                  if(trace)
-                  {
-                     log.trace("afterCompletion, clear tx for ctx="+ctx+", tx="+tx);
-                  }
-
-                  lock.endTransaction(tx);
-                  
-                  if(trace)
-                  {
-                     log.trace("afterCompletion, sent notify on TxLock for ctx="+ctx);
-                  }
-               }
-            }
-            finally
-            {
-               lock.releaseSync();
-               Thread.currentThread().setContextClassLoader(oldCl);               
-            }
-         } 
-         catch (InterruptedException ex)
-         {
-            log.error("Failed to lock.sync: " + ex);
-         }
-      }
+      // Dain: remove this when Multi instance is merged as a simple flag 
+      // on the container
+      ((EntityContainer)getContainer()).setMultiInstance(true);
    }
 }
