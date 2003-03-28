57a1ac1be410363e4264e78e5f9a69d82099be2f
fdietz <fdietz>
2003-03-28 17:11:23 +0000

    [bug] fixed #660260 not RFC2822 compliant

--- src/mail/independent/org/columba/mail/message/Rfc822Header.java
+++ src/mail/independent/org/columba/mail/message/Rfc822Header.java
@@ -37,176 +37,164 @@ import java.util.Hashtable;
  *  - priority, Integer
  */
 
-public class Rfc822Header implements HeaderInterface
-{
+public class Rfc822Header implements HeaderInterface {
 
-    public static final int ALL = 0;
-    public static final int NORMAL = 1;
-    public static final int BRIEF = 2;
-    public static final int USER_DEF = 3;
+	public static final int ALL = 0;
+	public static final int NORMAL = 1;
+	public static final int BRIEF = 2;
+	public static final int USER_DEF = 3;
 
-        // this hashtable stores all headeritems
-    protected Hashtable hashTable;
+	// this hashtable stores all headeritems
+	protected Hashtable hashTable;
 
-        // do we need this variables anymore ??
-    private String mimeVer = new String();
+	// do we need this variables anymore ??
+	private String mimeVer = new String();
 
-        // standard constructor
-    public Rfc822Header()
-    {
-        hashTable = new Hashtable();
+	// standard constructor
+	public Rfc822Header() {
+		hashTable = new Hashtable();
 
+	}
 
-    }
-    
-    public Flags getFlags() {
+	public Flags getFlags() {
 		return new Flags(this);
 	}
 
-        // returns the header as given from mailserver
-        // the items are split by \n
-    public String getHeader()
-    {
-        Enumeration keys;
-        StringBuffer output = new StringBuffer();
-        String aktKey;
+	// returns the header as given from mailserver
+	// the items are split by \n
+	public String getHeader() {
+		Enumeration keys;
+		StringBuffer output = new StringBuffer();
+		String aktKey;
 
-        keys = hashTable.keys();
+		keys = hashTable.keys();
 
-        while( keys.hasMoreElements() ) {
-            aktKey = (String) keys.nextElement();
+		while (keys.hasMoreElements()) {
+			aktKey = (String) keys.nextElement();
 
-            if ( aktKey.indexOf("columba") == -1 )
-            {
-                output.append( aktKey );
-                output.append(": ");
+			if (aktKey.indexOf("columba") == -1) {
+				output.append(aktKey);
+				output.append(": ");
 
-		output.append( hashTable.get( aktKey ) );
+				output.append(hashTable.get(aktKey));
 
-                output.append("\n");
-            }
-        }
-
-        return output.toString();
-    }
-
-
-    public void setHashtable( Hashtable h )
-    {
-        hashTable = h;
-    }
-
-    public Hashtable getHashtable()
-    {
-        return hashTable;
-    }
-
-        // return headeritem
-    public Object get( String s )
-    {
-        Object result = null;
-
-        s = s.toLowerCase();
-
-            // does this item exist?
-        if ( hashTable.containsKey( s ) == true )
-        {
-            result = hashTable.get( s );
-        }
-
-        return result;
-    }
-
-        // set headeritem
-    public void set( String s, Object o)
-    {
-        s = s.toLowerCase();
-
-        if ( ( s != null ) && ( o != null ) )
-        hashTable.put( s, o );
-    }
-
-
-
-    public void setMimeVer( String par )
-    {
-	mimeVer = par;
-    }
-
-    public String getMimeVer()
-    {
-	return mimeVer;
-    }
-
-    public String getHeaderEntry( String type )
-        {
-            StringBuffer output = new StringBuffer( type );
-
-            if( type.equals("from") ) {
-                output.append(" : ");
-                output.append( get("From") );
-                output.append('\n');
-                return output.toString();
-            }
-            if( type.equals("to") ) {
-                output.append(" : ");
-                output.append( get("To") );
-                output.append('\n');
-                return output.toString();
-            }
-            if( type.equals("cc") ) {
-                output.append(" : ");
-                output.append( get("Cc"));
-                output.append('\n');
-                return output.toString();
-            }
-            if( type.equals("bcc") ) {
-                output.append(" : ");
-                output.append( get("Bcc"));
-                output.append('\n');
-                return output.toString();
-            }
-            if( type.equals("date") ) {
-                output.append(" : ");
-                output.append( get("Date"));
-                output.append('\n');
-                return output.toString();
-            }
-            if( type.equals("subject") ) {
-                output.append(" : ");
-                output.append( get("Subject") );
-                output.append('\n');
-                return output.toString();
-            }
-
-            return null;
-        }
-
-    public String getHeaderInfo(int mode)
-    {
-        return null;
-    }
-
-    public Object clone()
-    {
-        Rfc822Header header = new Rfc822Header();
-        header.setHashtable( (Hashtable) hashTable.clone() );
-
-        return header;
-    }
-    
-	public void printDebug()
-		{
-			for (Enumeration keys = hashTable.keys(); keys.hasMoreElements();) {
-				String key = (String) keys.nextElement();
-				
-				Object value = hashTable.get(key);
-				
-				System.out.println("key="+key+" value="+value.toString());
-			}	
-	
+				output.append("\n");
+			}
 		}
 
+		return output.toString();
+	}
 
+	public void setHashtable(Hashtable h) {
+		hashTable = h;
+	}
+
+	public Hashtable getHashtable() {
+		return hashTable;
+	}
+
+	// return headeritem
+	public Object get(String s) {
+		Object result = null;
+
+		//  BUGFIX #660269 
+		//   -> email-headers should start with upper-case letters
+		//   -> it is wrong to use the lower-case string 
+
+		//s = s.toLowerCase();
+
+		// does this item exist?
+		if (hashTable.containsKey(s) == true) {
+			result = hashTable.get(s);
+		}
+
+		return result;
+	}
+
+	// set headeritem
+	public void set(String s, Object o) {
+
+		// BUGFIX #660269 
+		//   -> email-headers should start with upper-case letters
+		//   -> it is wrong to use the lower-case string 
+
+		//s = s.toLowerCase();
+
+		if ((s != null) && (o != null))
+			hashTable.put(s, o);
+	}
+
+	public void setMimeVer(String par) {
+		mimeVer = par;
+	}
+
+	public String getMimeVer() {
+		return mimeVer;
+	}
+
+	public String getHeaderEntry(String type) {
+		StringBuffer output = new StringBuffer(type);
+
+		if (type.equals("from")) {
+			output.append(" : ");
+			output.append(get("From"));
+			output.append('\n');
+			return output.toString();
+		}
+		if (type.equals("to")) {
+			output.append(" : ");
+			output.append(get("To"));
+			output.append('\n');
+			return output.toString();
+		}
+		if (type.equals("cc")) {
+			output.append(" : ");
+			output.append(get("Cc"));
+			output.append('\n');
+			return output.toString();
+		}
+		if (type.equals("bcc")) {
+			output.append(" : ");
+			output.append(get("Bcc"));
+			output.append('\n');
+			return output.toString();
+		}
+		if (type.equals("date")) {
+			output.append(" : ");
+			output.append(get("Date"));
+			output.append('\n');
+			return output.toString();
+		}
+		if (type.equals("subject")) {
+			output.append(" : ");
+			output.append(get("Subject"));
+			output.append('\n');
+			return output.toString();
+		}
+
+		return null;
+	}
+
+	public String getHeaderInfo(int mode) {
+		return null;
+	}
+
+	public Object clone() {
+		Rfc822Header header = new Rfc822Header();
+		header.setHashtable((Hashtable) hashTable.clone());
+
+		return header;
+	}
+
+	public void printDebug() {
+		for (Enumeration keys = hashTable.keys(); keys.hasMoreElements();) {
+			String key = (String) keys.nextElement();
+
+			Object value = hashTable.get(key);
+
+			System.out.println("key=" + key + " value=" + value.toString());
+		}
+
+	}
 
 }
