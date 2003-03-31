b698e7c468a25620e9fcd3158e8ca607f86a9055
javaprog <javaprog>
2003-03-31 18:37:28 +0000

    [feature] introduced i18n

--- src/mail/core/org/columba/mail/gui/mimetype/ChooseViewerDialog.java
+++ src/mail/core/org/columba/mail/gui/mimetype/ChooseViewerDialog.java
@@ -13,6 +13,7 @@
 //Portions created by Frederik Dietz and Timo Stich are Copyright (C) 2003. 
 //
 //All Rights Reserved.
+
 package org.columba.mail.gui.mimetype;
 
 import java.awt.BorderLayout;
@@ -21,22 +22,16 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 
-import javax.swing.BorderFactory;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JCheckBox;
-import javax.swing.JComponent;
-import javax.swing.JDialog;
-import javax.swing.JFileChooser;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.KeyStroke;
+import javax.swing.*;
 
 import org.columba.core.gui.util.DialogStore;
+import org.columba.mail.util.MailResourceLoader;
 
 public class ChooseViewerDialog implements ActionListener {
+        public static final String CMD_OK = "OK";
+        public static final String CMD_CANCEL = "CANCEL";
+        public static final String CMD_SEARCH = "SEARCH";
+
 	private JTextField viewerName;
 	private String viewer = null;
 	private JCheckBox saveCButton;
@@ -46,14 +41,10 @@ public class ChooseViewerDialog implements ActionListener {
 		String contentType,
 		String contentSubtype,
 		boolean save) {
-		//LOCALIZE
-		// super(owner,"Select Viewer for "+contentType+"/"+contentSubtype,true);
 
-		dialog =
-			DialogStore.getDialog(
-				"Select Viewer for " + contentType + "/" + contentSubtype);
+		dialog = DialogStore.getDialog(MailResourceLoader.getString("dialog", "mimetypeviewer", "dialog_title") + " " + contentType + "/" + contentSubtype);
 		init(save);
-		dialog.setSize(300, 150);
+		dialog.pack();
 		dialog.setLocationRelativeTo(null);
 		dialog.setVisible(true);
 	}
@@ -63,35 +54,31 @@ public class ChooseViewerDialog implements ActionListener {
 		contentPane.setBorder(BorderFactory.createEmptyBorder(12, 12, 11, 11));
 		JPanel viewerPanel = new JPanel();
 		viewerPanel.setLayout(new BoxLayout(viewerPanel, BoxLayout.X_AXIS));
-		//LOCALIZE
-		JLabel label = new JLabel("Viewer:");
+		JLabel label = new JLabel(MailResourceLoader.getString("dialog", "mimetypeviewer", "viewer_label"));
 		viewerPanel.add(label);
 		viewerPanel.add(Box.createHorizontalStrut(5));
 		viewerName = new JTextField();
-		viewerName.setActionCommand("OK");
+		viewerName.setActionCommand(CMD_OK);
 		viewerName.addActionListener(this);
 		label.setLabelFor(viewerName);
 		viewerPanel.add(viewerName);
 		viewerPanel.add(Box.createHorizontalStrut(10));
 		JButton searchButton = new JButton("...");
 		searchButton.addActionListener(this);
-		searchButton.setActionCommand("SEARCH");
+		searchButton.setActionCommand(CMD_SEARCH);
 		viewerPanel.add(searchButton);
 		contentPane.add(viewerPanel, BorderLayout.NORTH);
-		//LOCALIZE
-		saveCButton = new JCheckBox("Always open with this Viewer", save);
+		saveCButton = new JCheckBox(MailResourceLoader.getString("dialog", "mimetypeviewer", "save_viewer"), save);
 		saveCButton.setBorder(BorderFactory.createEmptyBorder(10, 0, 17, 0));
 		contentPane.add(saveCButton, BorderLayout.CENTER);
 		JPanel bottomPanel = new JPanel(new BorderLayout(0, 0));
 		JPanel buttonPanel = new JPanel(new GridLayout(1, 2, 5, 0));
-		//LOCALIZE
-		JButton okButton = new JButton("OK");
+		JButton okButton = new JButton(MailResourceLoader.getString("global", "ok"));
 		okButton.addActionListener(this);
-		okButton.setActionCommand("OK");
+		okButton.setActionCommand(CMD_OK);
 		buttonPanel.add(okButton);
-		//LOCALIZE
-		JButton cancelButton = new JButton("Cancel");
-		cancelButton.setActionCommand("CANCEL");
+		JButton cancelButton = new JButton(MailResourceLoader.getString("global", "cancel"));
+		cancelButton.setActionCommand(CMD_CANCEL);
 		cancelButton.addActionListener(this);
 		buttonPanel.add(cancelButton);
 		bottomPanel.add(buttonPanel, BorderLayout.EAST);
@@ -100,7 +87,7 @@ public class ChooseViewerDialog implements ActionListener {
 		dialog.getRootPane().setDefaultButton(okButton);
 		dialog.getRootPane().registerKeyboardAction(
 			this,
-			"CANCEL",
+			CMD_CANCEL,
 			KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
 			JComponent.WHEN_IN_FOCUSED_WINDOW);
 	}
@@ -115,18 +102,16 @@ public class ChooseViewerDialog implements ActionListener {
 
 	public void actionPerformed(ActionEvent e) {
 		String command = e.getActionCommand();
-		if (command.equals("SEARCH")) {
+		if (command == CMD_SEARCH) {
 			JFileChooser fileChooser = new JFileChooser();
-			//LOCALIZE
-			fileChooser.setDialogTitle("Choose Program");
-			int returnVal = fileChooser.showDialog(dialog, "OK");
-			if (returnVal == JFileChooser.APPROVE_OPTION) {
+			fileChooser.setDialogTitle(MailResourceLoader.getString("dialog", "mimetypeviewer", "choose_viewer"));
+			if (fileChooser.showDialog(dialog, MailResourceLoader.getString("global", "ok")) == JFileChooser.APPROVE_OPTION) {
 				viewerName.setText(fileChooser.getSelectedFile().toString());
 			}
-		} else if (command.equals("OK")) {
+		} else if (command == CMD_OK) {
 			viewer = viewerName.getText();
 			dialog.dispose();
-		} else if (command.equals("CANCEL")) {
+		} else if (command == CMD_CANCEL) {
 			dialog.dispose();
 		}
 	}
