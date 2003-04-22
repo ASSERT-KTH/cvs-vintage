849dc1e6c23d136f7b7bcfb66cde0dd026a3972d
tonyparent <tonyparent>
2003-04-22 16:03:48 +0000

    [intern] Fixed parsing of UIDs and flags

--- src/mail/independent/org/columba/mail/imap/parser/FlagsParser.java
+++ src/mail/independent/org/columba/mail/imap/parser/FlagsParser.java
@@ -1,16 +1,16 @@
 //The contents of this file are subject to the Mozilla Public License Version 1.1
-//(the "License"); you may not use this file except in compliance with the 
+//(the "License"); you may not use this file except in compliance with the
 //License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
 //
 //Software distributed under the License is distributed on an "AS IS" basis,
-//WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License 
+//WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
 //for the specific language governing rights and
 //limitations under the License.
 //
 //The Original Code is "The Columba Project"
 //
 //The Initial Developers of the Original Code are Frederik Dietz and Timo Stich.
-//Portions created by Frederik Dietz and Timo Stich are Copyright (C) 2003. 
+//Portions created by Frederik Dietz and Timo Stich are Copyright (C) 2003.
 //
 //All Rights Reserved.
 package org.columba.mail.imap.parser;
@@ -62,29 +62,22 @@ public class FlagsParser {
 				continue;
 
 			// for example: * 149 FETCH (UID 149 FLAGS (\Seen \Answered))
+            //              * 406 FETCH (FLAGS (\Seen) UID 57088)
 			String data = ImapParserUtils.parseData(responses[i].getSource());
-			//ColumbaLogger.log.debug("answer=" + data);
-			// answer=UID 149 FLAGS (\Seen \Answered)
-
+			//ColumbaLogger.log.debug("IMAP Flags: answer=" + data);
+			//System.out.println("IMAP Flags: answer=" + data);
 			// empty "()"
 			if ( data.indexOf("()") != -1) continue;
-			
+
 			// parse=(\Seen \Answered)
 			IMAPFlags flags = parseFlagsLine(ImapParserUtils.parseData(data));
 
-			int leftIndex = data.indexOf(" ");
-			int rightIndex = data.indexOf(" ", leftIndex + 1);
-
-			//ColumbaLogger.log.debug("left=" + leftIndex);
-			//ColumbaLogger.log.debug("right=" + rightIndex);
-
-			String uid = data.substring(leftIndex + 1, rightIndex);
-			//ColumbaLogger.log.debug("uid=<" + uid + ">");
+            // parse UID portion
+			String uid = parseUidsLine(data);
 
 			flags.setUid(uid);
 
 			v.add(flags);
-
 		}
 
 		IMAPFlags[] flags = new IMAPFlags[v.size()];
@@ -112,7 +105,7 @@ public class FlagsParser {
 			//System.out.println("deleted is true ");
 			flags.setDeleted(true);
 		}
-		
+
 		if (str.indexOf("Recent") != -1) {
 			//System.out.println("deleted is true ");
 			flags.setRecent(true);
@@ -123,13 +116,19 @@ public class FlagsParser {
 
 	protected static String parseUidsLine(String data) {
 
-		int leftIndex = data.indexOf(" ");
-		int rightIndex = data.indexOf(" ", leftIndex + 1);
+      // Find the start of the UID portion. Look for UID... as the UID
+      // portion isn't guarenteed to be at the beginning of the line.
+      int leftIndex  = data.indexOf("UID ") + 4;
+      int rightIndex = data.indexOf(" ", leftIndex);
 
-		//ColumbaLogger.log.debug("left=" + leftIndex);
-		//ColumbaLogger.log.debug("right=" + rightIndex);
-
-		return data.substring(leftIndex + 1, rightIndex);
+      if(rightIndex == -1){
+        // No rightIndex, therefore you went to the end of line, so just
+        // return from the left index to the end of the line
+        return data.substring(leftIndex);
+      }else{
+        // Return the sub string you found
+        return data.substring(leftIndex, rightIndex);
+      }
 	}
 
 	public static String parseVariant(int variant) {
