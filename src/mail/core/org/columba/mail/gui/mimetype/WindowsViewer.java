22e18733e8426596ed032fa3d4e2f6ea5ef74382
fdietz <fdietz>
2003-05-04 10:10:50 +0000

    [bug]added WindowsXP, to the Windows2000 if case

--- src/mail/core/org/columba/mail/gui/mimetype/WindowsViewer.java
+++ src/mail/core/org/columba/mail/gui/mimetype/WindowsViewer.java
@@ -25,38 +25,101 @@ import java.net.URL;
 
 import org.columba.mail.message.MimeHeader;
 
-public class WindowsViewer extends AbstractViewer
-{
+public class WindowsViewer extends AbstractViewer {
 
-    public Process openWith( MimeHeader header, File tempFile )
-    {
-        openDocument( tempFile.getPath() );
-        return null;
-    }
+	public Process openWith(MimeHeader header, File tempFile) {
+		openDocument(tempFile.getPath());
+		return null;
+	}
 
-    public Process open( MimeHeader header, File tempFile )
-    {
-        openDocument( tempFile.getPath() );
-        return null;
-    }
+	public Process open(MimeHeader header, File tempFile) {
+		openDocument(tempFile.getPath());
+		return null;
+	}
 
-    public Process openURL( URL url )
-    {
-        String osName = System.getProperty("os.name" );
-	if( osName.equals( "Windows 2000" ) ) {
-		Process proc = null;
+	public Process openURL(URL url) {
+		String osName = System.getProperty("os.name");
+		if ((osName.equals("Windows 2000")) || (osName.equals("Windows XP")) ){
+			Process proc = null;
+			try {
+				String[] cmd =
+					new String[] { "cmd.exe", "/C", "start", url.toString()};
+				Runtime rt = Runtime.getRuntime();
+				System.out.println(
+					"Executing "
+						+ cmd[0]
+						+ " "
+						+ cmd[1]
+						+ " "
+						+ cmd[2]
+						+ " "
+						+ cmd[3]);
+				proc = rt.exec(cmd);
+				// any error message?
+				StreamGobbler errorGobbler =
+					new StreamGobbler(proc.getErrorStream(), "ERROR");
+				errorGobbler.start();
+				// any output?
+				StreamGobbler outputGobbler =
+					new StreamGobbler(proc.getInputStream(), "OUTPUT");
+				outputGobbler.start();
+
+				// any error?
+				int exitVal = proc.waitFor();
+				System.out.println("ExitValue: " + exitVal);
+			} catch (Throwable t) {
+				t.printStackTrace();
+			}
+
+		} else {
+			openDocument(url.getPath());
+		}
+		return null;
+	}
+
+	public Process openWithURL(URL url) {
+		openDocument(url.getPath());
+		return null;
+	}
+
+	protected void openDocument(String filename) {
 		try {
-                	String[] cmd = new String[]{"cmd.exe","/C","start",url.toString()};
-			Runtime rt = Runtime.getRuntime();
-			System.out.println("Executing " + cmd[0] + " " + cmd[1] + " " + cmd[2] + " " +cmd[3]);
-			proc = rt.exec(cmd);
+			String osName = System.getProperty("os.name");
+			Process proc = null;
+			if (osName.equals("Windows NT")) {
+				String[] cmd = new String[] { "cmd.exe", "/C", filename };
+				Runtime rt = Runtime.getRuntime();
+				System.out.println(
+					"Executing " + cmd[0] + " " + cmd[1] + " " + cmd[2]);
+				proc = rt.exec(cmd);
+			} else if (
+				(osName.equals("Windows 95"))
+					|| (osName.equals("Windows 98"))) {
+				String[] cmd = new String[] { "start", filename };
+				Runtime rt = Runtime.getRuntime();
+				System.out.println("Executing " + cmd[0] + " " + cmd[1]);
+				proc = rt.exec(cmd);
+			} else if ((osName.equals("Windows 2000")) || (osName.equals("Windows XP")) ){
+				// this includes Windows XP
+				String[] cmd = new String[3];
+				cmd[0] = "cmd.exe";
+				cmd[1] = "/C";
+				cmd[2] =
+					filename.charAt(0) + "\"" + filename.substring(1) + "\"";
+
+				Runtime rt = Runtime.getRuntime();
+				System.out.println(
+					"Executing " + cmd[0] + " " + cmd[1] + " " + cmd[2]);
+				proc = rt.exec(cmd);
+			} 
+
 			// any error message?
-			StreamGobbler errorGobbler = new
-				StreamGobbler(proc.getErrorStream(), "ERROR");
+			StreamGobbler errorGobbler =
+				new StreamGobbler(proc.getErrorStream(), "ERROR");
 			errorGobbler.start();
 			// any output?
-			StreamGobbler outputGobbler = new
-				StreamGobbler(proc.getInputStream(), "OUTPUT");
+			StreamGobbler outputGobbler =
+				new StreamGobbler(proc.getInputStream(), "OUTPUT");
 			outputGobbler.start();
 
 			// any error?
@@ -65,93 +128,27 @@ public class WindowsViewer extends AbstractViewer
 		} catch (Throwable t) {
 			t.printStackTrace();
 		}
-
-	}else{
-	    openDocument( url.getPath() );
 	}
-        return null;
-    }
 
-    public Process openWithURL( URL url )
-    {
-        openDocument( url.getPath() );
-        return null;
-    }
+	class StreamGobbler extends Thread {
+		InputStream is;
+		String type;
 
-    protected void openDocument( String filename )
-    {
-        try
-        {
-            String osName = System.getProperty("os.name" );
-	    Process proc = null;
-            if ( osName.equals( "Windows NT" )  )
-            {
-                String[] cmd = new String[]{"cmd.exe","/C",filename};
-                Runtime rt = Runtime.getRuntime();
-                System.out.println("Executing " + cmd[0] + " " + cmd[1] + " " + cmd[2]);
-                proc = rt.exec(cmd);
-            }
-            else if ( ( osName.equals( "Windows 95" ) ) || ( osName.equals( "Windows 98" ) ) )
-            {
-                String[] cmd = new String[]{"start",filename};
-                Runtime rt = Runtime.getRuntime();
-                System.out.println("Executing " + cmd[0] + " " + cmd[1]);
-                proc = rt.exec(cmd);
-            }
-            else if ( osName.equals( "Windows 2000" )  )
-            {
-                String[] cmd = new String[3];
-                cmd[0] = "cmd.exe" ;
-                cmd[1] = "/C" ;
-                cmd[2] = filename.charAt(0) + "\"" + filename.substring( 1 ) + "\"";
+		StreamGobbler(InputStream is, String type) {
+			this.is = is;
+			this.type = type;
+		}
 
-                Runtime rt = Runtime.getRuntime();
-                System.out.println("Executing " + cmd[0] + " " + cmd[1] + " " + cmd[2]);
-                proc = rt.exec(cmd);
-            }
-
-            // any error message?
-            StreamGobbler errorGobbler = new
-                StreamGobbler(proc.getErrorStream(), "ERROR");
-            errorGobbler.start();
-            // any output?
-            StreamGobbler outputGobbler = new
-                StreamGobbler(proc.getInputStream(), "OUTPUT");
-            outputGobbler.start();
-
-            // any error?
-            int exitVal = proc.waitFor();
-            System.out.println("ExitValue: " + exitVal);
-        }
-        catch (Throwable t)
-        {
-            t.printStackTrace();
-        }
-    }
-
-    class StreamGobbler extends Thread
-    {
-        InputStream is;
-        String type;
-
-        StreamGobbler(InputStream is, String type)
-        {
-            this.is = is;
-            this.type = type;
-        }
-
-        public void run()
-        {
-            try
-            {
-                InputStreamReader isr = new InputStreamReader(is);
-                BufferedReader br = new BufferedReader(isr);
-                String line=null;
-                while ( (line = br.readLine()) != null)
-                      System.out.println(type + ">" + line);
-            } catch (IOException ioe) {
-                ioe.printStackTrace();
-            }
-        }
-    }
+		public void run() {
+			try {
+				InputStreamReader isr = new InputStreamReader(is);
+				BufferedReader br = new BufferedReader(isr);
+				String line = null;
+				while ((line = br.readLine()) != null)
+					System.out.println(type + ">" + line);
+			} catch (IOException ioe) {
+				ioe.printStackTrace();
+			}
+		}
+	}
 }
