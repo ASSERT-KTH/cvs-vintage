0c0d3e6f4f8887e5bcba8d4a19bbc386ffa9ea8b
tonyparent <tonyparent>
2003-05-07 20:19:08 +0000

    [bug] Fixed a nullPointerException when deleting IMAP messages

--- src/mail/core/org/columba/mail/folder/command/CopyMessageCommand.java
+++ src/mail/core/org/columba/mail/folder/command/CopyMessageCommand.java
@@ -1,16 +1,16 @@
 //The contents of this file are subject to the Mozilla Public License Version 1.1
-//(the "License"); you may not use this file except in compliance with the 
+//(the "License"); you may not use this file except in compliance with the
 //License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
 //
 //Software distributed under the License is distributed on an "AS IS" basis,
-//WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License 
+//WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
 //for the specific language governing rights and
 //limitations under the License.
 //
 //The Original Code is "The Columba Project"
 //
 //The Initial Developers of the Original Code are Frederik Dietz and Timo Stich.
-//Portions created by Frederik Dietz and Timo Stich are Copyright (C) 2003. 
+//Portions created by Frederik Dietz and Timo Stich are Copyright (C) 2003.
 //
 //All Rights Reserved.
 package org.columba.mail.folder.command;
@@ -79,6 +79,10 @@ public class CopyMessageCommand extends FolderCommand {
 		Object[] uids,
 		Worker worker)
 		throws Exception {
+
+        if(destUids == null){
+          destUids = new Object[uids.length];
+        }
 		for (int i = 0; i < uids.length; i++) {
 
 			Object uid = uids[i];
@@ -86,8 +90,7 @@ public class CopyMessageCommand extends FolderCommand {
 
 			if (srcFolder.exists(uid, worker)) {
 				String source = srcFolder.getMessageSource(uid, worker);
-
-				destUids[i] = destFolder.addMessage(source, worker);
+                destUids[i] = destFolder.addMessage(source, worker);
 			}
 
 			worker.setProgressBarValue(i);
@@ -119,7 +122,7 @@ public class CopyMessageCommand extends FolderCommand {
 				"Copying messages to " + destFolder.getName() + "...");
 			worker.setProgressBarMaximum(uids.length);
 
-			// compare source- and dest-folder		
+			// compare source- and dest-folder
 			if (srcFolder.getRootFolder().equals(destFolder.getRootFolder())) {
 				// source- and dest-folder match
 				//  -> user optimized copy operation
@@ -139,15 +142,15 @@ public class CopyMessageCommand extends FolderCommand {
 	public void undo(Worker worker) throws Exception {
 		/*
 		FolderCommandReference[] r = (FolderCommandReference[]) getReferences();
-		
+
 		Object[] uids = r[1].getUids();
-		
+
 		Folder srcFolder = (Folder) r[1].getFolder();
-		
+
 		for (int i = 0; i < uids.length; i++) {
 			Object uid = uids[i];
 			ColumbaLogger.log.debug("undo_copying UID=" + uid);
-		
+
 			srcFolder.removeMessage(uid, worker);
 		}
 		*/
