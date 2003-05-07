5a77bbbb1a6386f2d6b4f78bb878c78f5cd9811a
javaprog <javaprog>
2003-05-07 15:10:32 +0000

    [intern] actualized class path

--- run_source_jdk13.bat
+++ run_source_jdk13.bat
@@ -1,1 +1,1 @@
-java -cp .;ext\xalan.jar;ext\jaxp.jar;ext\parser.jar;ext\jython.jar;ext\log4j.jar org.columba.main.Main
+java -cp .;lib\xalan.jar;lib\jaxp.jar;lib\jython.jar;lib\log4j.jar;lib\jakarta-oro-2.0.6.jar;lib\lucene-1.2.jar;lib\langpack.jar org.columba.core.main.Main
--- run_source_jdk13.sh
+++ run_source_jdk13.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 COLUMBA_HOME=`dirname $0`
-java -cp "$COLUMBA_HOME":"$COLUMBA_HOME"/lib/xalan.jar:"$COLUMBA_HOME"/lib/jaxp.jar:"$COLUMBA_HOME"/lib/jython.jar:"$COLUMBA_HOME"/lib/log4j.jar org.columba.main.Main
+java -cp "$COLUMBA_HOME":"$COLUMBA_HOME"/lib/xalan.jar:"$COLUMBA_HOME"/lib/jaxp.jar:"$COLUMBA_HOME"/lib/jython.jar:"$COLUMBA_HOME"/lib/log4j.jar:"$COLUMBA_HOME"/lib/jargs.jar:"$COLUMBA_HOME"/lib/jakarta-oro-2.0.6.jar:"$COLUMBA_HOME"/lib/lucene-1.2.jar:"$COLUMBA_HOME"/langpack.jar org.columba.core.main.Main
