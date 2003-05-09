617d802c36c9f66b3871d61e6682df7cc431ddab
d_jencks <d_jencks>
2003-05-09 19:49:32 +0000

    Please use this to run a full checkout and test after a big commit

--- src/bin/co-run-test-email.sh
+++ src/bin/co-run-test-email.sh
@@ -1,18 +1,10 @@
 #!/bin/sh
 
-#This script removes any current jboss checkout, does a jboss checkout,
-#builds and starts jboss, runs the testsuite, and mails the results.
-#Specify the email addresses lower down.
-
 echo Put this file where you want the clean checkout of jboss to occur,
 echo and set the email addresses as appropriate.  Then comment out the 
 echo exit line and run...
 
 exit
-
-TO_EMAIL=noone@nowhere.com
-FROM_EMAIL=noone@nowhere.com
-EMAIL_SERVER=localhost
 
 #the module to check out
 
@@ -26,10 +18,11 @@ cvs -z9 -d:pserver:anonymous@cvs.jboss.sourceforge.net:/cvsroot/jboss co $MODULE
 
 cd $MODULE/build/
 
-./build.sh run-nightly \
- -Dtest.skipupdate=true \
- -Drun-nightly-sleep=3\
- -Drun.nightly.email.tolist=$TO_EMAIL \
- -Drun.nightly.email.from=$FROM_EMAIL \
- -Drun.nightly.email.mailhost=$EMAIL_SERVER
+
+./build.sh main tests\
+ -Dtest.report.email.tolist=somewhere@your.isp.net\
+ -Dtest.report.email.from=you@your.isp.net\
+ -Dtest.report.email.mailhost=localhost
+
+
 
