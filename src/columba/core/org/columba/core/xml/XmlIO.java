4c332aafecab943932bfab09f6b3cf333c0a3749
javaprog <javaprog>
2003-05-14 08:36:35 +0000

    [intern] changed license header from GPL to MPL

--- src/columba/core/org/columba/core/xml/XmlIO.java
+++ src/columba/core/org/columba/core/xml/XmlIO.java
@@ -1,51 +1,18 @@
-// this is -*- java -*- code
-/////////////////////////////////////////////////////////////////////////////
-// FILE:         XmlIO.java
-// SUMMARY:
-// USAGE:
+//The contents of this file are subject to the Mozilla Public License Version 1.1
+//(the "License"); you may not use this file except in compliance with the 
+//License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
 //
-// AUTHOR:       Tony Parent
-// ORG:          AMCC
-// ORIG-DATE:     8-Oct-02 at 12:24:05
-// LAST-MOD:      6-Jan-03 at 12:00:21 by Tony Parent
-// DESCRIPTION:
-// DESCRIP-END.
+//Software distributed under the License is distributed on an "AS IS" basis,
+//WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License 
+//for the specific language governing rights and
+//limitations under the License.
 //
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
+//The Original Code is "The Columba Project"
 //
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Library General Public License for more details.
+//The Initial Developers of the Original Code are Frederik Dietz and Timo Stich.
+//Portions created by Frederik Dietz and Timo Stich are Copyright (C) 2003. 
 //
-// You should have received a copy of the GNU General Public License along
-// with this program; if not, write to the Free Software Foundation, Inc.,
-// 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-//
-//
-// $Log: XmlIO.java,v $
-// Revision 1.7  2003/03/28 14:43:04  javaprog
-// [intern] don't start variable names with capital letters
-//
-// Revision 1.6  2003/03/09 13:08:36  fdietz
-// [intern]import cleanups
-//
-// Revision 1.5  2003/01/07 11:44:03  javaprog
-// [bug] XmlIO must use java.net.URL instead of java.io.File in order to load resources within .jar files
-//
-// Revision 1.3  2003/01/07 11:02:25  javaprog
-// [intern] reduced code size
-//
-// Revision 1.2  2003/01/06 19:18:26  tonyparent
-// [intern] Escape '<', '>', and '&' when writing data and attributes.
-//
-// Revision 1.1  2002/12/29 17:44:37  fdietz
-// [intern]source tree cleanup
-//
-/////////////////////////////////////////////////////////////////////////////
+//All Rights Reserved.
 
 package org.columba.core.xml;
 
