c876d64805836b2d255f1aabe13499991b2fc7ce
fdietz <fdietz>
2003-05-20 18:27:11 +0000

    [bug] fixed classpath

--- run_source.bat
+++ run_source.bat
@@ -1,2 +1,2 @@
-set CLASSPATH=.;.\ext\log4j.jar;.\ext\junit.jar;.\ext\jython.jar ;.\ext\lucene-1.3-rc1.jar
+set CLASSPATH=.;.\classes;.\lib\log4j.jar;.\lib\junit.jar;.\lib\jakarta-oro-2.0.6.jar;.\lib\lucene-1.3-rc1.jar 
 java org.columba.core.main.Main
