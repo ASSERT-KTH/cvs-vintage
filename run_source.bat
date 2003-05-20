04f5d647ccbe3b961546e3daec0b022c6dee5831
tstich <tstich>
2003-05-20 18:14:42 +0000

    [intern]lucene upgrade to version 1.3-rc1

--- run_source.bat
+++ run_source.bat
@@ -1,2 +1,2 @@
-set CLASSPATH=.;.\ext\log4j.jar;.\ext\junit.jar;.\ext\jython.jar 
+set CLASSPATH=.;.\ext\log4j.jar;.\ext\junit.jar;.\ext\jython.jar ;.\ext\lucene-1.3-rc1.jar
 java org.columba.core.main.Main
--- run_source_jdk13.bat
+++ run_source_jdk13.bat
@@ -1,1 +1,1 @@
-java -cp .;lib\xalan.jar;lib\jaxp.jar;lib\jython.jar;lib\log4j.jar;lib\jakarta-oro-2.0.6.jar;lib\lucene-1.2.jar;lib\langpack.jar org.columba.core.main.Main
+java -cp .;lib\xalan.jar;lib\jaxp.jar;lib\jython.jar;lib\log4j.jar;lib\jakarta-oro-2.0.6.jar;lib\lucene-1.3-rc1.jar;lib\langpack.jar org.columba.core.main.Main
