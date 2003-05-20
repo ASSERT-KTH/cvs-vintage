a0b903452c57566a2a4a5b4bd9bdb6ca67c4fe51
fdietz <fdietz>
2003-05-20 18:39:06 +0000

    [intern]fixed classpath

--- run_source.bat
+++ run_source.bat
@@ -1,2 +1,2 @@
-set CLASSPATH=.;.\classes;.\lib\log4j.jar;.\lib\junit.jar;.\lib\jakarta-oro-2.0.6.jar;.\lib\lucene-1.3-rc1.jar 
+set CLASSPATH=.;.\classes;.\lib\log4j.jar;.\lib\junit.jar;.\lib\jakarta-oro-2.0.6.jar;.\lib\lucene-1.3-rc1.jar;.\lib\jargs.jar 
 java org.columba.core.main.Main
--- run_source_jdk13.bat
+++ run_source_jdk13.bat
@@ -1,1 +1,1 @@
-java -cp .;.\classes;lib\xalan.jar;lib\jaxp.jar;lib\log4j.jar;lib\jakarta-oro-2.0.6.jar;lib\lucene-1.3-rc1.jar org.columba.core.main.Main
+java -cp .;.\classes;lib\xalan.jar;lib\jaxp.jar;lib\log4j.jar;lib\jakarta-oro-2.0.6.jar;lib\lucene-1.3-rc1.jar;lib\jargs.jar org.columba.core.main.Main
