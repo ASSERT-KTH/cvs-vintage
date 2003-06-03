c99fe77073801467a2bd32b4755258d2f450f84a
elicia <elicia>
2003-06-03 20:26:34 +0000

    * AttributeValue.java: fixing comment. Issue number:  18502 Reviewed by:   jmcnally

--- src/java/org/tigris/scarab/om/AttributeValue.java
+++ src/java/org/tigris/scarab/om/AttributeValue.java
@@ -71,7 +71,7 @@ import org.tigris.scarab.om.Module;
  * @author <a href="mailto:jmcnally@collab.net">John McNally</a>
  * @author <a href="mailto:jon@collab.net">Jon S. Stevens</a>
  * @author <a href="mailto:elicia@collab.net">Elicia David</a>
- * @version $Id: AttributeValue.java,v 1.96 2003/06/03 20:23:28 elicia Exp $
+ * @version $Id: AttributeValue.java,v 1.97 2003/06/03 20:26:34 elicia Exp $
  */
 public abstract class AttributeValue 
     extends BaseAttributeValue
@@ -325,7 +325,7 @@ Leaving here so that John can remove or fix.
             b = super.equals(obj);
             if (!b) 
             {
-                FIXME! this code will equate 2 AttributeValues that are
+                //FIXME! this code will equate 2 AttributeValues that are
                 // multivalued, but are in a flat list.  
                 AttributeValue aval = (AttributeValue)obj;
                 b = (getChainedValue() == null) && 
