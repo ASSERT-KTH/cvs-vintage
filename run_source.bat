4ed365429925225a8832969dab03c5bd6decce25
javaprog <javaprog>
2003-06-08 14:51:43 +0000

    [intern] prepare new wizard framework introduction

--- run_source.bat
+++ run_source.bat
@@ -1,2 +1,2 @@
-set CLASSPATH=.;.\classes;.\lib\log4j.jar;.\lib\junit.jar;.\lib\jakarta-oro-2.0.6.jar;.\lib\lucene-1.3-rc1.jar;.\lib\jargs.jar 
+set CLASSPATH=.;.\classes;.\lib\log4j.jar;.\lib\junit.jar;.\lib\jakarta-oro-2.0.6.jar;.\lib\lucene-1.3-rc1.jar;.\lib\jargs.jar;.\lib\JWizard-0.1.0-alpha.jar
 java org.columba.core.main.Main
--- run_source_jdk13.bat
+++ run_source_jdk13.bat
@@ -1,1 +1,1 @@
-java -cp .;.\classes;lib\xalan.jar;lib\jaxp.jar;lib\log4j.jar;lib\jakarta-oro-2.0.6.jar;lib\lucene-1.3-rc1.jar;lib\jargs.jar org.columba.core.main.Main
+java -cp .;.\classes;lib\xalan.jar;lib\jaxp.jar;lib\log4j.jar;lib\jakarta-oro-2.0.6.jar;lib\lucene-1.3-rc1.jar;lib\jargs.jar;lib\JWizard-0.1.0-alpha.jar org.columba.core.main.Main
