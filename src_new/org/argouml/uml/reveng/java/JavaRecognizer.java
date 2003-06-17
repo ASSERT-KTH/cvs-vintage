439a607087d08d7ea1e070f8e976b86cbdeae417
lepekhine <lepekhine>
2003-06-17 10:59:40 +0000

    Added condition for addAttribute to solve issue 1652

--- src_new/org/argouml/uml/reveng/java/JavaRecognizer.java
+++ src_new/org/argouml/uml/reveng/java/JavaRecognizer.java
@@ -34,12 +34,12 @@ import antlr.collections.impl.BitSet;
  *         for examples of how to modify a antlr grammar file</li>
  *     <li>save java.g and run argo's build script with the 
  *         "<strong>generateparser</strong>" target.</li>
- *     <li>build argo again</li>
+ *     <li>build argo again.</li>
  * </ol>
  * 
  * <p>Version tracking now done with following cvs ID:
  *
- * <p>$Id: JavaRecognizer.java,v 1.18 2003/03/20 19:43:47 alexb Exp $
+ * <p>$Id: JavaRecognizer.java,v 1.19 2003/06/17 10:59:40 lepekhine Exp $
  * 
  * --------- old notes: -------------------------------------------
  * <pre>
@@ -1454,7 +1454,11 @@ public JavaRecognizer(ParserSharedInputState state) {
 		initializer=varInitializer();
 		}
 		if ( inputState.guessing==0 ) {
-			getModeller().addAttribute(modifiers, varType+b, id.getText(), initializer, javadoc);
+			
+					if (!isInCompoundStatement()) {
+					getModeller().addAttribute(modifiers, varType+b, id.getText(), initializer, javadoc);
+					}
+					
 		}
 	}
 	
