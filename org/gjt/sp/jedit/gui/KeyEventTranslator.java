c7d08c8f2780b32ef01a7af496a08a9d96ff8cf9
spestov <spestov>
2003-07-02 19:22:37 +0000

    fixed alt-gr

--- org/gjt/sp/jedit/gui/KeyEventTranslator.java
+++ org/gjt/sp/jedit/gui/KeyEventTranslator.java
@@ -35,7 +35,7 @@ import org.gjt.sp.util.Log;
  * warts in the AWT key event API.
  *
  * @author Slava Pestov
- * @version $Id: KeyEventTranslator.java,v 1.11 2003/06/25 03:38:33 spestov Exp $
+ * @version $Id: KeyEventTranslator.java,v 1.12 2003/07/02 19:22:37 spestov Exp $
  */
 public class KeyEventTranslator
 {
@@ -102,9 +102,8 @@ public class KeyEventTranslator
 
 			boolean mod = (System.currentTimeMillis() -
 				KeyEventWorkaround.lastKeyTime < 750
-				&& KeyEventWorkaround.modifiers != 0
-				&& KeyEventWorkaround.modifiers
-				!= InputEvent.SHIFT_MASK);
+				&& (KeyEventWorkaround.modifiers
+				& ~(InputEvent.SHIFT_MASK | InputEvent.ALT_GRAPH_MASK)) == 0);
 
 			if(mod)
 			{
