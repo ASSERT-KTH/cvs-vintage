a3aeef540e7e2c53df344d03cbaa9a45ba63a368
waffel <waffel>
2003-07-11 10:37:53 +0000

    [intern] tests for Base64Encoder and Decoder of a String base

--- src/mail/test/org/columba/mail/coder/Base64EncoderTest.java
+++ src/mail/test/org/columba/mail/coder/Base64EncoderTest.java
@@ -6,6 +6,8 @@
  */
 package org.columba.mail.coder;
 
+import java.io.UnsupportedEncodingException;
+
 import junit.framework.TestCase;
 
 /**
@@ -22,12 +24,68 @@ public class Base64EncoderTest extends TestCase {
 	 */
 	public Base64EncoderTest(String arg0) {
 		super(arg0);
+		CoderRouter router = new CoderRouter();
 	}
 
 	/*
-	 * Test for String encode(String, String)
+	 * Test for String encode(String, String) with charset null. testing four alpha characters
 	 */
-	public void testEncodeStringString() {
+	public void testEncodeStringString1() {
+		String testStr = "abcd";
+		Base64Encoder columbaEncoder = new Base64Encoder();
+		try {
+			String codedStr = columbaEncoder.encode(testStr, null);
+			// should be 'YWJjZA=='
+			sun.misc.BASE64Encoder sunEncoder = new sun.misc.BASE64Encoder();
+			assertTrue(sunEncoder.encode(testStr.getBytes()).equals(codedStr));
+		} catch (UnsupportedEncodingException e) {
+			assertFalse(true);
+		}
+	}
+	/**
+	 * Test encoding (String,String) with charset null. Only numbers are tested on a 6 sign basis
+	 */
+	public void testEncodeStringString2() {
+		String testStr = "123456";
+		Base64Encoder columbaEncoder = new Base64Encoder();
+		try {
+			String codedStr = columbaEncoder.encode(testStr, null);
+			// should be 'MTIzNDU2'
+			sun.misc.BASE64Encoder sunEncoder = new sun.misc.BASE64Encoder();
+			assertTrue(sunEncoder.encode(testStr.getBytes()).equals(codedStr));
+		} catch (UnsupportedEncodingException e) {
+			assertFalse(true);
+		}
+	}
+	/**
+	 * Test encoding (String,String) with charset null. Test only extra characters
+	 */
+	public void testEncodeStringString3() {
+		String testStr = "?=)(/&%$§\"!";
+		Base64Encoder columbaEncoder = new Base64Encoder();
+		try {
+			String codedStr = columbaEncoder.encode(testStr, null);
+			sun.misc.BASE64Encoder sunEncoder = new sun.misc.BASE64Encoder();
+			assertTrue(sunEncoder.encode(testStr.getBytes()).equals(codedStr));
+		} catch (UnsupportedEncodingException e) {
+			assertFalse(true);
+		}
+	}
+	/**
+	 * Test encoding (String, String) with charset null. Test a long string
+	 */
+	public void testEncodeStringString4() {
+		String testStr ="abcdesfghijklmnopqrstuvwxyz1234567890!abcdesfghijklmnopqrstuvwxyz1234567890!abcdesfghijklmnopqrstuvwxyz1234567890!" +
+		"abcdesfghijklmnopqrstuvwxyz1234567890!abcdesfghijklmnopqrstuvwxyz1234567890!abcdesfghijklmnopqrstuvwxyz1234567890!"+
+		"abcdesfghijklmnopqrstuvwxyz1234567890!abcdesfghijklmnopqrstuvwxyz1234567890!abcdesfghijklmnopqrstuvwxyz1234567890!";
+		Base64Encoder columbaEncoder = new Base64Encoder();
+		try {
+			String codedStr = columbaEncoder.encode(testStr, null);
+			sun.misc.BASE64Encoder sunEncoder = new sun.misc.BASE64Encoder();
+			assertTrue(sunEncoder.encode(testStr.getBytes()).equals(codedStr));
+		} catch (UnsupportedEncodingException e) {
+			assertFalse(true);
+		}
 	}
 
 	/*
