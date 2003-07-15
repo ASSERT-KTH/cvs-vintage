398a56b30da6b771f62b84e4a1e71bd1252b8764
dlr <dlr>
2003-07-15 16:44:59 +0000

    * src/java/org/tigris/scarab/om/Issue.java   getModuleAttributeValuesMap(): No functional change.  Tweaked   indentation level of header JavaDoc.
    Issue: PCN19518

--- src/java/org/tigris/scarab/om/Issue.java
+++ src/java/org/tigris/scarab/om/Issue.java
@@ -97,7 +97,7 @@ import org.apache.commons.lang.StringUtils;
  * @author <a href="mailto:jmcnally@collab.net">John McNally</a>
  * @author <a href="mailto:jon@collab.net">Jon S. Stevens</a>
  * @author <a href="mailto:elicia@collab.net">Elicia David</a>
- * @version $Id: Issue.java,v 1.311 2003/07/14 10:22:41 venkatesh Exp $
+ * @version $Id: Issue.java,v 1.312 2003/07/15 16:44:59 dlr Exp $
  */
 public class Issue 
     extends BaseIssue
@@ -848,11 +848,11 @@ public class Issue
         return rmit;
     }
 
-   /**
-    * Calls the overloaded version by passing 'true' so that only active
-    * attributes will be considered.
-    * @see #getModuleAttributeValuesMap(boolean)
-    */
+    /**
+     * Calls the overloaded version by passing 'true' so that only active
+     * attributes will be considered.
+     * @see #getModuleAttributeValuesMap(boolean)
+     */
     public SequencedHashMap getModuleAttributeValuesMap()
         throws Exception
     {
