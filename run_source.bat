eb549b266080228673c6b0f611084b6d1518c9a9
fdietz <fdietz>
2003-07-19 10:26:19 +0000

    [intern] add plastic l&f

--- run_source.bat
+++ run_source.bat
@@ -1,2 +1,2 @@
-set CLASSPATH=.;.\classes;.\lib\log4j.jar;.\lib\junit.jar;.\lib\jakarta-oro-2.0.6.jar;.\lib\lucene-1.3-rc1.jar;.\lib\jargs.jar;.\lib\jwizz-0.1.0.jar
+set CLASSPATH=.;.\classes;.\lib\log4j.jar;.\lib\junit.jar;.\lib\jakarta-oro-2.0.6.jar;.\lib\lucene-1.3-rc1.jar;.\lib\jargs.jar;.\lib\jwizz-0.1.0.jar;.\lib\plastic.jar
 java org.columba.core.main.Main
