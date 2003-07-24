ece15bf0089380241cc07d6a00ced32fc9b83224
cmclaren <cmclaren>
2003-07-24 21:18:52 +0000

    *** empty log message ***

--- Eclipse UI/org/eclipse/ui/internal/commands/old/ContextAndHandlerManager.java
+++ Eclipse UI/org/eclipse/ui/internal/commands/old/ContextAndHandlerManager.java
@@ -52,6 +52,7 @@ import org.eclipse.ui.internal.AcceleratorMenu;
 import org.eclipse.ui.internal.IWorkbenchConstants;
 import org.eclipse.ui.internal.Workbench;
 import org.eclipse.ui.internal.WorkbenchWindow;
+import org.eclipse.ui.internal.commands.CommandManager;
 
 public class ContextAndHandlerManager implements IContextResolver {
 
@@ -164,7 +165,7 @@ public class ContextAndHandlerManager implements IContextResolver {
 
 		if (childSequenceMapForMode.isEmpty()) {
 			clear();			
-			ICommandDelegate commandDelegate = (ICommandDelegate) commandManager.getCommandDelegatesById().get((String) sequenceMapForMode.get(childMode));
+			ICommandDelegate commandDelegate = (ICommandDelegate) ((CommandManager) commandManager).getCommandDelegatesById().get((String) sequenceMapForMode.get(childMode));
 			
 			if (commandDelegate != null && commandDelegate.isEnabled())
 				try {			
@@ -189,8 +190,7 @@ public class ContextAndHandlerManager implements IContextResolver {
 			// TODO: get rid of this
 			if (contexts.size() == 0)
 				contexts.add(IWorkbenchConstants.DEFAULT_ACCELERATOR_SCOPE_ID);
-			
-			System.out.println(contexts);			
+
 			keyMachine.setContexts((String[]) contexts.toArray(new String[contexts.size()]));
 		} catch (IllegalArgumentException eIllegalArgument) {
 			System.err.println(eIllegalArgument);
@@ -209,9 +209,9 @@ public class ContextAndHandlerManager implements IContextResolver {
 			String command = (String) entry.getValue();		
 
 			if (sequence.isChildOf(mode, false)) {
-				//ICommandDelegate commandDelegate = (ICommandDelegate) commandManager.getCommandDelegatesById().get(command);
+				ICommandDelegate commandDelegate = (ICommandDelegate) ((CommandManager) commandManager).getCommandDelegatesById().get(command);
 				
-				//if (commandDelegate != null)
+				if (commandDelegate != null)
 					strokeSetForMode.add(sequence.getStrokes().get(size));	
 			}
 		}
