5aed8f41be597b1683ed304f14eb4ec8fa38221f
elicia <elicia>
2003-08-07 21:23:40 +0000

    * IssueType.java: change get getParentIssueType() to getIssueTypeForTemplateType() to make it more intuitive of a name.  Call superclass method. * GlobalMacros.vm: call new getIssueTypeForTemplateType() method. Issue number:  19890

--- src/java/org/tigris/scarab/om/IssueType.java
+++ src/java/org/tigris/scarab/om/IssueType.java
@@ -72,7 +72,7 @@ import org.tigris.scarab.workflow.WorkflowFactory;
  *
  * @author <a href="mailto:elicia@collab.net">Elicia David</a>
  * @author <a href="mailto:jon@collab.net">Jon S. Stevens</a>
- * @version $Id: IssueType.java,v 1.58 2003/08/07 01:05:24 elicia Exp $
+ * @version $Id: IssueType.java,v 1.59 2003/08/07 21:23:40 elicia Exp $
  */
 public  class IssueType 
     extends org.tigris.scarab.om.BaseIssueType
@@ -151,22 +151,12 @@ public  class IssueType
      * Gets the parent IssueType for this template IssueType. The template
      * is a special type of IssueType.
      */
-    public IssueType getParentIssueType()
+    public IssueType getIssueTypeForTemplateType()
         throws Exception
     {
         if (parentIssueType == null) 
         {        
-            Criteria crit = new Criteria();
-            crit.add(IssueTypePeer.ISSUE_TYPE_ID, getParentId());
-            List results = IssueTypePeer.doSelect(crit);
-            if (results.isEmpty() || results.size()>1)
-            {
-                throw new ScarabException("There has been an error.");
-            }
-            else
-            {
-                parentIssueType = (IssueType)results.get(0);
-            }
+            parentIssueType = getIssueTypeRelatedByParentId();
         }
         return parentIssueType;
     }
