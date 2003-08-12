e7d36ff5a222179cffbf35802017da4b3376c77c
fdietz <fdietz>
2003-08-12 17:52:20 +0000

    [feature]added config example plugin

---
+++ plugins/org.columba.example.ConfigExample/ConfigExample.java
@@ -0,0 +1,88 @@
+import java.awt.BorderLayout;
+
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import org.columba.core.facade.Facade;
+import org.columba.core.gui.plugin.AbstractConfigPlugin;
+import org.columba.core.xml.XmlElement;
+import org.columba.core.xml.XmlIO;
+
+/**
+ * @author fdietz
+ *
+ * This example demonstrates the use of the config extension point
+ * 
+ */
+public class ConfigExample extends AbstractConfigPlugin {
+
+	JTextField textField;
+	XmlElement parent;
+
+	XmlIO xmlFile;
+	/**
+	 * 
+	 */
+	public ConfigExample() {
+		super();
+
+		// open configuration file 
+		xmlFile = Facade.getPluginConfiguration("org.columba.example.ConfigExample");
+		xmlFile.load();
+		
+		parent = xmlFile.getRoot();
+			
+	}
+
+	/* (non-Javadoc)
+	 * @see org.columba.core.gui.plugin.AbstractConfigPlugin#updateComponents(boolean)
+	 */
+	public void updateComponents(boolean b) {
+		
+
+		if (b) {
+			// model -> view
+			// read configuration and set gui elements appropriately
+
+			// navigate to treenode "text"
+			XmlElement text = parent.getElement("/config/text");
+
+			// read attribute "value"
+			String attribute = text.getAttribute("value");
+
+			textField.setText(attribute);
+
+		} else {
+			// view -> model
+			// write configuration given the data the user entered in the gui
+
+			XmlElement text = parent.getElement("/config/text");
+			text.addAttribute("value", textField.getText());
+			
+			try
+			{
+				xmlFile.save();
+				
+			} catch ( Exception ex)
+			{
+				ex.printStackTrace();
+			}
+		}
+
+	}
+
+	/* (non-Javadoc)
+	 * @see org.columba.core.gui.plugin.AbstractConfigPlugin#createPanel()
+	 */
+	public JPanel createPanel() {
+
+		JPanel panel = new JPanel();
+		panel.setLayout(new BorderLayout());
+
+		textField = new JTextField(20);
+		panel.add(textField, BorderLayout.CENTER);
+
+		return panel;
+	}
+
+}
