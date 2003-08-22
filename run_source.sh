4baf4aae94b3d8f7af5d27db17fcd26be8da5548
javaprog <javaprog>
2003-08-22 17:14:45 +0000

    [intern] merged changes from branch_0_12 (upgraded jwizz and adapted class path)

--- run_source.sh
+++ run_source.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 COLUMBA_HOME=`dirname $0`
-java -cp "$COLUMBA_HOME":"$COLUMBA_HOME"/classes:"$COLUMBA_HOME"/lib/log4j.jar:"$COLUMBA_HOME"/lib/jargs.jar:"$COLUMBA_HOME"/lib/jakarta-oro-2.0.6.jar:"$COLUMBA_HOME"/lib/lucene-1.3-rc1.jar:"$COLUMBA_HOME"/lib/jwizz-0.1.0.jar:"$COLUMBA_HOME"/lib/plastic.jar:"$COLUMBA_HOME"/lib/jhall.jar:"$COLUMBA_HOME"/lib/usermanual.jar org.columba.core.main.Main $@
+java -cp "$COLUMBA_HOME":"$COLUMBA_HOME"/classes:"$COLUMBA_HOME"/lib/log4j.jar:"$COLUMBA_HOME"/lib/jargs.jar:"$COLUMBA_HOME"/lib/jakarta-oro-2.0.6.jar:"$COLUMBA_HOME"/lib/lucene-1.3-rc1.jar:"$COLUMBA_HOME"/lib/jwizz-0.1.1.jar:"$COLUMBA_HOME"/lib/plastic.jar:"$COLUMBA_HOME"/lib/jhall.jar:"$COLUMBA_HOME"/lib/usermanual.jar org.columba.core.main.Main $@
