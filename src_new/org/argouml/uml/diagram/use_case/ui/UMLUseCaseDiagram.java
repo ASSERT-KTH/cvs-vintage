dc90f390395789e317e6a8dc361b05c7228403cf
jjones <jjones>
2003-09-02 20:19:25 +0000

    Rename "Usecase" -> "Use Case".

--- src_new/org/argouml/uml/diagram/use_case/ui/UMLUseCaseDiagram.java
+++ src_new/org/argouml/uml/diagram/use_case/ui/UMLUseCaseDiagram.java
@@ -1,4 +1,4 @@
-// $Id: UMLUseCaseDiagram.java,v 1.28 2003/08/20 22:27:32 alexb Exp $
+// $Id: UMLUseCaseDiagram.java,v 1.29 2003/09/02 20:19:25 jjones Exp $
 // Copyright (c) 1996-99 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
@@ -25,7 +25,7 @@
 // File: UMLUseCaseDiagram.java
 // Classes: UMLUseCaseDiagram
 // Original Author: your email here
-// $Id: UMLUseCaseDiagram.java,v 1.28 2003/08/20 22:27:32 alexb Exp $
+// $Id: UMLUseCaseDiagram.java,v 1.29 2003/09/02 20:19:25 jjones Exp $
 
 // 3 Apr 2002: Jeremy Bennett (mail@jeremybennett.com). Extended to support the
 // Extend and Include relationships. JavaDoc added for clarity. Default
@@ -308,7 +308,7 @@ public class UMLUseCaseDiagram extends UMLDiagram {
 
     protected static String getNewDiagramName() {
         String name = null;
-        name = "Usecase Diagram " + _UseCaseDiagramSerial;
+        name = "Use Case Diagram " + _UseCaseDiagramSerial;
         _UseCaseDiagramSerial++;
         if (!(ProjectManager.getManager().getCurrentProject()
 	      .isValidDiagramName(name))) {
