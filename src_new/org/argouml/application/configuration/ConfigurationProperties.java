760def3b99697ff341d8360fc5bf5fd2e38c3f30
thierrylach <thierrylach>
2003-09-03 15:16:49 +0000

    Automatically try to create an empty configuration file if one is not found.

--- src_new/org/argouml/application/configuration/ConfigurationProperties.java
+++ src_new/org/argouml/application/configuration/ConfigurationProperties.java
@@ -1,4 +1,4 @@
-// $Id: ConfigurationProperties.java,v 1.7 2003/06/29 23:53:41 linus Exp $
+// $Id: ConfigurationProperties.java,v 1.8 2003/09/03 15:16:49 thierrylach Exp $
 // Copyright (c) 1996-2001 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
@@ -26,10 +26,8 @@
 package org.argouml.application.configuration;
 import org.argouml.application.api.*;
 import java.io.*;
-import java.awt.event.*;
 import java.util.*;
 import java.net.*;
-import javax.swing.*;
 
 /**
  *  This class provides a user configuration based upon properties files.
@@ -96,12 +94,26 @@ public class ConfigurationProperties extends ConfigurationHandler {
     public boolean loadFile(File file) {
 	try {
 	    _properties.load(new FileInputStream(file));
-	    Argo.log.info ("Configuration loaded from " + file + "\n");
+	    Argo.log.info ("Configuration loaded from " + file);
 	    return true;
 	}
 	catch (Exception e) {
-	    if (_canComplain)
-		Argo.log.warn ("Unable to load configuration " + file + "\n");
+	    if (_canComplain) {
+			Argo.log.warn ("Unable to load configuration " + file);
+	    }
+		// Try to create an empty file.
+		try {
+			file.createNewFile();
+			if (file.exists() && file.isFile()) {
+	    		Argo.log.info ("New configuration created as " + file);
+				// Pretend we loaded the file correctly
+				return true;
+			}
+		}
+		catch (IOException e1) {
+			// Ignore an error here
+			Argo.log.warn ("Unable to create configuration " + file, e1);
+		}
 	    _canComplain = false;
 	}
 
