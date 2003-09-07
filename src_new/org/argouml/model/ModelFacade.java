13a96404e59ad76052e81cd1dc2ea0c3ee11bb4f
linus <linus>
2003-09-07 07:34:58 +0000

    Removed JMI suggestion again. It was all wrong.

--- src_new/org/argouml/model/ModelFacade.java
+++ src_new/org/argouml/model/ModelFacade.java
@@ -1,4 +1,4 @@
-// $Id: ModelFacade.java,v 1.115 2003/09/06 15:24:39 bobtarling Exp $
+// $Id: ModelFacade.java,v 1.116 2003/09/07 07:34:58 linus Exp $
 // Copyright (c) 2003 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
@@ -320,11 +320,6 @@ public class ModelFacade {
      * @returns true if handle is an Association
      */
     public static boolean isAAssociation(Object handle) {
-	// Suggestion for how to make ModelFacade handle JMI
-	// if (handle instanceof javax.jmi.model.Association)
-	//     return true;
-	// It seems that not all NSUML UML 1.3 objects are JMI interfaces.
-	// This might not be as easy as I thought. /Linus
         return handle instanceof MAssociation;
     }
 
