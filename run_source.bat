6ed280679f717f7aa0e625be20aba638ba066afd
fdietz <fdietz>
2003-09-09 19:55:25 +0000

    [intern]added ristretto.jar !!!

--- run_source.bat
+++ run_source.bat
@@ -1,2 +1,2 @@
-set CLASSPATH=.;.\classes;.\lib\log4j.jar;.\lib\junit.jar;.\lib\jakarta-oro-2.0.6.jar;.\lib\lucene-1.3-rc1.jar;.\lib\jargs.jar;.\lib\jwizz-0.1.1.jar;.\lib\plastic.jar;.\lib\jhall.jar;.\lib\usermanual.jar;.\lib\forms.jar
+set CLASSPATH=.;.\classes;.\lib\log4j.jar;.\lib\junit.jar;.\lib\jakarta-oro-2.0.6.jar;.\lib\lucene-1.3-rc1.jar;.\lib\jargs.jar;.\lib\jwizz-0.1.1.jar;.\lib\plastic.jar;.\lib\jhall.jar;.\lib\usermanual.jar;.\lib\forms.jar;.\lib\ristretto-1.0pre1.jar
 java org.columba.core.main.Main
