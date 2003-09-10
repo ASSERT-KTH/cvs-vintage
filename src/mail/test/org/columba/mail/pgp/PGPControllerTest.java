90d7c2e7e9cdadb454633f49f9e014ea597f7525
waffel <waffel>
2003-09-10 16:07:42 +0000

    [feature] adding tests for encrypting and decrypting

--- src/mail/test/org/columba/mail/pgp/PGPControllerTest.java
+++ src/mail/test/org/columba/mail/pgp/PGPControllerTest.java
@@ -1,13 +1,17 @@
 /*
  * Created on Jul 10, 2003
  *
- * To change the template for this generated file go to
- * Window>Preferences>Java>Code Generation>Code and Comments
+
  */
 package org.columba.mail.pgp;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+
 import junit.framework.TestCase;
 
+import org.columba.core.io.StreamUtils;
 import org.columba.core.xml.XmlElement;
 import org.columba.mail.config.PGPItem;
 
@@ -28,7 +32,7 @@ public class PGPControllerTest extends TestCase {
 		xmle.addAttribute("id", "testid");
 	}
 
-	public void testVerifySig() {
+	public void testVerifySig() throws Exception {
 		String sigData =
 			"-----BEGIN PGP SIGNATURE-----\n"
 				+ "Version: GnuPG v1.2.2-rc1-SuSE (GNU/Linux)\n\n"
@@ -36,26 +40,75 @@ public class PGPControllerTest extends TestCase {
 				+ "UT8MCuHOLNANpmWGqGWFmkc=\n"
 				+ "=P9GA\n"
 				+ "-----END PGP SIGNATURE-----";
+		InputStream sigDataStream =
+			new ByteArrayInputStream(sigData.getBytes("ISO-8859-1"));
 		String pgpMessage = "eine Testmessage";
-		PGPController pgpContr =PGPController.getInstance();
+		InputStream message =
+			new ByteArrayInputStream(pgpMessage.getBytes("ISO-8859-1"));
+		PGPController pgpContr = PGPController.getInstance();
 		PGPItem item = new PGPItem(xmle);
-		int retVal = pgpContr.verifySignature(pgpMessage, sigData, item);
-		assertEquals(0,retVal);
+		assertTrue(pgpContr.verifySignature(message, sigDataStream, item));
 	}
 	/**
 	* We can only test this, if verify is ok! What we test is, if the given testMessage is correct signed
 	*/
-	public void testSign() {
+	public void testSign() throws Exception {
 		String testData = "eine Testmessage";
+		InputStream testDataStream =
+			new ByteArrayInputStream(testData.getBytes("ISO-8859-1"));
+		// we must remeber the first position, so later we can at the the begin of the Stream. In other cases (real running 
+		// system) we should create a new Stream 
+		testDataStream.mark(80000);
 		// new PGPController
 		PGPController pgpContr = PGPController.getInstance();
 		// new Item with null, this is while the passphrase is in other situations in the configFile, here we set it per Hand
 		PGPItem item = new PGPItem(xmle);
 		// test passphrase. You must have a test pgp-key with this passphrase
 		item.setPassphrase("test");
-		String signStr = pgpContr.sign(testData, item);
-		int retVal = pgpContr.verifySignature(testData, signStr, item);
-		assertEquals(0,retVal);
+		InputStream signStream = pgpContr.sign(testDataStream, item);
+		// go to the begin of the Stream
+		testDataStream.reset();
+		assertTrue(pgpContr.verifySignature(testDataStream, signStream, item));
+	}
+
+	public void testDecrypt() throws Exception {
+		String testData =
+			"-----BEGIN PGP MESSAGE-----\n"
+				+ "Version: GnuPG v1.2.2-rc1-SuSE (GNU/Linux)\n\n"
+				+ "hQEOA0/l9LX8K2GEEAP/drK0opSL8CFpzdH/HAx4tMFV1GYKEVWLcqgshksahAQ/\n"
+				+ "yqhr5vVcOoNqIkdx6w7txPJnbaoRvhBuz0TEw6hrMkNheEEAkucLHfdJ5Z9iFodv\n"
+				+ "g/VpF5gnsOYBSS6zgizp0DTtElUx3um+7u7PwB3s1Ud6Dhs6XuRTJPiyPl0isHcD\n"
+				+ "/iAPGMzC9Vs1txMPnP7y969K+fYsIDsFxIiNQcw72/Bg9esY3EyFw8hJ5lLN0mDs\n"
+				+ "lN5DnIDcRoVCRvFH+czVdoVpkQeLyB/a8t4P01Y7vgvNpoBJeUHNgdzoZ9QGQhGa\n"
+				+ "zNzkVobyO3Fbp11kgiKDTXaWrcdYj4nN/+QBi3QgD9zT0kwBg/RQoKBkHnPZlOia\n"
+				+ "EaR4QFQ9TFJqM5FG6xZp64XJ1hHArIXl7zowWfAXAOuMm6St3O9+JcVRHIqSzw63\n"
+				+ "1OLBw4uSX/6AakYfiVY1\n"
+				+ "=ABon\n"
+				+ "-----END PGP MESSAGE-----";
+		InputStream testDataStream =
+			new ByteArrayInputStream(testData.getBytes("ISO-8859-1"));
+		PGPController pgpController = PGPController.getInstance();
+		PGPItem item = new PGPItem(xmle);
+		item.setPassphrase("test");
+		InputStream ret = pgpController.decrypt(testDataStream, item);
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		StreamUtils.streamCopy(ret, out);
+		assertEquals("eine Testmessage", out.toString());
+	}
+	
+	public void testEncrypt() throws Exception {
+		String testData = "eine Testmessage";
+		InputStream testDataStream = new ByteArrayInputStream(testData.getBytes("ISO-8859-1"));
+		PGPController pgpController = PGPController.getInstance();
+		PGPItem item = new PGPItem(xmle);
+		item.set("recipients","testid");
+		item.setPassphrase("test");
+		InputStream encryptStream = pgpController.encrypt(testDataStream, item);
+		InputStream ret = pgpController.decrypt(encryptStream, item);
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		StreamUtils.streamCopy(ret, out);
+		assertEquals(testData, out.toString());
+		
 	}
 
 }
