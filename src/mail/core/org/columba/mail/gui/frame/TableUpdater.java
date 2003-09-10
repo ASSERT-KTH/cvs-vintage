45f8ca95df0ab107a14a079b1247d8269568e682
fdietz <fdietz>
2003-09-10 10:28:29 +0000

    [intern]javadoc

--- src/mail/core/org/columba/mail/gui/frame/TableUpdater.java
+++ src/mail/core/org/columba/mail/gui/frame/TableUpdater.java
@@ -1,9 +1,18 @@
-/*
- * Created on Jun 12, 2003
- *
- * To change the template for this generated file go to
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
+//The contents of this file are subject to the Mozilla Public License Version 1.1
+//(the "License"); you may not use this file except in compliance with the
+//License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
+//
+//Software distributed under the License is distributed on an "AS IS" basis,
+//WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+//for the specific language governing rights and
+//limitations under the License.
+//
+//The Original Code is "The Columba Project"
+//
+//The Initial Developers of the Original Code are Frederik Dietz and Timo Stich.
+//Portions created by Frederik Dietz and Timo Stich are Copyright (C) 2003.
+//
+//All Rights Reserved.
 package org.columba.mail.gui.frame;
 
 import java.util.Iterator;
@@ -13,20 +22,34 @@ import java.util.Vector;
 import org.columba.mail.gui.table.model.TableModelChangedEvent;
 
 /**
- * @author frd
+ * Sends update event to {@link TableController}.
+ * <p>
+ * Don't call <code>tableController.tableChanged(ev)</code> directly.
+ * <p>
+ * All commands use this static object to notify that the table model
+ * has changed.
+ * 
  *
- * To change the template for this generated type comment go to
- * Window>Preferences>Java>Code Generation>Code and Comments
+ * @author fdietz
  */
 public class TableUpdater {
 
+	/**
+	 * Listeners list
+	 */
 	protected static List list = new Vector();
 
-	
-	public static void tableChanged(TableModelChangedEvent ev) throws Exception {
+	/**
+	 * Notify all tables that the table model has changed.
+	 * 
+	 * @param ev			event  
+	 * @throws Exception
+	 */
+	public static void tableChanged(TableModelChangedEvent ev)
+		throws Exception {
 		for (Iterator it = list.iterator(); it.hasNext();) {
-						AbstractMailFrameController frame = (AbstractMailFrameController) it.next();
-
+			AbstractMailFrameController frame =
+				(AbstractMailFrameController) it.next();
 			(
 				(
 					ThreePaneMailFrameController) frame)
@@ -37,6 +60,11 @@ public class TableUpdater {
 
 	}
 
+	/**
+	 * Add listener.
+	 * 
+	 * @param frameController		frame controller with table component
+	 */
 	public static void add(TableOwnerInterface frameController) {
 		list.add(frameController);
 	}
