ab4aabb7703d881f58aa52965bc5034b01682d6e
tstich <tstich>
2003-10-18 12:34:45 +0000

    [bug]passphrasedialog from pgp was shown twice when sending a message

--- src/mail/core/org/columba/mail/gui/composer/command/SaveMessageCommand.java
+++ src/mail/core/org/columba/mail/gui/composer/command/SaveMessageCommand.java
@@ -1,16 +1,18 @@
-//The contents of this file are subject to the Mozilla Public License Version 1.1
-//(the "License"); you may not use this file except in compliance with the 
+// The contents of this file are subject to the Mozilla Public License Version
+// 1.1
+//(the "License"); you may not use this file except in compliance with the
 //License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
 //
 //Software distributed under the License is distributed on an "AS IS" basis,
-//WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License 
+//WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
 //for the specific language governing rights and
 //limitations under the License.
 //
 //The Original Code is "The Columba Project"
 //
-//The Initial Developers of the Original Code are Frederik Dietz and Timo Stich.
-//Portions created by Frederik Dietz and Timo Stich are Copyright (C) 2003. 
+//The Initial Developers of the Original Code are Frederik Dietz and Timo
+// Stich.
+//Portions created by Frederik Dietz and Timo Stich are Copyright (C) 2003.
 //
 //All Rights Reserved.
 package org.columba.mail.gui.composer.command;
@@ -32,11 +34,10 @@ import org.columba.ristretto.message.HeaderInterface;
 
 /**
  * @author freddy
- *
+ * 
  * To change this generated comment edit the template variable "typecomment":
- * Window>Preferences>Java>Templates.
- * To enable and disable the creation of type comments go to
- * Window>Preferences>Java>Code Generation.
+ * Window>Preferences>Java>Templates. To enable and disable the creation of
+ * type comments go to Window>Preferences>Java>Code Generation.
  */
 public class SaveMessageCommand extends FolderCommand {
 
@@ -45,6 +46,7 @@ public class SaveMessageCommand extends FolderCommand {
 
 	/**
 	 * Constructor for SaveMessageCommand.
+	 * 
 	 * @param frameController
 	 * @param references
 	 */
@@ -76,10 +78,13 @@ public class SaveMessageCommand extends FolderCommand {
 		AccountItem item =
 			((ComposerModel) composerController.getModel()).getAccountItem();
 
-		SendableMessage message =
-			new MessageComposer(
-				((ComposerModel) composerController.getModel())).compose(
-				worker);
+		SendableMessage message = (SendableMessage) r[0].getMessage();
+		if (message == null) {
+			message =
+				new MessageComposer(
+					((ComposerModel) composerController.getModel())).compose(
+					worker);
+		}
 
 		folder = (Folder) r[0].getFolder();
 
@@ -88,9 +93,9 @@ public class SaveMessageCommand extends FolderCommand {
 			// -> this is necessary because SendableMessage contains
 			// -> additional information (like recipients list) which
 			// -> would get lost otherwise
-			
+
 			Object uid = folder.addMessage(message);
-			
+
 		} else {
 			// we can't use this addMessage(message) here
 			// -> IMAP only supports adding sources
