941a30ff1bcbd451a8f2a8f4feeecdb691c26aed
fdietz <fdietz>
2003-10-26 12:14:58 +0000

    [bug] #826918 wheel mouse scrolling wasn't working

--- src/mail/core/org/columba/mail/gui/message/MessagePanel.java
+++ src/mail/core/org/columba/mail/gui/message/MessagePanel.java
@@ -1,16 +1,18 @@
-//The contents of this file are subject to the Mozilla Public License Version 1.1
-//(the "License"); you may not use this file except in compliance with the 
+// The contents of this file are subject to the Mozilla Public License Version
+// 1.1
+//(the "License"); you may not use this file except in compliance with the
 //License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
 //
 //Software distributed under the License is distributed on an "AS IS" basis,
-//WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License 
+//WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
 //for the specific language governing rights and
 //limitations under the License.
 //
 //The Original Code is "The Columba Project"
 //
-//The Initial Developers of the Original Code are Frederik Dietz and Timo Stich.
-//Portions created by Frederik Dietz and Timo Stich are Copyright (C) 2003. 
+//The Initial Developers of the Original Code are Frederik Dietz and Timo
+// Stich.
+//Portions created by Frederik Dietz and Timo Stich are Copyright (C) 2003.
 //
 //All Rights Reserved.
 package org.columba.mail.gui.message;
@@ -22,47 +24,47 @@ import java.awt.Rectangle;
 import javax.swing.JPanel;
 import javax.swing.Scrollable;
 
-import org.columba.core.config.Config;
+import org.columba.core.gui.util.FontProperties;
 
 /**
  * @author timo
- *
+ * 
  * To change this generated comment edit the template variable "typecomment":
- * Window>Preferences>Java>Templates.
- * To enable and disable the creation of type comments go to
- * Window>Preferences>Java>Code Generation.
+ * Window>Preferences>Java>Templates. To enable and disable the creation of
+ * type comments go to Window>Preferences>Java>Code Generation.
  */
 public class MessagePanel extends JPanel implements Scrollable {
 
 	/**
 	 * @see javax.swing.Scrollable#getPreferredScrollableViewportSize()
 	 */
-	
-	
+
 	public Dimension getPreferredScrollableViewportSize() {
 		return getPreferredSize();
-		
+
 	}
-	
+
 	/**
-	 * @see javax.swing.Scrollable#getScrollableUnitIncrement(java.awt.Rectangle, int, int)
+	 * @see javax.swing.Scrollable#getScrollableUnitIncrement(java.awt.Rectangle,
+	 *      int, int)
 	 */
 	public int getScrollableUnitIncrement(Rectangle arg0, int arg1, int arg2) {
-		Font textFont = Config.getOptionsConfig().getGuiItem().getTextFont();
-		
+		Font textFont = FontProperties.getTextFont();
+
 		return textFont.getSize() * 3;
 	}
 
 	/**
-	 * @see javax.swing.Scrollable#getScrollableBlockIncrement(java.awt.Rectangle, int, int)
+	 * @see javax.swing.Scrollable#getScrollableBlockIncrement(java.awt.Rectangle,
+	 *      int, int)
 	 */
 	public int getScrollableBlockIncrement(
 		Rectangle arg0,
 		int arg1,
 		int arg2) {
-			Font textFont = Config.getOptionsConfig().getGuiItem().getTextFont();
-		
-			return textFont.getSize() * 10;
+		Font textFont = FontProperties.getTextFont();
+
+		return textFont.getSize() * 10;
 	}
 
 	/**
