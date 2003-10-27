c381a81e8eae6621d14cc7fbbe69e84d6e858126
fdietz <fdietz>
2003-10-27 18:32:40 +0000

    [bug]filenames should be decoded properly

--- src/mail/core/org/columba/mail/gui/attachment/command/SaveAttachmentCommand.java
+++ src/mail/core/org/columba/mail/gui/attachment/command/SaveAttachmentCommand.java
@@ -1,16 +1,18 @@
-//The contents of this file are subject to the Mozilla Public License Version 1.1
-//(the "License"); you may not use this file except in compliance with the 
+// The contents of this file are subject to the Mozilla Public License Version
+// 1.1
+//(the "License"); you may not use this file except in compliance with the
 //License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
 //
 //Software distributed under the License is distributed on an "AS IS" basis,
-//WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License 
+//WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
 //for the specific language governing rights and
 //limitations under the License.
 //
 //The Original Code is "The Columba Project"
 //
-//The Initial Developers of the Original Code are Frederik Dietz and Timo Stich.
-//Portions created by Frederik Dietz and Timo Stich are Copyright (C) 2003. 
+//The Initial Developers of the Original Code are Frederik Dietz and Timo
+// Stich.
+//Portions created by Frederik Dietz and Timo Stich are Copyright (C) 2003.
 //
 //All Rights Reserved.
 package org.columba.mail.gui.attachment.command;
@@ -31,17 +33,17 @@ import org.columba.mail.command.FolderCommand;
 import org.columba.mail.command.FolderCommandReference;
 import org.columba.mail.folder.Folder;
 import org.columba.ristretto.coder.Base64DecoderInputStream;
+import org.columba.ristretto.coder.EncodedWord;
 import org.columba.ristretto.coder.QuotedPrintableDecoderInputStream;
 import org.columba.ristretto.message.LocalMimePart;
 import org.columba.ristretto.message.MimeHeader;
 
 /**
  * @author freddy
- *
+ * 
  * To change this generated comment edit the template variable "typecomment":
- * Window>Preferences>Java>Templates.
- * To enable and disable the creation of type comments go to
- * Window>Preferences>Java>Code Generation.
+ * Window>Preferences>Java>Templates. To enable and disable the creation of
+ * type comments go to Window>Preferences>Java>Code Generation.
  */
 public class SaveAttachmentCommand extends FolderCommand {
 
@@ -51,6 +53,7 @@ public class SaveAttachmentCommand extends FolderCommand {
 
 	/**
 	 * Constructor for SaveAttachmentCommand.
+	 * 
 	 * @param frameController
 	 * @param references
 	 */
@@ -79,6 +82,12 @@ public class SaveAttachmentCommand extends FolderCommand {
 		String fileName = part.getHeader().getContentParameter("name");
 		if (fileName == null)
 			fileName = part.getHeader().getDispositionParameter("filename");
+
+		// decode filename
+		if (fileName != null) {
+			StringBuffer buf = EncodedWord.decode(fileName);
+			fileName = buf.toString();
+		}
 
 		cFileChooser fileChooser;
 
@@ -122,14 +131,14 @@ public class SaveAttachmentCommand extends FolderCommand {
 		header = part.getHeader();
 		InputStream bodyStream = part.getInputStream();
 		String encoding = header.getContentTransferEncoding();
-		if( encoding != null ) {
-			if( encoding.equals("quoted-printable") ) {
+		if (encoding != null) {
+			if (encoding.equals("quoted-printable")) {
 				bodyStream = new QuotedPrintableDecoderInputStream(bodyStream);
-			} else if ( encoding.equals("base64")) {
-				bodyStream = new Base64DecoderInputStream( bodyStream );
+			} else if (encoding.equals("base64")) {
+				bodyStream = new Base64DecoderInputStream(bodyStream);
 			}
 		}
-				
+
 		StreamUtils.streamCopy(bodyStream, new FileOutputStream(tempFile));
 	}
 }
