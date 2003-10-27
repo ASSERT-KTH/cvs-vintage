b9235983e27751b7a8f98a6abce87c6851157e39
fdietz <fdietz>
2003-10-27 10:32:37 +0000

    [ui]mnemonic and layout cleanups

--- src/columba/core/org/columba/core/gui/externaltools/ExternalToolsDialog.java
+++ src/columba/core/org/columba/core/gui/externaltools/ExternalToolsDialog.java
@@ -1,16 +1,18 @@
-//The contents of this file are subject to the Mozilla Public License Version 1.1
-//(the "License"); you may not use this file except in compliance with the 
+// The contents of this file are subject to the Mozilla Public License Version
+// 1.1
+//(the "License"); you may not use this file except in compliance with the
 //License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
 //
 //Software distributed under the License is distributed on an "AS IS" basis,
-//WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License 
+//WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
 //for the specific language governing rights and
 //limitations under the License.
 //
 //The Original Code is "The Columba Project"
 //
-//The Initial Developers of the Original Code are Frederik Dietz and Timo Stich.
-//Portions created by Frederik Dietz and Timo Stich are Copyright (C) 2003. 
+//The Initial Developers of the Original Code are Frederik Dietz and Timo
+// Stich.
+//Portions created by Frederik Dietz and Timo Stich are Copyright (C) 2003.
 //
 //All Rights Reserved.
 package org.columba.core.gui.externaltools;
@@ -101,18 +103,17 @@ public class ExternalToolsDialog
 		JPanel mainPanel = new JPanel(new BorderLayout());
 		mainPanel.setBorder(BorderFactory.createEmptyBorder(12, 12, 12, 12));
 
-
 		// TODO: i18n
 		configButton = new ButtonWithMnemonic("Con&figure...");
 		configButton.setActionCommand("CONFIG");
 		configButton.addActionListener(this);
-                configButton.setEnabled(false);
+		configButton.setEnabled(false);
 
 		// TODO: i18n
 		infoButton = new ButtonWithMnemonic("&Details...");
 		infoButton.setActionCommand("INFO");
 		infoButton.addActionListener(this);
-                infoButton.setEnabled(false);
+		infoButton.setEnabled(false);
 
 		String[] ids = handler.getPluginIdList();
 		list = new JList(ids);
@@ -121,7 +122,7 @@ public class ExternalToolsDialog
 		// top panel
 
 		JPanel topPanel = new JPanel();
-		
+
 		topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.X_AXIS));
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		GridBagConstraints c = new GridBagConstraints();
@@ -129,7 +130,8 @@ public class ExternalToolsDialog
 
 		JPanel topBorderPanel = new JPanel();
 		topBorderPanel.setLayout(new BorderLayout());
-		//topBorderPanel.setBorder(BorderFactory.createEmptyBorder(0, 0, 5, 0));
+		//topBorderPanel.setBorder(BorderFactory.createEmptyBorder(0, 0, 5,
+		// 0));
 		topBorderPanel.add(topPanel);
 		//mainPanel.add( topBorderPanel, BorderLayout.NORTH );
 
@@ -175,23 +177,25 @@ public class ExternalToolsDialog
 		JPanel centerPanel = new JPanel(new BorderLayout());
 		centerPanel.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 6));
 		JScrollPane scrollPane = new JScrollPane(list);
-		scrollPane.setPreferredSize(new Dimension(150, 150));
+		scrollPane.setPreferredSize(new Dimension(250, 150));
 		scrollPane.getViewport().setBackground(Color.white);
 		centerPanel.add(scrollPane);
 
 		mainPanel.add(centerPanel);
 		getContentPane().add(mainPanel, BorderLayout.CENTER);
-		
+
 		JPanel bottomPanel = new JPanel(new BorderLayout());
 		bottomPanel.setBorder(new SingleSideEtchedBorder(SwingConstants.TOP));
 		JPanel buttonPanel = new JPanel(new GridLayout(1, 3, 6, 0));
 		buttonPanel.setBorder(BorderFactory.createEmptyBorder(12, 12, 12, 12));
 		ButtonWithMnemonic closeButton =
-			new ButtonWithMnemonic(MailResourceLoader.getString("global", "close"));
+			new ButtonWithMnemonic(
+				MailResourceLoader.getString("global", "close"));
 		closeButton.setActionCommand("CLOSE"); //$NON-NLS-1$
 		closeButton.addActionListener(this);
 		buttonPanel.add(closeButton);
-		ButtonWithMnemonic helpButton = new ButtonWithMnemonic(
+		ButtonWithMnemonic helpButton =
+			new ButtonWithMnemonic(
 				MailResourceLoader.getString("global", "help"));
 		// associate with JavaHelp
 		HelpManager.enableHelpOnButton(helpButton, "extending_columba_2");
@@ -234,13 +238,15 @@ public class ExternalToolsDialog
 		}
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see javax.swing.event.ListSelectionListener#valueChanged(javax.swing.event.ListSelectionEvent)
 	 */
 	public void valueChanged(ListSelectionEvent e) {
-                boolean enabled = !list.isSelectionEmpty();
-                configButton.setEnabled(enabled);
-                infoButton.setEnabled(enabled);
+		boolean enabled = !list.isSelectionEmpty();
+		configButton.setEnabled(enabled);
+		infoButton.setEnabled(enabled);
 		selection = (String) list.getSelectedValue();
 	}
 }
--- src/columba/core/org/columba/core/gui/util/InfoViewerDialog.java
+++ src/columba/core/org/columba/core/gui/util/InfoViewerDialog.java
@@ -1,16 +1,18 @@
-//The contents of this file are subject to the Mozilla Public License Version 1.1
-//(the "License"); you may not use this file except in compliance with the 
+// The contents of this file are subject to the Mozilla Public License Version
+// 1.1
+//(the "License"); you may not use this file except in compliance with the
 //License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
 //
 //Software distributed under the License is distributed on an "AS IS" basis,
-//WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License 
+//WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
 //for the specific language governing rights and
 //limitations under the License.
 //
 //The Original Code is "The Columba Project"
 //
-//The Initial Developers of the Original Code are Frederik Dietz and Timo Stich.
-//Portions created by Frederik Dietz and Timo Stich are Copyright (C) 2003. 
+//The Initial Developers of the Original Code are Frederik Dietz and Timo
+// Stich.
+//Portions created by Frederik Dietz and Timo Stich are Copyright (C) 2003.
 //
 //All Rights Reserved.
 package org.columba.core.gui.util;
@@ -41,13 +43,15 @@ import javax.swing.UIManager;
 import javax.swing.text.html.HTMLEditorKit;
 import javax.swing.text.html.StyleSheet;
 
+import net.javaprog.ui.wizard.plaf.basic.SingleSideEtchedBorder;
+
 import org.columba.mail.gui.util.URLController;
 import org.columba.mail.util.MailResourceLoader;
 
 /**
  * Dialg showing information to the user. This can be either a URL to document
  * or a string.
- *
+ * 
  * @author fdietz
  */
 public class InfoViewerDialog extends JDialog implements ActionListener {
@@ -58,7 +62,7 @@ public class InfoViewerDialog extends JDialog implements ActionListener {
 	JTextPane textPane;
 
 	public InfoViewerDialog(String message) {
-                this();
+		this();
 		HTMLEditorKit editorKit = new HTMLEditorKit();
 		StyleSheet styles = new StyleSheet();
 
@@ -76,34 +80,34 @@ public class InfoViewerDialog extends JDialog implements ActionListener {
 
 		textPane.setEditorKit(editorKit);
 		textPane.setText(message);
-                setVisible(true);
+		setVisible(true);
 	}
 
 	/**
 	 * @throws java.awt.HeadlessException
 	 */
 	public InfoViewerDialog(URL url) {
-                this();
+		this();
 		try {
 			textPane.setPage(url);
 		} catch (IOException ex) {
 			NotifyDialog d = new NotifyDialog();
 			d.showDialog(ex);
 		}
-                setVisible(true);
+		setVisible(true);
 	}
-        
-        protected InfoViewerDialog() {
+
+	protected InfoViewerDialog() {
 		super(new JFrame(), "Info", true);
 		initComponents();
 		pack();
 		setLocationRelativeTo(null);
 		textPane.setEditable(false);
-        }
+	}
 
 	protected void initComponents() {
 		JPanel mainPanel = new JPanel(new BorderLayout());
-		mainPanel.setBorder(BorderFactory.createEmptyBorder(12, 12, 0, 11));
+		mainPanel.setBorder(BorderFactory.createEmptyBorder(12, 12, 12, 12));
 		getContentPane().add(mainPanel);
 
 		// centerpanel
@@ -115,14 +119,17 @@ public class InfoViewerDialog extends JDialog implements ActionListener {
 		mainPanel.add(scrollPane);
 
 		JPanel bottomPanel = new JPanel(new BorderLayout());
-		JPanel buttonPanel = new JPanel(new GridLayout(1, 2, 5, 0));
-		buttonPanel.setBorder(BorderFactory.createEmptyBorder(17, 12, 11, 11));
-		JButton closeButton =
-			new JButton(MailResourceLoader.getString("global", "close"));
+		bottomPanel.setBorder(new SingleSideEtchedBorder(SwingConstants.TOP));
+		JPanel buttonPanel = new JPanel(new GridLayout(1, 2, 6, 0));
+		buttonPanel.setBorder(BorderFactory.createEmptyBorder(12, 12, 12, 12));
+		ButtonWithMnemonic closeButton =
+			new ButtonWithMnemonic(
+				MailResourceLoader.getString("global", "close"));
 		closeButton.setActionCommand("CLOSE"); //$NON-NLS-1$
 		closeButton.addActionListener(this);
 		buttonPanel.add(closeButton);
-		ButtonWithMnemonic helpButton = new ButtonWithMnemonic(
+		ButtonWithMnemonic helpButton =
+			new ButtonWithMnemonic(
 				MailResourceLoader.getString("global", "help"));
 		helpButton.setActionCommand("HELP");
 		helpButton.addActionListener(this);
