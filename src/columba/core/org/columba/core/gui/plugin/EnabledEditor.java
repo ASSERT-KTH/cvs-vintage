9997e0cced7b82b8dc4bd3ffb3100ce1c64d08ad
javaprog <javaprog>
2003-11-04 16:27:46 +0000

    [intern] code cleanup, removed System.out.println statement

--- src/columba/core/org/columba/core/gui/plugin/EnabledEditor.java
+++ src/columba/core/org/columba/core/gui/plugin/EnabledEditor.java
@@ -1,9 +1,19 @@
-/*
- * Created on 07.08.2003
- *
- * To change the template for this generated file go to
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
+//The contents of this file are subject to the Mozilla Public License Version 1.1
+//(the "License"); you may not use this file except in compliance with the 
+//License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
+//
+//Software distributed under the License is distributed on an "AS IS" basis,
+//WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License 
+//for the specific language governing rights and
+//limitations under the License.
+//
+//The Original Code is "The Columba Project"
+//
+//The Initial Developers of the Original Code are Frederik Dietz and Timo Stich.
+//Portions created by Frederik Dietz and Timo Stich are Copyright (C) 2003. 
+//
+//All Rights Reserved.
+
 package org.columba.core.gui.plugin;
 
 import java.awt.Component;
@@ -20,21 +30,17 @@ import javax.swing.table.TableCellEditor;
  * To change the template for this generated type comment go to
  * Window>Preferences>Java>Code Generation>Code and Comments
  */
-public class EnabledEditor
-	extends AbstractCellEditor
+public class EnabledEditor extends AbstractCellEditor
 	implements TableCellEditor {
 
-	JCheckBox component = new JCheckBox();
-
-	PluginNode currentNode;
+	protected JCheckBox component = new JCheckBox();
+	protected PluginNode currentNode;
 
 	/**
 	 * 
 	 */
 	public EnabledEditor() {
-
 		component.setHorizontalAlignment(SwingConstants.CENTER);
-		
 	}
 	
 	public int getClickCountToStart() {
@@ -55,13 +61,10 @@ public class EnabledEditor
 		 ((JCheckBox) component).setSelected(currentNode.isEnabled());
 
 		if (isSelected) {
-
 			((JCheckBox) component).setBackground(
 				table.getSelectionBackground());
 		} else {
-
 			((JCheckBox) component).setBackground(table.getBackground());
-
 		}
 
 		// Return the configured component
@@ -71,7 +74,6 @@ public class EnabledEditor
 	// This method is called when editing is completed.
 	// It must return the new value to be stored in the cell.
 	public Object getCellEditorValue() {
-		
 		Boolean b = Boolean.valueOf(((JCheckBox) component).isSelected());
 		
 		// enable/disable tree node
@@ -84,8 +86,6 @@ public class EnabledEditor
 		MainInterface.pluginManager.setEnabled(id, b.booleanValue());
 		*/
 		
-		System.out.println("cell-editor="+b);
-		
 		return b;
 	}
 	
@@ -93,6 +93,4 @@ public class EnabledEditor
 	{
 		return component;
 	}
-	
-	
 }
