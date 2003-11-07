452bd875ab09d9a4e8728aa0a9854871e1e853fa
fdietz <fdietz>
2003-11-07 22:07:26 +0000

    [bug]fixes linebreaks (patch submitted by Georg)

--- src/columba/core/org/columba/core/gui/util/MultiLineLabel.java
+++ src/columba/core/org/columba/core/gui/util/MultiLineLabel.java
@@ -1,16 +1,18 @@
-//The contents of this file are subject to the Mozilla Public License Version 1.1
-//(the "License"); you may not use this file except in compliance with the 
+// The contents of this file are subject to the Mozilla Public License Version
+// 1.1
+//(the "License"); you may not use this file except in compliance with the
 //License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
 //
 //Software distributed under the License is distributed on an "AS IS" basis,
-//WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License 
+//WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
 //for the specific language governing rights and
 //limitations under the License.
 //
 //The Original Code is "The Columba Project"
 //
-//The Initial Developers of the Original Code are Frederik Dietz and Timo Stich.
-//Portions created by Frederik Dietz and Timo Stich are Copyright (C) 2003. 
+//The Initial Developers of the Original Code are Frederik Dietz and Timo
+// Stich.
+//Portions created by Frederik Dietz and Timo Stich are Copyright (C) 2003.
 //
 //All Rights Reserved.
 
@@ -49,62 +51,68 @@ public class MultiLineLabel extends JTextArea {
 	}
 
 	private String createSizedString(final String message, final int pixels) {
-		if ( message==null ) return "";
-		if ( message.length() == 0) return "";
-		
+		if (message == null)
+			return "";
+		if (message.length() == 0)
+			return "";
+
 		FontMetrics fm = getFontMetrics(getFont());
-		StringTokenizer st = new StringTokenizer(message);
+
 		String word;
 		StringBuffer sb = new StringBuffer();
-		StringBuffer cursb = new StringBuffer();
-		while (st.hasMoreTokens()) {
-			word = st.nextToken();
-			if (fm.stringWidth(cursb.toString() + word) > pixels) {
-				sb.append(cursb.toString());
-				sb.append("\n");
-				cursb = new StringBuffer();
+		StringBuffer cursb;
+		StringTokenizer st;
+
+		/*
+		 * StringTokenizer will split a\n\nb in only 2 parts. String replace
+		 * all provides a workaround by generating a\n \nb,
+		 */
+		StringTokenizer stn =
+			new StringTokenizer(message.replaceAll("\n\n", "\n \n"), "\n");
+
+		while (stn.hasMoreTokens()) {
+			st = new StringTokenizer(stn.nextToken());
+			cursb = new StringBuffer();
+
+			while (st.hasMoreTokens()) {
+				word = st.nextToken();
+
+				if (fm.stringWidth(cursb.toString() + word) > pixels) {
+					sb.append(cursb.toString());
+					sb.append("\n");
+					cursb = new StringBuffer();
+				}
+				cursb.append(word);
+				cursb.append(" ");
 			}
-			cursb.append(word);
-			cursb.append(" ");
+			sb.append(cursb.toString() + "\n");
 		}
-		sb.append(cursb.toString());
 		return sb.toString();
 	}
-	
+
 	/*
-	public MultiLineLabel( String s )
-	{
-	    super( s );
-	
-	    //setBorder( UIManager.getBorder( "Label.border" ) );
-	    //setBorder( BorderFactory.createEmptyBorder( 5,5,5,5 ) );
-	    
-	    setEditable( false );
-	    setBackground( UIManager.getColor( "Label.background" ) );
-	    setFont( UIManager.getFont( "Label.font") );
-	    setWrapStyleWord( true );
-	
-	}
-	
-	
-		
-	public MultiLineLabel( String[] s )
-	{
-	    StringBuffer buf = new StringBuffer();
-	
-	    for ( int i=0; i<s.length; i++ )
-	    {
-	        buf.append( s[i] );
-	    }
-	
-	    setText( buf.toString() );
-	    setBorder( BorderFactory.createEmptyBorder( 10,10,10,10 ) );
-	    setEditable( false );
-	    setBackground( UIManager.getColor( "Label.background" ) );
-	    setFont( UIManager.getFont( "Label.font") );
-	    setWrapStyleWord( true );
-	}
-	*/
+	 * public MultiLineLabel( String s ) { super( s );
+	 * 
+	 * //setBorder( UIManager.getBorder( "Label.border" ) ); //setBorder(
+	 * BorderFactory.createEmptyBorder( 5,5,5,5 ) );
+	 * 
+	 * setEditable( false ); setBackground( UIManager.getColor(
+	 * "Label.background" ) ); setFont( UIManager.getFont( "Label.font") );
+	 * setWrapStyleWord( true );
+	 *  }
+	 * 
+	 * 
+	 * 
+	 * public MultiLineLabel( String[] s ) { StringBuffer buf = new
+	 * StringBuffer();
+	 * 
+	 * for ( int i=0; i <s.length; i++ ) { buf.append( s[i] ); }
+	 * 
+	 * setText( buf.toString() ); setBorder( BorderFactory.createEmptyBorder(
+	 * 10,10,10,10 ) ); setEditable( false ); setBackground(
+	 * UIManager.getColor( "Label.background" ) ); setFont( UIManager.getFont(
+	 * "Label.font") ); setWrapStyleWord( true ); }
+	 */
 
 	public void updateUI() {
 		super.updateUI();
