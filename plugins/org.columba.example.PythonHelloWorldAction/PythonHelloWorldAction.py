a9e926d1668ecf79387453a5613786c344431110
fdietz <fdietz>
2003-11-22 18:13:47 +0000

    [bug]fixed compile error

--- plugins/org.columba.example.PythonHelloWorldAction/PythonHelloWorldAction.py
+++ plugins/org.columba.example.PythonHelloWorldAction/PythonHelloWorldAction.py
@@ -4,7 +4,7 @@ from org.columba.core.action import AbstractColumbaAction
 
 class PythonHelloWorldAction(AbstractColumbaAction):
 	def __init__(self, controller):
-		FrameAction.__init__(self, controller,"Python Hello World", "This is the long version", "Show me this tooltip", "PYTHON_HELLO_WORLD", None, None, 0, None)
+		AbstractColumbaAction.__init__(self, controller,"Python Hello World")
 		
 	def actionPerformed(self, event):
 		print "Python Hello World!"
