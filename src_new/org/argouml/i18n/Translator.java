e6eb54c316dd3ea0c22a2e672936f6ddac62c451
jhraigniac <jhraigniac>
2003-12-08 08:55:01 +0000

    This variable should be set to any other value than "none" only when working on i18n stuff.

--- src_new/org/argouml/i18n/Translator.java
+++ src_new/org/argouml/i18n/Translator.java
@@ -1,4 +1,4 @@
-// $Id: Translator.java,v 1.18 2003/12/01 18:25:39 jhraigniac Exp $
+// $Id: Translator.java,v 1.19 2003/12/08 08:55:01 jhraigniac Exp $
 // Copyright (c) 1996-2003 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
@@ -62,7 +62,7 @@ public class Translator {
         org.workingfrog.i18n.util.Translator.setDefaultLocale(
 							      new Locale("en", ""));
         org.workingfrog.i18n.util.Translator.setBundlesPath("org.argouml.i18n");
-        org.workingfrog.i18n.util.Translator.setLogLevel("all");
+        org.workingfrog.i18n.util.Translator.setLogLevel("none");
   
         Localizer.addResource("GefBase",
 			      "org.tigris.gef.base.BaseResourceBundle");
