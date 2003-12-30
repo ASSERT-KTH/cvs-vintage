d981fa89ef35562d7057b8858b6c5a6075700b9f
javaprog <javaprog>
2003-12-30 17:56:57 +0000

    [intern] added license header

--- src/columba/core/org/columba/core/main/CmdLineArgumentHandler.java
+++ src/columba/core/org/columba/core/main/CmdLineArgumentHandler.java
@@ -1,8 +1,19 @@
-/*
- * Created on Apr 28, 2003
- * File CmdLineArgumentHandler.java
- *
- */
+//The contents of this file are subject to the Mozilla Public License Version 1.1
+//(the "License"); you may not use this file except in compliance with the 
+//License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
+//
+//Software distributed under the License is distributed on an "AS IS" basis,
+//WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License 
+//for the specific language governing rights and
+//limitations under the License.
+//
+//The Original Code is "The Columba Project"
+//
+//The Initial Developers of the Original Code are Frederik Dietz and Timo Stich.
+//Portions created by Frederik Dietz and Timo Stich are Copyright (C) 2003. 
+//
+//All Rights Reserved.
+
 package org.columba.core.main;
 
 import org.columba.core.logging.ColumbaLogger;
@@ -10,7 +21,6 @@ import org.columba.core.logging.ColumbaLogger;
 import org.columba.mail.gui.composer.ComposerController;
 import org.columba.mail.gui.composer.ComposerModel;
 import org.columba.mail.parser.MailUrlParser;
-
 
 /**
  * This class handles given arguments (in style of commandline arguments. If for example the
