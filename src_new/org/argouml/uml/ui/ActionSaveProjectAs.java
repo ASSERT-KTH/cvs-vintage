7a3c3659ca2bc0f6864d999e82cad9e8ac27131a
mvw <mvw>
2004-01-03 14:35:37 +0000

    Added a space in the window title between both items.

--- src_new/org/argouml/uml/ui/ActionSaveProjectAs.java
+++ src_new/org/argouml/uml/ui/ActionSaveProjectAs.java
@@ -1,4 +1,4 @@
-// $Id: ActionSaveProjectAs.java,v 1.20 2003/11/25 10:58:13 jhraigniac Exp $
+// $Id: ActionSaveProjectAs.java,v 1.21 2004/01/03 14:35:37 mvw Exp $
 // Copyright (c) 1996-2002 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
@@ -96,7 +96,7 @@ public class ActionSaveProjectAs extends ActionSaveProject {
 
         String sChooserTitle =
         Translator.localize("Actions", "filechooser.save-as-project");
-        chooser.setDialogTitle(sChooserTitle + p.getName());
+        chooser.setDialogTitle(sChooserTitle + " " + p.getName());
         chooser.setFileFilter(FileFilters.CompressedFileFilter);
 
         int retval = chooser.showSaveDialog(pb);
