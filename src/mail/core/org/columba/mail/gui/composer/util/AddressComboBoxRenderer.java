5f5ec0f7372aaa2ef9311ea5f7333d8817ad65eb
redsolo <redsolo>
2004-01-12 23:09:07 +0000

    [intern] Fixed a NPE occurance if there is no such email address stored in the addressbook.

--- src/mail/core/org/columba/mail/gui/composer/util/AddressComboBoxRenderer.java
+++ src/mail/core/org/columba/mail/gui/composer/util/AddressComboBoxRenderer.java
@@ -1,19 +1,25 @@
 //The contents of this file are subject to the Mozilla Public License Version 1.1
-//(the "License"); you may not use this file except in compliance with the 
+//(the "License"); you may not use this file except in compliance with the
 //License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
 //
 //Software distributed under the License is distributed on an "AS IS" basis,
-//WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License 
+//WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
 //for the specific language governing rights and
 //limitations under the License.
 //
 //The Original Code is "The Columba Project"
 //
 //The Initial Developers of the Original Code are Frederik Dietz and Timo Stich.
-//Portions created by Frederik Dietz and Timo Stich are Copyright (C) 2003. 
+//Portions created by Frederik Dietz and Timo Stich are Copyright (C) 2003.
 //
 //All Rights Reserved.
 package org.columba.mail.gui.composer.util;
+
+import org.columba.addressbook.folder.HeaderItem;
+
+import org.columba.core.gui.util.ImageLoader;
+
+import org.columba.mail.util.AddressCollector;
 
 import java.awt.Component;
 
@@ -22,88 +28,85 @@ import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.ListCellRenderer;
 
-import org.columba.addressbook.folder.HeaderItem;
-import org.columba.core.gui.util.ImageLoader;
-import org.columba.mail.util.AddressCollector;
 
 /**
- * 
+ *
  *
  * @author fdietz
  */
-public class AddressComboBoxRenderer
-	extends JLabel
-	implements ListCellRenderer {
+public class AddressComboBoxRenderer extends JLabel implements ListCellRenderer {
+    private ImageIcon contactIcon = ImageLoader.getSmallImageIcon("contact_small.png");
+    private ImageIcon groupIcon = ImageLoader.getSmallImageIcon("group_small.png");
 
-	ImageIcon contactIcon= ImageLoader.getSmallImageIcon("contact_small.png");
-	ImageIcon groupIcon= ImageLoader.getSmallImageIcon("group_small.png");
+    public AddressComboBoxRenderer() {
+        setOpaque(true);
+    }
 
-	public AddressComboBoxRenderer() {
-		setOpaque(true);
-	}
+    /** {@inheritDoc} */
+    public Component getListCellRendererComponent(JList list, Object value,
+        int index, boolean isSelected, boolean cellHasFocus) {
+        if (isSelected) {
+            setBackground(list.getSelectionBackground());
+            setForeground(list.getSelectionForeground());
+        } else {
+            setBackground(list.getBackground());
+            setForeground(list.getForeground());
+        }
 
-	public Component getListCellRendererComponent(
-		JList list,
-		Object value,
-		int index,
-		boolean isSelected,
-		boolean cellHasFocus) {
+        HeaderItem item = AddressCollector.getHeaderItem((String) value);
 
-		if (isSelected) {
-			setBackground(list.getSelectionBackground());
-			setForeground(list.getSelectionForeground());
-		} else {
-			setBackground(list.getBackground());
-			setForeground(list.getForeground());
-		}
+        if (item == null) {
+            setToolTipText("");
+            setText((String) value);
+            setIcon(null);
 
-		HeaderItem item= AddressCollector.getHeaderItem((String) value);
+        } else {
 
-		setText(item.toString());
+            setText(item.toString());
 
-		if (item.isContact()) {
-			String displayname= (String) item.get("displayname");
+            if (item.isContact()) {
+                String displayname = (String) item.get("displayname");
 
-			StringBuffer buf= new StringBuffer();
-			buf.append("<html><body>&nbsp;Name: " + convert(displayname));
-			buf.append(
-				"<br>&nbsp;eMail: " + convert((String) item.get("email;internet")));
-			buf.append("</body></html>");
-			setToolTipText(buf.toString());
+                StringBuffer buf = new StringBuffer();
+                buf.append("<html><body>&nbsp;Name: " + convert(displayname));
+                buf.append("<br>&nbsp;eMail: "
+                    + convert((String) item.get("email;internet")));
+                buf.append("</body></html>");
+                setToolTipText(buf.toString());
 
-			setIcon(contactIcon);
-		} else {
-			setIcon(groupIcon);
-			
-			setToolTipText("");
-		}
+                setIcon(contactIcon);
+            } else {
+                setIcon(groupIcon);
+                setToolTipText("");
+            }
+        }
 
-		return this;
-	}
+        return this;
+    }
 
-	private String convert(String str) {
-		if (str == null) {
-			return "";
-		}
+    private String convert(String str) {
+        if (str == null) {
+            return "";
+        }
 
-		StringBuffer result= new StringBuffer();
-		int pos= 0;
-		char ch;
+        StringBuffer result = new StringBuffer();
+        int pos = 0;
+        char ch;
 
-		while (pos < str.length()) {
-			ch= str.charAt(pos);
+        while (pos < str.length()) {
+            ch = str.charAt(pos);
 
-			if (ch == '<') {
-				result.append("&lt;");
-			} else if (ch == '>') {
-				result.append("&gt;");
-			} else {
-				result.append(ch);
-			}
+            if (ch == '<') {
+                result.append("&lt;");
+            } else if (ch == '>') {
+                result.append("&gt;");
+            } else {
+                result.append(ch);
+            }
 
-			pos++;
-		}
+            pos++;
+        }
 
-		return result.toString();
-	}
+        return result.toString();
+    }
 }
