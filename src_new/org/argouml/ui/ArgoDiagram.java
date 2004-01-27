d370724e20a8b20e676b8b4c9a608dffdafc337f
bobtarling <bobtarling>
2004-01-27 20:45:18 +0000

    Correct deprecation

--- src_new/org/argouml/ui/ArgoDiagram.java
+++ src_new/org/argouml/ui/ArgoDiagram.java
@@ -1,4 +1,4 @@
-// $Id: ArgoDiagram.java,v 1.25 2004/01/04 20:17:43 bobtarling Exp $
+// $Id: ArgoDiagram.java,v 1.26 2004/01/27 20:45:18 bobtarling Exp $
 // Copyright (c) 1996-99 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
@@ -157,13 +157,13 @@ public class ArgoDiagram extends Diagram {
     }
 
     /**
-     * @deprecated 0.16 in favour of getNodes(Collection)
+     * @deprecated 0.15.3 in favour of getNodes(Collection)
      */
     public Vector getNodes() {
         if (getGraphModel() != null) {
             return getGraphModel().getNodes();
         }
-        return super.getNodes();
+        return new Vector (getNodes(null));
     }
     
     /**
