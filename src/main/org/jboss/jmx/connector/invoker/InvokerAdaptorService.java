87cda068c1b22cf703412c67da17ec64d1cdbd34
starksm <starksm>
2004-01-30 05:37:37 +0000

    Merge the 3.2 changes to support remote callbacks via RMINotificationListener

--- src/main/org/jboss/jmx/connector/invoker/InvokerAdaptorService.java
+++ src/main/org/jboss/jmx/connector/invoker/InvokerAdaptorService.java
@@ -9,20 +9,26 @@ package org.jboss.jmx.connector.invoker;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.UndeclaredThrowableException;
+import java.rmi.RemoteException;
+import java.security.Principal;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-import java.security.Principal;
-
+import javax.management.InstanceNotFoundException;
+import javax.management.ListenerNotFoundException;
 import javax.management.MBeanServer;
+import javax.management.NotificationFilter;
 import javax.management.ObjectName;
 
 import org.jboss.invocation.Invocation;
 import org.jboss.invocation.MarshalledInvocation;
+import org.jboss.jmx.adaptor.rmi.NotificationListenerDelegate;
+import org.jboss.jmx.adaptor.rmi.RMIAdaptor;
+import org.jboss.jmx.adaptor.rmi.RMINotificationListener;
 import org.jboss.mx.server.ServerConstants;
-import org.jboss.system.ServiceMBeanSupport;
-import org.jboss.system.Registry;
 import org.jboss.security.SecurityAssociation;
+import org.jboss.system.Registry;
+import org.jboss.system.ServiceMBeanSupport;
 
 /**
  * A JBoss service exposes an invoke(Invocation) operation that maps
@@ -36,16 +42,17 @@ import org.jboss.security.SecurityAssociation;
  * from the other method arguments to avoid unmarshalling them
  * before the classloader is determined from the ObjectName.<p>
  *
- * The interface is configurable, it must be similar to MBeanServer,
- * though not necessarily derived from it<p>
+ * The interface is configurable, it must be similar to
+ * MBeanServerConnection, though not necessarily derived from it.
+ * The default interface is the org.
+ * <p>
  *
  * The invoker is configurable and must be specified
  *
- * @todo The interface should be MBeanServerConnection in jmx1.2
- *
+ * @see org.jboss.jmx.adaptor.rmi.RMIAdaptor
  * @author <a href="mailto:Adrian.Brock@HappeningTimes.com">Adrian Brock</a>
  * @author Scott.Stark@jboss.org
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  *
  * @jmx:mbean name="jboss.jmx:type=adaptor,protocol=INVOKER"
  *            extends="org.jboss.system.ServiceMBean"
@@ -68,8 +75,17 @@ public class InvokerAdaptorService
       }
    }
 
+   /** */
    private Map marshalledInvocationMapping = new HashMap();
-   private Class exportedInterface;
+   /** The supported interface, defaults to RMIAdaptor*/
+   private Class exportedInterface = RMIAdaptor.class;
+   /** The RMIAdaptor.addNotificationListener method */
+   private Method addNotificationListener;
+   /** The RMIAdaptor.removeNotificationListener method */
+   private Method removeNotificationListener;
+   /** A HashSet<RMINotificationListener, NotificationListenerDelegate> for the
+    registered listeners */
+   protected HashMap remoteListeners = new HashMap();
 
    public InvokerAdaptorService()
    {
@@ -104,6 +120,36 @@ public class InvokerAdaptorService
          tmpMap.put(hash, method);
       }
       marshalledInvocationMapping = Collections.unmodifiableMap(tmpMap);
+
+      /* Look for a void addNotificationListener(ObjectName name,
+            RMINotificationListener listener, NotificationFilter filter,
+            Object handback)
+      */
+      try
+      {
+         Class[] sig = {ObjectName.class, RMINotificationListener.class,
+            NotificationFilter.class, Object.class};
+         addNotificationListener = exportedInterface.getMethod(
+            "addNotificationListener", sig);
+      }
+      catch(Exception e)
+      {
+         log.debug("No removeNotificationListener(ObjectName, RMINotificationListener)", e);
+      }
+      /* Look for a void removeNotificationListener(ObjectName,
+         RMINotificationListener)
+      */
+      try
+      {
+         Class[] sig = {ObjectName.class, RMINotificationListener.class};
+         removeNotificationListener = exportedInterface.getMethod(
+            "removeNotificationListener", sig);
+      }
+      catch(Exception e)
+      {
+         log.debug("No removeNotificationListener(ObjectName, RMINotificationListener)", e);
+      }
+
       // Place our ObjectName hash into the Registry so invokers can resolve it
       Registry.bind(new Integer(serviceName.hashCode()), serviceName);
    }
@@ -176,12 +222,33 @@ public class InvokerAdaptorService
          Object value = null;
          SecurityAssociation.setPrincipal(principal);
          SecurityAssociation.setCredential(credential);
+
          try
          {
-            String name = method.getName();
-            Class[] paramTypes = method.getParameterTypes();
-            Method mbeanServerMethod = MBeanServer.class.getMethod(name, paramTypes);
-            value = mbeanServerMethod.invoke(server, args);
+            if( method.equals(addNotificationListener) )
+            {
+               ObjectName name = (ObjectName) args[0];
+               RMINotificationListener listener = (RMINotificationListener)
+                  args[1];
+               NotificationFilter filter = (NotificationFilter) args[2];
+               Object handback = args[3];
+               addNotificationListener(name, listener, filter, handback);
+            }
+            else if( method.equals(removeNotificationListener) )
+            {
+               ObjectName name = (ObjectName) args[0];
+               RMINotificationListener listener = (RMINotificationListener)
+                  args[1];
+               removeNotificationListener(name, listener);            
+            }
+            else
+            {
+               String name = method.getName();
+               Class[] paramTypes = method.getParameterTypes();
+               Method mbeanServerMethod = MBeanServer.class.getMethod(name,
+                  paramTypes);
+               value = mbeanServerMethod.invoke(server, args);
+            }
          }
          catch(InvocationTargetException e)
          {
@@ -196,9 +263,33 @@ public class InvokerAdaptorService
       }
       finally
       {
+         // Don't leak any security context 
          SecurityAssociation.clear();
          if (newCL != null && newCL != oldCL)
             thread.setContextClassLoader(oldCL);
       }
    }
+
+   public void addNotificationListener(ObjectName name,
+      RMINotificationListener listener, NotificationFilter filter,
+      Object handback)
+      throws InstanceNotFoundException, RemoteException
+   {
+      NotificationListenerDelegate delegate = new NotificationListenerDelegate(listener);
+      remoteListeners.put(listener, delegate);
+      getServer().addNotificationListener(name, delegate, filter, handback);
+   }
+
+   public void removeNotificationListener(ObjectName name,
+      RMINotificationListener listener)
+      throws InstanceNotFoundException, ListenerNotFoundException,
+      RemoteException
+   {
+      NotificationListenerDelegate delegate = (NotificationListenerDelegate)
+         remoteListeners.remove(listener);
+      if( delegate == null )
+         throw new ListenerNotFoundException("No listener matches: "+listener);
+      getServer().removeNotificationListener(name, delegate);
+   }
+
 }
