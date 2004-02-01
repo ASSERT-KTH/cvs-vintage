9346bd4d7df98c807525cc174142fa0cc733d5e3
dep4b <dep4b>
2004-02-01 00:19:21 +0000

    Unit test for starting Torque up

---
+++ src/test/org/tigris/scarab/test/StartingTorqueTest.java
@@ -0,0 +1,38 @@
+/*
+ * Created on Feb 1, 2004
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+package org.tigris.scarab.test;
+
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.torque.Torque;
+import org.tigris.scarab.om.ActivityManager;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Eric Pugh
+ *
+ * To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+public class StartingTorqueTest extends TestCase {
+
+	public void testStartingTorque() {
+		try {
+			Configuration config =
+				new PropertiesConfiguration("src/test/TestTurbineResources.properties");
+
+			Torque.init(config);
+
+			System.out.println("Success Initing Torque!");
+			ActivityManager.getInstance(new Long(1));
+		} catch (Exception e) {
+			System.out.println("Can't initialize Torque!!");
+			e.printStackTrace();
+		}
+	}
+}
