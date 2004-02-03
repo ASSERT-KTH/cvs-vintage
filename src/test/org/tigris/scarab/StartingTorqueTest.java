44366b5192b050bb2cd5ceebdbee090e06a6d114
dep4b <dep4b>
2004-02-03 13:15:32 +0000

    only test helper classes should bin in org.tigris.scarab.test, not the acutal tests!

--- src/test/org/tigris/scarab/StartingTorqueTest.java
+++ src/test/org/tigris/scarab/StartingTorqueTest.java
@@ -2,32 +2,32 @@ package org.tigris.scarab;
 
 /* ================================================================
  * Copyright (c) 2000-2002 CollabNet.  All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
  * met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright
  * notice, this list of conditions and the following disclaimer.
- * 
+ *
  * 2. Redistributions in binary form must reproduce the above copyright
  * notice, this list of conditions and the following disclaimer in the
  * documentation and/or other materials provided with the distribution.
- * 
+ *
  * 3. The end-user documentation included with the redistribution, if
  * any, must include the following acknowlegement: "This product includes
  * software developed by Collab.Net <http://www.Collab.Net/>."
  * Alternately, this acknowlegement may appear in the software itself, if
  * and wherever such third-party acknowlegements normally appear.
- * 
+ *
  * 4. The hosted project names must not be used to endorse or promote
  * products derived from this software without prior written
  * permission. For written permission, please contact info@collab.net.
- * 
- * 5. Products derived from this software may not use the "Tigris" or 
- * "Scarab" names nor may "Tigris" or "Scarab" appear in their names without 
+ *
+ * 5. Products derived from this software may not use the "Tigris" or
+ * "Scarab" names nor may "Tigris" or "Scarab" appear in their names without
  * prior written permission of Collab.Net.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
@@ -41,61 +41,42 @@ package org.tigris.scarab;
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  * ====================================================================
- * 
+ *
  * This software consists of voluntary contributions made by many
  * individuals on behalf of Collab.Net.
- */ 
+ */
 
-import java.util.List;
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.torque.Torque;
+import org.tigris.scarab.om.Activity;
+import org.tigris.scarab.om.ActivityManager;
 
-import org.apache.fulcrum.security.TurbineSecurity;
-import org.apache.fulcrum.security.entity.Role;
-import org.apache.fulcrum.security.entity.User;
-import org.apache.fulcrum.security.util.UnknownEntityException;
-import org.tigris.scarab.om.ScarabModule;
-import org.tigris.scarab.om.ScarabUserManager;
-import org.tigris.scarab.test.BaseTestCase;
+import junit.framework.TestCase;
 
 /**
- * Test to understand better how security works..
+ * Test that we can Start torque.  Simple test for debugging database
+ * connection issues
  *
  * @author <a href="mailto:epugh@opensourceconnections.com">Eric Pugh</a>
- * @version $Id: SecurityTest.java,v 1.2 2004/02/03 13:15:32 dep4b Exp $
+ * @version $Id: StartingTorqueTest.java,v 1.1 2004/02/03 13:15:32 dep4b Exp $
  */
-public class SecurityTest extends BaseTestCase
-{
-    public void testCreateAssignDeleteUser() throws Exception
-    {
-        ScarabModule sm = (ScarabModule)getModule();
-        User user = ScarabUserManager.getInstance();//ScarabUserManager.getInstance(sm.getOwnerId());
-        user.setEmail("test@test.com");
-        user.setConfirmed("CONFIRMED");
-        user.setFirstName("test");
-        user.setLastName("test");
-        user.setUserName("test");
-        TurbineSecurity.addUser(user,"test");
-        user = TurbineSecurity.getAuthenticatedUser("test","test");
-        assertNotNull(user);
-        Role role = TurbineSecurity.getRole("Project Owner");
-        
-        
-        TurbineSecurity.grant(user,sm,role);
-        List users =sm.getUsers("test","","","",this.getDefaultIssueType());
-        assertEquals(1,users.size());
-        assertTrue(users.contains(user));
-        TurbineSecurity.revoke(user,sm,role);
-        TurbineSecurity.removeUser(user);
-        try{
-            TurbineSecurity.getAuthenticatedUser("test","test");
-            fail("shouldn't find user!");
-        }
-        catch(UnknownEntityException uee){
-            
-        }
-        
-       
-        
-    }
-   
-    
+public class StartingTorqueTest extends TestCase {
+
+	public void testStartingTorque() {
+		try {
+			Configuration config =
+				new PropertiesConfiguration("src/test/TestTurbineResources.properties");
+
+			Torque.init(config);
+
+			System.out.println("Success Initing Torque!");
+			Activity activity = ActivityManager.getInstance(new Long(1));
+			assertNotNull(activity);
+			assertEquals(1,activity.getActivityId().intValue());
+		} catch (Exception e) {
+			System.out.println("Can't initialize Torque!!");
+			e.printStackTrace();
+		}
+	}
 }
