a5ba0044ffaaddec32da00c5cd59a239c06b6dee
javaprog <javaprog>
2004-02-05 16:52:49 +0000

    [comment] added documentation

--- src/columba/core/org/columba/core/action/AbstractSelectableAction.java
+++ src/columba/core/org/columba/core/action/AbstractSelectableAction.java
@@ -18,17 +18,31 @@ package org.columba.core.action;
 
 import org.columba.core.gui.frame.FrameMediator;
 
+/**
+ * This class subclasses AbstractColumbaAction to encapsulate a selection
+ * state. State changes are propagated to registered PropertyChangeListener
+ * instances.
+ */
 public abstract class AbstractSelectableAction extends AbstractColumbaAction {
     protected boolean state = false;
     
+    /**
+     * Creates a new action instance with a default selection state of false.
+     */
     public AbstractSelectableAction(FrameMediator controller, String name) {
         super(controller, name);
     }
     
+    /**
+     * Returns the action's selection state.
+     */
     public boolean getState() {
         return state;
     }
     
+    /**
+     * Sets the action's selection state and notifies registered listeners.
+     */
     public void setState(boolean state) {
         if (this.state != state) {
             Boolean oldValue = this.state ? Boolean.TRUE : Boolean.FALSE;
