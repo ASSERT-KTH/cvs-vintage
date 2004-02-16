a3abca28802cc4e4d2d444fbe18ef5eaf931950d
javaprog <javaprog>
2004-02-16 16:39:12 +0000

    [comment] added license header

--- src/columba/core/org/columba/core/gui/checkabletree/CheckRenderer.java
+++ src/columba/core/org/columba/core/gui/checkabletree/CheckRenderer.java
@@ -1,9 +1,19 @@
-/*
- * Created on 07.09.2003
- *
- * To change the template for this generated file go to
- * Window - Preferences - Java - Code Generation - Code and Comments
- */
+//The contents of this file are subject to the Mozilla Public License Version 1.1
+//(the "License"); you may not use this file except in compliance with the 
+//License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
+//
+//Software distributed under the License is distributed on an "AS IS" basis,
+//WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License 
+//for the specific language governing rights and
+//limitations under the License.
+//
+//The Original Code is "The Columba Project"
+//
+//The Initial Developers of the Original Code are Frederik Dietz and Timo Stich.
+//Portions created by Frederik Dietz and Timo Stich are Copyright (C) 2003. 
+//
+//All Rights Reserved.
+
 package org.columba.core.gui.checkabletree;
 
 import java.awt.Color;
@@ -20,7 +30,6 @@ import javax.swing.UIManager;
 import javax.swing.plaf.ColorUIResource;
 import javax.swing.tree.TreeCellRenderer;
 
-
 /**
  * @author frd
  *
@@ -31,9 +40,7 @@ public class CheckRenderer extends JPanel implements TreeCellRenderer {
     private JCheckBox check;
     private TreeLabel label;
 
-   
     public CheckRenderer() {
-       
         setLayout(null);
 
         check = new JCheckBox();
@@ -77,7 +84,7 @@ public class CheckRenderer extends JPanel implements TreeCellRenderer {
         Dimension d_label = label.getPreferredSize();
 
         return new Dimension(d_check.width + d_label.width,
-            ((d_check.height < d_label.height) ? d_label.height : d_check.height));
+            Math.max(d_check.height, d_label.height));
     }
 
     public void doLayout() {
