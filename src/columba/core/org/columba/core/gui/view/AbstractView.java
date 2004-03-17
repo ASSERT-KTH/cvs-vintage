108394b36aa8945b899574d1882851a98cc20bcb
diginetdev <diginetdev>
2004-03-17 21:03:46 +0000

    Frame non-specific base interface for views - Initial revision

---
+++ src/columba/core/org/columba/core/gui/view/AbstractView.java
@@ -0,0 +1,60 @@
+//The contents of this file are subject to the Mozilla Public License Version 1.1
+//(the "License"); you may not use this file except in compliance with the 
+//License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
+//
+//Software distributed under the License is distributed on an "AS IS" basis,
+//WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License 
+//for the specific language governing rights and
+//limitations under the License.
+//
+//The Original Code is "The Columba Project"
+//
+//The Initial Developers of the Original Code are Frederik Dietz and Timo Stich.
+//Portions created by Frederik Dietz and Timo Stich are Copyright (C) 2003. 
+//
+//All Rights Reserved.
+
+package org.columba.core.gui.view;
+
+import org.columba.core.gui.frame.FrameMediator;
+
+/**
+ *
+ * This interface provides methods and static data
+ * that are common to all views.
+ *
+ */
+public interface AbstractView {
+    /**
+     * internally used toolbar ID
+     */
+    public static final String MAIN_TOOLBAR = "main";
+
+    /**
+     * Gets the controller of this view
+     *
+     * @return  the ViewController or null if none
+     */
+    FrameMediator getViewController();
+
+    /**
+     * Sets the ViewController for this AbstractView
+     *
+     * @param controller assign a controller that this AbstractView.
+     */
+    void setViewController(FrameMediator controller);
+
+    /**
+     * Gets the model object presented by this view.
+     *
+     * @return the model object of this view.
+     */
+//    Object getModel();
+
+    /**
+     * Sets the model object for this view.
+     *
+     * @param model a model object that this view will present to the user.
+     */
+//    void setModel(Object model);
+}
