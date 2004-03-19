117825f0a6d096474591d52dfebac025617e6672
javaprog <javaprog>
2004-03-19 20:07:29 +0000

    [comment] added license header

--- plugins/org.columba.mail.BounceAction/BounceCommand.java
+++ plugins/org.columba.mail.BounceAction/BounceCommand.java
@@ -1,3 +1,19 @@
+//The contents of this file are subject to the Mozilla Public License Version 1.1
+//(the "License"); you may not use this file except in compliance with the
+//License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
+//
+//Software distributed under the License is distributed on an "AS IS" basis,
+//WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+//for the specific language governing rights and
+//limitations under the License.
+//
+//The Original Code is "The Columba Project"
+//
+//The Initial Developers of the Original Code are Frederik Dietz and Timo Stich.
+//Portions created by Frederik Dietz and Timo Stich are Copyright (C) 2003.
+//
+//All Rights Reserved.
+
 import java.io.InputStream;
 import java.util.StringTokenizer;
 
@@ -23,7 +39,6 @@ import org.columba.ristretto.message.Header;
 import org.columba.ristretto.message.MimeTree;
 import org.columba.ristretto.message.io.ByteBufferSource;
 import org.columba.ristretto.smtp.SMTPException;
-
 
 /**
  * @author frd
