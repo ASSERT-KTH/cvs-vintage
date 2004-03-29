81725de8288d96e60bc96da96ffcc25b6905e4ce
tdiesler <tdiesler>
2004-03-29 12:53:51 +0000

    add the notion of an anonymous run-as principal

--- src/main/org/jboss/security/RunAsIdentity.java
+++ src/main/org/jboss/security/RunAsIdentity.java
@@ -8,43 +8,62 @@
 package org.jboss.security;
 
 import java.security.Principal;
-import java.util.ArrayList;
-import javax.security.auth.Subject;
 
 /**
- * The RunAsIdentity is a principal that associates the run-as principal
+ * The RunAsIdentity is a CallerIdentity that associates the run-as principal
  * with his run-as role.
  *
  * @author Thomas.Diesler@jboss.org
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
-public class RunAsIdentity extends SimplePrincipal
+public class RunAsIdentity extends CallerIdentity
 {
    /** The run-as role */
    private Principal runAsRole;
+   private boolean anonymousPrincipal;
 
    // hash code cache
    private int hashCode;
 
+   // The name of the anonymous run-as principal
+   private static final String ANONYMOUS_PRINCIPAL = "anonymous";
+
    /**
-    * Construct an unmutable instance of a RunAsIdentity
+    * Construct an unmutable instance of an anonymous RunAsIdentity
     */
-   public RunAsIdentity(String runAsRole, String runAsPrincipal)
+   public RunAsIdentity(String runAsRole)
    {
-      super(runAsPrincipal != null ? runAsPrincipal : "nobody");
+      super(ANONYMOUS_PRINCIPAL, null);
 
       if (runAsRole == null)
          throw new IllegalArgumentException("runAsRole cannot be null");
 
       this.runAsRole = new SimplePrincipal(runAsRole);
+      this.anonymousPrincipal = true;
    }
 
    /**
-    * Get the run-as role.
+    * Construct an unmutable instance of a RunAsIdentity
     */
+   public RunAsIdentity(String runAsRole, String runAsPrincipal, Object runAsCredential)
+   {
+      super(runAsPrincipal != null ? runAsPrincipal : ANONYMOUS_PRINCIPAL, runAsCredential);
+      
+      if (runAsRole == null)
+         throw new IllegalArgumentException("runAsRole cannot be null");
+
+      this.runAsRole = new SimplePrincipal(runAsRole);
+      this.anonymousPrincipal = ANONYMOUS_PRINCIPAL.equals(runAsPrincipal);
+   }
+
    public Principal getRunAsRole()
    {
       return runAsRole;
+   }
+
+   public boolean isAnonymousPrincipal()
+   {
+      return anonymousPrincipal;
    }
 
    /**
@@ -65,7 +84,7 @@ public class RunAsIdentity extends SimplePrincipal
       if (obj instanceof RunAsIdentity)
       {
          RunAsIdentity other = (RunAsIdentity)obj;
-         return getName().equals(other.getName()) && runAsRole.equals(other.runAsRole);
+         return super.equals(obj) && runAsRole.equals(other.runAsRole);
       }
       return false;
    }
