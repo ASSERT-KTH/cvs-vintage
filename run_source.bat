efadfb3c83da0eae461814f72fda4d429bc0ec66
fdietz <fdietz>
2004-04-02 12:27:11 +0000

    [intern]removed complex run_source scripts, use ant "run" task instead

--- run_source.bat
+++ run_source.bat
@@ -1,2 +1,1 @@
-set CLASSPATH=.;.\classes;.\lib\junit.jar;.\lib\lucene-1.3-final.jar;.\lib\jargs.jar;.\lib\jwizz-0.1.1.jar;.\lib\plastic-1.2.0.jar;.\lib\jhall.jar;.\lib\usermanual.jar;.\lib\forms-1.0.3.jar;.\lib\ristretto-1.0pre1.jar
-java org.columba.core.main.Main
+ant run
--- run_source.sh
+++ run_source.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-
-COLUMBA_HOME=`dirname $0`
-java -cp "$COLUMBA_HOME":"$COLUMBA_HOME"/classes:"$COLUMBA_HOME"/lib/jargs.jar:"$COLUMBA_HOME"/lib/lucene-1.3-final.jar:"$COLUMBA_HOME"/lib/jwizz-0.1.1.jar:"$COLUMBA_HOME"/lib/plastic-1.2.0.jar:"$COLUMBA_HOME"/lib/jhall.jar:"$COLUMBA_HOME"/lib/usermanual.jar:"$COLUMBA_HOME"/lib/forms-1.0.3.jar:"$COLUMBA_HOME"/lib/ristretto-1.0pre1.jar org.columba.core.main.Main $@
+ant run
