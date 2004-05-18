3448d1c6b9beb52fc5c714e662ebf8fbb23d5f4c
fdietz <fdietz>
2004-05-18 14:44:38 +0000

    [intern]changed version to RC1-test2

--- src/columba/core/org/columba/core/main/MainInterface.java
+++ src/columba/core/org/columba/core/main/MainInterface.java
@@ -32,7 +32,7 @@ import org.columba.core.plugin.PluginManager;
  */
 public class MainInterface {
     /** Current version */
-    public static final String version = "1.0 Milestone M2";
+    public static final String version = "1.0 RC1-test2";
 
     /** If true, enables debugging output from org.columba.core.logging */
     public static boolean DEBUG = false;
