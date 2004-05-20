4b0de795db46e8e656d77e0e994176dfea450054
javaprog <javaprog>
2004-05-20 14:19:18 +0000

    [bug] now respects manual line breaks, such as newline and carriage return

--- src/columba/core/org/columba/core/gui/util/MultiLineLabel.java
+++ src/columba/core/org/columba/core/gui/util/MultiLineLabel.java
@@ -1,3 +1,19 @@
+//The contents of this file are subject to the Mozilla Public License Version 1.1
+//(the "License"); you may not use this file except in compliance with the 
+//License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
+//
+//Software distributed under the License is distributed on an "AS IS" basis,
+//WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License 
+//for the specific language governing rights and
+//limitations under the License.
+//
+//The Original Code is "The Columba Project"
+//
+//The Initial Developers of the Original Code are Frederik Dietz and Timo Stich.
+//Portions created by Frederik Dietz and Timo Stich are Copyright (C) 2003. 
+//
+//All Rights Reserved.
+
 package org.columba.core.gui.util;
 
 import java.awt.*;
@@ -6,7 +22,10 @@ import java.awt.font.TextAttribute;
 import java.awt.font.TextLayout;
 
 import java.text.AttributedString;
-import java.text.BreakIterator;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.swing.JComponent;
 import javax.swing.UIManager;
@@ -15,8 +34,8 @@ import javax.swing.UIManager;
  * A Swing component capable of displaying text in multiple lines.
  */
 public class MultiLineLabel extends JComponent {
-    private String text = "";
-    protected BreakIterator breakIterator;
+    private String text;
+    private int[] lineBreaks;
     protected LineBreakMeasurer measurer;
     protected int lineSpacing = 4;
     
@@ -24,7 +43,6 @@ public class MultiLineLabel extends JComponent {
      * Creates a new label with the given text.
      */
     public MultiLineLabel(String text) {
-        breakIterator = BreakIterator.getLineInstance();
         setForeground(UIManager.getColor("Label.foreground"));
         setFont(UIManager.getFont("Label.font"));
         setAlignmentX(LEFT_ALIGNMENT);
@@ -46,7 +64,6 @@ public class MultiLineLabel extends JComponent {
     public void setText(String text) {
         String oldValue = this.text;
         this.text = text;
-        breakIterator.setText(text);
         measurer = null;
         firePropertyChange("text", oldValue, text);
         revalidate();
@@ -67,6 +84,8 @@ public class MultiLineLabel extends JComponent {
         Integer oldValue = new Integer(this.lineSpacing);
         this.lineSpacing = lineSpacing;
         firePropertyChange("lineSpacing", oldValue, new Integer(lineSpacing));
+        revalidate();
+        repaint();
     }
     
     /**
@@ -85,11 +104,21 @@ public class MultiLineLabel extends JComponent {
         }
         LineBreakMeasurer measurer = getLineBreakMeasurer();
         TextLayout layout;
+        int i = 0;
         while (measurer != null && measurer.getPosition() < text.length()) {
-            layout = measurer.nextLayout(width - 20);
+            layout = measurer.nextLayout(width - 20, lineBreaks[i], false);
+            
+            //if we stopped at line break, increase array index pointer
+            if (measurer.getPosition() == lineBreaks[i]) {
+                i++;
+            }
+            
+            //increase minimum height by line height and line spacing
             height += layout.getAscent() + layout.getDescent() + 
                         layout.getLeading() + lineSpacing;
         }
+        
+        //add the component's border insets to our minimum dimension
         Insets insets = getInsets();
         return new Dimension(width + insets.left + insets.right,
                 height + insets.top + insets.bottom);
@@ -101,8 +130,26 @@ public class MultiLineLabel extends JComponent {
                 AttributedString string = new AttributedString(text);
                 string.addAttribute(TextAttribute.FONT, getFont());
                 measurer = new LineBreakMeasurer(string.getIterator(),
-                        breakIterator, ((Graphics2D)getGraphics())
-                                .getFontRenderContext());
+                        ((Graphics2D)getGraphics()).getFontRenderContext());
+                
+                //check for line breaks
+                List temp = new LinkedList();
+                int i;
+                char c;
+                for (i = 0; i < text.length(); i++) {
+                    c = text.charAt(i);
+                    if (c == '\r' || c == '\n') {
+                        temp.add(new Integer(i + 1));
+                    }
+                }
+                //put them into the array
+                i = 0;
+                lineBreaks = new int[temp.size() + 1];
+                Iterator iterator = temp.iterator();
+                while (iterator.hasNext()) {
+                    lineBreaks[i++] = ((Integer)iterator.next()).intValue();
+                }
+                lineBreaks[i] = text.length();
             }
         } else {
             measurer.setPosition(0);
@@ -122,8 +169,16 @@ public class MultiLineLabel extends JComponent {
         Insets insets = getInsets();
         Point pen = new Point(5 + insets.left, 5 + insets.top);
         TextLayout layout;
+        int i = 0;
         while (measurer.getPosition() < text.length()) {
-            layout = measurer.nextLayout(wrappingWidth);
+            layout = measurer.nextLayout(wrappingWidth, lineBreaks[i], false);
+            
+            //if we stopped at line break, increase array index pointer
+            if (measurer.getPosition() == lineBreaks[i]) {
+                i++;
+            }
+            
+            //draw line
             pen.y += layout.getAscent();
             float dx = layout.isLeftToRight() ?
                     0 : (wrappingWidth - layout.getAdvance());
