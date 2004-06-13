d575e5a011f1f274486697b1f4af616882255a5e
fdietz <fdietz>
2004-06-13 12:04:23 +0000

    [intern]added list of used third-party libraries to about-dialog, using texfiles for license and contributors listin res/ directory, this way it also works using webstart, etc.

--- src/columba/core/org/columba/core/gui/util/AboutDialog.java
+++ src/columba/core/org/columba/core/gui/util/AboutDialog.java
@@ -198,6 +198,7 @@ public class AboutDialog extends JDialog implements ActionListener {
 				KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
 				JComponent.WHEN_IN_FOCUSED_WINDOW);
 		pack();
+		setSize(new Dimension(450,350));
 		setLocationRelativeTo(null);
 	}
 
