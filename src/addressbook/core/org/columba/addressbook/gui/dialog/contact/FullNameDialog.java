237276d589294d3786b37a074e038f4eee6f546d
javaprog <javaprog>
2004-07-07 12:02:30 +0000

    [intern] improved i18n support, adapted to GUI design guidelines by making use of OK-Cancel pattern

--- src/addressbook/core/org/columba/addressbook/gui/dialog/contact/FullNameDialog.java
+++ src/addressbook/core/org/columba/addressbook/gui/dialog/contact/FullNameDialog.java
@@ -15,6 +15,7 @@
 //Portions created by Frederik Dietz and Timo Stich are Copyright (C) 2003.
 //
 //All Rights Reserved.
+
 package org.columba.addressbook.gui.dialog.contact;
 
 import java.awt.BorderLayout;
@@ -23,190 +24,175 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
-import javax.swing.JComponent;
-import javax.swing.JDialog;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.KeyStroke;
-import javax.swing.SwingConstants;
+import javax.swing.*;
 
 import net.javaprog.ui.wizard.plaf.basic.SingleSideEtchedBorder;
 
 import org.columba.addressbook.model.Contact;
 import org.columba.addressbook.model.VCARD;
 import org.columba.addressbook.util.AddressbookResourceLoader;
+import org.columba.core.gui.util.ButtonWithMnemonic;
 import org.columba.core.gui.util.DefaultFormBuilder;
+import org.columba.core.gui.util.LabelWithMnemonic;
 
 import com.jgoodies.forms.layout.FormLayout;
 
+/**
+ * A dialog for editing a contact's full name.
+ */
 public class FullNameDialog extends JDialog implements ActionListener {
-	private JLabel titleLabel;
-	private JTextField titleTextField;
-	private JLabel fornameLabel;
-	private JTextField fornameTextField;
-	private JLabel middlenameLabel;
-	private JTextField middlenameTextField;
-	private JLabel lastnameLabel;
-	private JTextField lastnameTextField;
-	private JLabel suffixLabel;
-	private JTextField suffixTextField;
-	private JButton okButton;
-	private JButton changeButton;
-	private IdentityPanel identityPanel;
-
-	private Contact contact;
-
-	public FullNameDialog(JDialog frame, IdentityPanel identityPanel,
-			Contact contact) {
-		super(frame, true);
-
-		this.identityPanel = identityPanel;
-		this.contact = contact;
-
-		initComponents();
-
-		layoutComponents();
-
-		pack();
-		setLocationRelativeTo(null);
-	}
-
-	public void updateComponents( boolean b) {
-		if (b) {
-			titleTextField.setText(contact.get(VCARD.N, VCARD.N_PREFIX)); //$NON-NLS-1$ //$NON-NLS-2$
-			lastnameTextField.setText(contact.get(VCARD.N, VCARD.N_FAMILY)); //$NON-NLS-1$ //$NON-NLS-2$
-			fornameTextField.setText(contact.get(VCARD.N, VCARD.N_GIVEN)); //$NON-NLS-1$ //$NON-NLS-2$
-			middlenameTextField.setText(contact.get(VCARD.N, VCARD.N_MIDDLE)); //$NON-NLS-1$ //$NON-NLS-2$
-			suffixTextField.setText(contact.get(VCARD.N, VCARD.N_SUFFIX)); //$NON-NLS-1$ //$NON-NLS-2$
-		} else {
-			contact.set(VCARD.N, VCARD.N_PREFIX, titleTextField.getText()); //$NON-NLS-1$ //$NON-NLS-2$
-			contact.set(VCARD.N, VCARD.N_FAMILY, lastnameTextField.getText()); //$NON-NLS-1$ //$NON-NLS-2$
-			contact.set(VCARD.N, VCARD.N_GIVEN, fornameTextField.getText()); //$NON-NLS-1$ //$NON-NLS-2$
-			contact.set(VCARD.N, VCARD.N_MIDDLE, middlenameTextField.getText()); //$NON-NLS-1$ //$NON-NLS-2$
-			contact.set(VCARD.N, VCARD.N_SUFFIX, suffixTextField.getText()); //$NON-NLS-1$ //$NON-NLS-2$
-		}
-	}
-
-	protected void layoutComponents() {
-		JPanel mainPanel = new JPanel();
-		mainPanel.setBorder(BorderFactory.createEmptyBorder(12, 12, 12, 12));
-
-		FormLayout layout = new FormLayout("right:default, 3dlu, default:grow",
-				"");
-
-		DefaultFormBuilder b = new DefaultFormBuilder(mainPanel, layout);
-		b.setRowGroupingEnabled(true);
-
-		getContentPane().setLayout(new BorderLayout());
-		getContentPane().add(mainPanel, BorderLayout.CENTER);
-
-		b.append(titleLabel);
-		b.append(titleTextField);
-
-		b.append(fornameLabel);
-		b.append(fornameTextField);
-
-		b.append(middlenameLabel);
-		b.append(middlenameTextField);
-
-		b.append(lastnameLabel);
-		b.append(lastnameTextField);
-
-		b.append(suffixLabel);
-		b.append(suffixTextField);
-
-		JPanel bottomPanel = new JPanel();
-		bottomPanel.setLayout(new BorderLayout());
-		bottomPanel.setBorder(new SingleSideEtchedBorder(SwingConstants.TOP));
-
-		JPanel buttonPanel = new JPanel();
-		buttonPanel.setLayout(new GridLayout(0, 2, 10, 0));
-		buttonPanel.setBorder(BorderFactory.createEmptyBorder(12, 12, 12, 12));
-		bottomPanel.add(buttonPanel, BorderLayout.EAST);
-
-		changeButton = new JButton(AddressbookResourceLoader.getString(
-				"dialog", "contact", "change_formatted_name")); //$NON-NLS-1$
-		changeButton.setActionCommand("CHANGE"); //$NON-NLS-1$
-		changeButton.addActionListener(this);
-		buttonPanel.add(changeButton);
-		okButton = new JButton("Close"); //$NON-NLS-1$
-		okButton.setActionCommand("OK"); //$NON-NLS-1$
-		okButton.addActionListener(this);
-		buttonPanel.add(okButton);
-
-		getRootPane().registerKeyboardAction(this, "OK", //$NON-NLS-1$
-				KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
-				JComponent.WHEN_IN_FOCUSED_WINDOW);
-
-		getRootPane().setDefaultButton(okButton);
-
-		getContentPane().add(bottomPanel, BorderLayout.SOUTH);
-	}
-
-	protected void initComponents() {
-
-		titleLabel = new JLabel(AddressbookResourceLoader.getString("dialog",
-				"contact", "title")); //$NON-NLS-1$
-		titleTextField = new JTextField(20);
-
-		fornameLabel = new JLabel(AddressbookResourceLoader.getString(
-				"dialog", "contact", "first_name")); //$NON-NLS-1$
-		fornameTextField = new JTextField(20);
-
-		middlenameLabel = new JLabel(AddressbookResourceLoader.getString(
-				"dialog", "contact", "middle_name")); //$NON-NLS-1$
-		middlenameTextField = new JTextField(20);
-
-		lastnameLabel = new JLabel(AddressbookResourceLoader.getString(
-				"dialog", "contact", "last_name")); //$NON-NLS-1$
-		lastnameTextField = new JTextField(20);
-
-		suffixLabel = new JLabel(AddressbookResourceLoader.getString("dialog",
-				"contact", "suffix")); //$NON-NLS-1$
-		suffixTextField = new JTextField(20);
-
-	}
-
-	public void actionPerformed(ActionEvent event) {
-		String action = event.getActionCommand();
-
-		if (action.equals("OK")) { //$NON-NLS-1$
-			setVisible(false);
-
-			if (identityPanel.fnIsEmpty() == true) {
-				setFormattedName();
-			}
-		} else if (action.equals("CHANGE")) { //$NON-NLS-1$
-			setFormattedName();
-		}
-	}
-
-	protected void setFormattedName() {
-		StringBuffer buf = new StringBuffer();
-
-		if (titleTextField.getText().length() > 0) {
-			buf.append(titleTextField.getText() + " "); //$NON-NLS-1$
-		}
-
-		if (fornameTextField.getText().length() > 0) {
-			buf.append(fornameTextField.getText() + " "); //$NON-NLS-1$
-		}
-
-		if (middlenameTextField.getText().length() > 0) {
-			buf.append(middlenameTextField.getText() + " "); //$NON-NLS-1$
-		}
-
-		if (lastnameTextField.getText().length() > 0) {
-			buf.append(lastnameTextField.getText() + " "); //$NON-NLS-1$
-		}
-
-		if (suffixTextField.getText().length() > 0) {
-			buf.append(suffixTextField.getText() + " "); //$NON-NLS-1$
-		}
-
-		identityPanel.setFn(buf.toString());
-	}
+    private JLabel titleLabel;
+    private JTextField titleTextField;
+    private JLabel fornameLabel;
+    private JTextField fornameTextField;
+    private JLabel middlenameLabel;
+    private JTextField middlenameTextField;
+    private JLabel lastnameLabel;
+    private JTextField lastnameTextField;
+    private JLabel suffixLabel;
+    private JTextField suffixTextField;
+    private JButton okButton;
+    private JButton cancelButton;
+    private IdentityPanel identityPanel;
+    
+    private Contact contact;
+    
+    public FullNameDialog(JDialog frame, IdentityPanel identityPanel, Contact contact) {
+        super(frame, AddressbookResourceLoader.getString("dialog", "contact",
+            "edit_fullname"), true);
+        
+        this.identityPanel = identityPanel;
+        this.contact = contact;
+        
+        initComponents();
+        layoutComponents();
+        
+        pack();
+        setLocationRelativeTo(null);
+    }
+    
+    public void updateComponents(boolean b) {
+        if (b) {
+            titleTextField.setText(contact.get(VCARD.N, VCARD.N_PREFIX));
+            lastnameTextField.setText(contact.get(VCARD.N, VCARD.N_FAMILY));
+            fornameTextField.setText(contact.get(VCARD.N, VCARD.N_GIVEN));
+            middlenameTextField.setText(contact.get(VCARD.N, VCARD.N_MIDDLE));
+            suffixTextField.setText(contact.get(VCARD.N, VCARD.N_SUFFIX));
+        } else {
+            contact.set(VCARD.N, VCARD.N_PREFIX, titleTextField.getText());
+            contact.set(VCARD.N, VCARD.N_FAMILY, lastnameTextField.getText());
+            contact.set(VCARD.N, VCARD.N_GIVEN, fornameTextField.getText());
+            contact.set(VCARD.N, VCARD.N_MIDDLE, middlenameTextField.getText());
+            contact.set(VCARD.N, VCARD.N_SUFFIX, suffixTextField.getText());
+        }
+    }
+    
+    protected void layoutComponents() {
+        JPanel mainPanel = new JPanel();
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(12, 12, 12, 12));
+        
+        FormLayout layout = new FormLayout("right:default, 3dlu, default:grow",
+        "");
+        
+        DefaultFormBuilder b = new DefaultFormBuilder(mainPanel, layout);
+        b.setRowGroupingEnabled(true);
+        
+        getContentPane().setLayout(new BorderLayout());
+        getContentPane().add(mainPanel, BorderLayout.CENTER);
+        
+        b.append(titleLabel);
+        b.append(titleTextField);
+        
+        b.append(fornameLabel);
+        b.append(fornameTextField);
+        
+        b.append(middlenameLabel);
+        b.append(middlenameTextField);
+        
+        b.append(lastnameLabel);
+        b.append(lastnameTextField);
+        
+        b.append(suffixLabel);
+        b.append(suffixTextField);
+        
+        JPanel bottomPanel = new JPanel(new BorderLayout());
+        bottomPanel.setBorder(new SingleSideEtchedBorder(SwingConstants.TOP));
+        
+        JPanel buttonPanel = new JPanel(new GridLayout(1, 2, 10, 0));
+        buttonPanel.setBorder(BorderFactory.createEmptyBorder(12, 12, 12, 12));
+        bottomPanel.add(buttonPanel, BorderLayout.EAST);
+        
+        cancelButton = new ButtonWithMnemonic(AddressbookResourceLoader.getString(
+            null, "cancel"));
+        cancelButton.addActionListener(this);
+        buttonPanel.add(cancelButton);
+        okButton = new ButtonWithMnemonic(AddressbookResourceLoader.getString(
+            null, "ok"));
+        okButton.addActionListener(this);
+        buttonPanel.add(okButton);
+        
+        getRootPane().registerKeyboardAction(this, "CANCEL",
+            KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
+            JComponent.WHEN_IN_FOCUSED_WINDOW);
+        
+        getRootPane().setDefaultButton(okButton);
+        getContentPane().add(bottomPanel, BorderLayout.SOUTH);
+    }
+    
+    protected void initComponents() {
+        titleLabel = new LabelWithMnemonic(AddressbookResourceLoader.getString(
+            "dialog", "contact", "title"));
+        titleTextField = new JTextField(20);
+        titleLabel.setLabelFor(titleTextField);
+        fornameLabel = new LabelWithMnemonic(AddressbookResourceLoader.getString(
+            "dialog", "contact", "first_name"));
+        fornameTextField = new JTextField(20);
+        fornameLabel.setLabelFor(fornameTextField);
+        middlenameLabel = new LabelWithMnemonic(AddressbookResourceLoader.getString(
+            "dialog", "contact", "middle_name"));
+        middlenameTextField = new JTextField(20);
+        middlenameLabel.setLabelFor(middlenameTextField);
+        lastnameLabel = new LabelWithMnemonic(AddressbookResourceLoader.getString(
+            "dialog", "contact", "last_name"));
+        lastnameTextField = new JTextField(20);
+        lastnameLabel.setLabelFor(lastnameTextField);
+        suffixLabel = new LabelWithMnemonic(AddressbookResourceLoader.getString(
+            "dialog", "contact", "suffix"));
+        suffixTextField = new JTextField(20);
+        suffixLabel.setLabelFor(suffixTextField);
+    }
+    
+    public void actionPerformed(ActionEvent event) {
+        Object source = event.getSource();
+        if (source == okButton) {
+            setFormattedName();
+        }
+        setVisible(false);
+    }
+    
+    protected void setFormattedName() {
+        StringBuffer buf = new StringBuffer();
+        
+        if (titleTextField.getText().length() > 0) {
+            buf.append(titleTextField.getText() + " ");
+        }
+        
+        if (fornameTextField.getText().length() > 0) {
+            buf.append(fornameTextField.getText() + " ");
+        }
+        
+        if (middlenameTextField.getText().length() > 0) {
+            buf.append(middlenameTextField.getText() + " ");
+        }
+        
+        if (lastnameTextField.getText().length() > 0) {
+            buf.append(lastnameTextField.getText() + " ");
+        }
+        
+        if (suffixTextField.getText().length() > 0) {
+            buf.append(suffixTextField.getText() + " ");
+        }
+        
+        identityPanel.setFn(buf.toString());
+    }
 }
