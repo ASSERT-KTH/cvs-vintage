6b13b664d9122d1d358c810e84eff830e67524ad
starksm <starksm>
2004-07-08 02:53:32 +0000

    Restore the SecurityInterceptor to the previous authentication and authorization roles. If you want to seperate authentication and authorization replace the SecurityInteceptor with the JaasAuthenticationInterceptor and SecurityRolesInterceptor.

--- src/main/org/jboss/ejb/plugins/SecurityInterceptor.java
+++ src/main/org/jboss/ejb/plugins/SecurityInterceptor.java
@@ -6,40 +6,55 @@
  */
 package org.jboss.ejb.plugins;
 
-import java.security.Principal;
-import java.util.Map;
-import java.util.Set;
-import javax.ejb.EJBException;
-
 import org.jboss.ejb.Container;
 import org.jboss.invocation.Invocation;
+import org.jboss.invocation.InvocationType;
 import org.jboss.metadata.ApplicationMetaData;
 import org.jboss.metadata.AssemblyDescriptorMetaData;
 import org.jboss.metadata.BeanMetaData;
 import org.jboss.metadata.SecurityIdentityMetaData;
+import org.jboss.security.AnybodyPrincipal;
 import org.jboss.security.AuthenticationManager;
+import org.jboss.security.RealmMapping;
 import org.jboss.security.RunAsIdentity;
 import org.jboss.security.SecurityAssociation;
 import org.jboss.security.SecurityRolesAssociation;
 
-/** The SecurityInterceptor is where the EJB 2.1 authentication is performed
- * along with the run-as identity establishment. 
+import javax.ejb.EJBException;
+import java.security.Principal;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * The SecurityInterceptor is where the EJB 2.0 declarative security model
+ * is enforced. This is where the caller identity propagation is controlled as well.
  *
  * @author <a href="on@ibis.odessa.ua">Oleg Nitz</a>
  * @author <a href="mailto:Scott.Stark@jboss.org">Scott Stark</a>.
  * @author <a href="mailto:Thomas.Diesler@jboss.org">Thomas Diesler</a>.
- * @version $Revision: 1.44 $
+ * @version $Revision: 1.45 $
  */
 public class SecurityInterceptor extends AbstractInterceptor
 {
-   /** The security domain authentication service
+   /**
+    * @supplierCardinality 0..1
+    * @supplierQualifier authentication
+    * @clientCardinality 1..*
     */
    protected AuthenticationManager securityManager;
-   
-   /** A static map of SecurityRolesMetaData from jboss.xml */
-   protected Map securityRoles;
-   /** The run-as identity for the ejb from ejb-jar.xml */
+
+   /**
+    * @supplierCardinality 0..1
+    * @clientCardinality 1..*
+    * @supplierQualifier identity mapping
+    */
+   protected RealmMapping realmMapping;
+
+   // The bean uses this run-as identity to call out
    protected RunAsIdentity runAsIdentity;
+
+   // A map of SecurityRolesMetaData from jboss.xml
+   protected Map securityRoles;
 
    /** Called by the super class to set the container to which this interceptor
     belongs. We obtain the security manager and runAs identity to use here.
@@ -52,6 +67,7 @@ public class SecurityInterceptor extends AbstractInterceptor
          BeanMetaData beanMetaData = container.getBeanMetaData();
          ApplicationMetaData applicationMetaData = beanMetaData.getApplicationMetaData();
          AssemblyDescriptorMetaData assemblyDescriptor = applicationMetaData.getAssemblyDescriptor();
+         securityRoles = assemblyDescriptor.getSecurityRoles();
 
          SecurityIdentityMetaData secMetaData = beanMetaData.getSecurityIdentityMetaData();
          if (secMetaData != null && secMetaData.getUseCallerIdentity() == false)
@@ -65,6 +81,7 @@ public class SecurityInterceptor extends AbstractInterceptor
          }
 
          securityManager = container.getSecurityManager();
+         realmMapping = container.getRealmMapping();
       }
    }
 
@@ -130,8 +147,10 @@ public class SecurityInterceptor extends AbstractInterceptor
       }
    }
 
-   /** Authenticate the caller using the principal and credentials in the
-    * Invocation
+   /** The EJB 2.0 declarative security algorithm:
+    1. Authenticate the caller using the principal and credentials in the MethodInfocation
+    2. Validate access to the method by checking the principal's roles against
+    those required to access the method.
     */
    private void checkSecurityAssociation(Invocation mi)
       throws Exception
@@ -149,14 +168,17 @@ public class SecurityInterceptor extends AbstractInterceptor
          return;
       }
 
-      // Authenticate the caller based on the method invocation credentials
+      if (realmMapping == null)
+      {
+         throw new EJBException("checkSecurityAssociation",
+            new SecurityException("Role mapping manager has not been set"));
+      }
+
+      // authenticate the current principal
       RunAsIdentity callerRunAsIdentity = SecurityAssociation.peekRunAsIdentity();
       if (callerRunAsIdentity == null)
       {
-         /* This call associates the statically defined roles with the
-         SecurityRolesAssociation thread local for use by 3.2 style of
-         login modules which combined authentication and authorization.
-         */
+         // Check the security info from the method invocation
          SecurityRolesAssociation.setSecurityRoles(securityRoles);
          if (securityManager.isValid(principal, credential) == false)
          {
@@ -172,6 +194,61 @@ public class SecurityInterceptor extends AbstractInterceptor
             if (trace)
             {
                log.trace("Authenticated  principal=" + principal);
+            }
+         }
+      }
+
+      // Get the method permissions
+      InvocationType iface = mi.getType();
+      Set methodRoles = container.getMethodPermissions(mi.getMethod(), iface);
+      if (methodRoles == null)
+      {
+         String method = mi.getMethod().getName();
+         String msg = "No method permissions assigned to method=" + method
+            + ", interface=" + iface;
+         log.error(msg);
+         SecurityException e = new SecurityException(msg);
+         throw new EJBException("checkSecurityAssociation", e);
+      }
+      else if (trace)
+      {
+         log.trace("method=" + mi.getMethod() + ", interface=" + iface
+            + ", requiredRoles=" + methodRoles);
+      }
+
+      // Check if the caller is allowed to access the method
+      if (methodRoles.contains(AnybodyPrincipal.ANYBODY_PRINCIPAL) == false)
+      {
+         // The caller is using a the caller identity
+         if (callerRunAsIdentity == null)
+         {
+            // Now actually check if the current caller has one of the required method roles
+            if (realmMapping.doesUserHaveRole(principal, methodRoles) == false)
+            {
+               Set userRoles = realmMapping.getUserRoles(principal);
+               String method = mi.getMethod().getName();
+               String msg = "Insufficient method permissions, principal=" + principal
+                  + ", method=" + method + ", interface=" + iface
+                  + ", requiredRoles=" + methodRoles + ", principalRoles=" + userRoles;
+               log.error(msg);
+               SecurityException e = new SecurityException(msg);
+               throw new EJBException("checkSecurityAssociation", e);
+            }
+         }
+
+         // The caller is using a run-as identity
+         else
+         {
+            // Check that the run-as role is in the set of method roles
+            if (callerRunAsIdentity.doesUserHaveRole(methodRoles) == false)
+            {
+               String method = mi.getMethod().getName();
+               String msg = "Insufficient method permissions, runAsPrincipal=" + callerRunAsIdentity.getName()
+                  + ", method=" + method + ", interface=" + iface
+                  + ", requiredRoles=" + methodRoles + ", runAsRoles=" + callerRunAsIdentity.getRunAsRoles();
+               log.error(msg);
+               SecurityException e = new SecurityException(msg);
+               throw new EJBException("checkSecurityAssociation", e);
             }
          }
       }
