2590bf4955c66a99775459dee16d5dce79d0a454
starksm <starksm>
2004-07-09 00:44:07 +0000

    Add a toInterfaceString() which returns the method-intf value for the type: "Remote", "Local", "Home", "LocalHome", "ServiceEndpoint"

--- src/main/org/jboss/invocation/InvocationType.java
+++ src/main/org/jboss/invocation/InvocationType.java
@@ -13,12 +13,16 @@ import java.io.ObjectStreamException;
  *
  * @author Scott.Stark@jboss.org
  * @author Christoph.Jung@infor.de
- * @version $Revision: 1.5 $
+ * @version $Revision: 1.6 $
  */
 public final class InvocationType implements Serializable
 {
    /** Serial Version Identifier. @since 1.2 */
    private static final long serialVersionUID = 6460196085190851775L;
+   /** The method-intf names for the InvocationType enums */
+   private static final String[] INTERFACE_NAMES = { "Remote",
+      "Local", "Home", "LocalHome", "ServiceEndpoint"
+   };
 
    /** The max ordinal value in use for the InvocationType enums. When you add a
     * new key enum value you must assign it an ordinal value of the current
@@ -28,7 +32,6 @@ public final class InvocationType implements Serializable
 
    /** The array of InvocationKey indexed by ordinal value of the key */
    private static final InvocationType[] values = new InvocationType[MAX_TYPE_ID+1];
-
    public static final InvocationType REMOTE =
          new InvocationType("REMOTE", 0);
    public static final InvocationType LOCAL =
@@ -55,6 +58,14 @@ public final class InvocationType implements Serializable
    public String toString()
    {
       return name;
+   }
+   /** Get the method-intf name for the type
+    * 
+    * @return one of: "Remote", "Local", "Home", "LocalHome", "ServiceEndpoint"
+    */ 
+   public String toInterfaceString()
+   {
+      return INTERFACE_NAMES[ordinal];
    }
 
    Object readResolve() throws ObjectStreamException
