5cac7b7c1b3450f1deb5296719e7b210c587bf68
mkl <mkl>
2004-07-18 14:59:41 +0000

    Check for partition is not required here. We have a new ActivtiyDiagramRenderer Issue number: Obtained from: Submitted by: Reviewed by:

--- src_new/org/argouml/uml/diagram/state/ui/StateDiagramRenderer.java
+++ src_new/org/argouml/uml/diagram/state/ui/StateDiagramRenderer.java
@@ -1,4 +1,4 @@
-// $Id: StateDiagramRenderer.java,v 1.19 2004/07/17 16:43:03 mvw Exp $
+// $Id: StateDiagramRenderer.java,v 1.20 2004/07/18 14:59:41 mkl Exp $
 // Copyright (c) 1996-99 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
@@ -32,7 +32,6 @@ import org.apache.log4j.Logger;
 
 import org.argouml.model.ModelFacade;
 import org.argouml.uml.diagram.activity.ui.FigActionState;
-// import org.argouml.uml.diagram.activity.ui.FigSwimlane;
 
 import org.tigris.gef.base.Layer;
 import org.tigris.gef.graph.GraphEdgeRenderer;
@@ -91,11 +90,6 @@ public class StateDiagramRenderer
         else if (org.argouml.model.ModelFacade.isAState(node)) {
             return new FigSimpleState(gm, node);
         }
-        /*
-        else if (ModelFacade.isAPartition(node)) {
-            return new FigSwimlane(gm, node);
-        }
-        */
         else if (org.argouml.model.ModelFacade.isAPseudostate(node)) {
             Object pState = node;
             Object kind = ModelFacade.getKind(pState);
