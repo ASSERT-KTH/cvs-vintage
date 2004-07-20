a7b5a6e6bfd04305130439fb55fbcd0b129a1fcc
d00mst <d00mst>
2004-07-20 17:34:52 +0000

    Submitted by: Sérgio Lopes <sergio.lopes@dei.uminho.pt> Removed an extra return before actually setting the stereotype. Added a todo about a discrepance between implementation and specification.

--- src_new/org/argouml/model/ModelFacade.java
+++ src_new/org/argouml/model/ModelFacade.java
@@ -1,4 +1,4 @@
-// $Id: ModelFacade.java,v 1.195 2004/07/19 23:30:20 d00mst Exp $
+// $Id: ModelFacade.java,v 1.196 2004/07/20 17:34:52 d00mst Exp $
 // Copyright (c) 2003-2004 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
@@ -6340,6 +6340,16 @@ public class ModelFacade {
      * copies a stereotype that is not a part of the current model to
      * the current model.<p>
      *
+     * <p>TODO: Currently does not copy the stereotype, but changes the
+     * namespace to the new model (kidnapping it). That might possibly be
+     * dangerous, especially if more complex profile models are developed.
+     * This documentation should say what is supposed to be done. I think
+     * it would have been better if the caller had been responsible for the
+     * stereotype being in the right model and been adviced of
+     * eg ModelManagementHelper.getCorrespondingElement(...). Or if that had
+     * been used here. This function could possibly assert that the caller had
+     * got it right.
+     *
      * @param handle model element
      * @param stereo stereotype
      */
@@ -6349,7 +6359,6 @@ public class ModelFacade {
             if (stereo instanceof MStereotype
                 && me.getModel() != ((MStereotype) stereo).getModel()) {
                 ((MStereotype) stereo).setNamespace(me.getModel());
-                return;
             }
             if (stereo == null || stereo instanceof MStereotype) {
                 me.setStereotype((MStereotype) stereo);
