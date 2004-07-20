c265ccbadf34ee458abaa832511067418c43b4c8
d00mst <d00mst>
2004-07-20 00:04:29 +0000

    Renamed variables named 'enum'. Removed a useless comment.

--- src_new/org/argouml/uml/cognitive/critics/CrUselessAbstract.java
+++ src_new/org/argouml/uml/cognitive/critics/CrUselessAbstract.java
@@ -1,4 +1,4 @@
-// $Id: CrUselessAbstract.java,v 1.9 2003/09/13 18:16:33 alexb Exp $
+// $Id: CrUselessAbstract.java,v 1.10 2004/07/20 00:04:29 d00mst Exp $
 // Copyright (c) 1996-99 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
@@ -27,7 +27,7 @@
 // File: CrUselessAbstract.java
 // Classes: CrUselessAbstract
 // Original Author: jrobbins@ics.uci.edu
-// $Id: CrUselessAbstract.java,v 1.9 2003/09/13 18:16:33 alexb Exp $
+// $Id: CrUselessAbstract.java,v 1.10 2004/07/20 00:04:29 d00mst Exp $
 
 package org.argouml.uml.cognitive.critics;
 
@@ -61,10 +61,9 @@ public class CrUselessAbstract extends CrUML {
 	    return false;  // original class was not abstract
 	VectorSet derived =
 	    (new VectorSet(cls)).reachable(new ChildGenDerivedClasses());
-	Enumeration enum = derived.elements();
-	Object c;
-	while (enum.hasMoreElements()) {
-	    c = /*(MClass)*/ enum.nextElement();
+	Enumeration subclasses = derived.elements();
+	while (subclasses.hasMoreElements()) {
+	    Object c = /*(MClass)*/ subclasses.nextElement();
 	    if (!ModelFacade.isAbstract(c))
 		return false;  // found a concrete subclass
 	}
@@ -85,12 +84,13 @@ class ChildGenDerivedClasses implements ChildGenerator {
 	// TODO: it would be nice to have a EnumerationXform
 	// and a Functor object in uci.util
 	Vector specClasses = new Vector(specs.size());
-	Enumeration enum = specs.elements();
-	while (enum.hasMoreElements()) {
-	    Object g = /*(MGeneralization)*/ enum.nextElement();
+	Enumeration elems = specs.elements();
+	while (elems.hasMoreElements()) {
+	    Object g = /*(MGeneralization)*/ elems.nextElement();
 	    Object ge = ModelFacade.getChild(g);
-	    // assert: ge != null
-	    if (ge != null) specClasses.addElement(ge);
+	    if (ge != null) {
+		specClasses.addElement(ge);
+	    }
 	}
 	return specClasses.elements();
     }
