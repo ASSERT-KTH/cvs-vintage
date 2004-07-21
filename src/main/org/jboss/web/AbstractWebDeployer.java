e726492cea5bd8fc3a45ef351ec4703247ec59e9
starksm <starksm>
2004-07-21 15:25:07 +0000

    Remove the todo regarding the extra parseMetaData call along with the parseMetaData as its handled by the web container service now.

--- src/main/org/jboss/web/AbstractWebDeployer.java
+++ src/main/org/jboss/web/AbstractWebDeployer.java
@@ -7,6 +7,27 @@
 
 package org.jboss.web;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import javax.management.MBeanServer;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.LinkRef;
+import javax.naming.NamingException;
+import javax.security.jacc.PolicyConfiguration;
+import javax.security.jacc.PolicyConfigurationFactory;
+import javax.security.jacc.PolicyContextException;
+import javax.security.jacc.WebResourcePermission;
+import javax.security.jacc.WebUserDataPermission;
+
 import org.jboss.deployment.DeploymentException;
 import org.jboss.deployment.DeploymentInfo;
 import org.jboss.deployment.J2eeApplicationMetaData;
@@ -19,7 +40,6 @@ import org.jboss.metadata.EnvEntryMetaData;
 import org.jboss.metadata.ResourceEnvRefMetaData;
 import org.jboss.metadata.ResourceRefMetaData;
 import org.jboss.metadata.WebMetaData;
-import org.jboss.metadata.XmlFileLoader;
 import org.jboss.metadata.WebSecurityMetaData;
 import org.jboss.metadata.WebSecurityMetaData.WebResourceCollection;
 import org.jboss.mx.loading.LoaderRepositoryFactory;
@@ -28,34 +48,6 @@ import org.jboss.naming.Util;
 import org.jboss.web.AbstractWebContainer.WebDescriptorParser;
 import org.jboss.webservice.WebServiceClientHandler;
 import org.omg.CORBA.ORB;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-
-import javax.management.MBeanServer;
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.LinkRef;
-import javax.naming.NamingException;
-import javax.security.jacc.PolicyConfigurationFactory;
-import javax.security.jacc.PolicyConfiguration;
-import javax.security.jacc.PolicyContextException;
-import javax.security.jacc.WebResourcePermission;
-import javax.security.jacc.WebUserDataPermission;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
-import java.util.HashMap;
-import java.util.Arrays;
-import java.util.Map;
 
 /** A template pattern class for web deployer integration into JBoss. This class
 should be subclasses by war deployers providers wishing to integrate into
@@ -150,7 +142,7 @@ thread context ClassLoader as was used to dispatch the http service request.
    extends="org.jboss.deployment.SubDeployerMBean"
 
 @author  Scott.Stark@jboss.org
-@version $Revision: 1.16 $
+@version $Revision: 1.17 $
 */
 public abstract class AbstractWebDeployer
 {
@@ -291,7 +283,7 @@ public abstract class AbstractWebDeployer
       thread.setContextClassLoader(warLoader);
       WebDescriptorParser webAppParser = ...;
       WebMetaData metaData = di.metaData;
-      parseMetaData(ctxPath, warUrl, metaData);
+      // Create JACC permissions, contextID, etc. ...
       WebApplication warInfo = new WebApplication(metaData);
       performDeploy(warInfo, warUrl, webAppParser);
       deploymentMap.put(warUrl, warInfo);
@@ -340,12 +332,8 @@ public abstract class AbstractWebDeployer
             log.debug("webAppParser: " + webAppParser);
          }
 
-         // Parse the web.xml and jboss-web.xml descriptors
+         // Get the web.xml and jboss-web.xml descriptor metadata
          WebMetaData webMetaData = (WebMetaData) di.metaData;
-
-         // This is alrady done in AbstractWebContainer.init
-         // [todo] Which one is right?
-         //parseMetaData(webContext, warURL, di.shortName, webMetaData);
 
          // inherit the security setup from jboss-app.xml
          if (di.parent != null && di.parent.metaData instanceof J2eeApplicationMetaData)
@@ -789,139 +777,6 @@ public abstract class AbstractWebDeployer
          URL url = urls[u];
          urlSet.add(url.toExternalForm());
       }
-   }
-
-
-   /** This method creates a context-root string from either the
-      WEB-INF/jboss-web.xml context-root element if one exists, or the
-      filename portion of the warURL. It is called if the DeploymentInfo
-      webContext value is null which indicates a standalone war deployment.
-      A war name of ROOT.war is handled as a special case of a war that
-      should be installed as the default web context.
-    */
-   protected void parseMetaData(String ctxPath, URL warURL, String warName,
-      WebMetaData metaData)
-      throws DeploymentException
-   {
-      InputStream jbossWebIS = null;
-      InputStream webIS = null;
-
-      // Parse the war deployment descriptors, web.xml and jboss-web.xml
-      try
-      {
-         // See if the warUrl is a directory
-         File warDir = new File(warURL.getFile());
-         if( warURL.getProtocol().equals("file") && warDir.isDirectory() == true )
-         {
-            File webDD = new File(warDir, "WEB-INF/web.xml");
-            if( webDD.exists() == true )
-               webIS = new FileInputStream(webDD);
-            File jbossWebDD = new File(warDir, "WEB-INF/jboss-web.xml");
-            if( jbossWebDD.exists() == true )
-               jbossWebIS = new FileInputStream(jbossWebDD);
-         }
-         else
-         {
-            // First check for a WEB-INF/web.xml and a WEB-INF/jboss-web.xml
-            InputStream warIS = warURL.openStream();
-            java.util.zip.ZipInputStream zipIS = new java.util.zip.ZipInputStream(warIS);
-            java.util.zip.ZipEntry entry;
-            byte[] buffer = new byte[512];
-            int bytes;
-            while( (entry = zipIS.getNextEntry()) != null )
-            {
-               if( entry.getName().equals("WEB-INF/web.xml") )
-               {
-                  ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                  while( (bytes = zipIS.read(buffer)) > 0 )
-                  {
-                     baos.write(buffer, 0, bytes);
-                  }
-                  webIS = new ByteArrayInputStream(baos.toByteArray());
-               }
-               else if( entry.getName().equals("WEB-INF/jboss-web.xml") )
-               {
-                  ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                  while( (bytes = zipIS.read(buffer)) > 0 )
-                  {
-                     baos.write(buffer, 0, bytes);
-                  }
-                  jbossWebIS = new ByteArrayInputStream(baos.toByteArray());
-               }
-            }
-            zipIS.close();
-         }
-
-         XmlFileLoader xmlLoader = new XmlFileLoader();
-         String warURI = warURL.toExternalForm();
-         try
-         {
-            if( webIS != null )
-            {
-               Document webDoc = xmlLoader.getDocument(webIS, warURI+"/WEB-INF/web.xml");
-               Element web = webDoc.getDocumentElement();
-               metaData.importXml(web);
-            }
-         }
-         catch(Exception e)
-         {
-            throw new DeploymentException("Failed to parse WEB-INF/web.xml", e);
-         }
-         try
-         {
-            if( jbossWebIS != null )
-            {
-               Document jbossWebDoc = xmlLoader.getDocument(jbossWebIS, warURI+"/WEB-INF/jboss-web.xml");
-               Element jbossWeb = jbossWebDoc.getDocumentElement();
-               metaData.importXml(jbossWeb);
-            }
-         }
-         catch(Exception e)
-         {
-            throw new DeploymentException("Failed to parse WEB-INF/jboss-web.xml", e);
-         }
-
-      }
-      catch(Exception e)
-      {
-         log.warn("Failed to parse descriptors for war("+warURL+")", e);
-      }
-
-      // Build a war root context from the war name if one was not specified
-      String webContext = ctxPath;
-      if( webContext == null )
-         webContext = metaData.getContextRoot();
-      if( webContext == null )
-      {
-         // Build the context from the war name, strip the .war suffix
-         webContext = warName;
-         webContext = webContext.replace('\\', '/');
-         if( webContext.endsWith("/") )
-            webContext = webContext.substring(0, webContext.length()-1);
-         int prefix = webContext.lastIndexOf('/');
-         if( prefix > 0 )
-            webContext = webContext.substring(prefix+1);
-         int suffix = webContext.lastIndexOf(".war");
-         if( suffix > 0 )
-            webContext = webContext.substring(0, suffix);
-          // Strip any '<int-value>.' prefix
-          int index = 0;
-          for(; index < webContext.length(); index ++)
-          {
-             char c = webContext.charAt(index);
-             if( Character.isDigit(c) == false && c != '.' )
-                break;
-          }
-          webContext = webContext.substring(index);
-      }
-
-      // Servlet containers are anal about the web context starting with '/'
-      if( webContext.length() > 0 && webContext.charAt(0) != '/' )
-         webContext = "/" + webContext;
-      // And also the default root context must be an empty string, not '/'
-      else if( webContext.equals("/") )
-         webContext = "";
-      metaData.setContextRoot(webContext);
    }
 
    /** Create the JACC permission based on the security constraints obtained
