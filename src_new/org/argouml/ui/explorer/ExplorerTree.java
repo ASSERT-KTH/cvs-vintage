33e6a94fa8363dcfce5c2e9c6582dbfeafb3034b
kataka <kataka>
2004-07-26 21:12:28 +0000

    Fix for issue 2727

--- src_new/org/argouml/ui/explorer/ExplorerTree.java
+++ src_new/org/argouml/ui/explorer/ExplorerTree.java
@@ -1,4 +1,4 @@
-// $Id: ExplorerTree.java,v 1.20 2004/06/23 07:02:44 linus Exp $
+// $Id: ExplorerTree.java,v 1.21 2004/07/26 21:12:28 kataka Exp $
 // Copyright (c) 1996-2004 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
@@ -330,7 +330,7 @@ public class ExplorerTree
             if (!updatingSelection) {
                 updatingSelection = true;
                 
-                Set targets = new HashSet();
+                List targets = new ArrayList();
                 
                 //get existing selection
                 for (int i = 0; i < getSelectionCount(); i++) {
