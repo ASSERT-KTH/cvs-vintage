9d7a5631fe61b533e1d2a9660361a4b361e4b755
javaprog <javaprog>
2004-07-30 13:39:32 +0000

    [comment] added license header

--- src/mail/core/org/columba/mail/gui/menu/MailMenu.java
+++ src/mail/core/org/columba/mail/gui/menu/MailMenu.java
@@ -1,9 +1,19 @@
-/*
- * Created on 26.03.2003
- *
- * To change this generated comment go to
- * Window>Preferences>Java>Code Generation>Code and Comments
- */
+//The contents of this file are subject to the Mozilla Public License Version 1.1
+//(the "License"); you may not use this file except in compliance with the 
+//License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
+//
+//Software distributed under the License is distributed on an "AS IS" basis,
+//WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License 
+//for the specific language governing rights and
+//limitations under the License.
+//
+//The Original Code is "The Columba Project"
+//
+//The Initial Developers of the Original Code are Frederik Dietz and Timo Stich.
+//Portions created by Frederik Dietz and Timo Stich are Copyright (C) 2003. 
+//
+//All Rights Reserved.
+
 package org.columba.mail.gui.menu;
 
 import org.columba.core.action.AbstractColumbaAction;
@@ -18,7 +28,6 @@ import org.columba.core.plugin.PluginHandlerNotFoundException;
 import org.columba.core.pluginhandler.MenuPluginHandler;
 
 import org.columba.mail.main.MailInterface;
-
 
 /**
  * @author frd
