05c291b27e18d06edc2c2de715caa26bac0431e0
mvw <mvw>
2004-08-04 16:54:40 +0000

    Use big port of ancestor instead of overriding local version (thanks for the example, Bob).

--- src_new/org/argouml/uml/diagram/deployment/ui/FigObject.java
+++ src_new/org/argouml/uml/diagram/deployment/ui/FigObject.java
@@ -1,4 +1,4 @@
-// $Id: FigObject.java,v 1.23 2004/08/01 18:00:56 mvw Exp $
+// $Id: FigObject.java,v 1.24 2004/08/04 16:56:48 mvw Exp $
 // Copyright (c) 1996-2004 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
@@ -25,7 +25,7 @@
 // File: FigObject.java
 // Classes: FigObject
 // Original Author: 5eichler@informatik.uni-hamburg.de
-// $Id: FigObject.java,v 1.23 2004/08/01 18:00:56 mvw Exp $
+// $Id: FigObject.java,v 1.24 2004/08/04 16:56:48 mvw Exp $
 
 package org.argouml.uml.diagram.deployment.ui;
 
@@ -54,7 +54,6 @@ public class FigObject extends FigNodeModelElement {
     // instance variables
 
     private FigRect cover;
-    private FigRect bigPort;
     private Object resident =
 	UmlFactory.getFactory().getCore().createElementResidence();
 
@@ -66,7 +65,7 @@ public class FigObject extends FigNodeModelElement {
      * Main constructor.
      */
     public FigObject() {
-	bigPort = new FigRect(10, 10, 90, 50, Color.cyan, Color.cyan);
+	setBigPort( new FigRect(10, 10, 90, 50, Color.cyan, Color.cyan));
 	cover = new FigRect(10, 10, 90, 50, Color.black, Color.white);
 	getNameFig().setLineWidth(0);
 	getNameFig().setFilled(false);
@@ -75,7 +74,7 @@ public class FigObject extends FigNodeModelElement {
 	getNameFig().setBounds(10, 10, nameMin.width + 20, nameMin.height);
 
 	// add Figs to the FigNode in back-to-front order
-	addFig(bigPort);
+	addFig(getBigPort());
 	addFig(cover);
 	addFig(getNameFig());
 
@@ -104,7 +103,7 @@ public class FigObject extends FigNodeModelElement {
     public Object clone() {
 	FigObject figClone = (FigObject) super.clone();
 	Iterator it = figClone.getFigs(null).iterator();
-	figClone.bigPort = (FigRect) it.next();
+	figClone.setBigPort((FigRect) it.next());
 	figClone.cover = (FigRect) it.next();
 	figClone.setNameFig((FigText) it.next());
 	return figClone;
@@ -166,7 +165,7 @@ public class FigObject extends FigNodeModelElement {
      * @see org.tigris.gef.presentation.Fig#getMinimumSize()
      */
     public Dimension getMinimumSize() {
-	Dimension bigPortMin = bigPort.getMinimumSize();
+	Dimension bigPortMin = getBigPort().getMinimumSize();
 	Dimension coverMin = cover.getMinimumSize();
 	Dimension nameMin = getNameFig().getMinimumSize();
 
@@ -187,7 +186,7 @@ public class FigObject extends FigNodeModelElement {
 
 	Dimension nameMin = getNameFig().getMinimumSize();
 
-	bigPort.setBounds(x, y, w, h);
+	getBigPort().setBounds(x, y, w, h);
 	cover.setBounds(x, y, w, h);
 	getNameFig().setBounds(x, y, nameMin.width + 10, nameMin.height + 4);
 
