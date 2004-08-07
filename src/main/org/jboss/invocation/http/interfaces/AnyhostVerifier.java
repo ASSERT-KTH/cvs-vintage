1e7468426b17ec45b3d0a6ccabea3a2d55ea35b9
starksm <starksm>
2004-08-07 18:23:47 +0000

    Get rid of the reflection and use the javax.net.ssl package directly

--- src/main/org/jboss/invocation/http/interfaces/AnyhostVerifier.java
+++ src/main/org/jboss/invocation/http/interfaces/AnyhostVerifier.java
@@ -7,10 +7,9 @@
 package org.jboss.invocation.http.interfaces;
 
 import java.net.HttpURLConnection;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLSession;
 
 /* An implementation of the HostnameVerifier that accepts any SSL certificate
 hostname as matching the https URL that was used to initiate the SSL connection.
@@ -18,54 +17,26 @@ This is useful for testing SSL setup in development environments using self
 signed SSL certificates.
 
  @author Scott.Stark@jboss.org
- @version $Revision: 1.5 $
+ @version $Revision: 1.6 $
  */
-public class AnyhostVerifier implements InvocationHandler
+public class AnyhostVerifier implements HostnameVerifier
 {
    public static void setHostnameVerifier(HttpURLConnection conn)
-      throws IllegalAccessException, InvocationTargetException
    {
-      Class httpsConnClass = conn.getClass();
-      ClassLoader loader = Thread.currentThread().getContextClassLoader();
-      InvocationHandler handler = new AnyhostVerifier();
-      // Get the HostnameVerifier type by calling getHostnameVerifier()
-      Class hostnameVerifierClass = null;
-      Method getHostnameVerifier = null;
-      Method setHostnameVerifier = null;
-      Class[] interfaces = {null};
-      try
+      if( conn instanceof HttpsURLConnection )
       {
-         Class[] empty = {};
-         getHostnameVerifier = httpsConnClass.getMethod("getHostnameVerifier", empty);
-         hostnameVerifierClass = getHostnameVerifier.getReturnType();
-         interfaces[0] = hostnameVerifierClass;
-         setHostnameVerifier = httpsConnClass.getMethod("setHostnameVerifier", interfaces);
+         HttpsURLConnection httpsConn = (HttpsURLConnection) conn;
+         AnyhostVerifier verifier = new AnyhostVerifier();
+         httpsConn.setHostnameVerifier(verifier);
       }
-      catch(NoSuchMethodException e)
-      {
-         throw new InvocationTargetException(e);
-      }
-
-      Object verifier = Proxy.newProxyInstance(loader, interfaces, handler);
-      Object[] args = {verifier};
-      setHostnameVerifier.invoke(conn, args);
    }
 
-   /** An implementation of the com.sun.net.ssl.HostnameVerifier or 
-    * javax.net.ssl.HostnameVerifier that returns true always.
-    * 
-    * @param proxy
-    * @param method the HostnameVerifier method invocation
-    * @param args the HostnameVerifier method args
-    * @return Boolean.TRUE for anything but toString invocations
-    * @throws Throwable
-    */ 
-   public Object invoke(Object proxy, Method method, Object[] args)
-      throws Throwable
+   /* Always validates the hostname.
+    * @see javax.net.ssl.HostnameVerifier#verify(java.lang.String, javax.net.ssl.SSLSession)
+    */
+   public boolean verify(String hostname, SSLSession session)
    {
-      String name = method.getName();
-      if( name.equals("toString"))
-         return super.toString();
-      return Boolean.TRUE;
+      return true;
    }
+
 }
