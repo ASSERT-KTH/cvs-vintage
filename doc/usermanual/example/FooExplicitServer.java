bda0e559713e74b0cc215aacf8ee3bf2393513b0
benoitf <benoitf>
2004-09-01 11:01:52 +0000

    checkstyle

--- doc/usermanual/example/FooExplicitServer.java
+++ doc/usermanual/example/FooExplicitServer.java
@@ -1,4 +1,5 @@
-//The foo remote object server
+// The foo remote object server
+
 import java.rmi.RemoteException;
 import javax.rmi.PortableRemoteObject;
 import org.objectweb.carol.util.configuration.CarolConfiguration;
@@ -6,17 +7,17 @@ import org.objectweb.carol.util.configuration.CarolConfiguration;
 public class Server {
 
     //The main method of this server
-    public static void main(String [] args)  {
-	try {
-	    //initialize carol
-	    CarolConfiguration.init();
+    public static void main(String[] args) {
+        try {
+            //initialize carol
+            CarolConfiguration.init();
 
-	    FooRemoteInterface myFoo = new Foo();
-	    //The object is explicitly exported on RMI IIOP:
-	    PortableRemoteObject.exportObject(myFoo);
-	} catch (RemoteException e) {
-	    //Foo construction problem
-	}
-	    
+            FooRemoteInterface myFoo = new Foo();
+            //The object is explicitly exported on RMI IIOP:
+            PortableRemoteObject.exportObject(myFoo);
+        } catch (RemoteException e) {
+            //Foo construction problem
+        }
+
     }
 }
