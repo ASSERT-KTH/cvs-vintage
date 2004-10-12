4d888c2148f9dd06bccd906a54ba50d40b8d5816
jorgeuriarte <jorgeuriarte>
2004-10-12 09:24:38 +0000

    Fixed lowercase table name.

--- src/sql/upgrade/mysql-upgrade-1.0b19-workflow-2.sql
+++ src/sql/upgrade/mysql-upgrade-1.0b19-workflow-2.sql
@@ -4,6 +4,6 @@
  * Sets the id_table record for scarab_transition.
  *
  */
-delete from id_table where table_name = 'SCARAB_TRANSITION';
+delete from ID_TABLE where table_name = 'SCARAB_TRANSITION';
 insert into ID_TABLE (id_table_id, table_name, next_id, quantity) VALUES (100, 'SCARAB_TRANSITION', 100, 10);
 
