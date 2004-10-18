c7eef2c52617f57321f54325a8c0dc4fae6e7637
mkl <mkl>
2004-10-18 12:24:15 +0000

    set to low priority Issue number:  2833 Obtained from: Submitted by: Reviewed by: CVS: ---------------------------------------------------------------------- CVS: Issue number: CVS:   If this change addresses one or more issues, CVS:   then enter the issue number(s) here. CVS: Obtained from: CVS:   If this change has been taken from another system, CVS:   then name the system in this line, otherwise delete it. CVS: Submitted by: CVS:   If this code has been contributed to the project by someone else; i.e., CVS:   they sent us a patch or a set of diffs, then include their name/email CVS:   address here. If this is your work then delete this line. CVS: Reviewed by: CVS:   If we are doing pre-commit code reviews and someone else has CVS:   reviewed your changes, include their name(s) here. CVS:   If you have not had it reviewed then delete this line.

--- src_new/org/argouml/uml/cognitive/critics/CrCircularComposition.java
+++ src_new/org/argouml/uml/cognitive/critics/CrCircularComposition.java
@@ -1,4 +1,4 @@
-// $Id: CrCircularComposition.java,v 1.15 2004/09/21 19:03:26 mvw Exp $
+// $Id: CrCircularComposition.java,v 1.16 2004/10/18 12:24:15 mkl Exp $
 // Copyright (c) 1996-2004 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
@@ -49,6 +49,7 @@ public class CrCircularComposition extends CrUML {
 	setHeadline("Remove Circular Composition");
 	addSupportedDecision(CrUML.DEC_CONTAINMENT);
 	setKnowledgeTypes(Critic.KT_SYNTAX);
+    setPriority(ToDoItem.LOW_PRIORITY);
 	// no good trigger
     }
 							  
