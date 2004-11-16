cf7e8b933ed6901fa22af947e405781a868191db
bobtarling <bobtarling>
2004-11-16 11:33:33 +0000

    Remove commented out code

--- src_new/org/argouml/uml/diagram/ui/CompartmentFigText.java
+++ src_new/org/argouml/uml/diagram/ui/CompartmentFigText.java
@@ -1,4 +1,4 @@
-// $Id: CompartmentFigText.java,v 1.22 2004/11/16 11:03:29 bobtarling Exp $
+// $Id: CompartmentFigText.java,v 1.23 2004/11/16 11:33:33 bobtarling Exp $
 // Copyright (c) 1996-2004 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
@@ -109,37 +109,6 @@ public class CompartmentFigText extends FigText {
         }
     }
 
-
-    ///////////////////////////////////////////////////////////////////////////
-    //
-    // Accessors
-    //
-    ///////////////////////////////////////////////////////////////////////////
-
-
-    // The following method overrides are necessary for proper graphical
-    // behavior
-
-//    /**
-//     * Override for correct graphical behaviour.<p>
-//     *
-//     * @param w  Desired line width. Overridden and set to zero anyway.
-//     */
-//    public void setLineWidth(int w) {
-//        super.setLineWidth(0);
-//    }
-
-
-//    /**
-//     * Override for correct graphical behaviour.<p>
-//     *
-//     * @return  Current line width&mdash;always 1.
-//     */
-//    public int getLineWidth() {
-//        return 1;
-//    }
-
-    
     /**
      * Override for correct graphical behaviour.<p>
      *
