a565bddfbcd4c05e488e17204662a35c45d82b4b
starksm <starksm>
2004-12-05 16:37:40 +0000

    Update the javadoc

--- src/main/org/jboss/proxy/IClientContainer.java
+++ src/main/org/jboss/proxy/IClientContainer.java
@@ -8,11 +8,26 @@ import org.jboss.invocation.InvocationContext;
  * the client proxy interceptors and InvocationContext.
  * 
  * @author Scott.Stark@jboss.org
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public interface IClientContainer
 {
+   /**
+    * Access a copy of the proxy container interceptor stack.
+    * @return ArrayList<org.jboss.proxy.Interceptor>
+    */ 
    public ArrayList getInterceptors();
+   /**
+    * Set the proxy container interceptor stack.
+    * @param interceptors - ArrayList<org.jboss.proxy.Interceptor> to
+    * install as the new interceptor stack
+    */ 
    public void setInterceptors(ArrayList interceptors);
+   /**
+    * Access the InvocationContext associated with the proxy by the
+    * server side proxy factory. The contents of this will depend on
+    * the proxy factory.
+    * @return The proxy creation time InvocationContext
+    */ 
    public InvocationContext getInvocationContext();
 }
