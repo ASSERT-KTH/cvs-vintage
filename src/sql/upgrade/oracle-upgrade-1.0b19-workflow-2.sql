b6f433c4effdec0724c6956fbfa1aa4ec47c7069
jorgeuriarte <jorgeuriarte>
2004-12-27 23:11:54 +0000

    Fixed duplicated key in ID_TABLE.

--- src/sql/upgrade/oracle-upgrade-1.0b19-workflow-2.sql
+++ src/sql/upgrade/oracle-upgrade-1.0b19-workflow-2.sql
@@ -7,4 +7,4 @@
 delete from id_table where table_name = 'SCARAB_TRANSITION';
 insert into ID_TABLE (id_table_id, table_name, next_id, quantity) VALUES (100, 'SCARAB_TRANSITION', 100, 10);
 delete from ID_TABLE where table_name = 'SCARAB_CONDITION';
-insert into ID_TABLE (id_table_id, table_name, next_id, quantity) VALUES (100, 'SCARAB_CONDITION', 100, 10);
+insert into ID_TABLE (id_table_id, table_name, next_id, quantity) VALUES (101, 'SCARAB_CONDITION', 100, 10);
