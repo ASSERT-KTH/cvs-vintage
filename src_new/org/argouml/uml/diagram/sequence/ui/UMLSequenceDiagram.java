5ff1505820ccbce34c88fa7bc9abf68ecf9b9455
mvw <mvw>
2005-01-29 15:47:17 +0000

    Fixed issue 3046.

--- src_new/org/argouml/uml/diagram/sequence/ui/UMLSequenceDiagram.java
+++ src_new/org/argouml/uml/diagram/sequence/ui/UMLSequenceDiagram.java
@@ -1,4 +1,4 @@
-// $Id: UMLSequenceDiagram.java,v 1.40 2005/01/27 21:42:31 linus Exp $
+// $Id: UMLSequenceDiagram.java,v 1.41 2005/01/29 15:47:17 mvw Exp $
 // Copyright (c) 1996-2005 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
@@ -214,7 +214,7 @@ public class UMLSequenceDiagram extends UMLDiagram {
     public void cleanUp() {
         Object collab =
             ((SequenceDiagramGraphModel) getGraphModel()).getCollaboration();
-        Model.getUmlFactory().delete(collab);
+        ProjectManager.getManager().getCurrentProject().moveToTrash(collab);
     }
 
 } /* end class UMLSequenceDiagram */
