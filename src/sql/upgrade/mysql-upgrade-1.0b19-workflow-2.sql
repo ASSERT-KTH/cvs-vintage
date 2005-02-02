78c3669ac249b9681c77e055c06d288a19d60e62
jorgeuriarte <jorgeuriarte>
2005-02-02 20:36:56 +0000

    Fixed line changes.

--- src/sql/upgrade/mysql-upgrade-1.0b19-workflow-2.sql
+++ src/sql/upgrade/mysql-upgrade-1.0b19-workflow-2.sql
@@ -8,7 +8,5 @@ delete from ID_TABLE where table_name = 'SCARAB_TRANSITION';
 insert into ID_TABLE (id_table_id, table_name, next_id, quantity) VALUES (100, 'SCARAB_TRANSITION', 100, 10);
 delete from ID_TABLE where table_name = 'SCARAB_CONDITION';
 insert into ID_TABLE (id_table_id, table_name, next_id, quantity) VALUES (101, 'SCARAB_CONDITION', 100, 10);
-insert into SCARAB_ATTRIBUTE_TYPE(ATTRIBUTE_TYPE_ID, ATTRIBUTE_CLASS_ID, ATTRIBU
-TE_TYPE_NAME, JAVA_CLASS_NAME)
-        values(13, 1, 'date', 'org.tigris.scarab.attribute.DateAttribute');
+insert into SCARAB_ATTRIBUTE_TYPE (ATTRIBUTE_TYPE_ID, ATTRIBUTE_CLASS_ID, ATTRIBUTE_TYPE_NAME, JAVA_CLASS_NAME) values (13, 1, 'date', 'org.tigris.scarab.attribute.DateAttribute'); 
 
