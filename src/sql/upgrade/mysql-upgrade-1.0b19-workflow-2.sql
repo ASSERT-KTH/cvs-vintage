67dbe2ffbf027f9b49bfdab7a66a29e305fe703a
jorgeuriarte <jorgeuriarte>
2005-02-02 20:12:20 +0000

    Fix: The migration script did not add the Date attribute type to the database.

--- src/sql/upgrade/mysql-upgrade-1.0b19-workflow-2.sql
+++ src/sql/upgrade/mysql-upgrade-1.0b19-workflow-2.sql
@@ -8,4 +8,7 @@ delete from ID_TABLE where table_name = 'SCARAB_TRANSITION';
 insert into ID_TABLE (id_table_id, table_name, next_id, quantity) VALUES (100, 'SCARAB_TRANSITION', 100, 10);
 delete from ID_TABLE where table_name = 'SCARAB_CONDITION';
 insert into ID_TABLE (id_table_id, table_name, next_id, quantity) VALUES (101, 'SCARAB_CONDITION', 100, 10);
+insert into SCARAB_ATTRIBUTE_TYPE(ATTRIBUTE_TYPE_ID, ATTRIBUTE_CLASS_ID, ATTRIBU
+TE_TYPE_NAME, JAVA_CLASS_NAME)
+        values(13, 1, 'date', 'org.tigris.scarab.attribute.DateAttribute');
 
--- src/sql/upgrade/oracle-upgrade-1.0b19-workflow-2.sql
+++ src/sql/upgrade/oracle-upgrade-1.0b19-workflow-2.sql
@@ -8,3 +8,5 @@ delete from id_table where table_name = 'SCARAB_TRANSITION';
 insert into ID_TABLE (id_table_id, table_name, next_id, quantity) VALUES (100, 'SCARAB_TRANSITION', 100, 10);
 delete from ID_TABLE where table_name = 'SCARAB_CONDITION';
 insert into ID_TABLE (id_table_id, table_name, next_id, quantity) VALUES (101, 'SCARAB_CONDITION', 100, 10);
+insert into SCARAB_ATTRIBUTE_TYPE(attribute_type_id, attribute_class_id, attribute_type_name, java_class_name) VALUES (13, 1, 'date', 'org.tigris.scarab.attribute.DateAttribute');
+
