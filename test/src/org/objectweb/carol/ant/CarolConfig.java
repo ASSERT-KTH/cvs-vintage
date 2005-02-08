3dfbeab5ea38d12bae34933cc08463b2d22cdb74
el-vadimo <el-vadimo>
2005-02-08 22:45:36 +0000

    See the log message on commit "2005-02-08 13:13" which is viewable, for example, on test/build.xml,v1.17:
    http://cvs.forge.objectweb.org/cgi-bin/viewcvs.cgi/carol/carol/test/build.xml?rev=1.17&content-type=text/vnd.viewcvs-markup
    
    "Single" tests have now been converted to run via the custom Ant
    task org.objectweb.carol.ant.CarolTestTask.
    
    "Multiple" tests are have not been converted yet and are currently
    disabled.  I will finish those tomorrow.

--- test/src/org/objectweb/carol/ant/CarolConfig.java
+++ test/src/org/objectweb/carol/ant/CarolConfig.java
@@ -18,20 +18,66 @@
  * USA
  *
  * --------------------------------------------------------------------------
- * $Id: CarolConfig.java,v 1.1 2005/02/08 18:13:11 el-vadimo Exp $
+ * $Id: CarolConfig.java,v 1.2 2005/02/08 22:45:36 el-vadimo Exp $
  * --------------------------------------------------------------------------
  */
 
+
 package org.objectweb.carol.ant;
 
-public final class CarolConfig {
-    private String name;
+import java.util.List;
+import java.util.Arrays;
 
-    public void setName(String name) {
-        this.name = name;
+import org.apache.tools.ant.BuildException;
+
+public final class CarolConfig {
+    public final static String IIOP    = "iiop";
+    public final static String JEREMIE = "jeremie";
+    public final static String JRMP    = "jrmp";
+
+    private final static List PROTOCOLS =
+        Arrays.asList(new String[] {IIOP, JEREMIE, JRMP});
+
+    private String proto1;
+    private String proto2;
+
+    public void setName(String name) throws BuildException {
+        if (name == null) {
+            throw new NullPointerException("name");
+        }
+
+        String trimmed = name.trim();
+        if ("".equals(trimmed)) {
+            throw new BuildException("name not set");
+        }
+
+        int dotIdx = trimmed.indexOf('.');
+        if (dotIdx < 0) {
+            proto1 = trimmed;
+        } else {
+            if (dotIdx == trimmed.length()-1) {
+                throw new BuildException("name can't end in a dot: " + name);
+            }
+            proto1 = trimmed.substring(0, dotIdx);
+            proto2 = trimmed.substring(dotIdx+1);
+        }
+        
+        
+        if (!PROTOCOLS.contains(proto1)) {
+            throw new BuildException
+                ("Unknown protocol: " + proto1 + "; known protocols are " + PROTOCOLS);
+        }
+        if (proto2 != null && !PROTOCOLS.contains(proto2)) {
+            throw new BuildException
+                ("Unknown protocol: " + proto2 + "; known protocols are " + PROTOCOLS);
+        }
     }
 
-    public String getName() {
-        return name;
+    public String getProto1() {
+        return proto1;
+    }
+
+    public String getProto2() {
+        return proto2;
     }
 }
--- test/src/org/objectweb/carol/ant/CarolTestTask.java
+++ test/src/org/objectweb/carol/ant/CarolTestTask.java
@@ -18,23 +18,36 @@
  * USA
  *
  * --------------------------------------------------------------------------
- * $Id: CarolTestTask.java,v 1.1 2005/02/08 18:13:11 el-vadimo Exp $
+ * $Id: CarolTestTask.java,v 1.2 2005/02/08 22:45:36 el-vadimo Exp $
  * --------------------------------------------------------------------------
  */
 
 package org.objectweb.carol.ant;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
 import java.util.Iterator;
-import java.util.List;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Properties;
 
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
 import org.apache.tools.ant.taskdefs.Ant;
+import org.apache.tools.ant.taskdefs.Property;
 
 public final class CarolTestTask extends Task {
     private final List configs;
     private String antfile;
+    private String propDestination;
+    private String propSource;
+    private File propDestDir;
+    private File propSourceDir;
 
     public CarolTestTask() {
         configs = new LinkedList();
@@ -44,26 +57,177 @@ public final class CarolTestTask extends Task {
         this.antfile = antfile;
     }
 
-    public CarolConfig createCarolConfig() {
+    public void setPropDestination(String propDestination) {
+        this.propDestination = propDestination;
+    }
+
+    public void setPropSource(String propSource) {
+        this.propSource = propSource;
+    }
+
+    public CarolConfig createCarolProtocols() {
         CarolConfig config = new CarolConfig();
         configs.add(config);
         return config;
     }
 
-    public void execute() throws BuildException {
+    private void checkSettings() throws BuildException {
+        if (antfile == null) {
+            throw new BuildException("antfile is not set");
+        }
+        if (propDestination == null) {
+            throw new BuildException("propDestination is not set");
+        }
+        if (propSource == null) {
+            throw new BuildException("propSource is not set");
+        }
         if (configs.size() == 0) {
             throw new BuildException("no nested config elements found");
         }
+    }
+
+    public void execute() throws BuildException {
+        checkSettings();
+
+        propDestDir = new File(getProject().getBaseDir(), propDestination);
+        assertIsDirectory("propDestination", propDestDir);
+
+        propSourceDir = new File(propSource);
+        assertIsDirectory("propSource", propSourceDir);
 
         for (Iterator ii=configs.iterator(); ii.hasNext(); ) {
             CarolConfig config = (CarolConfig) ii.next();
-            Ant ant = new Ant();
-            ant.setProject(getProject());
-            ant.setOwningTarget(getOwningTarget());
-            ant.setAntfile(getLocation().getFileName());
-            ant.setTaskName("antcall");
-            ant.setTarget(config.getName());
-            ant.execute();
+            if (config.getProto2() == null) {
+                executeSingle(config.getProto1());
+            } else {
+                // executeMulti(ant, config.getProto1(), config.getProto2());
+                throw new Error("not implemented");
+            }
         }
+    }
+
+    private Ant newAnt() {
+        Ant ant = new Ant();
+        ant.setProject(getProject());
+        ant.setOwningTarget(getOwningTarget());
+        ant.setAntfile(antfile);
+        return ant;
+    }
+
+    private static void assertIsDirectory(String propName, File dir) {
+        if (!dir.exists()) {
+            System.err.println("dir: " + dir);
+            throw new BuildException
+                (propName + " " + dir + " does not exist");
+        }
+        if (!dir.isDirectory()) {
+            throw new BuildException
+                (propName + " " + dir + " is not a directory");
+        }
+    }
+
+    private void executeSingle(String proto) throws BuildException {
+        // these are passed on to the Ant <target> task
+        Properties targetProps = loadProperties(proto);
+        final File clientPropsFile = new File(propDestDir, "client1.properties");
+        String clientPropsFilename = clientPropsFile.getAbsolutePath();
+        targetProps.setProperty("client.properties.file.name1", clientPropsFilename);
+        targetProps.setProperty("server.properties.file.name", clientPropsFilename);
+
+        int nVariations = CarolConfig.JRMP.equals(proto) ? 2 : 1;
+
+        // in the case of JRMP, we need to test two variations:
+        // "jrmp 1.1" and "jrmp 1.2"
+        for (int ii=1; ii<=nVariations; ii++) {
+
+            // We make two iterations.  During the first one, we start a name
+            // server externally.  During the second one, we let CAROL start
+            // the name server on its own.
+            for (int jj=0; jj<2; jj++) {
+
+                Properties clientProps = new Properties();
+                clientProps.setProperty("carol.protocols", proto);
+                clientProps.setProperty("carol.start.ns", jj==0 ? "true" : "false");
+
+                Ant ant = newAnt();
+
+                for (Enumeration props=targetProps.propertyNames(); props.hasMoreElements(); ) {
+                    String propName = (String) props.nextElement();
+
+                    if (propName.startsWith("carol.")) {
+                        clientProps.setProperty(propName, targetProps.getProperty(propName));
+                    } else {
+                        Property antProp = ant.createProperty();
+                        antProp.setName(propName);
+                        String propValue = alter(proto, ii, propName, targetProps.getProperty(propName));
+                        antProp.setValue(propValue);
+                    }
+                }
+                Property antProp = ant.createProperty();
+                StringBuffer testname = new StringBuffer(proto);
+
+                if (CarolConfig.JRMP.equals(proto)) {
+                    testname.append("1.").append(ii);
+                }
+
+                if (jj == 1) {
+                    testname.append(".nons");
+                }
+
+                antProp.setName("test.name");
+                antProp.setValue(testname.toString());
+
+                try {
+                    FileOutputStream os = new FileOutputStream(clientPropsFile);
+                    clientProps.store(os, null);
+                    os.close();
+                    System.err.println("wrote " + clientPropsFile);
+                } catch (IOException ex) {
+                    throw new BuildException
+                        ("couldn't create " + clientPropsFile, ex);
+                }
+                ant.execute();
+            }
+        }
+    }
+
+    private static String alter(String proto, int variation, String name, String value) {
+        if (!CarolConfig.JRMP.equals(proto)) {
+            return value;
+        }
+        String suffix = variation == 1 ? "1.1" : "1.2";
+        if (name.startsWith("stub.jar.name")) {
+            return value + suffix + ".jar";
+        }
+
+        if (name.startsWith("test.name")) {
+            return value + suffix;
+        }
+
+        return value;
+    }
+
+
+    private Properties loadProperties(String proto) throws BuildException {
+        String filename = proto + ".properties";
+        File propFile = new File(propSourceDir, filename);
+        InputStream is;
+        try {
+            is = new FileInputStream(propFile);
+        } catch (FileNotFoundException ex) {
+            throw new BuildException(filename + " not found", ex);
+        }
+
+        Properties props = new Properties();
+
+        try {
+            props.load(is);
+        } catch (IOException ex) {
+            throw new BuildException("couldn't load " + filename, ex);
+        } finally {
+            try { is.close(); }
+            catch (IOException ex) { ; }
+        }
+        return props;
     }
 }
