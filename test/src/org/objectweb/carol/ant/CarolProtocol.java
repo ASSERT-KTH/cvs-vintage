ece0cef2865cbfd52421872ff13a6c84e0b3968e
benoitf <benoitf>
2005-02-14 09:34:05 +0000

    javadoc

--- test/src/org/objectweb/carol/ant/CarolProtocol.java
+++ test/src/org/objectweb/carol/ant/CarolProtocol.java
@@ -1,4 +1,6 @@
 /**
+ * CAROL: Common Architecture for RMI ObjectWeb Layer
+ *
  * This library is developed inside the ObjectWeb Consortium,
  * http://www.objectweb.org
  *
@@ -18,7 +20,7 @@
  * USA
  *
  * --------------------------------------------------------------------------
- * $Id: CarolProtocol.java,v 1.3 2005/02/11 12:06:54 benoitf Exp $
+ * $Id: CarolProtocol.java,v 1.4 2005/02/14 09:34:05 benoitf Exp $
  * --------------------------------------------------------------------------
  */
 
@@ -29,18 +31,41 @@ package org.objectweb.carol.ant;
  */
 final class CarolProtocol {
 
-    public  static final CarolProtocol IIOP = protocol("iiop");
+    /**
+     * IIOP protocol
+     */
+    public static final CarolProtocol IIOP = protocol("iiop");
 
-    public  static final CarolProtocol JEREMIE = protocol("jeremie");
+    /**
+     * Jeremie protocol
+     */
+    public static final CarolProtocol JEREMIE = protocol("jeremie");
 
-    public  static final CarolProtocol JRMP11 = protocol("jrmp", "1.1");
+    /**
+     * JRMP protocol (v 1.1)
+     */
+    public static final CarolProtocol JRMP11 = protocol("jrmp", "1.1");
 
-    public  static final CarolProtocol JRMP12 = protocol("jrmp", "1.2");
+    /**
+     * JRMP protocol (v 1.2)
+     */
+    public static final CarolProtocol JRMP12 = protocol("jrmp", "1.2");
 
+    /**
+     * Name of the protocol
+     */
     private final String name;
 
+    /**
+     * Version of the protocol (JRMP)
+     */
     private final String version;
 
+    /**
+     * Build a new carol protocol with the given name and its version
+     * @param name of the protocol
+     * @param version of the protocol (JRMP)
+     */
     private CarolProtocol(String name, String version) {
         this.name = name;
         if (version == null) {
@@ -50,18 +75,35 @@ final class CarolProtocol {
         }
     }
 
+    /**
+     * Build a new carol protocol without specified version
+     * @param name name of the protocol
+     * @return the protocol built
+     */
     private static CarolProtocol protocol(String name) {
         return new CarolProtocol(name, null);
     }
 
+    /**
+     * Build a new carol protocol with a given version
+     * @param name of the protocol
+     * @param version of the protocol
+     * @return the protocol built
+     */
     private static CarolProtocol protocol(String name, String version) {
         return new CarolProtocol(name, version);
     }
 
+    /**
+     * @return name of the protocol
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * @return version of the protocol
+     */
     public String getNameVersion() {
         return version;
     }
