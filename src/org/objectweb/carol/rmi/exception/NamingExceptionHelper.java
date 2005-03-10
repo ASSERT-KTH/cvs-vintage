ee320ff132a1ba1177760df3be3ee833d172f7e3
benoitf <benoitf>
2005-03-10 09:51:16 +0000

    Allow to throw a Naming Exception by setting the initial cause (avoid to forget original exception)

---
+++ src/org/objectweb/carol/rmi/exception/NamingExceptionHelper.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (C) 2005 - Bull S.A.
+ *
+ * CAROL: Common Architecture for RMI ObjectWeb Layer
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ * --------------------------------------------------------------------------
+ * $Id: NamingExceptionHelper.java,v 1.1 2005/03/10 09:51:16 benoitf Exp $
+ * --------------------------------------------------------------------------
+ */
+package org.objectweb.carol.rmi.exception;
+
+import javax.naming.NamingException;
+
+/**
+ * This class throw NamingException by using provided exception, and set the
+ * initCause of the newly created exception. This avoid to forget initial
+ * exception
+ * @author Florent Benoit
+ */
+public class NamingExceptionHelper {
+
+    /**
+     * Utility class, no constructor
+     */
+    private NamingExceptionHelper() {
+    }
+
+    /**
+     * Build a new exception with the given exception by wrapping it in a NamingException
+     * @return built exception
+     * @param message text error for the exception
+     * @param originalException original exception
+     */
+    public static NamingException create(String message, Exception originalException) {
+        NamingException ne = new NamingException(message);
+        ne.initCause(originalException);
+        return ne;
+    }
+
+}
