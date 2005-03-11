45645c2cb49cc6e33216b0286e7949042d0c99d5
benoitf <benoitf>
2005-03-11 13:54:44 +0000

    avoid to use enum for JDK 5

--- src/org/objectweb/carol/cmi/InetMask.java
+++ src/org/objectweb/carol/cmi/InetMask.java
@@ -5,12 +5,12 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
@@ -78,16 +78,16 @@ public class InetMask {
     public LinkedList filterLocal() {
         LinkedList l = new LinkedList();
         try {
-            Enumeration enum;
+            Enumeration en;
             Class cl;
             Object[] obj0 = {
             };
             cl = Class.forName("java.net.NetworkInterface");
             Method meth = cl.getMethod("getNetworkInterfaces", new Class[0]);
             Method getInet = cl.getMethod("getInetAddresses", new Class[0]);
-            enum = (Enumeration) meth.invoke(cl, obj0);
-            while (enum.hasMoreElements()) {
-                Object o = enum.nextElement();
+            en = (Enumeration) meth.invoke(cl, obj0);
+            while (en.hasMoreElements()) {
+                Object o = en.nextElement();
                 Enumeration enum2 = (Enumeration) getInet.invoke(o, obj0);
                 while (enum2.hasMoreElements()) {
                     InetAddress a = (InetAddress) enum2.nextElement();
