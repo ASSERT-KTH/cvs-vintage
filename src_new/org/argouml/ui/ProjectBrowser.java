6448b3fd5e0feb18ca3b7922403e30a7c3a0aa07
bobtarling <bobtarling>
2005-06-21 20:59:45 +0000

    Allow access to the GEF undo action. Lots left to implement here.

--- src_new/org/argouml/ui/ProjectBrowser.java
+++ src_new/org/argouml/ui/ProjectBrowser.java
@@ -1,4 +1,4 @@
-// $Id: ProjectBrowser.java,v 1.151 2005/06/18 17:52:07 linus Exp $
+// $Id: ProjectBrowser.java,v 1.152 2005/06/21 20:59:45 bobtarling Exp $
 // Copyright (c) 1996-2005 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
@@ -45,6 +45,7 @@ import java.util.Locale;
 import java.util.Map;
 import java.util.Vector;
 
+import javax.swing.Action;
 import javax.swing.ImageIcon;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
@@ -77,6 +78,7 @@ import org.argouml.uml.ui.ActionSaveProjectAs;
 import org.argouml.uml.ui.TabProps;
 import org.tigris.gef.base.Diagram;
 import org.tigris.gef.base.Globals;
+import org.tigris.gef.base.UndoAction;
 import org.tigris.gef.ui.IStatusBar;
 import org.tigris.swidgets.BorderSplitPane;
 import org.tigris.swidgets.Horizontal;
@@ -162,6 +164,11 @@ public class ProjectBrowser
      * The todopane (lower left corner of screen).
      */
     private ToDoPane todoPane;
+
+    /**
+     * The action to undo the last user interaction
+     */
+    private final UndoAction undoAction = new UndoAction(Translator.localize("action.undo"));
 
     /**
      * For testing purposes. In tests this constructor can be called so
@@ -1186,5 +1193,13 @@ public class ProjectBrowser
         }
         FindDialog.getInstance().doClearTabs();
         FindDialog.getInstance().doResetFields();
+    }
+    
+    /**
+     * Get the action that can undo the last user interaction on this project.
+     * @return the undo action.
+     */
+    public Action getUndoAction() {
+        return undoAction;
     }
 } /* end class ProjectBrowser */
