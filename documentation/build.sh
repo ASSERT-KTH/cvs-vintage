8a8952cdd2bc8b34890c110f2008fcaee3951a65
linus <linus>
2005-06-25 23:35:42 +0000

    Change to newer version of ant.

--- documentation/build.sh
+++ documentation/build.sh
@@ -1,12 +1,12 @@
 #! /bin/sh
 #
 
-# $Id: build.sh,v 1.7 2002/11/23 22:04:38 kataka Exp $
+# $Id: build.sh,v 1.8 2005/06/25 23:35:42 linus Exp $
 
 #
  
 # Always use the ant that comes with ArgoUML
-ANT_HOME=../tools/ant-1.4.1
+ANT_HOME=../tools/ant-1.6.2
 
 echo ANT_HOME is: $ANT_HOME
 echo
