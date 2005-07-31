b4863df8d126284d47702531071ac5b2e6ae842f
mkl <mkl>
2005-07-31 21:24:56 +0000

    refactoring of PropPanelSignal class

--- src_new/org/argouml/uml/ui/behavior/common_behavior/PropPanelSignal.java
+++ src_new/org/argouml/uml/ui/behavior/common_behavior/PropPanelSignal.java
@@ -1,4 +1,4 @@
-// $Id: PropPanelSignal.java,v 1.57 2005/05/26 20:35:24 bobtarling Exp $
+// $Id: PropPanelSignal.java,v 1.58 2005/07/31 21:24:56 mkl Exp $
 // Copyright (c) 1996-2005 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
@@ -24,16 +24,11 @@
 
 package org.argouml.uml.ui.behavior.common_behavior;
 
-import java.awt.event.ActionEvent;
 
-import javax.swing.Action;
 import javax.swing.JScrollPane;
 
 import org.argouml.i18n.Translator;
-import org.argouml.model.Model;
-import org.argouml.ui.targetmanager.TargetManager;
 import org.argouml.uml.ui.AbstractActionAddModelElement;
-import org.argouml.uml.ui.AbstractActionNewModelElement;
 import org.argouml.uml.ui.ActionDeleteSingleModelElement;
 import org.argouml.uml.ui.ActionNavigateNamespace;
 import org.argouml.uml.ui.ActionDeleteModelElements;
@@ -81,39 +76,6 @@ public class PropPanelSignal extends PropPanelModelElement {
         addButton(new PropPanelButton2(new ActionNewStereotype(),
                 lookupIcon("Stereotype")));
         addButton(new PropPanelButton2(new ActionDeleteSingleModelElement()));
-    }
-
-    /**
-     * Create a new Signal.
-     */
-    private class ActionNewSignal
-        extends AbstractActionNewModelElement {
-
-        /**
-         * The constructor.
-         */
-        public ActionNewSignal() {
-            super("button.new-signal");
-            putValue(Action.NAME,
-                    Translator.localize("button.new-signal"));
-        }
-
-        /**
-         * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
-         */
-        public void actionPerformed(ActionEvent e) {
-            Object target = TargetManager.getInstance().getModelTarget();
-            if (Model.getFacade().isASignal(target)) {
-                Object ns = Model.getFacade().getNamespace(target);
-                if (ns != null) {
-                    Object newSig = Model.getCommonBehaviorFactory()
-                        .createSignal();
-                    Model.getCoreHelper().addOwnedElement(ns, newSig);
-                    TargetManager.getInstance().setTarget(newSig);
-                    super.actionPerformed(e);
-                }
-            }
-        }
     }
 
 
