024457df01ecc123c7e4aedcebf57c070266992c
mvw <mvw>
2005-08-14 13:33:10 +0000

    Fixed issue 3120: Remove "Report on Load Times" item on settings dialog. It now appears in the logging unconditionally.

--- src_new/org/argouml/ui/SettingsTabPreferences.java
+++ src_new/org/argouml/ui/SettingsTabPreferences.java
@@ -1,4 +1,4 @@
-// $Id: SettingsTabPreferences.java,v 1.12 2005/07/22 12:29:07 mkl Exp $
+// $Id: SettingsTabPreferences.java,v 1.13 2005/08/14 13:33:10 mvw Exp $
 // Copyright (c) 1996-2005 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
@@ -47,7 +47,6 @@ public class SettingsTabPreferences extends SettingsTabHelper
 
     private JCheckBox chkSplash = null;
     private JCheckBox chkPreload = null;
-    private JCheckBox chkProfile = null;
     private JCheckBox chkReloadRecent = null;
 
     /**
@@ -99,10 +98,6 @@ public class SettingsTabPreferences extends SettingsTabHelper
  	top.add(chkPreload, checkConstraints);
 
 	checkConstraints.gridy = 3;
-        chkProfile = createCheckBox("label.profile");
- 	top.add(chkProfile, checkConstraints);
-
-	checkConstraints.gridy = 4;
         chkReloadRecent = createCheckBox("label.reload-recent");
  	top.add(chkReloadRecent, checkConstraints);
 
@@ -116,8 +111,6 @@ public class SettingsTabPreferences extends SettingsTabHelper
         chkSplash.setSelected(Configuration.getBoolean(Argo.KEY_SPLASH, true));
         chkPreload.setSelected(Configuration.getBoolean(Argo.KEY_PRELOAD,
                 true));
-        chkProfile.setSelected(Configuration.getBoolean(Argo.KEY_PROFILE,
-                false));
         chkReloadRecent.setSelected(
 		Configuration.getBoolean(Argo.KEY_RELOAD_RECENT_PROJECT,
 					 false));
@@ -129,7 +122,6 @@ public class SettingsTabPreferences extends SettingsTabHelper
     public void handleSettingsTabSave() {
         Configuration.setBoolean(Argo.KEY_SPLASH, chkSplash.isSelected());
         Configuration.setBoolean(Argo.KEY_PRELOAD, chkPreload.isSelected());
-        Configuration.setBoolean(Argo.KEY_PROFILE, chkProfile.isSelected());
         Configuration.setBoolean(Argo.KEY_RELOAD_RECENT_PROJECT,
 				 chkReloadRecent.isSelected());
     }
