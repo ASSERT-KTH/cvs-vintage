998df85c0682f721728204f6fc9125bc3428e853
tstich <tstich>
2005-09-06 09:57:40 +0000

    [intern]Some more IMAP tweaks.

--- src/mail/core/org/columba/mail/folder/command/CheckForNewMessagesCommand.java
+++ src/mail/core/org/columba/mail/folder/command/CheckForNewMessagesCommand.java
@@ -66,7 +66,7 @@ public class CheckForNewMessagesCommand extends Command {
 
 		// check for new headers
 		try {
-			imapFolder.ensureFolderIsSynced(false);
+			imapFolder.ensureFolderIsSynced(false, false);
 		} catch (IOException e) {
 			worker.cancel();
 			throw new CommandCancelledException(e);
