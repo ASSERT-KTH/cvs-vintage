33b76752c385979aead6870434aa6288bb45bdcf
benoitf <benoitf>
2005-10-19 08:55:36 +0000

    Missing header, missing javadoc, checkstyle invalid, etc.

--- src/org/objectweb/carol/jndi/wrapping/RemoteReference.java
+++ src/org/objectweb/carol/jndi/wrapping/RemoteReference.java
@@ -1,9 +1,45 @@
+/**
+ * Copyright (C) 2002,2005 - INRIA (www.inria.fr)
+ *
+ * CAROL: Common Architecture for RMI ObjectWeb Layer
+ *
+ * This library is developed inside the ObjectWeb Consortium,
+ * http://www.objectweb.org
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+ * USA
+ *
+ * --------------------------------------------------------------------------
+ * $Id: RemoteReference.java,v 1.2 2005/10/19 08:55:36 benoitf Exp $
+ * --------------------------------------------------------------------------
+ */
 package org.objectweb.carol.jndi.wrapping;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 import javax.naming.Reference;
 
+/**
+ * This interface wrap Reference objects into a remote object
+ */
 public interface RemoteReference extends Remote {
-    public Reference getReference() throws RemoteException;
+
+    /**
+     * Get the <code>Reference</code> reference
+     * @return the <code>Reference</code> reference
+     * @throws RemoteException if the reference cannot be returned
+     */
+    Reference getReference() throws RemoteException;
 }
