518dff33d0d9df7b7924370a99436083fe52729b
tfmorris <tfmorris>
2005-11-02 21:26:58 +0000

    Make sure Explorer responds to DeleteInstance events

--- src_new/org/argouml/ui/explorer/ExplorerEventAdaptor.java
+++ src_new/org/argouml/ui/explorer/ExplorerEventAdaptor.java
@@ -1,4 +1,4 @@
-// $Id: ExplorerEventAdaptor.java,v 1.18 2005/11/02 13:28:16 mvw Exp $
+// $Id: ExplorerEventAdaptor.java,v 1.19 2005/11/02 21:26:58 tfmorris Exp $
 // Copyright (c) 1996-2005 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
@@ -30,6 +30,7 @@ import org.argouml.application.api.Configuration;
 import org.argouml.kernel.ProjectManager;
 import org.argouml.model.AddAssociationEvent;
 import org.argouml.model.AttributeChangeEvent;
+import org.argouml.model.DeleteInstanceEvent;
 import org.argouml.model.Model;
 import org.argouml.model.RemoveAssociationEvent;
 import org.argouml.notation.Notation;
@@ -193,6 +194,11 @@ public final class ExplorerEventAdaptor
         
         if (pce instanceof AttributeChangeEvent) {
             treeModel.modelElementChanged(pce.getSource());
+        }
+        
+        if (pce instanceof DeleteInstanceEvent) {
+            treeModel.modelElementRemoved(((DeleteInstanceEvent) pce)
+                    .getSource());
         }
 
     }
