1adb99b63c48b1ca24f245c6e781e8b5d1198908
mvw <mvw>
2005-11-06 13:56:44 +0000

    Style issue.

--- src_new/org/argouml/uml/notation/ModelElementNameNotation.java
+++ src_new/org/argouml/uml/notation/ModelElementNameNotation.java
@@ -1,4 +1,4 @@
-// $Id: ModelElementNameNotation.java,v 1.1 2005/11/02 13:07:42 mvw Exp $
+// $Id: ModelElementNameNotation.java,v 1.2 2005/11/06 13:56:44 mvw Exp $
 // Copyright (c) 2005 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
@@ -32,7 +32,6 @@ import org.argouml.model.Model;
  * Subclass this for all languages.
  * 
  * @author mvw@tigris.org
- *
  */
 public abstract class ModelElementNameNotation extends ValueHandler {
 
@@ -41,7 +40,6 @@ public abstract class ModelElementNameNotation extends ValueHandler {
     
     /**
      * The constructor.
-     * 
      */
     public ModelElementNameNotation(Object modelElement) {
         if (!Model.getFacade().isAModelElement(modelElement)) {
