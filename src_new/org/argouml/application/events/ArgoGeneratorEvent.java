f093b6ff5dfc42afa4eddddd2d08e7715be58bf6
tfmorris <tfmorris>
2005-11-14 17:57:26 +0000

    changed keyword expansion to -kkv

--- src_new/org/argouml/application/events/ArgoGeneratorEvent.java
+++ src_new/org/argouml/application/events/ArgoGeneratorEvent.java
@@ -1,4 +1,4 @@
-// $Id: ArgoGeneratorEvent.java,v 1.1 2005/10/24 17:06:13 mvw Exp $
+// $Id: ArgoGeneratorEvent.java,v 1.2 2005/11/14 17:57:26 tfmorris Exp $
 // Copyright (c) 1996-2005 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
