6cd4b8b54bdea3901bb3af360706fcfd634d257f
mvw <mvw>
2005-12-09 17:30:11 +0000

    This prevents an exception I got while testing for issue 3736: A Submachine state should not have a AddConcurrentRegion button on its proppanel, since that leads to odd behaviour & exceptions.

--- src_new/org/argouml/uml/ui/behavior/state_machines/PropPanelSubmachineState.java
+++ src_new/org/argouml/uml/ui/behavior/state_machines/PropPanelSubmachineState.java
@@ -1,4 +1,4 @@
-// $Id: PropPanelSubmachineState.java,v 1.20 2005/09/08 18:23:59 mkl Exp $
+// $Id: PropPanelSubmachineState.java,v 1.21 2005/12/09 17:30:11 mvw Exp $
 // Copyright (c) 1996-2005 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
@@ -96,6 +96,13 @@ public class PropPanelSubmachineState extends PropPanelCompositeState {
                 new JScrollPane(new UMLMutableLinkedList(
                         new UMLCompositeStateSubvertexListModel(), null,
                         ActionNewStubState.getInstance())));
+    }
+
+    /**
+     * @see org.argouml.uml.ui.behavior.state_machines.PropPanelStateVertex#addExtraButtons()
+     */
+    protected void addExtraButtons() {
+        // Intentionally do nothing.
     }
 
     /**
