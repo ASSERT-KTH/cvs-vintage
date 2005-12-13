cf2667ac101cba243cb3ae82e5bb89e025b055c7
bobtarling <bobtarling>
2005-12-13 12:49:14 +0000

    Issue 3755: The save action should be disabled on startup.

--- src_new/org/argouml/uml/ui/ActionSaveProject.java
+++ src_new/org/argouml/uml/ui/ActionSaveProject.java
@@ -1,4 +1,4 @@
-// $Id: ActionSaveProject.java,v 1.52 2005/12/13 00:47:29 bobtarling Exp $
+// $Id: ActionSaveProject.java,v 1.53 2005/12/13 12:49:14 bobtarling Exp $
 // Copyright (c) 1996-2005 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
@@ -71,6 +71,7 @@ public class ActionSaveProject extends AbstractAction {
     protected ActionSaveProject() {
         super(Translator.localize("action.save-project"),
                 ResourceLoaderWrapper.lookupIcon("action.save-project"));
+        super.setEnabled(false);
     }
 
     /**
