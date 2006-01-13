7c1f00e18662e1d6a13ff490326a4e549b108742
dpollock <dpollock>
2006-01-13 16:14:42 +0000

    Bug 123442.  Actions from action sets are no longer being hooked directly into the handler mechanism.  A legacy persistence class converts them directly into handlers.  This fixes a case where the action was a retarget action.  SubActionBars now take a service locator as an argument.  The component framework is gone.  The handler service is available all the way down to the smallest little site (including PageSite and MultiPageEditorSite).  There is a notion of depth added to the handler service, which allows it to resolve nesting issues.  Default expressions on slave services are now additive.  Oh, and I think Eclipse can now make you a cup of coffee.  Have a nice day.

--- Eclipse UI/org/eclipse/ui/internal/part/NullEditorInput.java
+++ Eclipse UI/org/eclipse/ui/internal/part/NullEditorInput.java
@@ -8,7 +8,7 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
-package org.eclipse.ui.internal.part.services;
+package org.eclipse.ui.internal.part;
 
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.IEditorInput;
