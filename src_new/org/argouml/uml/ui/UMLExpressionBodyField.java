37d2a6a8cd41ad4c1b98cf53618a4ae700bacb75
mvw <mvw>
2006-03-13 21:34:38 +0000

    Fix for issue 4072: Caption missing for script and recurrence items in actions + font. Solved by setting the font, and adding tooltips.

--- src_new/org/argouml/uml/ui/UMLExpressionBodyField.java
+++ src_new/org/argouml/uml/ui/UMLExpressionBodyField.java
@@ -1,5 +1,5 @@
-// $Id: UMLExpressionBodyField.java,v 1.22 2005/11/13 11:01:14 linus Exp $
-// Copyright (c) 1996-2005 The Regents of the University of California. All
+// $Id: UMLExpressionBodyField.java,v 1.23 2006/03/13 21:34:38 mvw Exp $
+// Copyright (c) 1996-2006 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
 // agreement is hereby granted, provided that the above copyright notice
@@ -31,6 +31,8 @@ import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
 import org.apache.log4j.Logger;
+import org.argouml.i18n.Translator;
+import org.argouml.ui.LookAndFeelMgr;
 
 /**
  * This text field shows the body of a UML expression.
@@ -61,6 +63,8 @@ public class UMLExpressionBodyField extends JTextArea
         model = m;
         notifyModel = n;
         getDocument().addDocumentListener(this);
+        setToolTipText(Translator.localize("label.body.tooltip"));
+        setFont(LookAndFeelMgr.getInstance().getSmallFont());
     }
 
     /**
