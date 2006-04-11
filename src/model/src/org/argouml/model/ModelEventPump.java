7ffced9ddfe17f27147830912e5c36f6449da52b
linus <linus>
2006-04-11 06:10:37 +0000

    Finally an answer to Toms question in the TODO:.

--- src/model/src/org/argouml/model/ModelEventPump.java
+++ src/model/src/org/argouml/model/ModelEventPump.java
@@ -1,4 +1,4 @@
-// $Id: ModelEventPump.java,v 1.12 2006/04/10 18:47:20 tfmorris Exp $
+// $Id: ModelEventPump.java,v 1.13 2006/04/11 06:10:37 linus Exp $
 // Copyright (c) 2004-2005 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
@@ -47,7 +47,11 @@ import javax.swing.Action;
  * handle them in a harmless way, this approach should not be used.
  *
  * TODO: (Is this still true or does it refer to the NSUML
- * implementation? - tfm 20051109) <p>
+ * implementation? - tfm 20051109)
+ * (This is part of the contract that is established between the Model
+ * subsystem and its users. If that is not fulfilled by the current
+ * implementation, then the current implementation is incorrect.
+ * Linus 20060411).<p>
  *
  * TODO: What event names?
  * The event names generated are {@link String}s and their values and
