4aaf0f9a838ee9d79fd6ef9270d22265f39f8206
linus <linus>
2006-04-20 20:10:52 +0000

    Updated the list of projects.

--- tools/bin/do-for-projects.sh
+++ tools/bin/do-for-projects.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
-# $Id: do-for-projects.sh,v 1.1 2006/03/21 21:04:29 linus Exp $
+# $Id: do-for-projects.sh,v 1.2 2006/04/20 20:10:52 linus Exp $
 
 # Do the same thing for each project involved in the release.
 
-CHECKOUT_PROJECTS="argouml \
+PROJECTS="CHECKOUT_PROJECTS="argouml \
+    argouml-classfile \
+    argouml-cpp \
     argouml-csharp \
-    argouml-de argouml-es argouml-en-gb argouml-fr argouml-nb argouml-ru \
+    argouml-idl \
+    argouml-php \
+    argouml-de argouml-es argouml-en-gb argouml-fr argouml-nb \
+    argouml-pt argouml-ru \
     argouml-i18n-zh"
-
-PROJECTS="$CHECKOUT_PROJECTS \
-    argouml/modules/cpp \
-    argouml/modules/classfile \
-    argouml/modules/idl \
-    argouml/modules/php"
 
 case $1 in
 --checkout)
