43cf2249fd37d49193e84856692c947b7541cf09
tfmorris <tfmorris>
2006-08-11 17:00:35 +0000

    Fix getAllNamespaces to not copy array every time.  Inefficient.

--- src/model-mdr/src/org/argouml/model/mdr/ModelManagementHelperMDRImpl.java
+++ src/model-mdr/src/org/argouml/model/mdr/ModelManagementHelperMDRImpl.java
@@ -1,4 +1,4 @@
-// $Id: ModelManagementHelperMDRImpl.java,v 1.8 2006/06/14 18:48:51 tfmorris Exp $
+// $Id: ModelManagementHelperMDRImpl.java,v 1.9 2006/08/11 17:00:35 tfmorris Exp $
 // Copyright (c) 1996-2006 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
@@ -62,7 +62,7 @@ import org.omg.uml.modelmanagement.UmlPackage;
  * @author Thierry Lach
  */
 class ModelManagementHelperMDRImpl implements ModelManagementHelper {
-
+    
     /**
      * The model implementation.
      */
@@ -88,7 +88,7 @@ class ModelManagementHelperMDRImpl implements ModelManagementHelper {
         if (!(ns instanceof Namespace)) {
             throw new IllegalArgumentException();
         }
-
+        
         Iterator it = ((Namespace) ns).getOwnedElement().iterator();
         List list = new ArrayList();
         while (it.hasNext()) {
@@ -112,8 +112,9 @@ class ModelManagementHelperMDRImpl implements ModelManagementHelper {
      * 
      */
     public Collection getAllNamespaces(Object ns) {
+
         if (ns == null || !(ns instanceof Namespace)) {
-            return new ArrayList();
+            return Collections.EMPTY_LIST;
         }
 
         Collection namespaces = ((Namespace) ns).getOwnedElement();
@@ -125,17 +126,13 @@ class ModelManagementHelperMDRImpl implements ModelManagementHelper {
             return Collections.EMPTY_LIST;
         }
 
-        // work with an array instead of iterator.
-        Object[] nsArray = namespaces.toArray();
-
-        for (int i = 0; i < nsArray.length; i++) {
-
-            Object o = nsArray[i];
+        for (Iterator it = namespaces.iterator(); it.hasNext();) {
+            Object o = it.next();
             if (o instanceof Namespace) {
 
                 // only build a namepace if needed, with
                 if (list == Collections.EMPTY_LIST) {
-                    list = new ArrayList(nsArray.length);
+                    list = new ArrayList(namespaces.size());
                 }
 
                 list.add(o);
