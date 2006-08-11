e7fd513d60c5f72757aadd2a216ee2e204fa8d2c
tfmorris <tfmorris>
2006-08-11 19:26:59 +0000

    Make sure the path to the current model is on our search path.

--- src_new/org/argouml/persistence/ZipFilePersister.java
+++ src_new/org/argouml/persistence/ZipFilePersister.java
@@ -1,4 +1,4 @@
-// $Id: ZipFilePersister.java,v 1.11 2006/08/11 19:12:24 mvw Exp $
+// $Id: ZipFilePersister.java,v 1.12 2006/08/11 19:26:59 tfmorris Exp $
 // Copyright (c) 1996-2006 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
@@ -215,6 +215,14 @@ public class ZipFilePersister extends XmiFilePersister {
             InputSource is = new InputSource(
                     new XmiInputStream(stream, this, 10000000, 100000));
             is.setSystemId(file.toURL().toExternalForm());
+
+            // Add the path of the current model to the search path, so we can
+            // find linked models relative it
+            String path = file.getParent();
+            if (path != null) {
+                System.setProperty("org.argouml.model.modules_search_path",
+                        path);
+            }
             
             XMIParser.getSingleton().readModels(p, is);
             Object model = XMIParser.getSingleton().getCurModel();
