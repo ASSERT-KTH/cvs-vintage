30f1df1f234cb757bb788c079c318fde1cb1caf7
bobtarling <bobtarling>
2006-09-18 23:18:55 +0000

    Allow .todo zip entrys without prolog

--- src_new/org/argouml/persistence/ZargoFilePersister.java
+++ src_new/org/argouml/persistence/ZargoFilePersister.java
@@ -1,4 +1,4 @@
-// $Id: ZargoFilePersister.java,v 1.43 2006/09/18 22:37:06 bobtarling Exp $
+// $Id: ZargoFilePersister.java,v 1.44 2006/09/18 23:18:55 bobtarling Exp $
 // Copyright (c) 1996-2006 The Regents of the University of California. All
 // Rights Reserved. Permission to use, copy, modify, and distribute this
 // software and its documentation without fee, and without a written
@@ -325,13 +325,18 @@ class ZargoFilePersister extends UmlFilePersister {
             if (zis != null) {
                 InputStreamReader isr = new InputStreamReader(zis, encoding);
                 reader = new BufferedReader(isr);
-                // Skip the 2 lines
-                //<?xml version = "1.0" encoding = "UTF-8" ?>
-                //<!DOCTYPE todo SYSTEM "todo.dtd" >
-                // TODO: This could be made more robust, these 2 lines should be
-                // there but what if they don't exist?
-                reader.readLine();
-                reader.readLine();
+                
+                String firstLine = reader.readLine();
+                if (firstLine.startsWith("<?xml")) {
+                    // Skip the 2 lines
+                    //<?xml version="1.0" encoding="UTF-8" ?>
+                    //<!DOCTYPE todo SYSTEM "todo.dtd" >
+                    reader.readLine();
+                } else {
+                    writer.println(firstLine);
+                }
+                
+                
 
                 readerToWriter(reader, writer);
 
