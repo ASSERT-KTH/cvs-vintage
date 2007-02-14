b99d12a2dd6a56e7edf5e291cc76b3e2abc030b9
tod <tod>
2007-02-14 19:26:12 +0000

    Fix for Bug 164677 [EditorMgmt] can we remove IURIEditorInput?

--- Eclipse UI/org/eclipse/ui/IURIEditorInput.java
+++ Eclipse UI/org/eclipse/ui/IURIEditorInput.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006 IBM Corporation and others.
+ * Copyright (c) 2006, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -29,6 +29,8 @@ import java.net.URI;
  *
  * @see URI
  * @since 3.3
+ * @deprecated replaced by IURIEditorInput in the org.eclipse.ui.ide plug-in. This will be deleted during
+ * 3.3 M6.
  */
 public interface IURIEditorInput extends IEditorInput {
     /**
